87b3b8bd0315d06ae00e900a0a3ab1fe305f70f3
Dan Croak <dcroak@thoughtbot.com>
2010-12-29 11:49:55 -0500

    fresh suspenders app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Health::Application.config.secret_token = '35122c7acdd75318386952affbdf9b115e26d8ff61ec2b75c4fa15c68fb688dbb9b53083264d94d2a6f05057ac7e6bce8ab43abf95c46a8fe57950baab1a5cf2'
