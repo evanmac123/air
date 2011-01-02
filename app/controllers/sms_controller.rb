cc9fefe4dbc868df0696d0d92b82255cb1d2478c
Dan Croak <dcroak@thoughtbot.com>
2011-01-02 01:13:08 -0500

    experimenting with returning text/plain to Twilio SMS

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -3,12 +3,14 @@ class SmsController < ActionController::Metal
     body = params['Body'].downcase
     from = params['From']
 
-    if body.include?("broccoli")
-      SMS.send(from, "Yum. +2 points. Broccoli helps your body fight cancer. You're now in 1st place.")
+    self.content_type = "text/plain"
+
+    self.response_body = if body.include?("broccoli")
+      "Yum. +2 points. Broccoli helps your body fight cancer. You're now in 1st place."
     elsif body.include?("#plu")
-      SMS.send(from, "Sweet. +2 points. Bananas help you fight cancer.")
+      "Sweet. +2 points. Bananas help you fight cancer."
     elsif body.include?("#walk")
-      SMS.send(from, "Nice job. +1 point. Walking improves bone health.")
+      "Nice job. +1 point. Walking improves bone health."
     end
   end
 end
