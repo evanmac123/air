4686b586719d9324339f3b282eeca9a3ba19747f
Dan Croak <dcroak@thoughtbot.com>
2011-01-02 00:54:19 -0500

    fixing character

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -4,7 +4,7 @@ class SmsController < ActionController::Metal
     from = params['From']
 
     if body.include?("broccoli")
-      SMS.send(from, "Yum. +2 points. Broccoli helps your body fight cancer. You’re now in 1st place.")
+      SMS.send(from, "Yum. +2 points. Broccoli helps your body fight cancer. You're now in 1st place.")
     elsif body.include?("#plu")
       SMS.send(from, "Sweet. +2 points. Bananas help you fight cancer.")
     elsif body.include?("#walk")
