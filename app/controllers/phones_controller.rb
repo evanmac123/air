ab1c41236b5092c3ecd4324358957c3f4a32de69
Dan Croak <dcroak@thoughtbot.com>
2011-01-16 23:37:04 -0500

    improving the interaction of accepting the invitation a little with a flash message.

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,7 +1,9 @@
 class PhonesController < ApplicationController
   def create
-    @user = User.find(params[:user_id])
+    @user = User.find_by_slug(params[:user_id])
     @user.join_game(params[:phone][:number])
+
+    flash[:success] = "Welcome to the game! You're now signed in. Players' activity is below to the left. The basic rules are below to the right."
     redirect_to "/activity"
   end
 end
