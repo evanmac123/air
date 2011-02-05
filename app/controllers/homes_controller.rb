494a05155847e2a7d6c537852881c859e1e89dd2
Phil Darnowsky <phil@darnowsky.com>
2011-02-05 18:59:38 -0500

    reverted HomesController

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
   def show
     @demo  = Demo.find_by_company_name("Alpha")
-    #@user  = @demo.users.build
-    @users = @demo.try(:users).try(:top, 5) || []
+    @user  = @demo.users.build
+    @users = @demo.users.top(5)
   end
 end
