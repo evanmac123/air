34487c744d682d5925b51540614b673a1788e802
Phil Darnowsky <phil@darnowsky.com>
2011-02-26 20:08:59 -0500

    badly needed refactoring on Act.parse done

--- app/models/act.rb
+++ app/models/act.rb
@@ -26,37 +26,50 @@ class Act < ActiveRecord::Base
       return "Score points by texting this number your latest lifestyle act. Examples: ate a banana, smoked a cigarette, played basketball"
     end
 
-    if value.nil?
-      if rule = CodedRule.where('value ILIKE ?', key_name).first
-        create(:user => user, :text => rule.to_s, :rule => rule)
+    rule = if value.nil?
+             find_coded_rule(key_name)
+           else
+             find_regular_rule(key_name, value)
+           end
 
-        reply = rule.reply
-        if (victory_threshold = user.demo.victory_threshold)
-          reply += " You have #{user.points} out of #{victory_threshold} points."
-        end
-
-        return reply
-      end
-    end
-
-    if key = Key.where(:name => key_name).first
-      if rule = Rule.where(:key_id => key.id, :value => value).first
-        create(:user => user, :text => body.downcase, :rule => rule)
-
-        reply = rule.reply
-        if (victory_threshold = user.demo.victory_threshold)
-          reply += " You have #{user.points} out of #{victory_threshold} points."
-        end
-
-        return reply
-      else
-        # record this somewhere
-        good_value = key.rules.first.value
-        return "We understand #{key_name} but not #{value}. Try: #{key_name} #{good_value}"
-      end
+    if rule
+      return record_act(user, rule)
+    elsif error_message = generate_helpful_error(key_name, value)
+      return error_message
     else
-      # record this somewhere
+      # TODO: record this somewhere
       return "We didn't understand. Try: help"
     end
+  end
+
+
+  private
+
+  def self.find_coded_rule(key_name)
+    CodedRule.where('value ILIKE ?', key_name).first
+  end
+
+  def self.find_regular_rule(key_name, value)
+    if key = Key.where(:name => key_name).first
+      Rule.where(:key_id => key.id, :value => value).first
+    end
+  end
+
+  def self.generate_helpful_error(key_name, value)
+    ## TODO: record this somewhere
+    return nil unless value && key = Key.find_by_name(key_name)
+    good_value = key.rules.first.value
+    return "We understand #{key_name} but not #{value}. Try: #{key_name} #{good_value}"
+  end
+
+  def self.record_act(user, rule)
+    create(:user => user, :text => rule.to_s, :rule => rule)
+
+    reply = rule.reply
+    if (victory_threshold = user.demo.victory_threshold)
+      reply += " You have #{user.points} out of #{victory_threshold} points."
+    end
+
+    reply
   end
 end
