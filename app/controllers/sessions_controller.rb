dd82f274740efa61609481b17f46f066664e1d40
Dan Croak <dcroak@thoughtbot.com>
2011-03-02 23:36:49 -0500

    fixing bad translation, session error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,19 @@
 class SessionsController < Clearance::SessionsController
   before_filter :downcase_email
 
+  def create
+    @user = ::User.authenticate(params[:session][:email],
+                                params[:session][:password])
+    if @user.nil?
+      flash_failure_after_create
+      render :template => 'sessions/new'
+    else
+      sign_in(@user)
+      flash_success_after_create
+      redirect_back_or(url_after_create)
+    end
+  end
+
   def url_after_create
     activity_path
   end
