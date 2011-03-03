08caf14c930bc280cdacfe5f2fbbb90663c21007
Phil Darnowsky <phil@darnowsky.com>
2011-03-03 01:12:39 -0500

    redirect to activity page from home only if not mobile

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,9 +1,16 @@
 class HomesController < ApplicationController
+  before_filter :redirect_to_activity, :unless => :is_mobile_device?
+
   def show
+    @demo  = current_user.demo
+    @user  = @demo.users.build
+    # TODO: the next line is ugly, wrote it in a big hurry
+    @users = @demo.users.order('points DESC')
+  end
+
+  private
+
+  def redirect_to_activity
     redirect_to activity_path
-    #@demo  = current_user.demo
-    #@user  = @demo.users.build
-    ## TODO: the next line is ugly, wrote it in a big hurry
-    #@users = @demo.users.order('points DESC')
   end
 end
