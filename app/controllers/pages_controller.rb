4b14840604257c2b1c14efebe6d7df7db538ecb2
Dan Croak <dcroak@thoughtbot.com>
2011-03-19 15:54:54 -0400

    trying to get features passing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < HighVoltage::PagesController
   skip_before_filter :authenticate
   before_filter :force_html_format
+  before_filter :signed_out_only
+
+  protected
+
+  def signed_out_only
+    redirect_to home_path if signed_in?
+  end
 end
