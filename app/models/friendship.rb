d90b5f38264f4f44e3ccbccf4245ab5224ff2793
Phil Darnowsky <phil@darnowsky.com>
2011-05-05 16:34:28 -0400

    user gets notified via SMS when someone follows them

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,10 @@
 class Friendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, :class_name => "User"
+
+  after_create :send_follow_notification
+
+  def send_follow_notification
+    SMS.send(self.friend.phone_number, "#{self.user.name} is now following you on HEngage.")
+  end
 end
