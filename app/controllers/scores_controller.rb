8766db990463e40482ac516df7b0f0cc0be579d3
Phil Darnowsky <phil@darnowsky.com>
2011-05-08 17:00:56 -0400

    AJAXy link to display all ranked users in a demo

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,6 +1,8 @@
 class ScoresController < ApplicationController
+  respond_to :js, :mobile
+
   def index
     @demo  = current_user.demo
-    @users = @demo.users.order('points DESC')
+    @users = @demo.users.ranked.order('points DESC')
   end
 end
