5d28f8ecb9072e04f8b526aff898d31f0abdc0d9
Phil Darnowsky <phil@darnowsky.com>
2011-06-05 23:45:57 -0400

    got tired of not seeing threshold params in Hoptoad, workaround

--- app/controllers/admin/demos_controller.rb
+++ app/controllers/admin/demos_controller.rb
@@ -2,6 +2,7 @@ class Admin::DemosController < AdminBaseController
   def new
     @demo = Demo.new
     @demo.bonus_thresholds = [BonusThreshold.new]
+    #@demo.levels = [Level.new]
   end
 
   def create
@@ -16,20 +17,37 @@ class Admin::DemosController < AdminBaseController
     end
 
     bonus_thresholds_params = params[:demo].delete(:bonus_thresholds)
+    #levels_params = params[:demo].delete(:levels)
 
-    Demo.transaction do
-      @demo = Demo.new(params[:demo])
-      @demo.save
+    begin
+      Demo.transaction do
+        @demo = Demo.new(params[:demo])
+        @demo.save
 
-      bonus_thresholds_params.values.each do |value|
-        next if value.values.all?(&:blank?)
+        # TODO: DRY this mess up, maybe wait until we trigger the Rule Of 3.
 
-        @demo.bonus_thresholds.create!(value)
+        bonus_thresholds_params.values.each do |value|
+          next if value.values.all?(&:blank?)
+
+          @demo.bonus_thresholds.create!(value)
+        end
+
+        #levels_params.values.each do |value|
+          #next if value.values.all?(&:blank?)
+          #@demo.levels.create!(value)
+        #end
       end
-    end
 
-    flash[:success] = "Demo created."
-    redirect_to admin_demo_path(@demo)
+      flash[:success] = "Demo created."
+      redirect_to admin_demo_path(@demo)
+    rescue Exception => e
+      # Restore bonus threshold and level parameters to params so we can see
+      # them in Hoptoad later.
+     
+      params[:demo][:bonus_thresholds] = bonus_thresholds_params
+      #params[:demo][:levels] = levels_params
+      raise e
+    end
   end
 
   def show
@@ -37,5 +55,6 @@ class Admin::DemosController < AdminBaseController
     @users = @demo.users.alphabetical
     @user_with_mobile_count = @demo.users.where("phone_number IS NOT NULL AND phone_number != ''").count
     @bonus_thresholds = @demo.bonus_thresholds.in_threshold_order
+    #@levels = @demo.levels.in_threshold_order
   end
 end
