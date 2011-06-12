7e76273451ac07044df17a1ac72b1672a143bff7
Phil Darnowsky <phil@darnowsky.com>
2011-06-11 21:50:32 -0400

    first box of new marketing page in the can

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,17 @@ class PagesController < HighVoltage::PagesController
   before_filter :force_html_format
   before_filter :signed_out_only
 
+  layout :layout_for_page
+
   protected
+
+  def layout_for_page
+    if params[:id] == 'new_marketing'
+      'new_pages'
+    else
+      'pages'
+    end
+  end
 
   def signed_out_only
     redirect_to home_path if signed_in?
