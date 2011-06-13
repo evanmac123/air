42017b0453d7e71ebe706ad68e87570ef0afa41c
Phil Darnowsky <phil@darnowsky.com>
2011-06-13 01:24:20 -0400

    changed conference feed page over to IABC game. someday I swear I will re-do this the correct way.

--- app/controllers/conference_feeds_controller.rb
+++ app/controllers/conference_feeds_controller.rb
@@ -4,7 +4,7 @@ class ConferenceFeedsController < ApplicationController
 
   def show
     # TODO: I shouldn't have to tell you what's wrong with this next line.
-    @demo  = Demo.where(:company_name => 'Healthbuzz').first
+    @demo  = Demo.where(:company_name => 'IABC Game').first
     @users = @demo.users.ranked.order('ranking ASC')
     @acts  = @demo.acts.order('created_at DESC').limit(20).includes(:user, :rule)
   end
