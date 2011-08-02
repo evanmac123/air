87f655a7623648622ce4e9ead454d3b3992102bf
Phil Darnowsky <phil@darnowsky.com>
2011-08-02 13:01:54 -0400

    added big old circular next section button

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,9 @@
 module PagesHelper
+  def next_section_link(target)
+    link_to(
+      image_tag("new_marketing/circle_big.png"),
+      "##{target}",
+      :class => "nav next-section-link"
+    )
+  end
 end
