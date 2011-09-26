fd762d0480893ba265622c9ea85b48159e3c4177
Phil Darnowsky <phil@darnowsky.com>
2011-09-26 14:14:42 -0400

    updated redirector to handle two separate URLs

--- app/controllers/michelin_redirector_controller.rb
+++ app/controllers/michelin_redirector_controller.rb
@@ -1,15 +1,21 @@
 class MichelinRedirectorController < ActionController::Metal
-  MICHELIN_URL = ENV['MICHELIN_URL'] || 'http://fakemichelinsystem.com'
+  MICHELIN_ENROLLMENT_URL = ENV['MICHELIN_ENROLLMENT_URL'] || 'http://fakemichelinsystemenrollment.com'
+  MICHELIN_INCENTIVE_URL = ENV['MICHELIN_INCENTIVE_URL'] || 'http://fakemichelinsystemincentive.com'
+
+  LINK_TYPE_MAPPINGS = {
+    "et" => MICHELIN_ENROLLMENT_URL,
+    "ie" => MICHELIN_INCENTIVE_URL
+  }
 
   def show
     self.status = 302
-    self.headers['Location'] = "#{MICHELIN_URL}?#{reserialize_params}"
+    self.headers['Location'] = "#{LINK_TYPE_MAPPINGS[params[:link_type]]}?#{reserialize_params}"
     self.response_body = "Redirecting..."
   end
 
   protected
 
   def reserialize_params
-    params.reject{|k,v| k == 'action' || k == 'controller'}.to_param
+    params.reject{|k,v| %w(action controller link_type).include?(k)}.to_param
   end
 end
--- spec/controllers/michelin_redirector_controller_spec.rb
+++ spec/controllers/michelin_redirector_controller_spec.rb
@@ -4,17 +4,24 @@ metal_testing_hack(MichelinRedirectorController)
 
 describe MichelinRedirectorController do
   describe "#show" do
-    it "should redirect to the proper place with parameters intact" do
-      @params = {
-        "foo" => "bar",
-        "baz" => "quux"
-      }
+    {"et" => MichelinRedirectorController::MICHELIN_ENROLLMENT_URL,
+     "ie"  => MichelinRedirectorController::MICHELIN_INCENTIVE_URL
+    }.each do |link_type, expected_url|
+      context "when redirecting to #{link_type}" do
+        it "should redirect to the proper place with parameters intact" do
+          @params = {
+            "foo" => "bar",
+            "baz" => "quux",
+            "link_type" => link_type
+          }
 
-      get "show", @params
+          get "show", @params
 
-      response.status.should == 302
-      response.headers['Location'].should == "#{MichelinRedirectorController::MICHELIN_URL}?baz=quux&foo=bar"
-      response.body.should == "Redirecting..."
+          response.status.should == 302
+          response.headers['Location'].should == "#{expected_url}?baz=quux&foo=bar"
+          response.body.should == "Redirecting..."
+        end
+      end
     end
   end
 end
