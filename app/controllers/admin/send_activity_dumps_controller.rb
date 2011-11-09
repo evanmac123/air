9c69fdb9321b60822643817937d81dc81889b1fa
Phil Darnowsky <phil@darnowsky.com>
2011-11-09 11:27:53 -0500

    report generation shifted into a DJ

--- app/controllers/admin/send_activity_dumps_controller.rb
+++ app/controllers/admin/send_activity_dumps_controller.rb
@@ -1,8 +1,8 @@
 class Admin::SendActivityDumpsController < ApplicationController
   def create
-    Report::Activity.new(params[:demo_id]).email_to('vlad@hengage.com')
+    Report::Activity.new(params[:demo_id]).delay.email_to('vlad@hengage.com')
 
-    flash[:success] = "Aaaaaaand...sent!"
+    flash[:success] = "Aaaaaaand...scheduled! Check for it in a few minutes."
     redirect_to :back
   end
 end
