2d4ecfff7aa75243675d148f83014e693aaf7ca3
Phil Darnowsky <phil@darnowsky.com>
2011-11-14 10:29:11 -0500

    old marketing pages removed

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,18 +2,22 @@ require 'spec_helper'
 
 describe PagesController do
   it "should not require authentication" do
-    get :show, :id => 'invitation'
+    get :show, :id => 'new_marketing'
     response.should_not be_redirect
   end
 
   it "should not force SSL" do
-    $test_force_ssl = true
+    begin
+      $test_force_ssl = true
 
-    request.ssl?.should be_false
-    get :show, :id => 'invitation'
+      request.ssl?.should be_false
+      get :show, :id => 'new_marketing'
 
-    $test_force_ssl = false
+      $test_force_ssl = false
 
-    response.should_not be_redirect
+      response.should_not be_redirect
+    ensure
+      $test_force_ssl = false
+    end
   end
 end
