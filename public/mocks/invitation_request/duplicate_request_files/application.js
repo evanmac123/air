2868ffc9c8efcfc1cab3e78c087a88f2fbca1695
Phil Darnowsky <phil@darnowsky.com>
2011-12-13 15:37:05 -0500

    static versions of pages in self-inviting flow for Connie to style

---
+++ public/mocks/invitation_request/duplicate_request_files/application.js
@@ -0,0 +1,61 @@
+$(function() {
+  $('#add-new-user').live('click', function() {
+    $('#new_user').parent('.hidden-form').show();
+    $('#user_name').focus();
+    return false;
+  });
+
+  $('.set-avatar').submit(function(e) {
+    $('.set-avatar-submit').attr('disabled', 'disabled');
+    $(this).children('.loading-message').show();
+  });
+
+  $('.clear-avatar').submit(function(e) {
+    if(confirm("Are you sure you want to clear your avatar?") == false) {
+      e.preventDefault();
+    }
+  });
+
+  $('.change-avatar-link').click(function(e) {
+    mainProfileHeight = $('#main-profile-section').height();
+
+    $('#main-profile-section').data('original-height', mainProfileHeight);
+    $('#main-profile-section').height(mainProfileHeight + $('.avatar-controls').height() + 100);
+    $('.avatar-controls').show();
+    e.preventDefault();
+  });
+
+  $('.with-hint-text').live('focus', (function(e) {
+    $(this).attr('value', '').removeClass('with-hint-text');
+  }));
+});
+
+function lengthInBytes(string) {
+  var result = 0;
+  for(i = 0; i < string.length; i++) {
+    var code = string.charCodeAt(i);
+    if(code < 128) {
+      result += 0.875;
+    } else {
+      while(code > 0) {
+        result += 1;
+        code >>= 8;
+      }
+    }
+  }
+
+  return result;
+}
+
+function updateCharacterCounter(from, to) {
+  maxLength = $(from).attr('maxlength');
+  currentLength = lengthInBytes($(from).val());
+  $(to).text('' + ((maxLength * 7 / 8) - currentLength) + ' bytes left');
+}
+
+function addByteCounterFor(locator) {
+  var ghettoUniqueId = "counter_" + Math.round(Math.random() * 10000000);
+  $(locator).after('<span class="character-counter" id="' + ghettoUniqueId + '"></span>');
+  updateCharacterCounter(locator, '#'+ghettoUniqueId);
+  $(locator).keypress(function() {updateCharacterCounter(locator, '#'+ghettoUniqueId)});
+}
