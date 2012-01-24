528487b9fbdc0775663550394ff683bbd8f33779
Phil Darnowsky <phil@darnowsky.com>
2012-01-24 15:08:54 -0500

    fixed migration

--- db/migrate/20120124172046_add_user_sign_ins_field.rb
+++ db/migrate/20120124172046_add_user_sign_ins_field.rb
@@ -1,7 +1,10 @@
 class AddUserSignInsField < ActiveRecord::Migration
   def self.up
-    add_column :users, :session_count, :integer, :null => false, :default => 0
+    add_column :users, :session_count, :integer
+    execute "update users set session_count = 100"
+    change_column :users, :session_count, :integer, :null => false, :default => 0
   end
+
   def self.down
     remove_columns :users, :session_count
   end
