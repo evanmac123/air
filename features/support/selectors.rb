ad563d9e2cb1ed6d12aea7860a0812f37f8d6f1d
Jack Desert <jackdesert556@gmail.com>
2012-02-07 13:50:05 -0500

    @@ Now you can't invite yourself to the game on a prepopulated domain

--- features/support/selectors.rb
+++ features/support/selectors.rb
@@ -26,8 +26,11 @@ module HtmlSelectorsHelpers
     # This allows you to provide a quoted selector as the scope
     # for "within" steps as was previously the default for the
     # web steps:
+    when /Suggested Users/
+      [:xpath, "//*[@id='suggestions']"]
     when /^"(.+)"$/
       $1
+      
 
     else
       raise "Can't find mapping from \"#{locator}\" to a selector.\n" +
