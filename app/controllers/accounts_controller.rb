2f80ade1b7d886c6201fb153314b3ac3c5d7cc04
Phil Darnowsky <phil@darnowsky.com>
2012-02-09 15:03:13 -0500

    user can now cancel a new phone number

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -24,13 +24,17 @@ class AccountsController < ApplicationController
     submitted_number = params[:user].delete(:phone_number)
 
     if submitted_number.blank? && current_user.phone_number.present?
-      current_user.phone_number = ''
+      current_user.update_attributes(:phone_number => '')
+      current_user.cancel_new_phone_number
       add_success "OK, you won't get any more text messages from us."
       return
     end
 
     normalized_phone_number = PhoneNumber.normalize submitted_number
-    return if current_user.phone_number == normalized_phone_number
+    if current_user.phone_number == normalized_phone_number
+      current_user.cancel_new_phone_number
+      return
+    end
 
     current_user.new_phone_number = normalized_phone_number
     current_user.generate_short_numerical_validation_token
