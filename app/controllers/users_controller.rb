48ccf7172416758da30285d757e5b82118018e05
Jack Desert <jackdesert556@gmail.com>
2012-02-11 16:22:35 -0500

    @@ Improved suggestion text for user directory

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,21 @@ class UsersController < Clearance::UsersController
   def index
     @other_users = User.claimed.where(['demo_id = ? AND id != ?', current_user.demo_id, current_user.id]).alphabetical
     @friend_ids = current_user.friend_ids
+    @search_link_text = "our search bar"
     text = params[:search_string]
     if text
+      @search_string = text
       text = text.downcase
       demo = current_user.demo
       names = @other_users.where("LOWER(name) like ?", "%" + text + "%")
       slugs = @other_users.where("LOWER(sms_slug) like ?", "%" + text + "%")
       emails = @other_users.where("LOWER(email) like ?", "%" + text + "%")
       @other_users = names + slugs + emails
+      @search_link_text = "refining your search"
     end
-    @other_users = @other_users[0,50]
+    user_limit = 50
+    @users_cropped = user_limit if @other_users.length > user_limit
+    @other_users = @other_users[0,user_limit]
   end
 
   def show
--- public/javascripts/directory.js
+++ public/javascripts/directory.js
@@ -6,4 +6,8 @@ $(function() {
   $('.stop-following-btn').click(function(){
     $(this).find('form').submit();
   });
+  
+  $('.focus_on_search_bar a').click(function(){
+    $('#search_string').focus();
+  });
 });
