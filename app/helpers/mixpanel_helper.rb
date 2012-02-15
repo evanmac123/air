03fb36ec5c004d0f7ebe332dc389adb3cd2a9ee7
Phil Darnowsky <phil@darnowsky.com>
2012-02-15 09:12:05 -0500

    commented out mixpanel

--- app/helpers/mixpanel_helper.rb
+++ app/helpers/mixpanel_helper.rb
@@ -1,8 +1,8 @@
 module MixpanelHelper
   def mp_track(event, properties={})
-    javascript_tag do
-      raw "mpq.track('#{event}', #{properties.to_json})"
-    end
+    #javascript_tag do
+      #raw "mpq.track('#{event}', #{properties.to_json})"
+    #end
   end
 
   def mp_track_page(page_name, properties = {})
