4b3499b613cda24f3928adbcad0a508a5a6db68a
Phil Darnowsky <phil@darnowsky.com>
2012-02-15 19:22:25 -0500

    added some more mixpanel tracking of failed avatar uploads

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -2,7 +2,7 @@ class AvatarsController < ApplicationController
   def update
     if params[:user].blank? || params[:user][:avatar].blank?
       flash[:failure] = "Please choose a file to use for your avatar."
-      flash[:mp_track_avatar] = ["changed avatar", {:success => false}]
+      flash[:mp_track_avatar] = ["changed avatar", {:success => false, :reason => "no file chosen"}]
     else
       begin
         current_user.avatar = params[:user][:avatar]
@@ -10,7 +10,7 @@ class AvatarsController < ApplicationController
         flash[:mp_track_avatar] = ["changed avatar", {:success => true}]
       rescue ActiveRecord::RecordInvalid => e
         flash[:failure] = "Sorry, I didn't understand that file you tried to upload as an image file."
-        flash[:mp_track_avatar] = ["changed avatar", {:success => false}]
+        flash[:mp_track_avatar] = ["changed avatar", {:success => false, :reason => e.message, :error_class => e.class, :content_type => params[:user][:avatar].content_type, :original_filename => params[:user][:avatar].original_filename}]
       end
     end
 
