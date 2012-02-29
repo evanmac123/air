b070f8b3d53cfe1a947f8768a84a71c7beed4866
Phil <phil@bartholomew.(none)>
2012-02-28 20:25:32 -0500

    and now that I can run rcov, found and excised a ton of dead code, w00t

--- app/helpers/acts_helper.rb
+++
@@ -1,6 +0,0 @@
-module ActsHelper
-  def ranking_phrase(ranking, out_of)
-    ordinal_particle = ranking.ordinalize[-2,2]
-    "<span class=\"rank-number\">#{ranking}</span><span class=\"ordinal-particle\">#{ordinal_particle}</span><br/> out of #{out_of}"
-  end
-end
