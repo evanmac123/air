5ced63e37e19790c8700e462add5e74b858f1c6c
Phil <phil@bartholomew.(none)>
2012-03-06 17:11:14 -0500

    respect my authoritah, indices

--- db/migrate/20120306211225_add_displayability_index_to_acts.rb
+++ db/migrate/20120306211225_add_displayability_index_to_acts.rb
@@ -1,9 +1,15 @@
 class AddDisplayabilityIndexToActs < ActiveRecord::Migration
   def self.up
     add_index :acts, [:hidden, :demo_id, :user_id, :privacy_level]
+    remove_index :acts, :hidden
+    remove_index :acts, :demo_id
+    remove_index :acts, :privacy_level
   end
 
   def self.down
+    add_index :acts, :demo_id
+    add_index :acts, :hidden
+    add_index :acts, :privacy_level
     remove_index :acts, :column => [:hidden, :demo_id, :user_id, :privacy_level]
   end
 end
