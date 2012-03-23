5dc4abb2adb59b1ff5aadf43df9af9c47e2aa889
Jack Desert <jack@hengage.com>
2012-03-23 11:29:16 -0400

    @@ In regards to #179    Password Reset Emails: Real FROM address    Changed to 'H Engage <donotreply@hengage.com>'

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,4 +1,4 @@
 Clearance.configure do |config|
-  config.mailer_sender = 'donotreply@hengage.com'
+  config.mailer_sender = 'H Engage <donotreply@hengage.com>'
   config.cookie_expiration = lambda {5.minutes.from_now.utc}
 end
