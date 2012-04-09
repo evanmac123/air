23b9a155e363882e9556f6e4389dace6e88f6f48
Phil <phil@bartholomew.(none)>
2012-04-09 14:16:27 -0400

    tests verifying that segmentation on multiple characteristics works just fine

--- spec/acceptance/admin_segments_users_by_characteristics_spec.rb
+++ spec/acceptance/admin_segments_users_by_characteristics_spec.rb
@@ -1,79 +1,126 @@
 require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
 
-feature "Admin Segments Users By Characteristics" do
+feature "Admin segmentation" do
   before(:each) do
     @demo = Factory :demo
-    @generic_characteristic_1 = Factory :characteristic, :name => "Color", :allowed_values => %w(red orange yellow green blue indigo violet)
-    @generic_characteristic_2 = Factory :characteristic, :name => "Favorite Beatle", :allowed_values => %w(john paul george ringo)
-    @generic_characteristic_3 = Factory :characteristic, :name => "LOLPhrase", :allowed_values => %w(i can haz cheezburger)
-    @demo_specific_characteristic_1 = Factory :demo_specific_characteristic, :name => "Height", :demo => @demo, :allowed_values => %w(low medium high)
-    @demo_specific_characteristic_2 = Factory :demo_specific_characteristic, :name => "Favorite number", :demo => @demo, :allowed_values => %w(seven eight nine)
-    @demo_specific_characteristic_3 = Factory :demo_specific_characteristic, :name => "MomPhrase", :demo => @demo, :allowed_values => %w(hi mom)
-
-    @loser = Factory :user, :demo => @demo
-    @reds = []
-    @blues = []
-    @greens = []
-
-    14.times do |i|
-      @reds << Factory(:user, :name => "Red Guy #{i}", :demo => @demo, :characteristics => {@generic_characteristic_1.id => "red"})
-      @blues << Factory(:user, :name => "Blue Guy #{i}", :demo => @demo, :characteristics => {@generic_characteristic_1.id => "blue"})
-      @greens << Factory(:user, :name => "Green Guy #{i}", :demo => @demo, :characteristics => {@generic_characteristic_1.id => "green"})
-    end
-
-    %w(john john paul paul paul george george george george ringo ringo ringo ringo ringo).each_with_index do |name, i|
-      [@reds, @blues, @greens].each do |color_array|
-        color_array[i].characteristics[@generic_characteristic_2.id] = name
-        color_array[i].save!
-      end
-    end
-
-    %w(low low low low low low low medium medium medium high high high high).each_with_index do |name, i|
-      [@reds, @blues, @greens].each do |color_array|
-        color_array[i].characteristics[@demo_specific_characteristic_1.id] = name
-        color_array[i].save!
-      end
-    end
-
-    crank_off_dj
-
-    signin_as_admin
-    visit admin_demo_segmentation_path(@demo)
   end
 
+  context "segmenting users" do
+    before(:each) do
+      @generic_characteristic_1 = Factory :characteristic, :name => "Color", :allowed_values => %w(red orange yellow green blue indigo violet)
+      @generic_characteristic_2 = Factory :characteristic, :name => "Favorite Beatle", :allowed_values => %w(john paul george ringo)
+      @generic_characteristic_3 = Factory :characteristic, :name => "LOLPhrase", :allowed_values => %w(i can haz cheezburger)
+      @demo_specific_characteristic_1 = Factory :demo_specific_characteristic, :name => "Height", :demo => @demo, :allowed_values => %w(low medium high)
+      @demo_specific_characteristic_2 = Factory :demo_specific_characteristic, :name => "Favorite number", :demo => @demo, :allowed_values => %w(seven eight nine)
+      @demo_specific_characteristic_3 = Factory :demo_specific_characteristic, :name => "MomPhrase", :demo => @demo, :allowed_values => %w(hi mom)
 
+      @loser = Factory :user, :demo => @demo
+      @reds = []
+      @blues = []
+      @greens = []
 
-  scenario "sees users segmented by characteristics", :js => true do
-    # Basic segmentation on just one characteristic
-    select "Color", :from => "segment_column[0]"
-    select "red", :from => "segment_value[0]"
+      14.times do |i|
+        @reds << Factory(:user, :name => "Red Guy #{i}", :demo => @demo, :characteristics => {@generic_characteristic_1.id => "red"})
+        @blues << Factory(:user, :name => "Blue Guy #{i}", :demo => @demo, :characteristics => {@generic_characteristic_1.id => "blue"})
+        @greens << Factory(:user, :name => "Green Guy #{i}", :demo => @demo, :characteristics => {@generic_characteristic_1.id => "green"})
+      end
 
-    click_button "Find segment"
+      %w(john john paul paul paul george george george george ringo ringo ringo ringo ringo).each_with_index do |name, i|
+        [@reds, @blues, @greens].each do |color_array|
+          color_array[i].characteristics[@generic_characteristic_2.id] = name
+          color_array[i].save!
+        end
+      end
 
-    expect_content "Segmenting on: Color is red"
-    expect_content "14 users in segment"
-    click_link "Show users"
-    @reds.each do |red|
-      expect_content "#{red.name}: #{red.email}"
+      %w(low low low low low low low medium medium medium high high high high).each_with_index do |name, i|
+        [@reds, @blues, @greens].each do |color_array|
+          color_array[i].characteristics[@demo_specific_characteristic_1.id] = name
+          color_array[i].save!
+        end
+      end
+
+      crank_off_dj
+
+      signin_as_admin
+      visit admin_demo_segmentation_path(@demo)
     end
 
-    # Segmenting on multiple characteristics
-    select "Color", :from => "segment_column[0]"
-    select "red", :from => "segment_value[0]"
-    click_link "Segment on more characteristics"
-    select "Favorite Beatle", :from => "segment_column[1]"
-    select "george", :from => "segment_value[1]"
+    scenario "sees users segmented by one characteristic", :js => true do
+      # Basic segmentation on just one characteristic
+      select "Color", :from => "segment_column[0]"
+      select "red", :from => "segment_value[0]"
 
-    click_button "Find segment"
+      click_button "Find segment"
 
-    expect_content "Color is red"
-    expect_content "Favorite Beatle is george"
-    expect_content "4 users in segment"
-    click_link "Show users"
-    [@reds[5], @reds[6], @reds[7], @reds[8]].each do |red|
-      expect_content "#{red.name}: #{red.email}"
+      expect_content "Segmenting on: Color is red"
+      expect_content "14 users in segment"
+      click_link "Show users"
+      @reds.each do |red|
+        expect_content "#{red.name}: #{red.email}"
+      end
     end
-    pending
+
+    scenario "sees users segmented by two characteristics", :js => true do
+      # Segmenting on multiple characteristics
+      select "Color", :from => "segment_column[0]"
+      select "red", :from => "segment_value[0]"
+      click_link "Segment on more characteristics"
+      select "Favorite Beatle", :from => "segment_column[1]"
+      select "george", :from => "segment_value[1]"
+
+      click_button "Find segment"
+
+      expect_content "Color is red"
+      expect_content "Favorite Beatle is george"
+      expect_content "4 users in segment"
+      click_link "Show users"
+      [@reds[5], @reds[6], @reds[7], @reds[8]].each do |red|
+        expect_content "#{red.name}: #{red.email}"
+      end
+    end
+
+    scenario "sees users segmented by three characteristics", :js => true do
+      # How about three?
+      
+      select "Color", :from => "segment_column[0]"
+      select "green", :from => "segment_value[0]"
+      click_link "Segment on more characteristics"
+      select "Favorite Beatle", :from => "segment_column[1]"
+      select "ringo", :from => "segment_value[1]"
+      click_link "Segment on more characteristics"
+      select "Height", :from => "segment_column[2]"
+      select "medium", :from => "segment_value[2]"
+
+      click_button "Find segment"
+
+      expect_content "Color is green"
+      expect_content "Favorite Beatle is ringo"
+      expect_content "Height is medium"
+      expect_content "1 users in segment"
+
+      click_link "Show users"
+      expect_content "#{@greens[9].name}: #{@greens[9].email}"
+    end
+
+    scenario "segments in a way that no employees match", :js => true do
+      # And let's go for a shutout
+      select "Color", :from => "segment_column[0]"
+      select "green", :from => "segment_value[0]"
+      click_link "Segment on more characteristics"
+      select "Favorite Beatle", :from => "segment_column[1]"
+      select "john", :from => "segment_value[1]"
+      click_link "Segment on more characteristics"
+      select "Height", :from => "segment_column[2]"
+      select "high", :from => "segment_value[2]"
+
+      click_button "Find segment"
+
+      expect_content "Color is green"
+      expect_content "Favorite Beatle is john"
+      expect_content "Height is high"
+      expect_content "0 users in segment"
+      expect_no_content "Show users"
+    end
   end
 
   scenario 'should have a proper link from somewhere'
