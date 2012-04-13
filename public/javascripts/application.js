7a523d17c73019b34e3cb2d1fa76764722461f43
Jack Desert <jack@hengage.com>
2012-04-13 16:30:43 -0400

    Third fix to byte counter

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -386,7 +386,8 @@ function addByteCounterFor(locator) {
   var ghettoUniqueId = "counter_" + Math.round(Math.random() * 10000000);
   $(locator).after('<span class="character-counter" id="' + ghettoUniqueId + '"></span>');
   updateCharacterCounter(locator, '#'+ghettoUniqueId);
-  $(locator).keydown(function() {
+  // I'm using keyup here instead of keypress so that it plays nicely in Chrome
+  $(locator).keyup(function() {
     // Put a tiny timeout in this so it waits for the data to hit the field before it calculates it
     setTimeout(function(){
       updateCharacterCounter(locator, '#'+ghettoUniqueId);
