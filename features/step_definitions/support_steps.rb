f597b2fdd7ff4200f3a640e062a92a1bb1c62e0f
Jack Desert <jack@hengage.com>
2012-05-01 12:00:08 -0400

    Got two tests passing

--- features/step_definitions/support_steps.rb
+++ features/step_definitions/support_steps.rb
@@ -2,7 +2,8 @@ Given /^support should have received a support email about "([^"]*)" with recent
   expected_name, expected_game, expected_email, expected_phone = user_specs.split('/')
   expected_bad_messages = recent_messages.split('/').join("\n")
 
-  step "\"support@hengage.com\" opens the email with subject \"Help request from core app for #{expected_name} of #{expected_game} \\(#{expected_email}, \\#{expected_phone}\\)\""
+
+  step "\"support@hengage.com\" opens the email with subject \"Help request from core app for #{expected_name} of #{expected_game}\""
   step "they should see \"Support requested by #{expected_name} of #{expected_game} (#{expected_email}, #{expected_phone})\" in the email body"
   step "they should see \"#{expected_bad_messages}\" in the email body"
 end
