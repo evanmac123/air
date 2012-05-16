39614b7542450bfb5814b902b32b14417e95f33a
Phil <phil@bartholomew.(none)>
2012-05-16 13:10:41 -0400

    goddammit Mail::Message, why can't you keep your fields consistent.

--- lib/monkey_patches/mail/message.rb
+++ lib/monkey_patches/mail/message.rb
@@ -4,13 +4,18 @@ class Mail::Message
 
     begin
       OutgoingEmail.create(subject: self.subject,
-                           from:    self.from.join(","),
-                           to:      self.to.join(","),
+                           from:    join_if_joinable(self.from),
+                           to:      join_if_joinable(self.to),
                            raw:     self.to_s
                           )
     rescue StandardError => e
       Airbrake.notify(e)
     end
+  end
+
+  def join_if_joinable(values_to_join)
+    return values_to_join unless values_to_join.respond_to?(:join)
+    values_to_join.join(",")
   end
 
   alias_method_chain :deliver, :outgoing_email_logging
