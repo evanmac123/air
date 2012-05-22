ed4a70fedfd2b56615e796d62685d42ac62cc41b
Phil <phil@bartholomew.(none)>
2012-05-22 11:55:18 -0400

    .pptx hack didn't work, just adding a regular .ppt

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -4,6 +4,3 @@
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
 Mime::Type.register_alias "text/html", :mobile
-
-# Hack to allow frontend server to serve Powerpoint 2007 docs
-Rack::Mime::MIME_TYPES['pptx'] = "application/vnd.openxmlformats" 
