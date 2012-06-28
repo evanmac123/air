ac28c34a658996343d0c8093835b09a342e51deb
Phil <phil@bartholomew.(none)>
2012-06-28 16:21:40 -0400

    converted user_navigates_site to steak

---
+++ spec/acceptance/user_navigates_site_with_navbar_spec.rb
@@ -0,0 +1,24 @@
+require 'acceptance/acceptance_helper'
+
+feature 'User navigates site with navbar' do
+  before(:each) do
+    @joe = FactoryGirl.create(:site_admin, name: 'Joe')
+    has_password(@joe, 'foobar')
+    signin_as(@joe, 'foobar')
+  end
+
+  {
+    'Admin'      => "admin_path",
+    'Settings'   => "edit_account_settings_path",
+    'Sign Out'   => "sign_in_path",
+    'Home'       => 'activity_path',
+    'My Profile' => 'user_path(@joe)',
+    'Directory'  => 'users_path',
+    'Help'       => 'faq_path'
+  }.each do |link_text, page_path_code|
+    it "should have a working link to #{link_text}" do
+      click_link link_text
+      should_be_on eval(page_path_code)
+    end
+  end
+end
