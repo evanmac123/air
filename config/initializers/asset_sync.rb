dad2cfb1cd2d072752ab1c87d76e4c2f532bd74c
Phil <phil@bartholomew.(none)>
2012-08-09 16:52:43 -0400

    sigh

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -2,23 +2,23 @@ begin
   AssetSync.configure do |config|
     config.fog_provider = 'AWS'
     config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
-    config.aws_secret_access_key = ENV['wVWjV8UxSl4y22x3SmSNsmUvRrRSGCIdXOEr9rM6']
-    config.fog_directory = ENV['FOG-DIRECTORY']
+    config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+    config.fog_directory = ENV['FOG_DIRECTORY']
     
-    ## Increase upload performance by configuring your region
-    ## config.fog_region = 'eu-west-1'
-    ##
-    ## Don't delete files from the store
-    ## config.existing_remote_files = "keep"
-    ##
-    ## Automatically replace files with their equivalent gzip compressed version
-    ## config.gzip_compression = true
-    ##
-    ## Use the Rails generated 'manifest.yml' file to produce the list of files to 
-    ## upload instead of searching the assets directory.
-    ## config.manifest = true
-    ##
-    ## Fail silently.  Useful for environments such as Heroku
+    # Increase upload performance by configuring your region
+    # config.fog_region = 'eu-west-1'
+    #
+    # Don't delete files from the store
+    # config.existing_remote_files = "keep"
+    #
+    # Automatically replace files with their equivalent gzip compressed version
+    # config.gzip_compression = true
+    #
+    # Use the Rails generated 'manifest.yml' file to produce the list of files to 
+    # upload instead of searching the assets directory.
+    # config.manifest = true
+    #
+    # Fail silently.  Useful for environments such as Heroku
     config.fail_silently = true
   end
 rescue NameError
