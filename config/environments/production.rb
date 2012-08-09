d2204162e5f1cbb01bf11970e57b5323e2e18dc4
Phil <phil@bartholomew.(none)>
2012-08-09 15:46:26 -0400

    get out of here, asset_sync. gonna go do this in a topic branch

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Health::Application.configure do
   config.serve_static_assets = false
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
-  config.action_controller.asset_host = "https://#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
+  #config.action_controller.asset_host = "https://hengage-production.s3.amazonaws.com"
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
