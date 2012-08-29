515417d27d322707eb03e43b664d57a6a836cc63
Jack Desert <jackdesert@gmail.com>
2012-08-29 11:12:22 -0400

    @@ Fixed the way we define    authorize_without_game_begun_check    (we are now using an around alias)

--- spec/controllers/interstitial_verifications_controller_spec.rb
+++ spec/controllers/interstitial_verifications_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe InterstitialVerificationsController do
+  it "should redirect if not signed in" do
+    get :show
+    response.should be_redirect
+  end
+    
 
 end
