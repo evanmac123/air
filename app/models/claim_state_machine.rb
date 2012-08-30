b60438dda1bd5ab5cc200a62686972a2fedf0c83
Phil <phil@bartholomew.(none)>
2012-08-30 14:02:22 -0400

    added Michelin-style claiming

--- app/models/claim_state_machine.rb
+++ app/models/claim_state_machine.rb
@@ -64,5 +64,34 @@ class ClaimStateMachine < ActiveRecord::Base
         ambiguity_message:                "Sorry, we're having a little trouble, it looks like we'll have to get a human involved. Please contact support@hengage.com for help joining the game. Thank you!"
       )
     }
+
+    MICHELIN_STYLE = {
+      1 => ClaimState.new(
+        finder_method: :by_claim_code, 
+        next_state_on_ambiguity_id: 2, 
+        ambiguity_message: "Sorry, we need a little more information to figure out who you are. Please send E if you are a Michelin employee, S if the spouse of a Michelin employee.", 
+        unrecognized_information_message: "Sorry, I don't recognize that. Please send your first initial and last name (if you're John Smith, send JSMITH)", 
+        already_claimed_message: "It looks like you're already signed up. Please contact support@hengage.com if you need help."
+      ),
+
+      2 => ClaimState.new(
+        finder_method: :by_employee_or_spouse_code, 
+        next_state_on_ambiguity_id: 3, 
+        ambiguity_message: "Sorry, we need a little more information to figure out who you are. Please send your CHORUS ID (or your spouse's if they're the Michelin employee).",
+        unrecognized_information_message: "Sorry, I don't recognize that. Please send your CHORUS ID (or your spouse's if they're the Michelin employee)", 
+        valid_format: /(e|E|s|S)/, 
+        invalid_format_message: "Please send E if you are a Michelin employee, S if the spouse of a Michelin employee.", 
+        already_claimed_message: "It looks like you're already signed up. Please contact support@hengage.com if you need help."
+      ),
+
+      3 => ClaimState.new(
+        finder_method: :by_employee_id, 
+        next_state_on_ambiguity_id: 3, 
+        unrecognized_information_message: "Sorry, I don't recognize that. Please try sending your (or your spouse's) CHORUS ID for help, or contact support@hengage.com for help.", 
+        valid_format: /^\d{8}$/, 
+        invalid_format_message: "Sorry, I don't recognize that. Please try sending your (or your spouse's) CHORUS ID: it should be an 8-digit number.", 
+        already_claimed_message: "It looks like you're already signed up. Please contact support@hengage.com if you need help."
+      ),
+    }
   end
 end
