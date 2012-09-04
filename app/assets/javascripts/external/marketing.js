2bb4a9647262d66938a60fb0bf4667f5cdebbce2
Jack Desert <jackdesert@gmail.com>
2012-09-04 15:32:02 -0400

    Fixed javascript in marketing.js

--- app/assets/javascripts/external/marketing.js
+++ app/assets/javascripts/external/marketing.js
@@ -1,31 +1,43 @@
-// Homepage personas module
-$('.a-feature').hover(function() {
-    //hide
-    $('.a-feature').removeClass('selected');
-    $('div.employee li').hide();
-    $('div.employee-quote li').hide();
-    //show
-    $(this).addClass('selected');
-    $('div.employee li.'+this.id).show();
-    $('div.employee-quote li.'+this.id).show();
+$(document).ready(function() {
+  connectPersonas();
+  connectTour();
+  connectSolutions();
 });
+
+// Homepage personas module
+function connectPersonas(){
+  $('.a-feature').hover(function() {
+      //hide
+      $('.a-feature').removeClass('selected');
+      $('div.employee li').hide();
+      $('div.employee-quote li').hide();
+      //show
+      $(this).addClass('selected');
+      $('div.employee li.'+this.id).show();
+      $('div.employee-quote li.'+this.id).show();
+  });
+}
 
 // Product Tour: Achieve Perpectual Engagement
-$('#perpeng #perpeng-features li').hover(function() {
-    $('#perpeng #perpeng-features li').removeClass('selected');
-    $('#perpeng div.image img').hide();
+function connectTour(){
+  $('#perpeng #perpeng-features li').hover(function() {
+      $('#perpeng #perpeng-features li').removeClass('selected');
+      $('#perpeng div.image img').hide();
 
-    $(this).addClass('selected');
-    $('#perpeng div.image img.'+this.id).show();
-});
+      $(this).addClass('selected');
+      $('#perpeng div.image img.'+this.id).show();
+  });
+}
 
 // Solutions page
-$('a.solution-name').click(function() {
-    $('#offerings .wrap').hide();
-    $('#measure ul').hide();
-    $('a.solution-name').removeClass('selected');
+function connectSolutions(){
+  $('a.solution-name').click(function() {
+      $('#offerings .wrap').hide();
+      $('#measure ul').hide();
+      $('a.solution-name').removeClass('selected');
 
-    $('#'+this.id+'-feats').fadeIn();
-    $('#'+this.id+'-measure').fadeIn();
-    $(this).addClass('selected');
-});
+      $('#'+this.id+'-feats').fadeIn();
+      $('#'+this.id+'-measure').fadeIn();
+      $(this).addClass('selected');
+  });
+}
