cf8bbe604e18cc87a652f007b422ab8302e9b2c8
Phil <phil@bartholomew.(none)>
2012-09-06 10:50:30 -0400

    more info requests associated with user

--- app/models/special_command_handlers/more_info_handler.rb
+++ app/models/special_command_handlers/more_info_handler.rb
@@ -3,8 +3,8 @@ require 'special_command_handlers/base'
 class SpecialCommandHandlers::MoreInfoHandler < SpecialCommandHandlers::Base
   def handle_command
     MoreInfoRequest.create!(
-      :phone_number => @user.phone_number,
-      :command      => 'moreinfo'
+      :user    => @user,
+      :command => 'info'
     )
 
     parsing_success_message("Great, we'll be in touch. Stay healthy!")
