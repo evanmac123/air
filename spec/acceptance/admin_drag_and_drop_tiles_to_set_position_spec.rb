d95b1903f8df4760f58d12c69d8b300f614e4c92
Phil <phil@bartholomew.(none)>
2012-09-26 15:24:28 -0400

    admin adds image to tile

--- spec/acceptance/admin_drag_and_drop_tiles_to_set_position_spec.rb
+++ spec/acceptance/admin_drag_and_drop_tiles_to_set_position_spec.rb
@@ -7,15 +7,11 @@ feature "Admin drags and drops tiles into position" do
   before(:each) do
     Demo.find_each { |f| f.destroy }
     @fun = FactoryGirl.create(:demo, name: 'Frickin FUN')
-    @tile_1 = FactoryGirl.create(:tile, demo: @fun, name: 'Tile 1')
-    @tile_2 = FactoryGirl.create(:tile, demo: @fun, name: 'Tile 2')
-    @tile_3 = FactoryGirl.create(:tile, demo: @fun, name: 'Tile 3')
-    @tile_4 = FactoryGirl.create(:tile, demo: @fun, name: 'Tile 4')
+    @tile_1 = FactoryGirl.create(:tile, demo: @fun, name: 'Tile 1', position: 1)
+    @tile_2 = FactoryGirl.create(:tile, demo: @fun, name: 'Tile 2', position: 2)
+    @tile_3 = FactoryGirl.create(:tile, demo: @fun, name: 'Tile 3', position: 3)
+    @tile_4 = FactoryGirl.create(:tile, demo: @fun, name: 'Tile 4', position: 4)
     signin_as_admin
-    @tile_1.position.should == 1
-    @tile_2.position.should == 2
-    @tile_3.position.should == 3
-    @tile_4.position.should == 4
   end
 
   it "should update the position", js: true do
