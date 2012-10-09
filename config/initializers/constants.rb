70dd6ea2a2a83d699052a2fdbdb2dfe239ac11eb
Larry Hannay <larry@hengage.com>
2012-10-09 16:41:42 -0400

    Added 'unless defined?' clauses on some constants so as to remove warning messages

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,13 +1,13 @@
 module SavedFlashes
-  SUCCESS_KEY = 'saved_flash_success'
-  FAILURE_KEY = 'saved_flash_failure'
+  SUCCESS_KEY = 'saved_flash_success' unless defined?(SUCCESS_KEY)
+  FAILURE_KEY = 'saved_flash_failure' unless defined?(FAILURE_KEY)
 end
 
 module FailureMessages
-  SESSION_EXPIRED = 'Your session has expired. Please log back in to continue.'
+  SESSION_EXPIRED = 'Your session has expired. Please log back in to continue.' unless defined?(SESSION_EXPIRED)
 end
 
 module SendGrid
-  DEV_PASSWORD = "8765432"
-  DEV_USERNAME = "hengage-devel"
+  DEV_PASSWORD = "8765432" unless defined?(DEV_PASSWORD)
+  DEV_USERNAME = "hengage-devel" unless defined?(DEV_USERNAME)
 end
