17520aeb6727696c67ee4ebf0e2ad048e7f8587b
Larry Hannay <larry@hengage.com>
2012-10-24 14:35:14 -0400

    Deleted 2 files associated with old "send activity reports" implementation: send_activity_dumps_controller.rb and activity_dump.rake

--- app/controllers/admin/send_activity_dumps_controller.rb
+++
@@ -1,8 +0,0 @@
-class Admin::SendActivityDumpsController < AdminBaseController
-  def create
-    Report::Activity.new(params[:demo_id]).delay.email_to('larry@hengage.com')
-
-    flash[:success] = "Aaaaaaand...scheduled! Check for it in a few minutes."
-    redirect_to :back
-  end
-end
