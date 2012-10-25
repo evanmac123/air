95e0cd821cbe82c93b20f4aa97db1e2a9e8f97c2
Larry Hannay <larry@hengage.com>
2012-10-25 16:23:48 -0400

    Cleaned up activity model for sending activity report

--- app/models/report/activity.rb
+++ app/models/report/activity.rb
@@ -6,9 +6,9 @@ class Report::Activity
     @demo_id = demo_id       # Gets stuffed in the DelayedJob table/queue => just save the id
   end
 
-  def send_email(addresses)
+  def send_email(address)
     @demo = Demo.find @demo_id
-    addresses.split(/,/).each { |address| Mailer.activity_report(csv_data, @demo.name, Time.now, address).deliver }
+    Mailer.activity_report(csv_data, @demo.name, Time.now, address).deliver
   end
 
   protected
@@ -17,23 +17,16 @@ class Report::Activity
     ("id,location,rule,date" + "\n" + csv_data_per_act.join('')).strip
   end
 
+  # Generate CSV file piecemeal (in batches of 1000) so don't exceed Heroku's memory limitation
   def csv_data_per_act
     csv_all_acts = []
-
-    @demo.rule_based_acts.find_each(:batch_size => 1000) { |act| csv_all_acts << CSV.generate_line(act_data(act)) }
-
-    # 1000 records is the default. Specifying just to make clear what we are doing and in case we need to tweak it.
-    #Act.find_each(conditions: ["demo_id = ? AND rule_id IS NOT NULL", @demo_id],
-    #              batch_size: 1000) do |act|
-    #  csv_all_acts << CSV.generate_line(act_data(act))
-    #end
-
+    @demo.rule_based_acts.find_each { |act| csv_all_acts << CSV.generate_line(act_data(act)) }
     csv_all_acts
   end
 
   def act_data(act)
     id = act.user_id
-    location = act.user.location.try(:name)
+    location = act.user.location.try(:name)  # Not all users have a location
     rule = act.rule.primary_value.value
     date = act.created_at.strftime("%m-%d-%Y")
 
