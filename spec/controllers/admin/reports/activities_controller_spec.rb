7550c75bbcb525e3d0faa7df2023235aad3b7027
Larry Hannay <larry@hengage.com>
2012-10-30 15:54:35 -0400

    Comments

--- spec/controllers/admin/reports/activities_controller_spec.rb
+++ spec/controllers/admin/reports/activities_controller_spec.rb
@@ -13,7 +13,9 @@ describe Admin::Reports::ActivitiesController do
 
     it 'should add a send-report-activity email task to the DelayedJobs table' do
       # 2 jobs are stuffed into the DJ queue before the email one => 'DJ.last'
-      # Actual entry looks something like the following ('m' modifier => 'multiline' => '.' can match newline characters)
+      # '/~~~/m' modifier => 'multiline' => '.*' can match newline characters
+      #
+      # Actual entry looks something like the following:
       # "!ruby/object:Delayed::PerformableMethod \nargs: \n- larry@hengage.com\nmethod_name: :send_email\nobject: !ruby/object:Report::Activity \n  demo_id: 1\n"
       Delayed::Job.last.handler.should =~ /send_email.*Report::Activity/m
     end
