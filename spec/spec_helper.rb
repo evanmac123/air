650284e4c74bd1988ff9e81cebf20d0ca4720c12
Phil Darnowsky <phil@darnowsky.com>
2012-11-26 16:18:19 -0500

    cover_me requires removed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,9 +27,6 @@ Spork.prefork do
   Spork.trap_method(ActiveSupport::Dependencies::Loadable, :load)                   #
   ###################################################################################
 
-  # See Gemfile for why we require this but never use it.
-  require "cover_me"
-
   # This file is copied to spec/ when you run 'rails generate rspec:install'
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
