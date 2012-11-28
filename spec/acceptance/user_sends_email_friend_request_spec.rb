51caaec51eff5b29a8d5a2b4b06544c44a92a39b
Larry Hannay <larry@hengage.com>
2012-11-28 11:07:24 -0500

    Updated specs for sending friend-request email

--- spec/acceptance/user_sends_email_friend_request_spec.rb
+++ spec/acceptance/user_sends_email_friend_request_spec.rb
@@ -6,25 +6,30 @@ feature "User tries to friend someone" do
     user.name.split[0]
   end
 
-  def deliver_and_open_email(recipient)
+  # NOTE: For debugging, use statement below to create a file like /tmp/email-123456789.txt
+  # EmailSpec::EmailViewer.save_and_open_email(current_email)
+  def deliver_and_open_email_for(recipient)
     crank_dj_clear
     open_email(recipient.email)
   end
 
-  let(:demo) { FactoryGirl.create :demo }
-  let(:user) { FactoryGirl.create :user, :claimed, demo: demo, name: 'Joe User' }
+  def accept_the_friendship
+    # Use this method which finds/clicks a link based upon actual href
+    # (In the HTML email the word "Accept" is actually in a <span> tag, not an <a href='xxx'> one
+    click_email_link_matching /accept/
+  end
+
+  # ========================================================================================
+
+  let(:demo)    { FactoryGirl.create :demo }
+  let(:user)    { FactoryGirl.create :user, :claimed, demo: demo, name: 'Joe User'   }
+  let!(:friend) { FactoryGirl.create :user, :claimed, demo: demo, name: 'Sue Friend' }
 
   before(:each) do
     # NOTE: Need these guys to get rid of overlay screens for talking-chicken tutorial and inviting people
     # to join the game. If don't get rid of these screens => can't click on any links because they are "covered".
     User.any_instance.stubs(:create_tutorial_if_none_yet) # Uses 'fancybox' css selectors
     user.update_attribute :session_count, 10              # Uses 'facebox'  css selectors
-
-    # NOTE: Can't use the 'let!' statement below with the other 'let's (above) because the
-    # friend never gets created, even though there's a '!' on the end of 'let'
-    # let!(:friend) { FactoryGirl.create :user, :claimed, demo: demo, name: 'Sue Friend' }
-
-    @friend = FactoryGirl.create :user, :claimed, demo: demo, name: 'Sue Friend', email: 'sue@friend.com'
   end
 
   background do
@@ -40,54 +45,77 @@ feature "User tries to friend someone" do
 
     page.find('div.follow-btn').click
     page.should have_content "OK, you'll be friends with Sue Friend, pending their acceptance"
+  end
 
-    crank_dj_clear
-    open_email(@friend.email)
+  scenario "A correct friend-request email is sent", js: true  do
+    deliver_and_open_email_for(friend)
 
-    # NOTE: For debugging, creates a file like /tmp/email-123456789.txt
-    # EmailSpec::EmailViewer.save_and_open_email(current_email)
-
-    current_email.should be_delivered_to(@friend.email)
-    current_email.should be_delivered_from(@friend.reply_email_address)
+    current_email.should be_delivered_to(friend.email)
+    current_email.should be_delivered_from(friend.reply_email_address)
 
     current_email.should have_subject("#{user.name} wants to be your friend on H Engage")
 
-    current_email.should have_body_text /Hi #{first_name(@friend)}/
+    current_email.should have_body_text /Hi #{first_name(friend)}/
     current_email.should have_body_text /#{user.name} has asked to be your friend on H Engage./
   end
 
-  scenario "User accepts the friend request; notices are displayed; notification email is sent", js: true  do
-    page.should_not have_content "is now friends with #{first_name(@friend)}"
+  scenario "when the friend accepts the request, the user should get a notification email \
+            see updated friend status, and actually be in a friendship", js: true  do
+
+    page.should_not have_content "is now friends with #{first_name(friend)}"
 
     visit profile_page(user)
     page.should have_content "No friends yet"
 
-    deliver_and_open_email @friend
-
-    # Use this method which finds/clicks a link based upon actual href
-    # (In the HTML email the word "Accept" is actually in a <span> tag, not an <a href='xxx'> one
-    click_email_link_matching /accept/
-
-    page.should have_content "OK, you are now friends with #{user.name}."
+    deliver_and_open_email_for(friend)
+    accept_the_friendship
 
     visit profile_page(user)
-    page.should have_content "is now friends with #{first_name(@friend)}"
+    page.should have_content "is now friends with #{first_name(friend)}"
 
-    user.should be_friends_with @friend
-    @friend.should be_friends_with user
+    user.should be_friends_with friend
+    friend.should be_friends_with user
 
-    crank_dj_clear
-    open_email(user.email)
+    deliver_and_open_email_for(user)
 
     current_email.should be_delivered_to(user.email)
     current_email.should be_delivered_from(user.reply_email_address)
 
     current_email.should have_subject("Message from H Engage")
-    current_email.should have_body_text /#{@friend.name} has approved your friendship request./
+    current_email.should have_body_text /#{friend.name} has approved your friendship request./
   end
 
-  scenario "User tries to force processing of the friend request with an invalid token", js: true do
-    deliver_and_open_email @friend
+  scenario "A logged-in friend should see a a flash notification upon accepting the friendship", js: true  do
+    click_link "Sign Out"
+    signin_as(friend, friend.password)
+
+    deliver_and_open_email_for(friend)
+    accept_the_friendship
+
+    page.should have_content "OK, you are now friends with #{user.name}."
+  end
+
+  scenario "A not-logged-in friend should see a a flash notification upon \
+            accepting the friendship and logging in", js: true  do
+
+    click_link "Sign Out"
+
+    deliver_and_open_email_for(friend)
+    accept_the_friendship
+
+    page.should have_content "Log In Your session has expired"
+    signin_as(friend, friend.password)
+
+    page.should have_content "OK, you are now friends with #{user.name}."
+  end
+
+  scenario "A logged-in friend should see a flash error message and not become friends \
+            when he tries to process the friend request with an invalid token", js: true do
+
+    click_link "Sign Out"
+    signin_as(friend, friend.password)
+
+    deliver_and_open_email_for(friend)
 
     # Chop off the last character of the authenticity token
     link = links_in_email(current_email).find { |link| link =~ /accept/ }.chop
@@ -95,8 +123,27 @@ feature "User tries to friend someone" do
 
     page.should have_content "Invalid authenticity token. Friendship operation cancelled."
 
-    user.should_not be_friends_with @friend
-    @friend.should_not be_friends_with user
+    user.should_not be_friends_with friend
+    friend.should_not be_friends_with user
+  end
+
+  scenario "A not-logged-in friend should see a flash error message and not become friends \
+            when he tries to process the friend request with an invalid token and then logs in", js: true do
+
+    click_link "Sign Out"
+    deliver_and_open_email_for(friend)
+
+    # Chop off the last character of the authenticity token
+    link = links_in_email(current_email).find { |link| link =~ /accept/ }.chop
+    visit request_uri(link)
+
+    page.should have_content "Log In Your session has expired"
+    signin_as(friend, friend.password)
+
+    page.should have_content "Invalid authenticity token. Friendship operation cancelled."
+
+    user.should_not be_friends_with friend
+    friend.should_not be_friends_with user
   end
 end
 
