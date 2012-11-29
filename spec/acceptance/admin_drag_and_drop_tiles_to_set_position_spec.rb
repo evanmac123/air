f5947dfdc4ccc6e6e1596eee9d4f9bf3a6b62e06
Larry Hannay <larry@hengage.com>
2012-11-29 14:46:16 -0500

    Moved signin_as_admin step above tile-creation steps in the always-entertaining "Admin drags and drops tiles" spec

--- spec/acceptance/admin_drag_and_drop_tiles_to_set_position_spec.rb
+++ spec/acceptance/admin_drag_and_drop_tiles_to_set_position_spec.rb
@@ -6,12 +6,12 @@ require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
 feature "Admin drags and drops tiles into position" do
   before(:each) do
     Demo.find_each { |f| f.destroy }
+    signin_as_admin
     @fun = FactoryGirl.create(:demo, name: 'Frickin FUN')
     @tile_1 = FactoryGirl.create(:tile, demo: @fun, headline: 'Tile 1', position: 1)
     @tile_2 = FactoryGirl.create(:tile, demo: @fun, headline: 'Tile 2', position: 2)
     @tile_3 = FactoryGirl.create(:tile, demo: @fun, headline: 'Tile 3', position: 3)
     @tile_4 = FactoryGirl.create(:tile, demo: @fun, headline: 'Tile 4', position: 4)
-    signin_as_admin
   end
 
   it "should update the position", js: true do
