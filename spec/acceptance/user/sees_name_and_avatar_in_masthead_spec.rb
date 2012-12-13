11d323d435ee37700f21d23d5b2f03f0ac42b236
Phil Darnowsky <phil@darnowsky.com>
2012-12-13 12:47:05 -0500

    masthead now has total points in it

--- spec/acceptance/user/sees_name_and_avatar_in_masthead_spec.rb
+++ spec/acceptance/user/sees_name_and_avatar_in_masthead_spec.rb
@@ -1,16 +1,16 @@
 require 'acceptance/acceptance_helper'
 
-feature "User Sees Name And Avatar In Masthead" do
-
-  scenario "User sees their first name and avatar in the masthead" do
-    user = FactoryGirl.create :user, :name => "John Fitzgerald Kennedy", :avatar_file_name => 'ein_berliner.jpg'
+feature "User proper stuff in masthead" do
+  scenario "User sees their first name, avatar and score in the masthead" do
+    user = FactoryGirl.create :user, :name => "John Fitzgerald Kennedy", :avatar_file_name => 'ein_berliner.jpg', :points => 1234
     has_password(user, "foobar")
 
     signin_as(user, "foobar")
     should_be_on activity_path(:format => :html)
 
     within('.masthead') do
-      page.should have_content "Welcome back, John"
+      page.should have_content "Hi, John"
+      page.should have_content "Total points: 1,234"
       expect_avatar_in_masthead('ein_berliner.png')
     end
 
