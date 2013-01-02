20e4fb0549d006493838723221d416805cac4d38
Larry Hannay <larry@hengage.com>
2013-01-02 12:20:03 -0500

    Minor changes to some spec helpers

--- spec/acceptance/user/tiles_with_mulitiple_triggers_spec.rb
+++ spec/acceptance/user/tiles_with_mulitiple_triggers_spec.rb
@@ -32,17 +32,18 @@ feature 'User interacts with single- and multiple-trigger tiles' do
   let(:or_tile)     { FactoryGirl.create :tile, demo: demo, poly: false, headline: 'Or Tile' }
   let(:and_tile)    { FactoryGirl.create :tile, demo: demo, poly: true,  headline: 'And Tile' }
 
+  # Note the '!'s because these variables aren't referenced in the tests
+  let!(:trigger_single) { FactoryGirl.create :rule_trigger, tile: single_tile, rule: fish_rule }
+
+  let!(:trigger_or_1) { FactoryGirl.create :rule_trigger, tile: or_tile, rule: walk_rule }
+  let!(:trigger_or_2) { FactoryGirl.create :rule_trigger, tile: or_tile, rule: run_rule }
+
+  let!(:trigger_and_1) { FactoryGirl.create :rule_trigger, tile: and_tile, rule: beans_1_rule }
+  let!(:trigger_and_2) { FactoryGirl.create :rule_trigger, tile: and_tile, rule: beans_2_rule }
+  let!(:trigger_and_3) { FactoryGirl.create :rule_trigger, tile: and_tile, rule: beans_3_rule }
+
   before(:each) do
     bypass_modal_overlays(user)
-
-    FactoryGirl.create :rule_trigger, tile: single_tile, rule: fish_rule
-
-    FactoryGirl.create :rule_trigger, tile: or_tile, rule: walk_rule
-    FactoryGirl.create :rule_trigger, tile: or_tile, rule: run_rule
-
-    FactoryGirl.create :rule_trigger, tile: and_tile, rule: beans_1_rule
-    FactoryGirl.create :rule_trigger, tile: and_tile, rule: beans_2_rule
-    FactoryGirl.create :rule_trigger, tile: and_tile, rule: beans_3_rule
   end
 
   background do
@@ -68,11 +69,19 @@ feature 'User interacts with single- and multiple-trigger tiles' do
     find("a[href='#{tile_path(tile)}']").click
   end
 
+  def show_previous_tile
+    page.find("#prev").click
+  end
+
+  def show_next_tile
+    page.find("#next").click
+  end
+
   def current_slideshow_tile
     sleep 0.5  # Give tile attributes time to change
 
     # Use the z-index to determine which tile is visible. (Not crazy about it, but since it's the only thing that works...)
-    current_tile = all('#slideshow img').sort_by { |img| img[:style].slice(/(z-index: )(\d)/, 2) }.reverse.first
+    current_tile = all('#slideshow img').sort_by { |img| img[:style].slice(/(z-index: )(\d)/, 2) }.last
 
     current_tile[:id]  # 'current_tile' is a Capybara::Node::Element => return its 'id' attribute
   end
@@ -104,21 +113,22 @@ Since we need to test which tiles are visible... well, which one would you use?
     click_carousel_tile(or_tile)
     current_slideshow_tile.should be_tile(or_tile)
 
-    page.find("#prev").click
+    show_previous_tile
     current_slideshow_tile.should be_tile(single_tile)
 
-    page.find("#prev").click
+    show_previous_tile
     current_slideshow_tile.should be_tile(and_tile)
 
-    page.find("#prev").click
+    show_previous_tile
     current_slideshow_tile.should be_tile(or_tile)
 
-    page.find("#next").click
+    show_next_tile
     current_slideshow_tile.should be_tile(and_tile)
 
     # TODO Uncomment either of the first statements in the groups => all steps still run to completion
     # TODO Then uncomment either of the second statements in the groups => hangs.
     # TODO But these same group of statements work upstairs. ^%$#@!$#@!!
+    # TODO Well, this used to be true, but now uncommenting the first stmt. in the first group no longer passes. Sigh...
 
     #click_link "Home"
     #click_carousel_tile(single_tile)
