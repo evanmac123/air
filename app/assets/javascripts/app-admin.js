1ff60ae73765189cec6bf0727ced296f4ba3a00c
Larry Hannay <larry@hengage.com>
2013-01-07 13:35:05 -0500

    Specified highchart javascript files to load in assets manifest

--- app/assets/javascripts/app-admin.js
+++ app/assets/javascripts/app-admin.js
@@ -1,6 +1,11 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.sortable
+
+// highcharts.js and exporting.js are both from Highcharts, and the load order matters
+//= require ../../../vendor/assets/javascripts/admin/highcharts
+//= require ../../../vendor/assets/javascripts/admin/exporting
+
 //= require_tree ../../../vendor/assets/javascripts/admin/.
 //= require_self
 //= require_tree ./admin/.
