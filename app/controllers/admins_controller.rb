f56617e762c51b863dbd687d6f19e52aa9afb11c
Larry Hannay <larry@hengage.com>
2013-01-09 10:52:29 -0500

    Moved highcharts test code from admin controller/show to client-admin controller/show

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,16 +2,6 @@ class AdminsController < AdminBaseController
   def show
     @current_user = current_user
     @demos = Demo.alphabetical
-
-    min_date = Act.minimum :created_at
-    ids = Act.pluck :id
-
-    @highchart = LazyHighCharts::HighChart.new('graph', style: '') do |f|
-      f.chart(defaultSeriesType: 'area')
-      f.series(name: 'Act ID', data: ids, point_start: min_date.to_time.to_i, color: '#4D7A36')
-      f.xAxis(type: :datetime, title: {text: 'What the fuck'})
-    end
-
     render :template => 'admin/show'
   end
 end
--- app/controllers/client_admins_controller.rb
+++ app/controllers/client_admins_controller.rb
@@ -12,5 +12,33 @@ class ClientAdminsController < ClientAdminBaseController
     @with_peer_invitation_fraction = demo.claimed_user_with_peer_invitation_fraction.as_rounded_percentage
 
     Mixpanel::Tracker.new(MIXPANEL_TOKEN, {}).delay.track("viewed page", {page_name: 'client admin dashboard'}.merge(current_user.data_for_mixpanel))
+
+    # ============= My New Stuff ===============
+
+    acts = Demo.find(13).acts
+    h = acts.group_by(&:group_by_date)
+    h.each { |k,v| h[k] = v.length }
+    #data = h.collect { |k,v| [k,v] }
+    data = h.sort  # returns [ [k,v] [k,v] ... [k,v] ] sorted by keys, i.e. creation date
+
+    min_date = acts.minimum(:created_at).to_date
+    max_date = acts.maximum(:created_at).to_date
+
+    p "******* min is #{min_date} and max is #{max_date}"
+
+    #ids = acts.pluck :id
+    #plain_ids = ids.map { |id| id + 20 }
+
+    @hc = LazyHighCharts::HighChart.new('graph') do |f|
+      #f.series(name: 'Act ID', data: ids, point_start: min_date.to_time.to_i, color: '#4D7A36')
+      #f.options[:plotOptions][:line][:pointStart] = min_date
+      #f.plotOptions(:line][:pointStart] = min_date
+      #f.options[:plotOptions][:line][:pointInterval] =  24 * 3600 * 1000 # one day
+
+      f.chart(zoomType: 'x')
+      f.xAxis(type: 'datetime')
+      f.series(name: 'Number of acts per day', data: data, color: '#4D7A36', pointStart: min_date.to_datetime.utc)
+      #f.series(name: 'Number of acts per day', data: data, color: '#4D7A36', pointStart: min_date, pointInterval: 24 * 3600 * 1000)
+    end
   end
 end
