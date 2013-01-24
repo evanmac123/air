9faab7a7edac0d85b0b16f711a9325bf5a5f6a06
Larry Hannay <larry@hengage.com>
2013-01-24 08:47:54 -0500

    Kept start date in synch with end date if they changed the latter while in hourly mode

--- app/assets/javascripts/admin/chart.js
+++ app/assets/javascripts/admin/chart.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
   $('#chart_start_date, #chart_end_date').datepicker();
 
+  // This is the most "usual" case
   $('#chart_interval, #chart_start_date').change(function() {
     if ($('#chart_interval').val() == 'Hourly')
       $('#chart_end_date').val($('#chart_start_date').val());
+  });
+
+  // This can happen, and since we want to keep these dates the same if 'hourly'...
+  $('#chart_end_date').change(function() {
+    if ($('#chart_interval').val() == 'Hourly')
+      $('#chart_start_date').val($('#chart_end_date').val());
   });
 });
