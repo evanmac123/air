7d1a08fac3653a411c44aa3539ac518445da5822
Phil Darnowsky <phil@darnowsky.com>
2013-01-28 18:36:40 -0500

    a note for later

--- app/models/command.rb
+++ app/models/command.rb
@@ -2,6 +2,9 @@ module Command
   def self.parse(user_or_phone, text, options={})
     allow_claim_account = options.delete(:allow_claim_account)
 
+    # HRFF: We could cut down DB load significantly by looking up the user here
+    # once and passing it around, rather than passing in user_or_phone
+
     SpecialCommand.parse(user_or_phone, text, options) ||
     User.send_invitation_if_claimed_sms_user_texts_us_an_email_address(user_or_phone, text, options) ||
     (allow_claim_account && User.claim_account(user_or_phone, options[:receiving_number], text, options).try(:first)) ||
