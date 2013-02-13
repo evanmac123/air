a4d03886583cf91a748ebff7312cf2a5b7aa24c5
Phil Darnowsky <phil@darnowsky.com>
2013-02-12 20:16:50 -0500

    line chopper loads up a separate queue with every line

--- app/models/s3_line_chopper_to_redis.rb
+++ app/models/s3_line_chopper_to_redis.rb
@@ -4,12 +4,17 @@ class S3LineChopperToRedis < S3LineChopper
 
     chop do |line|
       redis.lpush(redis_preview_queue_key, line) if count < lines_to_preview
+      redis.lpush(redis_load_queue_key, line)
       count += 1
     end
   end
 
   def redis_preview_queue_key
     "bulk_upload:preview:#{@object_key}"
+  end
+
+  def redis_load_queue_key
+    "bulk_upload:load:#{@object_key}"
   end
 
   protected
--- spec/models/s3_line_chopper_to_redis_spec.rb
+++ spec/models/s3_line_chopper_to_redis_spec.rb
@@ -7,30 +7,46 @@ describe S3LineChopperToRedis do
   before do
     mock_s3 = MockS3.install
     mock_s3.mount_file(EXPECTED_OBJECT_KEY, TEST_FILE_PATH, 100)
-    @chopper = S3LineChopperToRedis.new("some_bucket", EXPECTED_OBJECT_KEY)
-  
-    # there is probably a better way to flush Redis
-    Redis.new(url: ENV['REDISTOGO_URL']).del(@chopper.send(:redis_preview_queue_key))
+    Redis.new.flushdb
   end
+
+  let(:chopper) {S3LineChopperToRedis.new("some_bucket", EXPECTED_OBJECT_KEY)}
 
   describe "#redis_preview_queue_key" do
     it "should return a key based on the S3 object key" do
-      @chopper.redis_preview_queue_key.should == "bulk_upload:preview:#{EXPECTED_OBJECT_KEY}"
+      chopper.redis_preview_queue_key.should == "bulk_upload:preview:#{EXPECTED_OBJECT_KEY}"
+    end
+  end
+
+  describe "#redis_load_queue_key" do
+    it "should return a key based on the S3 object key" do
+      chopper.redis_load_queue_key.should == "bulk_upload:load:#{EXPECTED_OBJECT_KEY}"
     end
   end
 
   describe "#feed_to_redis" do
-    it "should put each line of the S3 object into a Redis queue, up to a limit" do
-      lines_to_preview = 5
-      @chopper.feed_to_redis(5)
-
-      expected_lines = File.read(TEST_FILE_PATH).lines.to_a[0, lines_to_preview]
-
+    let (:lines_to_preview) {5}
+    
+    before(:each) do
+      chopper.feed_to_redis(lines_to_preview)
+    end
+   
+    def expect_lines_in_queue(key, expected_count)
       redis = Redis.new
-      redis.llen(@chopper.redis_preview_queue_key).should == lines_to_preview
+      redis.llen(key).should == expected_count
+
+      expected_lines = File.read(TEST_FILE_PATH).lines.to_a[0, expected_count]
       expected_lines.each do |line|
-        redis.rpop(@chopper.redis_preview_queue_key).should == line
+        redis.rpop(key).should == line
       end
+    end
+
+    it "should put a limited number of keys into a Redis queue for preview" do
+      expect_lines_in_queue(chopper.redis_preview_queue_key, lines_to_preview)
+    end
+
+    it "should put every line into a Redis queue for loading" do
+      expect_lines_in_queue(chopper.redis_load_queue_key, File.read(TEST_FILE_PATH).lines.to_a.length)
     end
   end
 end
