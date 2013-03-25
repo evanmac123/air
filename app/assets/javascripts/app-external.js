247f3ba6c57d33896e381667e56ec7aed2730a24
Connie Chan <hello.cchan@gmail.com>
2013-03-25 13:07:48 -0400

    removed more old marketing stuff

--- app/assets/javascripts/app-external.js
+++ app/assets/javascripts/app-external.js
@@ -1,7 +1,4 @@
 //= require jquery
-//= require jquery_ujs
 //= require_tree ../../../vendor/assets/javascripts/external/.
-//= require fancybox
-//= require_self
 //= require_tree ./external/.
 //= require_tree ./internal_and_external/.
