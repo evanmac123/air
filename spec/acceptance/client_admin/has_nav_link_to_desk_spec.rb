23d0bf1c4bb15a42d91fc308470c10176d82981e
Phil Darnowsky <phil@darnowsky.com>
2013-05-13 15:49:26 -0400

    for now, only site admins get link to desk

--- spec/acceptance/client_admin/has_nav_link_to_desk_spec.rb
+++ spec/acceptance/client_admin/has_nav_link_to_desk_spec.rb
@@ -1,13 +1,28 @@
 require 'acceptance/acceptance_helper'
 
 feature 'Has nav link to desk' do
-  scenario 'with the correct URL' do
-    fake_url = "https://www.example.com/hengage-desk"
-    DeskSSO.any_instance.stubs(:url).returns(fake_url)
+  let(:fake_url)            { "https://www.example.com/hengage-desk" }
+  let(:desk_link_selector)  { "a[href='#{fake_url}']" }
+  let(:desk_link_text)      { "Help pages" }
 
-    visit(client_admin_path(as: a_client_admin))
+  before do
+    DeskSSO.any_instance.stubs(:url).returns(fake_url)
+  end
+
+  # Until we get a bit of content up there, this is restricted to site admins
+  # only.
+
+  scenario 'with the correct URL' do
+    #visit(client_admin_path(as: a_client_admin))
+    visit(client_admin_path(as: an_admin))
     
-    desk_link = page.find("a[href='#{fake_url}']")
-    desk_link.text.should == "Help pages"
+    desk_link = page.find(desk_link_selector)
+    desk_link.text.should == desk_link_text
+  end
+
+  scenario "but only for a site admin (for the moment)" do
+    visit(client_admin_path(as: a_client_admin))
+    page.all(desk_link_selector).should be_empty
+    expect_no_content(desk_link_text)
   end
 end
