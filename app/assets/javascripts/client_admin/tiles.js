1cc0c52f78d214650a830b93d716d0cc672bc501
Larry Hannay <larry@hengage.com>
2013-05-22 18:21:47 -0400

    Added a spinner for the ajax operations

--- app/assets/javascripts/client_admin/tiles.js
+++ app/assets/javascripts/client_admin/tiles.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
+
+  $(document).ajaxStart(function() { $('#show-user-spinner').show(); })
+             .ajaxStop(function()  { $('#show-user-spinner').hide(); });
+
+  var add_csrf_protection = function(params) {
+    var csrf_param = $('meta[name=csrf-param]').attr('content'),
+        csrf_token = $('meta[name=csrf-token]').attr('content');
+
+    params[csrf_param] = csrf_token;
+    return params;
+  };
+
   var sendOnDayChange = function() {
-    alert('Day Changed');
+    var $sendOn = $(this);
+
+    var params = { send_on: $sendOn.val(), _method: 'put' };
+    params = add_csrf_protection(params);
+
+    $.post($sendOn.attr('data-action'), params, function(data) { /*$('#report_link_response').html(data);*/ });
   };
 
   var sendNow = function() {
