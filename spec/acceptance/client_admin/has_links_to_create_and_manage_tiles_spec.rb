332c1e50ce850799e38f16c0df8b1f8626040287
Phil Darnowsky <phil@darnowsky.com>
2013-06-11 11:42:10 -0400

    link under carousel to manage tiles for client admin

--- spec/acceptance/client_admin/has_links_to_create_and_manage_tiles_spec.rb
+++ spec/acceptance/client_admin/has_links_to_create_and_manage_tiles_spec.rb
@@ -6,13 +6,19 @@ feature 'there is a link to add new tile from activity page' do
     page.all("a[href='#{new_client_admin_tile_path}']", text: "Add a new tile").first
   end
 
+  def manage_tile_link
+    page.all("a[href='#{client_admin_tiles_path}']", text: "Manage tiles").first
+  end
+
   scenario 'visible to a client admin' do
     visit activity_path(as: a_client_admin)
     add_tile_link.should be_present
+    manage_tile_link.should be_present
   end
 
   scenario 'invisible to a non-admin user' do
     visit activity_path(as: a_regular_user)
     add_tile_link.should be_nil
+    manage_tile_link.should be_nil
   end
 end
