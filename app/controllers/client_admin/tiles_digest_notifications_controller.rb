ea443f881be74ce6a6eeec1b9aad8f45defd8e28
larry <larry@hengagae.com>
2013-06-13 17:27:49 -0400

    Changed digest email drop-down to show a flash message when changing the day.

--- app/controllers/client_admin/tiles_digest_notifications_controller.rb
+++ app/controllers/client_admin/tiles_digest_notifications_controller.rb
@@ -2,15 +2,18 @@ class ClientAdmin::TilesDigestNotificationsController < ClientAdminBaseControlle
   before_filter :get_demo
 
   def create
-    TilesDigestMailer.notify(@demo).deliver
+    #TilesDigestMailer.delay.notify(@demo.id)
 
-    @demo.update_attributes tile_digest_email_sent_at: Time.now
-    @tile_digest_email_sent_at = @demo.tile_digest_email_sent_at
+    flash[:success] = "Tiles digest email was sent"
+    redirect_to client_admin_tiles_path
+
+    #@demo.update_attributes tile_digest_email_sent_at: Time.now
+    #@tile_digest_email_sent_at = @demo.tile_digest_email_sent_at
   end
 
   def update
     @demo.update_attributes tile_digest_email_send_on: params[:send_on]
-    render text: "Send-on day updated to #{@demo.tile_digest_email_send_on}"
+    flash[:success] = "Digest email weekly-send-on day was updated to #{@demo.tile_digest_email_send_on}"
   end
 
   private
