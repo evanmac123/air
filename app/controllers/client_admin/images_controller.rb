3221d36290f015339997e131098ab46f8871f8c7
Phil Darnowsky <phil@darnowsky.com>
2013-06-18 15:54:54 -0400

    updating tile image also updates thumbnail

--- app/controllers/client_admin/images_controller.rb
+++ app/controllers/client_admin/images_controller.rb
@@ -2,7 +2,7 @@ class ClientAdmin::ImagesController < ClientAdminBaseController
   def update
     tile = current_user.demo.tiles.find params[:tile_id]
     new_image = params[:tile_builder_form].try(:[], :image)
-    tile.image = new_image
+    tile.image = tile.thumbnail = new_image
 
     if tile.save(:context => :client_admin)
       flash[:success] = "OK, you've uploaded a new image."
