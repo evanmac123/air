96e2380858834341ce245a40282e5697b808c6c7
larry <larry@hengagae.com>
2013-06-18 11:49:09 -0400

    Switching over to list of users for digest email

--- app/mailers/tiles_digest_mailer.rb
+++ app/mailers/tiles_digest_mailer.rb
@@ -1,15 +1,15 @@
 class TilesDigestMailer < ActionMailer::Base
-  TEST_EMAIL = 'connie@hengage.com'
 
   helper :email                # loads 'app/helpers/email_helper.rb' & includes 'EmailHelper' into the View
   helper 'client_admin/tiles'  # ditto for 'tiles_helper.rb'
 
   has_delay_mail  # Some kind of monkey-patch workaround (not even sure need)
 
-  def notify(tile_ids)
+  def notify(user_id, tile_ids)
     @tile_ids = tile_ids
+    @user = user_id
 
-    mail  to:      TEST_EMAIL,
+    mail  to:      @user.email_with_name,
           from:    'donotreply@hengage.com',
           subject: 'Newly-added H.Engage Tiles'
   end
