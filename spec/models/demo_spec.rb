bc3bdf19a527fb5c321fe5a5a9342bc71f203018
larry <larry@hengagae.com>
2013-06-21 14:15:46 -0400

    Spec for ensuring that Demo finder for digest emails returns the correct demos

--- spec/models/demo_spec.rb
+++ spec/models/demo_spec.rb
@@ -193,3 +193,123 @@ describe Demo, "when internal email domains are changed" do
     [user1, user3].all?{|u| u.segmentation_data.reload.email_has_internal_domain == true}.should be_true
   end
 end
+
+# Don't actually need everything to have a variable name, but adds to readability and might come in handy for future tests
+describe Demo, '#send_digest_email' do
+  it 'returns only those demos which should send out a digest email on the current day' do
+
+    Timecop.travel 1.week.ago  # All demos will have their 'tile_digest_email_sent_at' date well in the past
+
+    # -------------------------------------
+
+    monday_demo_1_with_tiles = FactoryGirl.create :demo, tile_digest_email_send_on: 'Monday'
+    monday_demo_2_with_tiles = FactoryGirl.create :demo, tile_digest_email_send_on: 'Monday'
+    monday_demo_3_with_tiles = FactoryGirl.create :demo, tile_digest_email_send_on: 'Monday'
+
+    monday_demo_with_no_tiles_to_send = FactoryGirl.create :demo, tile_digest_email_send_on: 'Monday'
+
+    # -------------------------------------
+
+    tuesday_demo_with_no_tiles_at_all  = FactoryGirl.create :demo, tile_digest_email_send_on: 'Tuesday'
+    # Not even any demos for Wednesday
+    thursday_demo_with_no_tiles_at_all = FactoryGirl.create :demo, tile_digest_email_send_on: 'Thursday'
+
+    # -------------------------------------
+
+    friday_demo_1_with_tiles = FactoryGirl.create :demo, tile_digest_email_send_on: 'Friday'
+    friday_demo_2_with_tiles = FactoryGirl.create :demo, tile_digest_email_send_on: 'Friday'
+    friday_demo_3_with_tiles = FactoryGirl.create :demo, tile_digest_email_send_on: 'Friday'
+
+    friday_demo_with_no_tiles_to_send = FactoryGirl.create :demo, tile_digest_email_send_on: 'Friday'
+
+    # -------------------------------------
+
+    Timecop.return  # Get back to today so can add and then process tiles...
+
+    Timecop.travel Date.today.beginning_of_week(:sunday)  # All newly-created tiles will qualify for the digest email
+
+    # -------------------------------------
+
+    # Tiles that should go out (created after the 'last-sent-date' and not 'archived')
+    monday_demo_1_tile_1 = FactoryGirl.create :tile, demo: monday_demo_1_with_tiles, headline: 'monday_demo_1_tile_1'
+
+    monday_demo_2_tile_1 = FactoryGirl.create :tile, demo: monday_demo_2_with_tiles, headline: 'monday_demo_2_tile_1'
+    monday_demo_2_tile_2 = FactoryGirl.create :tile, demo: monday_demo_2_with_tiles, headline: 'monday_demo_2_tile_2'
+
+    monday_demo_3_tile_1 = FactoryGirl.create :tile, demo: monday_demo_3_with_tiles, headline: 'monday_demo_3_tile_1'
+    monday_demo_3_tile_2 = FactoryGirl.create :tile, demo: monday_demo_3_with_tiles, headline: 'monday_demo_3_tile_2'
+    monday_demo_3_tile_3 = FactoryGirl.create :tile, demo: monday_demo_3_with_tiles, headline: 'monday_demo_3_tile_3'
+
+    # -------------------------------------
+
+    friday_demo_1_tile_1 = FactoryGirl.create :tile, demo: friday_demo_1_with_tiles, headline: 'friday_demo_1_tile_1'
+
+    friday_demo_2_tile_1 = FactoryGirl.create :tile, demo: friday_demo_2_with_tiles, headline: 'friday_demo_2_tile_1'
+    friday_demo_2_tile_2 = FactoryGirl.create :tile, demo: friday_demo_2_with_tiles, headline: 'friday_demo_2_tile_2'
+
+    friday_demo_3_tile_1 = FactoryGirl.create :tile, demo: friday_demo_3_with_tiles, headline: 'friday_demo_3_tile_1'
+    friday_demo_3_tile_2 = FactoryGirl.create :tile, demo: friday_demo_3_with_tiles, headline: 'friday_demo_3_tile_2'
+    friday_demo_3_tile_3 = FactoryGirl.create :tile, demo: friday_demo_3_with_tiles, headline: 'friday_demo_3_tile_3'
+
+    # -------------------------------------
+
+    # Tiles that should not go out
+
+    # Tiles that were created before the 'last-sent-at' date ( => supposedly went out in that batch)
+    monday_no_tile_1 = FactoryGirl.create :tile, demo: monday_demo_with_no_tiles_to_send, headline: 'monday_no_tile_1', created_at: 2.weeks.ago
+    monday_no_tile_2 = FactoryGirl.create :tile, demo: monday_demo_with_no_tiles_to_send, headline: 'monday_no_tile_2', created_at: 2.weeks.ago
+
+    # Tiles that have been 'archived'
+    friday_no_tile_1 = FactoryGirl.create :tile, demo: friday_demo_with_no_tiles_to_send, headline: 'friday_no_tile_1', status: Tile::ARCHIVE
+    friday_no_tile_2 = FactoryGirl.create :tile, demo: friday_demo_with_no_tiles_to_send, headline: 'friday_no_tile_2', status: Tile::ARCHIVE
+
+    # -------------------------------------
+
+    Timecop.travel Date.today.beginning_of_week(:monday)
+    demos = Demo.send_digest_email
+
+    demos.should have(3).demos
+
+    [monday_demo_1_with_tiles, monday_demo_2_with_tiles, monday_demo_3_with_tiles].each_with_index do |demo, i|
+      demos.should include(demo)
+      eval("monday_demo_#{i + 1}_with_tiles").should have(i + 1).tiles
+    end
+
+    # -------------------------------------
+
+    Timecop.travel Date.today.beginning_of_week(:tuesday)
+    demos = Demo.send_digest_email
+
+    demos.should have(0).demos
+
+    # -------------------------------------
+
+    Timecop.travel Date.today.beginning_of_week(:wednesday)
+    demos = Demo.send_digest_email
+
+    demos.should have(0).demos
+
+    # -------------------------------------
+
+    Timecop.travel Date.today.beginning_of_week(:thursday)
+    demos = Demo.send_digest_email
+
+    demos.should have(0).demos
+
+    # -------------------------------------
+
+    Timecop.travel Date.today.beginning_of_week(:friday)
+    demos = Demo.send_digest_email
+
+    demos.should have(3).demos
+
+    [friday_demo_1_with_tiles, friday_demo_2_with_tiles, friday_demo_3_with_tiles].each_with_index do |demo, i|
+      demos.should include(demo)
+      eval("friday_demo_#{i + 1}_with_tiles").should have(i + 1).tiles
+    end
+
+    # -------------------------------------
+
+    Timecop.return
+  end
+end
