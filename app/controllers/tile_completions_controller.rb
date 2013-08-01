a353b97f95a4177fb751bece64be7dce9b6c1a5f
Phil Darnowsky <phil@darnowsky.com>
2013-08-01 18:58:39 -0400

    Hey let's actually complete a tile for a guy, what do you say.

--- app/controllers/tile_completions_controller.rb
+++ app/controllers/tile_completions_controller.rb
@@ -1,6 +1,7 @@
 class TileCompletionsController < ApplicationController
   def create
     tile = Tile.find(params[:tile_id])
+    TileCompletion.create!(:tile_id => tile.id, :user_id => current_user.id)    
     act = Act.create(user: current_user, demo_id: current_user.demo_id, inherent_points: tile.points, text: tile.text_of_completion_act)
     reply = ["That's right!", act.post_act_summary].join
     flash[:success] = reply
