14e2c273020badb844dfcae24aa6f26117c06475
Phil Darnowsky <phil@darnowsky.com>
2013-08-16 15:06:01 -0400

    fixed polymorphic tile migration

--- db/migrate/20130716154004_make_tiles_polymorphic.rb
+++ db/migrate/20130716154004_make_tiles_polymorphic.rb
@@ -1,9 +1,9 @@
 class MakeTilesPolymorphic < ActiveRecord::Migration
   def up
     add_column :tiles, :type, :string
-    execute "UPDATE tiles SET type='OldSchoolTile'      WHERE tiles.question IS NULL AND tiles.multiple_choice_answers IS NULL"
-    execute "UPDATE tiles SET type='KeywordTile'        WHERE tiles.question IS NOT NULL AND tiles.multiple_choice_answers IS NULL"
-    execute "UPDATE tiles SET type='MultipleChoiceTile' WHERE tiles.question IS NOT NULL AND tiles.multiple_choice_answers IS NOT NULL"
+    execute "UPDATE tiles SET type='OldSchoolTile'      WHERE tiles.question = ''  AND tiles.multiple_choice_answers IS NULL"
+    execute "UPDATE tiles SET type='KeywordTile'        WHERE tiles.question != '' AND tiles.multiple_choice_answers IS NULL"
+    execute "UPDATE tiles SET type='MultipleChoiceTile' WHERE tiles.question != '' AND tiles.multiple_choice_answers IS NOT NULL"
   end
 
   def down
