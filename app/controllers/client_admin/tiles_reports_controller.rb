5e903ba52854de8718573e25206bab395883f7c0
larry <larry@hengagae.com>
2013-08-30 10:59:13 -0400

    Basic plumbing in place for downloading csv file of tile stats

--- app/controllers/client_admin/tiles_reports_controller.rb
+++ app/controllers/client_admin/tiles_reports_controller.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class ClientAdmin::TilesReportsController < ClientAdminBaseController
 
   ReportRow = Struct.new :thumbnail, :headline, :num_completed, :percent_claimed, :percent_all
@@ -5,32 +7,43 @@ class ClientAdmin::TilesReportsController < ClientAdminBaseController
   def index
     demo = current_user.demo
 
-    num_all_users     = demo.users.count
-    num_claimed_users = demo.claimed_user_count
+    respond_to do |format|
+      format.html do
+        num_all_users     = demo.users.count
+        num_claimed_users = demo.claimed_user_count
 
-    # Returns a hash of { tile_id: num_completions } => set default value of 0 => "no users have completed this tile"
-    num_tile_completions = demo.num_tile_completions
-    num_tile_completions.default = 0
+        # Returns a hash of { tile_id: num_completions } => set default value of 0 => "no users have completed this tile"
+        num_tile_completions = demo.num_tile_completions
+        num_tile_completions.default = 0
 
-    @active_tiles  = []
-    @archive_tiles = []
+        @active_tiles  = []
+        @archive_tiles = []
 
-    demo.active_tiles.each do |tile|
-      completed_percent = num_tile_completions[tile.id] * 100.0
-      @active_tiles << ReportRow.new(tile.thumbnail,
-                                     tile.headline,
-                                     num_tile_completions[tile.id],
-                                     completed_percent / num_claimed_users,
-                                     completed_percent / num_all_users)
-    end
+        demo.active_tiles.each do |tile|
+          completed_percent = num_tile_completions[tile.id] * 100.0
+          @active_tiles << ReportRow.new(tile.thumbnail,
+                                         tile.headline,
+                                         num_tile_completions[tile.id],
+                                         completed_percent / num_claimed_users,
+                                         completed_percent / num_all_users)
+        end
 
-    demo.archive_tiles.each do |tile|
-      completed_percent = num_tile_completions[tile.id] * 100.0
-      @archive_tiles << ReportRow.new(tile.thumbnail,
-                                      tile.headline,
-                                      num_tile_completions[tile.id],
-                                      completed_percent / num_claimed_users,
-                                      completed_percent / num_all_users)
+        demo.archive_tiles.each do |tile|
+          completed_percent = num_tile_completions[tile.id] * 100.0
+          @archive_tiles << ReportRow.new(tile.thumbnail,
+                                          tile.headline,
+                                          num_tile_completions[tile.id],
+                                          completed_percent / num_claimed_users,
+                                          completed_percent / num_all_users)
+        end
+      end
+
+      format.csv do
+        @tiles = params[:report] == Tile::ACTIVE ? demo.active_tiles : demo.archive_tiles
+
+        response.headers['Content-Disposition'] = "attachment; filename=#{params[:report]}_tiles_report_#{Time.zone.now.to_s(:csv_file_date_stamp)}.csv"
+        render content_type: "text/csv"
+      end
     end
   end
 end
--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -4,6 +4,7 @@
   chart_start_end_day:          "%m/%d/%Y",         # 07/21/2013
   tile_digest_email_sent_at:    "%A, %B %d, %Y",    # Thursday, July 04, 2013
   tile_digest_email_shelf_life: "%B %d, %Y",        # July 04, 2013
+  csv_file_date_stamp:          "%m_%d_%y",         # 07_04_13
 }.each do |format_name, format_string|
    Time::DATE_FORMATS[format_name] = format_string
 end
