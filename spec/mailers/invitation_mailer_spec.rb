218099f914c4c2540413c1f2a6f6b2ad89eafc47
larry <larry@hengagae.com>
2013-09-10 18:03:12 -0400

    Refactored invitation email spec

--- spec/mailers/invitation_mailer_spec.rb
+++ spec/mailers/invitation_mailer_spec.rb
@@ -1,8 +1,8 @@
 require "spec_helper"
 
 describe Mailer do
-  describe '#invitation' do
-    subject { Mailer.invitation(FactoryGirl.create :user) }
-    it { should have_body_text 'Please do not forward it to others.' }
+  it '#invitation contains a do-not-forward warning message' do
+    email = Mailer.invitation(FactoryGirl.create :user)
+    email.should have_body_text 'Please do not forward it to others.'
   end
 end
