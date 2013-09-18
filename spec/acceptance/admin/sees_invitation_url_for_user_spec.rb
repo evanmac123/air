af13ed734972bef6568f9ac05d22278a2bebd334
Phil Darnowsky <phil@darnowsky.com>
2013-09-18 11:01:38 -0400

    site admin sees invitation URL for users

---
+++ spec/acceptance/admin/sees_invitation_url_for_user_spec.rb
@@ -0,0 +1,9 @@
+require 'acceptance/acceptance_helper'
+
+feature 'Admin sees invitation url for user' do
+  scenario 'in client admin user page' do
+    user = FactoryGirl.create(:user)
+    visit admin_demo_users_path(user.demo, as: an_admin)
+    expect_content invitation_url(user.invitation_code)
+  end
+end
