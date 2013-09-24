049a58a05fa18365079569ae5738daa0fda56af2
Phil Darnowsky <phil@darnowsky.com>
2013-09-24 18:39:06 -0400

    lightbox displayed set on migration

--- db/migrate/20130924172201_add_get_started_lightbox_displayed_to_user.rb
+++ db/migrate/20130924172201_add_get_started_lightbox_displayed_to_user.rb
@@ -1,5 +1,10 @@
 class AddGetStartedLightboxDisplayedToUser < ActiveRecord::Migration
-  def change
+  def up
     add_column :users, :get_started_lightbox_displayed, :boolean
+    execute "UPDATE users SET get_started_lightbox_displayed=sample_tile_completed"
+  end
+
+  def down
+    remove_column :users, :get_started_lightbox_displayed
   end
 end
