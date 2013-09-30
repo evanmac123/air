04434629bc3da98a2493e8d5db0027ccd8732568
Connie Chan <hello.cchan@gmail.com>
2013-09-30 18:37:54 -0400

    updated list of no SSL pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -71,7 +71,7 @@ class PagesController < HighVoltage::PagesController
   end
 
   def force_no_ssl_on_marketing
-    return unless page_name == 'marketing' || page_name == 'initial_landing'
+    return unless page_name == 'welcome' || page_name == 'more_info' || page_name == 'pricing'
     force_no_ssl
   end
 end
