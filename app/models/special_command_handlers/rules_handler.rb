69405f779c0239fd9be7f856c585a5df33dc3502
larry <larry@hengagae.com>
2013-10-31 15:57:37 -0400

    Another "friend" to "connection" change

--- app/models/special_command_handlers/rules_handler.rb
+++ app/models/special_command_handlers/rules_handler.rb
@@ -6,6 +6,6 @@ class SpecialCommandHandlers::RulesHandler < SpecialCommandHandlers::Base
     # leave it out of email entirely, so we use channel_specific_translations
     # to stick it back into SMS and web replies.
 
-    parsing_success_message("FRIEND [someone's ID] - become friends (ex: \"friend bob12\")\nMYID - see your ID\nRANKING - see scoreboard\n@{help_command_explanation}PRIZES - see what you can win")
+    parsing_success_message("CONNECT [someone's ID] - become connected (ex: \"connection bob12\")\nMYID - see your ID\nRANKING - see connections\n@{help_command_explanation}PRIZES - see what you can win")
   end
 end
