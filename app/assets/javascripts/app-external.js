7753b0006805b5fe664bbfb10f234b6155a4b6a7
Connie Chan <hello.cchan@gmail.com>
2013-11-13 12:20:50 -0500

    removed blog feed from footer

--- app/assets/javascripts/app-external.js
+++ app/assets/javascripts/app-external.js
@@ -3,6 +3,5 @@
 //= require ./internal_and_external/underscore-min
 //= require_tree ../../../vendor/assets/javascripts/external/
 //= require ./external/prefilled_input.js
-//= require ./external/blog_rss.js
 //= require ./internal_and_external/autocomplete.js
 //= require ./external/textscroll.js
