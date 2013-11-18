6b6d4b0540928e5a0946aee76e68d59fc12e6933
Connie Chan <hello.cchan@gmail.com>
2013-11-18 18:53:46 -0500

    public board landing

--- app/assets/javascripts/app-landing.js
+++ app/assets/javascripts/app-landing.js
@@ -2,3 +2,5 @@
 //= require mobvious-rails
 //= require_tree ../../../vendor/assets/javascripts/external/
 //= require ./external/prefilled_input.js
+//= require ../../../vendor/assets/javascripts/internal/jquery.jpanelmenu.min
+//= require ../../../vendor/assets/javascripts/internal/jRespond.min
