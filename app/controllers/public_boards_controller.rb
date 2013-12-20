8631ea3335fb90d09bb4aca176706f464f7ddc8b
Phil Darnowsky <phil@darnowsky.com>
2013-12-19 19:31:44 -0500

    Session cookie turned from session-expiration to 10 years

--- app/controllers/public_boards_controller.rb
+++ app/controllers/public_boards_controller.rb
@@ -1,9 +1,10 @@
 class PublicBoardsController < ApplicationController
   skip_before_filter :authorize
+  before_filter :allow_guest_user
 
   def show
     demo = Demo.find_by_public_slug(params[:public_slug])
-    login_as_guest(demo)
+    login_as_guest(demo) unless current_user
     redirect_to activity_path
   end
 
