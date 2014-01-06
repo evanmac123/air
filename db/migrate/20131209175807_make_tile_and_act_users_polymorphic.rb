80b306868a44bb7bafbb708e538ed10b3a634080
Phil Darnowsky <phil@darnowsky.com>
2014-01-06 18:36:37 -0500

    Removed backfill from migration, it shouldn't lock tables but GUESS WHAT it does

--- db/migrate/20131209175807_make_tile_and_act_users_polymorphic.rb
+++ db/migrate/20131209175807_make_tile_and_act_users_polymorphic.rb
@@ -3,8 +3,8 @@ class MakeTileAndActUsersPolymorphic < ActiveRecord::Migration
     # Make these associations polymorphic
     add_column :tile_completions, :user_type, :string
     add_column :acts, :user_type, :string
-    execute "UPDATE tile_completions SET user_type='User'"
-    execute "UPDATE acts SET user_type='User'"
+    #execute "UPDATE tile_completions SET user_type='User'"
+    #execute "UPDATE acts SET user_type='User'"
 
     add_index :tile_completions, :user_type
     add_index :acts, :user_type
