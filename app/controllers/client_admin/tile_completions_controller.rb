925a1ca2339c17f9d199c2f7ef6fe98320307047
Hisham Malik <hisham@arkhitech.com>
2014-02-14 01:18:04 +0500

    used custom_order attribute of wice_grid to handle sort conflict

--- app/controllers/client_admin/tile_completions_controller.rb
+++ app/controllers/client_admin/tile_completions_controller.rb
@@ -1,26 +1,31 @@
 class ClientAdmin::TileCompletionsController < ClientAdminBaseController
   #Tiles completion report
+  ORDER_BY_USER_NAME  = 'users.name'.freeze
   def index    
     @tile = Tile.find(params[:tile_id])
     params[:grid] = {page: params[:page]}
     if params[:nc_grid]
       return non_completions
     else
-      if params[:tc_grid] && params[:tc_grid][:order] == 'users.name'
-        params[:tc_grid][:order] = nil#eat up order param as we're doing it custom
+      if params[:tc_grid] && params[:tc_grid][:order] == ORDER_BY_USER_NAME
+        #params[:tc_grid][:order] = nil#eat up order param as we're doing it custom
         
         tile_completions = TileCompletion.joins("LEFT JOIN #{User.table_name} on #{TileCompletion.table_name}.user_id = #{User.
         table_name}.id AND user_type = '#{User.name}'").
         joins("LEFT JOIN #{GuestUser.table_name} on user_id = #{GuestUser.
         table_name}.id AND user_type = '#{GuestUser.name}'").
-        where("#{TileCompletion.table_name}.tile_id = ?", @tile.id).
-          order("CASE WHEN #{User.
-          table_name}.id IS NULL THEN 'Guest User[' || #{GuestUser.
-          table_name}.id ||']' ELSE #{User.table_name}.name end #{params[:tc_grid][:order_direction]}")
+        where("#{TileCompletion.table_name}.tile_id = ?", @tile.id)
+      
+        @tile_completions_grid = initialize_grid(tile_completions, {name: 'tc_grid',
+          custom_order: {ORDER_BY_USER_NAME => "CASE WHEN #{User.
+            table_name}.id IS NULL THEN 'Guest User[' || #{GuestUser.
+            table_name}.id ||']' ELSE #{User.table_name}.name end"
+          }}
+        )
       else
         tile_completions = TileCompletion.where(tile_id: @tile.id)
+        @tile_completions_grid = initialize_grid(tile_completions, name: 'tc_grid')
       end
-      @tile_completions_grid = initialize_grid(tile_completions, name: 'tc_grid')
     end
   end
   
