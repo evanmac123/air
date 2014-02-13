2b84c523651ef200a84830ec7c4ff281a59c8972
Hisham Malik <hisham@arkhitech.com>
2014-02-13 07:02:32 +0500

    adjusted query for supporting sort on polymorphic association, fixed up test cases, and filtering, adjusted links and css

--- app/controllers/client_admin/tile_completions_controller.rb
+++ app/controllers/client_admin/tile_completions_controller.rb
@@ -3,14 +3,33 @@ class ClientAdmin::TileCompletionsController < ClientAdminBaseController
   def index    
     @tile = Tile.find(params[:tile_id])
     params[:grid] = {page: params[:page]}
-    @tile_completions = TileCompletion.where(tile_id: @tile.id).page(params[:page]).per(1)
-    @tile_completions_grid = initialize_grid(TileCompletion.where(tile_id: @tile.id))
+    if params[:nc_grid]
+      return non_completions
+    else
+      if params[:tc_grid] && params[:tc_grid][:order] == 'users.name'
+        #params[:tc_grid][:order] = nil#tile_completions.sort_by(&:users.name)
+        
+        tile_completions = TileCompletion.joins("LEFT JOIN #{User.table_name} on user_id = #{User.
+        table_name}.id AND user_type = '#{User.name}'").
+        joins("LEFT JOIN #{GuestUser.table_name} on user_id = #{GuestUser.
+        table_name}.id AND user_type = '#{GuestUser.name}' AND #{TileCompletion.table_name}.tile_id = #{@tile.id}").
+          order("CASE WHEN #{User.
+          table_name}.id IS NULL THEN 'Guest User[' || #{GuestUser.
+          table_name}.id ||']' ELSE #{User.table_name}.name end #{params[:tc_grid][:order_direction]}")
+      else
+        tile_completions = TileCompletion.where(tile_id: @tile.id)
+      end
+      @tile_completions_grid = initialize_grid(tile_completions, name: 'tc_grid')
+    end
   end
   
   def non_completions
     @tile = Tile.find(params[:tile_id])
-    @non_completions_grid = initialize_grid(@tile.demo.users.
-        joins("LEFT JOIN #{TileCompletion.table_name} on user_id = #{User.
-        table_name}.id").where("#{TileCompletion.table_name}.id is null"))
+    @non_completions_grid = initialize_grid(@tile.demo.users.joins("LEFT JOIN #{TileCompletion.
+        table_name} on #{TileCompletion.table_name}.user_id = #{User.
+        table_name}.id AND #{TileCompletion.table_name}.user_type = '#{User.
+        name}' AND #{TileCompletion.table_name}.tile_id = #{@tile.id}").
+        where("#{TileCompletion.table_name}.id is null"), name: 'nc_grid')
+    render :non_completions
   end
 end
--- spec/acceptance/client_admin/sees_tile_completion_details.rb
+++ spec/acceptance/client_admin/sees_tile_completion_details.rb
@@ -8,7 +8,6 @@ feature "sees tile completion details" do
   let(:user_first) {FactoryGirl.create(:user, demo: client_admin.demo, name: '000')}
   let(:user_last) {FactoryGirl.create(:user, demo: client_admin.demo, name: 'ZZZ')}
   let(:user_latest) {FactoryGirl.create(:user, demo: client_admin.demo)}
-  let(:tile_completion_latest) {FactoryGirl.create(:tile_completion, user: user_latest, tile: tile)}
 
   context "tiles detail page" do
     before do 
@@ -20,9 +19,33 @@ feature "sees tile completion details" do
       page.should have_link '1 user', href: client_admin_tile_tile_completions_path(tile)
     end
   end
-  context "tile reports page" do
+  context 'no one has completed the tile', js:true do
     before do
-      100.times {FactoryGirl.create(:user, :with_tile_completed, demo: client_admin.demo)}
+      visit client_admin_tile_tile_completions_path(tile, as: client_admin)
+    end    
+    scenario "not completed report should show no record found" do
+      click_link 'show_completed'
+      page.should have_css('.grid_no_record')
+      page.find('.grid_no_record').should have_content 'There are no users that have completed this tile yet.'
+    end
+  end
+  context "everyone has completed the tile", js: true do
+    before do
+      FactoryGirl.create(:tile_completion, user: client_admin, tile: tile)
+      visit client_admin_tile_tile_completions_path(tile, as: client_admin)
+    end    
+    scenario "completed report should show no record found" do
+      click_link 'show_not_completed'
+      page.should have_css('.grid_no_record')
+      page.find('.grid_no_record').should have_content 'Everyone has completed this tile!'
+    end
+  end
+  context "tile reports page with data", js:true do
+    before do
+      2.times do
+        u = FactoryGirl.create(:user, demo: client_admin.demo)
+        FactoryGirl.create(:tile_completion, user: u, tile: tile)
+      end
       FactoryGirl.create(:tile_completion, user: user_first, tile: tile)
       FactoryGirl.create(:tile_completion, user: user_last, tile: tile)
       visit client_admin_tile_tile_completions_path(tile, as: client_admin)
@@ -41,10 +64,10 @@ feature "sees tile completion details" do
       # By default the "yes" option is selected.
     scenario "I see the column titles: Name, Email, Date, Joined" do
       # I see the column titles: Name, Email, Date, Joined (date = date completed, joined = yes or no)
-      page.should have_content "Name"
-      page.should have_content "Email"
-      page.should have_content "Date"
-      page.should have_content "Joined"
+      page.should have_content "NAME"
+      page.should have_content "EMAIL"
+      page.should have_content "DATE"
+      page.should have_content "JOINED"
     end
     scenario "sees pagination at bottom" do
       # bottom has pagination 
@@ -52,22 +75,26 @@ feature "sees tile completion details" do
     end
     scenario "on click name, list should show name descending" do
       click_link 'Name'
-      page.find('tbody tr:last td:first').should have_content('000')
-      page.find('tbody tr:first td:first').should have_content('ZZZ')
+      page.find('tbody tr:first td:first').should have_content('000')
+      page.find('tbody tr:last td:first').should have_content('ZZZ')
     end
     scenario "should be sortable by date" do
+      sleep 1
+      tile_completion_latest = FactoryGirl.create(:tile_completion, user: user_latest, tile: tile)
+      click_link 'Date'
       click_link 'Date'
       page.find('tbody tr:first').should have_content(tile_completion_latest.created_at.to_s)
     end
-    context "clicking no should show users who have not completed the tiles" do
+    context "clicking no should show users who have not completed the tiles", js: true do
       before do 
         FactoryGirl.create :user, demo: client_admin.demo
         visit client_admin_tile_tile_completions_path(tile, as: client_admin)
       end
-      scenario "I see the column Name and Email" do
+      scenario "I see the column Name, Email and joined" do
         click_link 'show_not_completed'
-        page.should have_content "Name"
-        page.should have_content "Email"
+        page.should have_content "NAME"
+        page.should have_content "EMAIL"
+        page.should have_content "JOINED"
       end
     end
   end
