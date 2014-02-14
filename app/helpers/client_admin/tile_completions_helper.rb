e6e6ed90c12439c2fc0873cf5c0360d9bf8122b6
Hisham Malik <hisham@arkhitech.com>
2014-02-15 01:57:50 +0500

    removed time from tooltip

--- app/helpers/client_admin/tile_completions_helper.rb
+++ app/helpers/client_admin/tile_completions_helper.rb
@@ -1,6 +1,6 @@
 module ClientAdmin::TileCompletionsHelper
   def display_time_ago_and_date(dt)
     content_tag(:span, "#{time_ago_in_words(dt)} ago", class: 'has-tip', 
-        title: dt.strftime(Wice::Defaults::DATETIME_FORMAT), data: {tooltip: ''})
+        title: dt.strftime(Wice::Defaults::DATE_FORMAT), data: {tooltip: ''})
   end
 end
