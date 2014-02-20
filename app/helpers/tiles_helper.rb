fb0860fcb5f1a2af72b24c7434d334af392d8da5
Hisham Malik <hisham@arkhitech.com>
2014-02-21 04:07:34 +0500

    show completed tiles as completed in the current tile viewer

---
+++ app/helpers/tiles_helper.rb
@@ -0,0 +1,5 @@
+module TilesHelper
+  def tile_class(tile)
+    (@show_completed_tiles == true) || current_user.tile_completions.where(tile_id: tile.id).exists? ? 'completed' : 'not_completed'
+  end
+end
