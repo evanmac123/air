8069739d741895f55c121310e234777e67429357
avbrychak <a.v.brychak@gmail.com>
2014-03-10 13:00:35 -0400

    pagination on client_admin/draft_tiles page

--- app/controllers/client_admin/draft_tiles_controller.rb
+++ app/controllers/client_admin/draft_tiles_controller.rb
@@ -1,5 +1,21 @@
 class ClientAdmin::DraftTilesController < ClientAdminBaseController
+  PER_PAGE = 12.freeze
   def index
-    @draft_tiles = current_user.demo.draft_tiles_with_placeholders
+    @raw_tiles = get_page(params[:page])
+    @draft_tiles = add_creation_and_other_placeholders(params[:page], @raw_tiles)
+  end
+  def get_page page
+    if page.nil? || page == 1 || page == ""
+      current_user.demo.draft_tiles.page(1).per(PER_PAGE).limit(11)
+    else
+      current_user.demo.draft_tiles.page(page).per(PER_PAGE).padding(-1)
+    end
+  end
+  def add_creation_and_other_placeholders page, tiles
+    if page.nil? || page == 1 || page == ""
+      current_user.demo.add_placeholders([TileCreationPlaceholder.new] + tiles)
+    else
+      current_user.demo.add_placeholders(tiles)
+    end
   end
 end
--- app/controllers/client_admin/inactive_tiles_controller.rb
+++ app/controllers/client_admin/inactive_tiles_controller.rb
@@ -1,6 +1,7 @@
 class ClientAdmin::InactiveTilesController < ClientAdminBaseController
+  PER_PAGE = 12.freeze
   def index
-    @raw_tiles = current_user.demo.archive_tiles.page(params[:page]).per(12)
+    @raw_tiles = current_user.demo.archive_tiles.page(params[:page]).per(PER_PAGE)
     @archive_tiles = current_user.demo.add_placeholders @raw_tiles
   end
 end
