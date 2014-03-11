611a6b4d55248cf768844b8e392f8c1566d9c7ed
avbrychak <a.v.brychak@gmail.com>
2014-03-11 12:26:58 -0400

    tests on pagination

--- spec/acceptance/client_admin/sees_all_draft_tiles_on_a_separate_tiles_spec.rb
+++ spec/acceptance/client_admin/sees_all_draft_tiles_on_a_separate_tiles_spec.rb
@@ -22,9 +22,33 @@ feature 'Sees all draft tiles on a separate page' do
     should_be_on client_admin_draft_tiles_path
   end
 
-   it "has a placeholder that you can click on to create a new tile" do
-     visit client_admin_draft_tiles_path(as: client_admin)
-     click_new_tile_placeholder
-     should_be_on new_client_admin_tile_path
-   end
+  it "has a placeholder that you can click on to create a new tile" do
+    visit client_admin_draft_tiles_path(as: client_admin)
+    click_new_tile_placeholder
+    should_be_on new_client_admin_tile_path
+  end
+
+  context "should show 12 tiles per page" do
+    it "should show first 11 tiles on first page(one placeholder for create)" do
+      tiles = []
+      11.times { |i| tiles << FactoryGirl.create(:tile, :draft, demo: demo, headline: "Tile #{i}") }
+      visit client_admin_draft_tiles_path(as: client_admin)
+
+      tiles.each {|tile| expect_content tile.headline }
+
+      tile_12 = FactoryGirl.create(:tile, :draft, demo: demo, headline: "Tile 12")
+      page_text.should_not include(tile_12.headline)
+    end
+
+    it "should show second 12 tiles on second page" do
+      tiles_2 = []
+      (1..12).each { |i| tiles_2 << FactoryGirl.create(:tile, :draft, demo: demo, headline: "Tile #{i}") }
+      tiles_1 = []
+      (13..23).each{ |i| tiles_1 << FactoryGirl.create(:tile, :draft, demo: demo, headline: "Tile #{i}") }
+      visit client_admin_draft_tiles_path(as: client_admin, page: 2)
+
+      tiles_1.each {|tile| page_text.should_not include(tile.headline) }
+      tiles_2.each {|tile| expect_content tile.headline }
+    end
+  end
 end
--- spec/acceptance/client_admin/sees_all_inactive_tiles_on_a_separate_page_spec.rb
+++ spec/acceptance/client_admin/sees_all_inactive_tiles_on_a_separate_page_spec.rb
@@ -8,7 +8,7 @@ feature 'Sees all inactive tiles on a separate page' do
     page.find(".all_inactive").click
   end
 
-  it "should show all inactive tiles in the demo" do
+  it "should show inactive tiles in the demo" do
     tiles = []
     5.times { |i| tiles << FactoryGirl.create(:tile, :archived, demo: demo, headline: "Tile #{i}") }
     visit client_admin_inactive_tiles_path(as: client_admin)
@@ -20,5 +20,29 @@ feature 'Sees all inactive tiles on a separate page' do
     visit client_admin_tiles_path(as: client_admin)
     click_see_all_inactive_tiles_link
     should_be_on client_admin_inactive_tiles_path
+  end
+
+  context "should show 12 tiles per page" do
+    it "should show first 12 tiles on first page" do
+      tiles = []
+      12.times { |i| tiles << FactoryGirl.create(:tile, :archived, demo: demo, headline: "Tile #{i}") }
+      visit client_admin_inactive_tiles_path(as: client_admin)
+
+      tiles.each {|tile| expect_content tile.headline }
+
+      tile_13 = FactoryGirl.create(:tile, :archived, demo: demo, headline: "Tile 13")
+      page_text.should_not include(tile_13.headline)
+    end
+
+    it "should show second 12 tiles on second page" do
+      tiles_2 = []
+      (1..12).each { |i| tiles_2 << FactoryGirl.create(:tile, :archived, demo: demo, headline: "Tile #{i}") }
+      tiles_1 = []
+      (13..24).each{ |i| tiles_1 << FactoryGirl.create(:tile, :archived, demo: demo, headline: "Tile #{i}") }
+      visit client_admin_inactive_tiles_path(as: client_admin, page: 2)
+
+      tiles_1.each {|tile| page_text.should_not include(tile.headline) }
+      tiles_2.each {|tile| expect_content tile.headline }
+    end
   end
 end
