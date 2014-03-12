2d8b9ff07e68218c3a837a7fd45058c69740bb89
Hisham Malik <hisham@arkhitech.com>
2014-03-12 18:26:52 -0400

    adjusted UI, and moved to using bottom tipLocation

--- app/assets/javascripts/client_admin/tiles.js
+++ app/assets/javascripts/client_admin/tiles.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
   $('#tile-manager-tabs, #tile-reports-tabs').tabs();
 
-  $(document).foundation('joyride', 'start');
+  $('#tile-joyride').foundation('joyride', 'start', {
+      nextButton: false,
+      tipLocation: 'left',
+      template : { // HTML segments for tip layout
+        link    : '',
+        tip     : '<div class="joyride-tip-guide tile"><span class="joyride-nub"></span></div>'
+      }
+    });
 });
