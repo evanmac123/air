e6fa85978799d71e05b5711b1e8b3fd96aa381ba
avbrychak <a.v.brychak@gmail.com>
2014-04-09 17:28:51 -0400

    url check

--- app/helpers/tiles_helper.rb
+++ app/helpers/tiles_helper.rb
@@ -2,4 +2,17 @@ module TilesHelper
   def tile_class(tile)
     (@show_completed_tiles == true) || current_user.tile_completions.where(tile_id: tile.id).exists? ? 'completed' : 'not_completed'
   end
+
+  def is_url? str
+    (!str.nil? && \
+      str.include?(".") && \
+      !str.include?(" ") && \
+      str[/[a-zA-Z]+/] && \
+      !str[/\.{2,}/] && \
+      str[/^[\w].*[^.]$/]) ? true : false
+  end
+
+  def make_url str
+    (str.start_with?("http://", "https://") ? "" : "http://") + str
+  end
 end
