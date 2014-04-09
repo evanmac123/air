fb1053401a06dd6cf43a5eaa0db725540d6b3ff7
avbrychak <a.v.brychak@gmail.com>
2014-04-09 17:52:17 -0400

    url comments

--- app/helpers/tiles_helper.rb
+++ app/helpers/tiles_helper.rb
@@ -3,6 +3,12 @@ module TilesHelper
     (@show_completed_tiles == true) || current_user.tile_completions.where(tile_id: tile.id).exists? ? 'completed' : 'not_completed'
   end
 
+  # nil is definitely not a url
+  # with dots
+  # without spaces
+  # have at least one letter
+  # no two dots together
+  #starts with letter, ends with not dot
   def is_url? str
     (!str.nil? && \
       str.include?(".") && \
@@ -12,7 +18,7 @@ module TilesHelper
       str[/^[\w].*[^.]$/]) ? true : false
   end
 
-  def make_url str
+  def make_full_url str
     (str.start_with?("http://", "https://") ? "" : "http://") + str
   end
 end
