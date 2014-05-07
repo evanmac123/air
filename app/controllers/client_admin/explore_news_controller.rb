fa5ccda61817f9f69414c05e4f82a8e733eaa1c0
Phil Darnowsky <phil@darnowsky.com>
2014-05-07 11:42:04 -0400

    fix for last fix

--- app/controllers/client_admin/explore_news_controller.rb
+++ app/controllers/client_admin/explore_news_controller.rb
@@ -1,5 +1,12 @@
 class ClientAdmin::ExploreNewsController < ClientAdminBaseController  
+  before_filter :find_current_user_demo
+
   def show
+  end
+
+  protected
+
+  def find_current_user_demo
     @demo = current_user.demo
   end
 end
