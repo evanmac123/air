6f0cd14776f1d5750ecfc6f3c920bcc33dd7fc32
Phil Darnowsky <phil@darnowsky.com>
2014-05-08 14:23:30 -0400

    NO NO NO do not EVER send emails SYNCHRONOUSLY DO NOT DO THIS EVER

--- app/models/user_tile_copy.rb
+++ app/models/user_tile_copy.rb
@@ -3,6 +3,6 @@ class UserTileCopy < ActiveRecord::Base
   belongs_to :tile
   
   after_create do
-    Mailer.notify_creator_for_social_interaction(tile, user, 'copied').deliver
+    Mailer.delay_mail(:notify_creator_for_social_interaction, tile, user, 'copied')
   end
 end
--- app/models/user_tile_like.rb
+++ app/models/user_tile_like.rb
@@ -4,6 +4,6 @@ class UserTileLike < ActiveRecord::Base
   validates_uniqueness_of :tile_id, :scope => [:user_id]
   
   after_create do
-    Mailer.delay(run_at: 1.second.from_now).notify_creator_for_social_interaction(tile, user, 'liked')
+    Mailer.delay_mail(:notify_creator_for_social_interaction, tile, user, 'liked')
   end
 end
