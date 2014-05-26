cc0da98fb67ed084eef53ef90ca01fdc2f869995
avbrychak <a.v.brychak@gmail.com>
2014-05-25 23:02:32 -0400

    piggy section

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -7,4 +7,26 @@ module PagesHelper
       ["Sustainability", "Consumerism", "Philanthropy", "Compliance"]
     ]
   end
+
+  def tile_features
+    [
+      {
+        title: "Visually interesting", 
+        desc: "An image to capture attention"
+      },
+      {
+        title: "Easy to understand",
+        desc: "375 characters of content."
+      },
+      {
+        title: "Links to increase participation",
+        desc: "Like vendor sites intranets."
+      },
+      {
+        title: "Interactive",
+        desc: "Reader earns points for engaging. " + "\n" +
+              "You can add prices and leaderboards."
+      }
+    ]
+  end
 end
