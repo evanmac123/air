b917bad6a13f20d4c26dd380c0357f289a475924
avbrychak <a.v.brychak@gmail.com>
2014-05-25 21:37:35 -0400

    any communication block

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -3,30 +3,8 @@ module PagesHelper
     [
       ["Wellness", "Open Enrollment", "Onboarding", "Learning & Development"],
       ["Health Care Reform", "Retirement", "Safety", "Leadership Updates"],
-      ["Employee Research", "Events", "Social Recruiting", "Financial Education"],
+      ["Employee Research", "Events", "Social Recruting", "Financial Education"],
       ["Sustainability", "Consumerism", "Philanthropy", "Compliance"]
-    ]
-  end
-
-  def tile_features
-    [
-      {
-        title: "Visually interesting", 
-        desc: "An image to capture attention."
-      },
-      {
-        title: "Easy to understand",
-        desc: "375 characters of content."
-      },
-      {
-        title: "Links to increase participation",
-        desc: "Like vendor sites and intranets."
-      },
-      {
-        title: "Interactive",
-        desc: "Employees earn points for engaging. " + "\n" +
-              "You can add prizes and leaderboards."
-      }
     ]
   end
 end
