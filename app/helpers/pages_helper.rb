534a001a724dee70ff4e6c45f743868218fecae2
avbrychak <a.v.brychak@gmail.com>
2014-05-25 21:37:35 -0400

    any communication block

---
+++ app/helpers/pages_helper.rb
@@ -0,0 +1,10 @@
+module PagesHelper
+  def advantages
+    [
+      ["Wellness", "Open Enrollment", "Onboarding", "Learning & Development"],
+      ["Health Care Reform", "Retirement", "Safety", "Leadership Updates"],
+      ["Employee Research", "Events", "Social Recruting", "Financial Education"],
+      ["Sustainability", "Consumerism", "Philanthropy", "Compliance"]
+    ]
+  end
+end
