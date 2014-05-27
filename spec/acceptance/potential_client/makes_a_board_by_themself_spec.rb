cec2240b959bf0bc6fe9836c1f334117fad5cdfc
Phil Darnowsky <phil@darnowsky.com>
2014-05-27 19:52:08 -0400

    few tests fixed

--- spec/acceptance/potential_client/makes_a_board_by_themself_spec.rb
+++ spec/acceptance/potential_client/makes_a_board_by_themself_spec.rb
@@ -102,13 +102,13 @@ feature 'Makes a board by themself' do
     it "should send an appropriate ping for the board creation" do
       FakeMixpanelTracker.clear_tracked_events
       crank_dj_clear
-      FakeMixpanelTracker.should have_event_matching("Board - New", source: 'marketing page')
+      FakeMixpanelTracker.should have_event_matching("Board - New", source: 'Boards - New')
     end
 
     it "should send an appropriate ping for the creator creation" do
       FakeMixpanelTracker.clear_tracked_events
       crank_dj_clear
-      FakeMixpanelTracker.should have_event_matching("Creator - New", source: 'marketing page')
+      FakeMixpanelTracker.should have_event_matching("Creator - New", source: 'Boards - New')
     end
         
   end
