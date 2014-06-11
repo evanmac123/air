60fb0ba82d17b5bd4460bfefd89f0b843660a080
avbrychak <a.v.brychak@gmail.com>
2014-06-11 11:38:42 -0400

    fix follow up email in gmail for android

--- app/mailers/tiles_digest_mailer.rb
+++ app/mailers/tiles_digest_mailer.rb
@@ -48,7 +48,7 @@ class TilesDigestMailer < ActionMailer::Base
       @title = "Join my #{@demo.name}"      
       @email_heading = "Join my #{@demo.name}"
     else
-      @title = @email_heading = @follow_up_email ? 'Here the tiles you missed' : 'Your new tiles are here'
+      @title = @email_heading = @follow_up_email ? 'Here are the tiles you missed' : 'Your new tiles are here'
     end
     
     @invitation_url = @user.claimed? ? nil : invitation_url(@user.invitation_code, protocol: email_link_protocol, host: email_link_host)
