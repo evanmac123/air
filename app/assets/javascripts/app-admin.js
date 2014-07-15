a297365b8ab45c60b7fc3e20ac1b088cfd903b08
Phil Darnowsky <phil@darnowsky.com>
2014-07-15 13:01:40 -0400

    Big chunk of JS moved from view into proper JS asset file

--- app/assets/javascripts/app-admin.js
+++ app/assets/javascripts/app-admin.js
@@ -16,3 +16,4 @@
 //= require internal_and_external/nerf_links_with_login_modal
 //= require internal_and_external/topbar_togglers
 //= require internal/validate_new_board
+//= require internal/board_settings_controls
