808d0c701c832a3962079b4952389d30cc95d76c
avbrychak <a.v.brychak@gmail.com>
2014-08-18 11:39:13 -0400

    add tests on explore carousel

---
+++ spec/acceptance/client_admin/explore_carousel_spec.rb
@@ -0,0 +1,88 @@
+require 'acceptance/acceptance_helper'
+
+feature 'Carousel on Explore Tile Preview Page' do
+  before(:each) do
+    %w(Spam Fish).each do |title|
+      FactoryGirl.create(:tile_tag, title: title)
+    end
+
+    @tags = TileTag.all
+
+    0.upto(5) do |i|
+      tag = i.even? ? @tags[0] : @tags[1]
+      FactoryGirl.create(:multiple_choice_tile, :public, headline: "Tile#{i}", created_at: Time.now + i.day, tile_tags: [tag])
+    end
+
+    @tiles = Tile.order(:created_at)
+  end
+
+  context "if user comes from explore main page" do
+    before(:each) do
+      visit explore_path(as: a_client_admin)
+      expect_content "Spam"
+      click_link @tiles.first.headline
+    end
+
+    it "should show all tiles upwards", js: true do
+      expect_content @tiles.first.headline
+      1.upto(5) do |i|
+        show_next_tile
+        expect_content @tiles[i].headline
+      end
+    end
+
+    it "should show all tiles backwards", js: true do
+      expect_content @tiles.first.headline
+
+      5.downto(1) do |i|
+        show_previous_tile
+        expect_content @tiles[i].headline
+      end
+    end
+
+    it "should send ping on #next button click", js: true do
+      show_next_tile
+
+      FakeMixpanelTracker.clear_tracked_events
+      crank_dj_clear
+      FakeMixpanelTracker.should have_event_matching('Explore page - Interaction', action: "Clicked arrow to next tile")
+    end
+
+    it "should send ping on #next button click", js: true do
+      show_previous_tile
+
+      FakeMixpanelTracker.clear_tracked_events
+      crank_dj_clear
+      FakeMixpanelTracker.should have_event_matching('Explore page - Interaction', action: "Clicked arrow to previous tile")
+    end
+  end
+
+  context "if user comes from explore topic page" do
+    before(:each) do
+      visit explore_path(as: a_client_admin)
+      within page.find(".tags") do
+        click_link @tags[0].title
+      end
+    end
+
+    it "should show tiles with chosen tag upwards", js: true do
+      click_link @tiles.first.headline
+      expect_content @tiles.first.headline
+
+      [2,4].each do |i|
+        show_next_tile
+        expect_content @tiles[i].headline
+      end
+    end
+
+    it "should show tiles with chosen tag backwards", js: true do
+      click_link @tiles.first.headline
+      expect_content @tiles.first.headline
+
+      [4,2].each do |i|
+        show_previous_tile
+        expect_content @tiles[i].headline
+      end
+    end
+  end
+end
