fe35b7dfde225870b7fc06d24765fc59ea110b94
avbrychak <a.v.brychak@gmail.com>
2014-09-01 14:27:24 -0400

    test on success phrase for one answer

--- spec/acceptance/guest_user/pings_on_answering_tile_in_preview_spec.rb
+++ spec/acceptance/guest_user/pings_on_answering_tile_in_preview_spec.rb
@@ -1,6 +1,25 @@
 require 'acceptance/acceptance_helper'
 
 feature "Guest user answering a tile in preview" do
+  it "should show success phrase for right answer", js: true do
+    tile = FactoryGirl.create(:multiple_choice_tile, :public)
+
+    visit explore_tile_preview_path(id: tile.id)
+
+    click_link "Eggs"
+    page.should have_content("Correct!")
+  end
+
+  it "should not show success phrase if there is only one answer", js: true do
+    tile = FactoryGirl.create(:multiple_choice_tile, :public, 
+      multiple_choice_answers: ["Eggs"], correct_answer_index: 0)
+
+    visit explore_tile_preview_path(id: tile.id)
+
+    click_link "Eggs"
+    page.should_not have_content("Correct!")
+  end
+
   it "should send the appropriate ping", js: true do
     tile = FactoryGirl.create(:multiple_choice_tile, :public)
 
@@ -11,7 +30,6 @@ feature "Guest user answering a tile in preview" do
     FakeMixpanelTracker.clear_tracked_events
 
     click_link "Eggs"
-    page.should have_content("Correct!") # to let Ajax settle
     crank_dj_clear
 
     FakeMixpanelTracker.should have_event_matching("Explore page - Interaction", "action" => 'Clicked Answer', user_type: 'guest', 'tile_id' => tile.id.to_s)
