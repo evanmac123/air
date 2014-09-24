513cf1860e65d32a82195b37c67bc41f3ad701d2
avbrychak <a.v.brychak@gmail.com>
2014-09-24 13:33:56 -0400

    change check in potential user conversion

--- app/controllers/potential_user_conversions_controller.rb
+++ app/controllers/potential_user_conversions_controller.rb
@@ -1,18 +1,15 @@
 class PotentialUserConversionsController < ApplicationController
   def create
-    unless current_user.is_a? PotentialUser
-      render inline: ''
-      return
-    end 
-
-    full_user = current_user.convert_to_full_user! params[:potential_user_name]
-    if full_user
-      full_user.credit_game_referrer current_user.game_referrer
-      session[:potential_user_id] = nil
-      flash[:success] = "Welcome, #{full_user.name}"
-      sign_in full_user
-    else
-      flash[:failure] = "Ooops!"
+    if current_user.is_a? PotentialUser
+      full_user = current_user.convert_to_full_user! params[:potential_user_name]
+      if full_user
+        full_user.credit_game_referrer current_user.game_referrer
+        session[:potential_user_id] = nil
+        flash[:success] = "Welcome, #{full_user.name}"
+        sign_in full_user
+      else
+        flash[:failure] = "Wrong name!"
+      end
     end
     redirect_to activity_path
   end
