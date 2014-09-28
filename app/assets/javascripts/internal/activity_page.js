ecfa7800c3d54c876b6a0ed04bb92cbd7ff02a9f
avbrychak <a.v.brychak@gmail.com>
2014-09-28 15:20:28 -0400

    move activity pop ups to separate files

--- app/assets/javascripts/internal/activity_page.js
+++ app/assets/javascripts/internal/activity_page.js
@@ -6,3 +6,36 @@ $(document).ready(function() {
       }
     });
 });
+
+window.getStartedLightbox = function(){
+  $('#close_get_started_lightbox').click(function(event) {
+    $.post("/ping", {event: 'Saw welcome pop-up', properties: {action: "Click 'Get Started'"}});
+    event.preventDefault();
+    $('#get_started_lightbox').trigger('close');
+  });
+
+  $('#get_started_lightbox').lightbox_me({
+    onClose: function(){ 
+      if( window.showRaffleAfterLightbox == true ){
+        showRaffleBox("New Raffle!");
+        prizeModalPing("Saw Prize Modal");
+      } 
+    }
+  });
+}
+
+window.registerPotentialUserLightbox = function(){
+  $('#register_potential_user_lightbox').lightbox_me({closeClick: false});
+  $("#potential_user_name").on('input propertychange paste', function() {
+    submit_button = $("#register_potential_user_lightbox input[type='submit']")
+    if( $(this).val().match(/\w+\s+\w+/) ){
+      submit_button.removeAttr("disabled")
+    }else{
+      submit_button.attr("disabled", "disabled")
+    }
+    if(!window.enteredName){
+      $.post("/ping", {event: 'Saw welcome pop-up', properties: {action: "Entered Name"}})
+    }
+    window.enteredName = true
+  });
+}
