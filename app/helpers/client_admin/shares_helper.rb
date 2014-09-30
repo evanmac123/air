751631c3514ef71529dff37af7068c6a3baab40a
Phil Darnowsky <phil@darnowsky.com>
2014-09-30 13:20:38 -0400

    Also remove truncation on linked share link

--- app/helpers/client_admin/shares_helper.rb
+++ app/helpers/client_admin/shares_helper.rb
@@ -21,7 +21,7 @@ module ClientAdmin::SharesHelper
       mini: true,
       url: explore_tile_preview_url(tile),
       title: tile.headline,
-      summary: tile.supporting_content.truncate(100),
+      summary: tile.supporting_content,
       source: "http://www.air.bo"
     }
     "https://www.linkedin.com/shareArticle?#{params.to_query}"
