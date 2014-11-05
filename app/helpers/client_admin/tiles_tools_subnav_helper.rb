16bd00336bb103fad52b1ab7352216798a74f754
avbrychak <a.v.brychak@gmail.com>
2014-11-05 15:50:22 -0500

    move item logic for nav bar to helpers

---
+++ app/helpers/client_admin/tiles_tools_subnav_helper.rb
@@ -0,0 +1,87 @@
+module ClientAdmin::TilesToolsSubnavHelper 
+  def tiles_to_be_sent(user)
+    demo = user.demo
+    demo.digest_tiles(demo.tile_digest_email_sent_at).count
+  end
+
+  def block_nav? user
+    demo = user.demo
+    demo.non_activated? && user.show_onboarding?
+  end
+
+  def subnav_item_block params = {}
+    params[:icon] ||= nil
+    params[:image] ||= nil
+    params[:blocked] ||= nil
+    render partial: "client_admin/tiles/subnav_item", locals: params
+  end
+
+  def subnav_elements
+    [
+      {
+        item_id: "explore", 
+        link: explore_path, 
+        icon: "rocket", 
+        text: "Explore"
+      },
+      {
+        item_id: "home_nav", 
+        link: activity_path, 
+        image: "airbo_logo_lightblue_square.png", 
+        text: "Preview"
+      },
+      {
+        item_id: "managing_tiles", 
+        link: client_admin_tiles_path, 
+        icon: "square-o", 
+        text: "Create"
+      },
+      {
+        item_id: "share_tiles", 
+        link: client_admin_share_path, 
+        icon: "share-alt", 
+        text: "Share"
+      },
+      {
+        item_id: "board_activity", 
+        link: client_admin_path, 
+        icon: "line-chart", 
+        text: "Activity"
+      },
+      {
+        item_id: "prizes_nav", 
+        link: client_admin_prizes_path, 
+        icon: "trophy", 
+        text: "Prizes"
+      },
+      {
+        item_id: "users", 
+        link: client_admin_users_path, 
+        icon: "users", 
+        text: "Users"
+      }
+    ]
+  end
+
+  def list_of_blocked_items
+    ["Share", "Activity", "Prizes", "Users"]
+  end
+
+  def subnav_elements_with_blocked_items
+    elements = subnav_elements
+    elements.each do |element|
+      if list_of_blocked_items.include? element[:text]
+        element[:blocked] = true
+      end
+    end
+    elements
+  end
+
+  def set_subnav_elements
+    if block_nav? current_user
+      subnav_elements_with_blocked_items
+    else
+      subnav_elements
+    end
+  end
+end
