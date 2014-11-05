65539320652c35b1db01ea49cea398fe47c01aa6
Phil Darnowsky <phil@darnowsky.com>
2014-11-05 17:26:26 -0500

    Fucking with different kinds of spaces isn't getting us anywhere, applying white-space: pre-wrap to paragraphs in supporting content.

--- spec/models/presenters/full_size_tile_presenter_spec.rb
+++ spec/models/presenters/full_size_tile_presenter_spec.rb
@@ -7,19 +7,19 @@ describe FullSizeTilePresenter do
     it "should turn all-whitespace lines into HTML-safed non-breaking spaces" do
       tile = FactoryGirl.build_stubbed(:tile, supporting_content: "Line 1\n\nLine 2")
       presenter = FullSizeTilePresenter.new(tile, user, false)
-      presenter.supporting_content.should == "<p>Line&thinsp;1</p><p>&nbsp;</p><p>Line&thinsp;2</p>"
+      presenter.supporting_content.should == "<p>Line 1</p><p>&nbsp;</p><p>Line 2</p>"
     end
 
     it "should retain multiple spaces by turning them into non-breaking spaces" do
       tile = FactoryGirl.build_stubbed(:tile, supporting_content: "   Here  There   Everywhere")
       presenter = FullSizeTilePresenter.new(tile, user, false)
-      presenter.supporting_content.should == "<p>&thinsp;&thinsp;&thinsp;Here&thinsp;&thinsp;There&thinsp;&thinsp;&thinsp;Everywhere</p>"
+      presenter.supporting_content.should == "<p>   Here  There   Everywhere</p>"
     end
 
     it "should escape tags in the supporting content" do
       tile = FactoryGirl.build_stubbed(:tile, supporting_content: "<script>alert('Bad evil!')</script>")
       presenter = FullSizeTilePresenter.new(tile, user, false)
-      presenter.supporting_content.should == "<p>&lt;script&gt;alert(&#x27;Bad&thinsp;evil!&#x27;)&lt;/script&gt;</p>"
+      presenter.supporting_content.should == "<p>&lt;script&gt;alert(&#x27;Bad evil!&#x27;)&lt;/script&gt;</p>"
     end
   end
 end
