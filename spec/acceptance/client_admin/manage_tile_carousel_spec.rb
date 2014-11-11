ba9d22b3088d4eba0e4c2e2fece31ed0a9ce42a5
avbrychak <a.v.brychak@gmail.com>
2014-11-11 13:56:02 -0500

    add tests for arrows

--- spec/acceptance/client_admin/manage_tile_carousel_spec.rb
+++ spec/acceptance/client_admin/manage_tile_carousel_spec.rb
@@ -2,19 +2,50 @@ require 'acceptance/acceptance_helper'
 
 feature 'Carousel on Client Admin Tile Page' do
   include TileManagerHelpers
-
-  def test_caruosel tiles, current_position = 0, offset = 1
-    
-  end
+  include WaitForAjax
 
   let!(:admin) {FactoryGirl.create(:client_admin, share_section_intro_seen: true)}
+  let!(:demo)  { admin.demo  }
 
   background do
     bypass_modal_overlays(admin)
     signin_as(admin, admin.password)
   end
 
-  context "Move " do
+  shared_examples_for 'Moves by arrows' do |section, tiles_num, arrow_selector|
+    scenario "tiles from #{section} section. moves to the #{arrow_selector} tile", js: true do
+      create_tiles_for_sections section => tiles_num
+      tiles = demo.send(:"#{section}_tiles").to_a
 
+      i = 0
+      offset = arrow_selector == "#next" ? 1 : -1
+      visit client_admin_tile_path(tiles[i])
+      expect_content tiles[i].headline
+
+      (tiles_num - 1).times do
+        i += offset
+        page.find(arrow_selector).click
+        expect_content tiles[i].headline
+      end
+
+      page.find(arrow_selector).click
+      expect_content tiles[0].headline
+    end
   end
+
+  it_should_behave_like "Moves by arrows", "draft",   3, "#next"
+  it_should_behave_like "Moves by arrows", "active",  5, "#next"
+  it_should_behave_like "Moves by arrows", "archive", 7, "#next"
+
+  it_should_behave_like "Moves by arrows", "draft",   4, "#prev"
+  it_should_behave_like "Moves by arrows", "active",  6, "#prev"
+  it_should_behave_like "Moves by arrows", "archive", 2, "#prev"
+
+  it_should_behave_like "Moves by arrows", "draft",   1, "#next"
+  it_should_behave_like "Moves by arrows", "active",  1, "#next"
+  it_should_behave_like "Moves by arrows", "archive", 1, "#next"
+
+  it_should_behave_like "Moves by arrows", "draft",   1, "#prev"
+  it_should_behave_like "Moves by arrows", "active",  1, "#prev"
+  it_should_behave_like "Moves by arrows", "archive", 1, "#prev"  
 end
