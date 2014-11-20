4f801cd770c105d0c62542f338c5cd1c0f3307c9
avbrychak <a.v.brychak@gmail.com>
2014-11-20 15:10:28 -0500

    don't set placeholder as value in ie9. just focus in and out

--- app/assets/javascripts/external/placeholder_ie.js
+++ app/assets/javascripts/external/placeholder_ie.js
@@ -6,12 +6,7 @@ $().ready(function(){
   if (!supports_input_placeholder()) {
     var fields = document.getElementsByTagName('INPUT');
     for (var i = 0; i < fields.length; i++) {
-      if (fields[i].hasAttribute('placeholder') && $(fields[i]).attr("value") == "") {
-        fields[i].defaultValue = fields[i].getAttribute('placeholder');
-        fields[i].value = fields[i].defaultValue;
-        fields[i].onfocus = function () { if (this.value == this.defaultValue) this.value = ''; }
-        fields[i].onblur = function () { if (this.value == '') this.value = this.defaultValue; }
-      }
+      $(fields[i]).trigger( "focusin" ).trigger( "focusout" );
     }
   }
 });
