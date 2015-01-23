76f4a73f1d173341867e95c1f76a0e0ae1fa3b3c
avbrychak <a.v.brychak@gmail.com>
2015-01-23 14:25:29 -0500

    add confirmation modal to styleguide

--- app/assets/javascripts/app-pages.js
+++ app/assets/javascripts/app-pages.js
@@ -1,8 +1,10 @@
 //= require jquery
 //= require ../../../vendor/assets/javascripts/external/foundation.min
+//= require ../../../vendor/assets/javascripts/client_admin/confirm_with_reveal
 //= require_tree ./pages
 //= require_tree ../../../vendor/assets/javascripts/external/
 
 $(document).ready(function() {
   $(document).foundation();
 });
+$(document).confirmWithReveal();
