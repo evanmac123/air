ad5b6c0363b9e781d544469dbc5ada542aae5fd7
Phil Darnowsky <phil@darnowsky.com>
2015-01-26 21:45:15 -0500

    #find_each rather than #each on big backfill

--- db/migrate/20150115133119_create_tile_viewings.rb
+++ db/migrate/20150115133119_create_tile_viewings.rb
@@ -11,7 +11,7 @@ class CreateTileViewings < ActiveRecord::Migration
 
     add_index :tile_viewings, [:tile_id, :user_id, :user_type], unique: true, name: "index_tile_viewings_on_tile_and_user"
 
-    TileCompletion.all.each do |tc|
+    TileCompletion.all.find_each do |tc|
       TileViewing.create(tile: tc.tile, user: tc.user)
     end
   end
