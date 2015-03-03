66441c5664c7cb8796e7b9c2228bb8ab72ea4f55
avbrychak <a.v.brychak@gmail.com>
2015-03-03 13:39:31 -0500

    tests for image_styles in tile presenter

--- spec/models/presenters/full_size_tile_presenter_spec.rb
+++ spec/models/presenters/full_size_tile_presenter_spec.rb
@@ -4,6 +4,10 @@ describe FullSizeTilePresenter do
   describe "#supporting_content" do
     let(:user) {FactoryGirl.build_stubbed(:user)}
 
+    def ie8
+      'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.2; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)'
+    end
+
     it "should turn all-whitespace lines into HTML-safed non-breaking spaces" do
       tile = FactoryGirl.build_stubbed(:tile, supporting_content: "Line 1\n\nLine 2")
       presenter = FullSizeTilePresenter.new(tile, user, false, [], Browser.new)
@@ -20,6 +24,19 @@ describe FullSizeTilePresenter do
       tile = FactoryGirl.build_stubbed(:tile, supporting_content: "<script>alert('Bad evil!')</script>")
       presenter = FullSizeTilePresenter.new(tile, user, false, [], Browser.new)
       presenter.supporting_content.should == "<p>&lt;script&gt;alert(&#x27;Bad evil!&#x27;)&lt;/script&gt;</p>"
+    end
+
+    it "should have styles for ie8" do
+      tile = FactoryGirl.build_stubbed(:tile)
+      browser = Browser.new(ua: ie8)
+      presenter = FullSizeTilePresenter.new(tile, user, false, [], browser)
+      presenter.image_styles.should be_present
+    end
+
+    it "should not have styles be default" do
+      tile = FactoryGirl.build_stubbed(:tile)
+      presenter = FullSizeTilePresenter.new(tile, user, false, [], Browser.new)
+      presenter.image_styles.should_not be_present
     end
   end
 end
