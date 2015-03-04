e744ee4e6bcc31dde4b8a5804bcb9c960b2f1ef8
Phil Darnowsky <phil@darnowsky.com>
2015-03-04 17:55:15 -0500

    WIP moving logic for displaying user progress into a presenter

---
+++ app/presenters/user_progress_presenter.rb
@@ -0,0 +1,36 @@
+class UserProgressPresenter
+  include ActionView::Helpers::NumberHelper
+
+  def initialize(user, raffle, browser)
+    @user = user
+    @raffle = raffle
+    @browser = browser
+  end
+
+#  def cache_key
+    #@cache_key ||= [
+      #self.class.to_s, 
+      #'v1.pwd'
+    #].join('-')
+  #end
+
+  def available_tile_count
+    @available_tile_count ||= @user.available_tiles_on_current_demo.count
+  end
+
+  def completed_tile_count
+    @completed_tile_count ||= @user.completed_tiles_on_current_demo.count
+  end
+
+  def some_tiles_undone?
+    available_tile_count != completed_tile_count  
+  end
+
+  def points
+    @points ||= number_with_delimiter(@user.points)
+  end
+
+  def old_browser?
+    @browser.ie6? || @browser.ie7? || @browser.ie8?  
+  end
+end
