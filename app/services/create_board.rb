dc5ec300cc82ca314b4f3f52c46e153b4584c336
avbrychak <a.v.brychak@gmail.com>
2015-04-03 15:22:20 -0400

    add email generator to create board service because public slug and email don't correspond to each other. public slug can be unqiue while email is not

--- app/services/create_board.rb
+++ app/services/create_board.rb
@@ -11,17 +11,27 @@ class CreateBoard
     set_board_defaults
     board_saved_successfully = @board.save
     # We do this separately so that we know the board has a unique public slug
-
-    if board_saved_successfully
-      email_local_part = @board.public_slug.gsub(/-/, '')
-      @board.update_attributes(email: email_local_part + "@ourairbo.com")
-    end
+    set_board_email if board_saved_successfully
   end
 
   def set_board_defaults
     @board.game_referrer_bonus = 5
     @board.referred_credit_bonus = 2
     @board.credit_game_referrer_threshold = 100000
+  end
+
+  def set_board_email
+    email_local_part = @board.public_slug.gsub(/-/, '')
+    offset = 2 # in case of a collision on the slug "foobar", we'll try "foobar2" first
+
+    Demo.transaction do
+      while (demo = Demo.find_by_email(email_local_part + "@ourairbo.com")).present?
+        break if demo.id == @board.id
+        email_local_part += offset.to_s
+        offset += 1
+      end
+      @board.update_attributes(email: email_local_part + "@ourairbo.com")
+    end
   end
 
   attr_reader :board
