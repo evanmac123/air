34a522154107d018d059d38721b9056b6a5f74ac
avbrychak <a.v.brychak@gmail.com>
2015-04-13 12:08:36 -0400

    test for tile image pagination

---
+++ spec/controllers/client_admin/tile_images_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe ClientAdmin::TileImagesController do
+  describe "GET index" do
+    it "should return correct tiles" do
+      demo = FactoryGirl.create(:demo)
+      client_admin = FactoryGirl.create(:client_admin, demo: demo)
+      tile_images = FactoryGirl.create_list :tile_image, 55
+      TileImage.update_all(image_processing: false, thumbnail_processing: false)
+
+      sign_in_as(client_admin)
+      get :index, page: 2
+      assigns(:tile_images).count.should == 18
+      assigns(:tile_images).map(&:id).should == TileImage.order{ created_at.desc }[35..52].map(&:id)
+    end
+  end
+end
