ff91782a2605814c615f32737973e598d06953ad
avbrychak <a.v.brychak@gmail.com>
2015-05-08 12:37:25 -0400

    add removing to saving process

--- app/controllers/client_admin/suggestions_access_controller.rb
+++ app/controllers/client_admin/suggestions_access_controller.rb
@@ -6,8 +6,6 @@ class ClientAdmin::SuggestionsAccessController < ClientAdminBaseController
       params[:demo][:everyone_can_make_tile_suggestions]
 
     unless @demo.everyone_can_make_tile_suggestions
-      # user_ids = params[:allowed_users]
-      # users = @demo.users.find(user_ids)
       User.allow_to_make_tile_suggestions params[:allowed_users], @demo
     end
 
