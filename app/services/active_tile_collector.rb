66ff66e90957442480fc69a77acf2a91d3414640
hraynaud <herby.raynaud@gmail.com>
2015-05-26 14:56:02 -0400

    WIP

---
+++ app/services/active_tile_collector.rb
@@ -0,0 +1,27 @@
+class ActiveTileCollector 
+	attr_accessor :board, :beg_date, :end_date
+	def	initialize board, beg_date, end_date
+		@beg_date = beg_date 
+		@end_date = end_date 
+		@board = board 
+	end
+
+	def collect
+		viewings.map(&:tile) | completions.map(&:tile)
+	end
+
+	private
+
+	def has_tile_activity?
+		viewings.any? || completions.any?
+	end
+
+	def viewings 
+		@viewings ||=board.tile_viewings.for_period(beg_date, end_date)
+	end
+
+	def completions 
+		@completions ||= board.tile_completions.for_period(beg_date, end_date)
+	end
+
+end
