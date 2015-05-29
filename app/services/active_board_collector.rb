9bf6bb9dde3bd9d5e1029997195427c5296f5f04
Herby Raynaud <herbyraynaud@Herbys-MacBook-Pro.local>
2015-05-29 18:14:12 -0400

    Finally completes active board and tile collector specs with passing test

--- app/services/active_board_collector.rb
+++ app/services/active_board_collector.rb
@@ -1,51 +1,55 @@
 require 'ostruct'
 
 class ActiveBoardCollector
-	attr_accessor :beg_date, :end_date, :admin_board_memberships
 
-	def initialize opts={}
-		@admin_board_memberships = opts[:selected_boards] || BoardMembership.admins
-		binding.pry
-		validate_report_dates opts
-		map_admins_to_boards
-	end
+  def initialize opts={}
+    @admin_board_memberships = opts[:selected_boards] || BoardMembership.admins
+    validate_report_dates opts
+    map_admins_to_boards
+  end
 
-	def collect
-		rep = Struct.new(:board_id, :admins, :tiles )
-		active_boards = []
-		@board_admins.each do |board, admins| 
-			tile_collector =ActiveTileCollector.new(board, beg_date, end_date)
-			if tile_collector.has_tile_activity?  
-				tiles = tile_collector.collect
-				active_boards.push rep.new(board,admins,tiles ) 
-			end
-		end
-		binding.pry
-		active_boards
-	end
+  def boards
+    @active_boards ||=collect
+  end
 
-	private
+  private
+  def collect
+    rep = Struct.new(:board_id, :admins, :tiles )
+    @active_boards = []
+    @board_admins.each do |board, admins| 
+      tile_collector =ActiveTileCollector.new(board, @beg_date, @end_date)
+      tiles = tile_collector.collect
+      @active_boards.push rep.new(board,admins,tiles ) if tiles.any?
+    end
 
-	def validate_report_dates opts
-		period_start = opts[:period_start]
-		period_end = opts[:period_end]
+    @active_boards
+  end
 
-		if period_start && period_end && period_start.is_a?(DateTime) < period_end.is_a?(DateTime)
-			@beg_date = period_start 
-			@end_date = period_end 
-		else
-			#NOTE Default to last week
-			@beg_date = Date.today.beginning_of_week(:sunday).prev_week(:sunday).at_midnight
-			@end_date = beg_date.end_of_week(:sunday).end_of_day
-		end
-	end
 
-	def map_admins_to_boards
-		@board_admins = Hash.new{|h,k|h[k]=[]}
-		@admin_board_memberships.includes(demo: [tiles: [:tile_viewings, :tile_completions]]).each do |mem|
-			@board_admins[mem.demo].push(mem.user.name)
-		end
-	end
+  def validate_report_dates opts
+    period_start = opts[:beg_date]
+    period_end = opts[:end_date]
+
+    if period_start && period_end && period_start.is_a?(Time) && 
+      period_end.is_a?(Time) && period_start < period_end
+
+      @beg_date = period_start 
+      @end_date = period_end 
+    else
+      #NOTE Default to last week
+      @beg_date = Date.today.beginning_of_week(:sunday).prev_week(:sunday).at_midnight
+      @end_date = beg_date.end_of_week(:sunday).end_of_day
+    end
+  end
+
+  def map_admins_to_boards
+    @board_admins = Hash.new{|h,k|h[k]=[]}
+    @admin_board_memberships.includes(
+      demo: [tiles: [:tile_viewings, :tile_completions]]).each do |mem|
+
+      @board_admins[mem.demo].push(mem.user.name)
+    end
+  end
 
 
 end
--- spec/models/active_board_collector_spec.rb
+++ spec/models/active_board_collector_spec.rb
@@ -2,34 +2,48 @@ require 'spec_helper'
 
 describe ActiveBoardCollector do
 
-	describe "#collect" do
+  describe "#boards" do
 
     before do
-		@viewing){FactoryGirl.create(:tile_viewing)}
-		@completion){FactoryGirl.create(:tile_completion)}
-		@tile){viewing.tile}
-		@demo){tile.demo}
-		@beg_date){1.week.ago}
-		@end_date){Time.now}
-			@collector = ActiveBoardCollector.new(beg_date: beg_date, end_date: end_date) 
-		end
+      @user=FactoryGirl.create(:user)
+      @demo=@user.demo
+      @tile1=FactoryGirl.create(:tile, demo: @demo)
+      @tile2=FactoryGirl.create(:tile, demo: @demo)
+      @viewing=FactoryGirl.create(:tile_viewing, tile: 
+                                  @tile1, user: @user)
+      @completion=FactoryGirl.create(:tile_completion, 
+                                     tile: @tile2, user: @user)
+      @beg_date=1.week.ago
+      @end_date=Time.now
 
-		it "returns true if tile viewing activity exists for date range" do
-			viewing.update_attribute(:created_at,  2.days.ago)
-			expect(@collector.collect.any?).to be_true
-		end 
+      bm = BoardMembership.new
+      bm.user = @demo.users.first
+      bm.demo = @demo
+      bm.is_client_admin=true;
+      bm.save
+      @collector = ActiveBoardCollector.new(beg_date: @beg_date, 
+                                            end_date: @end_date) 
+    end
 
-		it "returns true if tile completion activity exists for date range" do
-			completion.update_attribute(:created_at,  2.days.ago)
-			expect(@collector.collect.any?).to be_true
-		end 
+    it "returns non-empty if tile viewing activity exists for date range" do
+      @viewing.created_at = 2.days.ago
+      @viewing.save
+      expect(@collector.boards.any?).to be_true
+    end 
 
-		it "returns false if neither tile completion nor viewing activity  exists for date range" do
-			viewing.update_attribute(:created_at,  2.weeks.ago)
-			completion.update_attribute(:created_at,  2.weeks.ago)
-			expect(@collector.collect.any?).to be_false
-		end 
+    it "returns non-empty if tile completion activity exists for date range" do
+      @completion.created_at = 2.days.ago
+      expect(@collector.boards.any?).to be_true
+    end 
 
-	end
+    it "returns empty if no completion nor viewing activity for date range" do
+      @viewing.created_at =  2.weeks.ago
+      @completion.created_at =  2.weeks.ago
+      @viewing.save
+      @completion.save
+      expect(@collector.boards.empty?).to be_true
+    end 
+
+  end
 
 end
