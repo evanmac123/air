1e725da8a28130e1f967ffea76449f78bec751c3
avbrychak <a.v.brychak@gmail.com>
2015-05-29 07:30:32 -0400

    add tests for ignored tile and intro to submitted tile

--- spec/acceptance/client_admin/suggestion_box/tile_preview_page_menu_spec.rb
+++ spec/acceptance/client_admin/suggestion_box/tile_preview_page_menu_spec.rb
@@ -15,7 +15,33 @@ feature 'Client Admin uses menu on tile preview page of suggested tile' do
     page.all(".preview_menu_item .header_text")
   end
 
-  context "submitted tile" do
+  def intro_tooltip
+    page.find(".tile_preview_intro")
+  end
+
+  def intro_text
+    "Accept the tile to use it in your Board, or Ignore it to mark it as reviewed."
+  end
+
+  context "Intro For Submitted Tile" do
+    let!(:tile) { FactoryGirl.create :multiple_choice_tile, :user_submitted, demo: demo }
+
+    before do
+      client_admin.update_attribute(:submitted_tile_menu_intro_seen, false)
+      visit client_admin_tile_path(tile, as: client_admin)
+    end
+
+    it "should show intro first time", js: true do
+      intro_tooltip.should be_present
+      expect_content intro_text
+      click_link "Got it"
+      expect_no_content intro_text
+      visit client_admin_tile_path(tile, as: client_admin)
+      expect_no_content intro_text
+    end
+  end
+
+  context "Submitted Tile" do
     let!(:tile) { FactoryGirl.create :multiple_choice_tile, :user_submitted, demo: demo }
 
     before do
@@ -74,6 +100,40 @@ feature 'Client Admin uses menu on tile preview page of suggested tile' do
         expect_content "The #{tile.headline} tile has been ignored"
         menu_header.text.should == "Ignored"
         tile.reload.status.should == Tile::IGNORED
+      end
+    end
+  end
+
+  context "Ignored Tile" do
+    let!(:tile) { FactoryGirl.create :multiple_choice_tile, :ignored, demo: demo }
+
+    before do
+      visit client_admin_tile_path(tile, as: client_admin)
+    end
+
+    it "should have right menu header" do
+      menu_header.text.should == "Ignored"
+    end
+
+    it "should have right menu items" do
+      menu_items.map(&:text).should == ["Back to Tiles", "Undo Ignore"]
+    end
+
+    context "Back to Tiles link" do
+      it "should move user to suggestion box" do
+        click_link "Back to Tiles"
+        current_path.should == client_admin_tiles_path
+        page.find(".suggestion_box_selected").should be_present
+        headline(page.find(:tile, tile)).should == tile.headline
+      end
+    end
+
+    context "Undo Ignore Link" do
+      it "should make tile submitted" do
+        click_link "Undo Ignore"
+        expect_content "The #{tile.headline} tile has been moved to submitted"
+        menu_header.text.should == "Submitted"
+        tile.reload.status.should == Tile::USER_SUBMITTED
       end
     end
   end
