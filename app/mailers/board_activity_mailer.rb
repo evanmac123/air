bd51df5c6583f5d690e59464441a1d6895acb23b
Herby Raynaud <herbyraynaud@Herbys-MacBook-Pro.local>
2015-05-31 14:45:55 -0400

    WIP Activity Mailer

--- app/mailers/board_activity_mailer.rb
+++ app/mailers/board_activity_mailer.rb
@@ -4,39 +4,46 @@ class BoardActivityMailer < ActionMailer::Base
   helper 'client_admin/tiles'
   helper ApplicationHelper
 
-  has_delay_mail       # Some kind of monkey-patch workaround (not even sure need)
+  has_delay_mail
 
-  include EmailHelper  # Well, the 'helper' above method might include it into the view, but it don't include it in here
+  include EmailHelper
   include ClientAdmin::TilesHelper # and ditto
 
   layout nil#'mailer'
 
   default reply_to: 'support@airbo.com'
 
-  def notify_one(demo_id, user_id, tile_ids, subject, follow_up_email, 
-      custom_headline, custom_message, custom_from=nil, is_new_invite = nil)
-    @demo = Demo.find demo_id
-    @user  = User.find user_id # XTR
-    return nil unless @user.email.present? # no wasting our time trying to email people for whom we don't have an address # XTR2
+  def active_boards
+    @collector = ActiveBoardCollector.new
 
-    presenter_class = follow_up_email ? TilesDigestMailFollowupPresenter : TilesDigestMailDigestPresenter
-    @presenter = presenter_class.new(@user, @demo, custom_from, custom_headline, custom_message, is_new_invite)
+    @collector.boards.each do |active_board|
+      notify_admin(active_board.demo.id, active_board.user.id, active_board.iles.map(&:id))
+    end
+  end
+
+  def notify_admin(demo_id, user_id, tile_ids)
+    @demo = Demo.find demo_id
+    @user  = User.find user_id
+    return nil unless @user.email.present? 
+
+    @presenter = TilesDigestMailDigestPresenter.new(
+      @user, @demo, custom_from, 
+      custom_headline, custom_message, is_new_invite)
 
     email_type = @presenter.email_type
     ping_on_digest_email email_type, @user
 
-    @tiles = TileBoardDigestDecorator.decorate_collection Tile.where(id: tile_ids).ordered_by_position, \
-                                                          context: {
-                                                            demo: @demo,
-                                                            user: @user,
-                                                            follow_up_email: @follow_up_email,
-                                                            email_type: email_type,
-                                                          }
-    mail  to:      @user.email_with_name,
-          from:    @presenter.from_email,
-          subject: subject
-  end
+    @tiles = TileBoardDigestDecorator.decorate_collection Tile.where(
+      id: tile_ids).ordered_by_position,
+      context: {
+        demo: @demo,
+        user: @user,
+        follow_up_email: @follow_up_email,
+        email_type: email_type,
+      }
 
+      mail to: @user.email_with_name, from: @presenter.from_email, subject: subject
+  end
 
   def ping_on_digest_email email_type, user
     TrackEvent.ping( "Email Sent", {email_type: ping_message[email_type]}, user )
