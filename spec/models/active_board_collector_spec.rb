7bb4ee7f01df5df26b8501a70554f94aa1e3b56e
hraynaud <herby.raynaud@gmail.com>
2015-05-31 22:41:46 -0400

    Refactors test spec to remove a lot of duplication and unnecessary setup code

--- spec/models/active_board_collector_spec.rb
+++ spec/models/active_board_collector_spec.rb
@@ -2,111 +2,112 @@ require 'spec_helper'
 
 describe ActiveBoardCollector do
 
-    before do
-      @demo=FactoryGirl.create(:demo)
-      @user=FactoryGirl.create(:user, demo: @demo)
-      @tile1=FactoryGirl.create(:tile, demo: @demo)
-      @tile2=FactoryGirl.create(:tile, demo: @demo)
-			@viewing=FactoryGirl.create(:tile_viewing, tile: @tile1, user: @user)
-			@completion=FactoryGirl.create(:tile_completion, tile: @tile2, user: @user)
+	before do
+		@demo, user, tile1, tile2 = setup_user_board
+		@viewing = setup_viewing user, tile1
+		@completion = setup_completion(user, tile2)
+		setup_board_membership BoardMembership.first, user, @demo
+		@beg_date=1.week.ago
+		@end_date=Time.now
+	end
 
-			bm = BoardMembership.first
-			bm.user = @user
-			bm.demo = @demo
-			bm.is_client_admin=true;
-			bm.save
-    end
   describe "#boards" do
-
 		before do
-			@beg_date=1.week.ago
-			@end_date=Time.now
 			@collector = ActiveBoardCollector.new(beg_date: @beg_date, end_date: @end_date) 
 		end
 
 		it "returns non-empty if tile viewing activity exists for date range" do
-			@viewing.created_at = 2.days.ago
-			@viewing.save
 			expect(@collector.active_boards.any?).to be_true
 		end 
 
 		it "returns non-empty if tile completion activity exists for date range" do
-			@completion.created_at = 2.days.ago
 			expect(@collector.active_boards.any?).to be_true
 		end
 
 	  it "has only one active board" do
-			@completion.created_at = 2.days.ago
 			expect(@collector.active_boards.count).to eq(1) 
 		end	
 
-		it "correctly finds multiple active boards" do
-      demo=FactoryGirl.create(:demo)
-      user=FactoryGirl.create(:user, demo: demo)
-      tile1=FactoryGirl.create(:tile, demo: demo)
-      tile2=FactoryGirl.create(:tile, demo: demo)
-			viewing=FactoryGirl.create(:tile_viewing, tile: tile1, user: user)
-			completion=FactoryGirl.create(:tile_completion, tile: tile2, user: user)
+		it "returns empty if no tile activity exists for date range" do
+			@completion.update_attribute(:created_at, 2.weeks.ago)
+			@viewing.update_attribute(:created_at, 2.weeks.ago)
 
-			bm =demo.board_memberships.first
-			bm.user = user
-			bm.demo = demo
-			bm.is_client_admin=true;
-			bm.save
-
-			viewing.created_at =  2.days.ago
-			completion.created_at =  2.days.ago
-			viewing.save
-			completion.save
 			@collector = ActiveBoardCollector.new(beg_date: @beg_date, end_date: @end_date) 
+
+			expect(@collector.active_boards.blank?).to be_true
+		end
+
+		it "correctly finds multiple active boards" do
+			demo, user, tile1, tile2 = setup_user_board
+			viewing = setup_viewing user, tile1
+			setup_board_membership demo.board_memberships.first, user, demo
+
+			@collector = ActiveBoardCollector.new(beg_date: @beg_date, end_date: @end_date) 
+
 			expect(@collector.active_boards.count).to eq(2) 
 		end 
-
-	it "returns non-empty if tile completion activity exists for date range" do
-			@completion.created_at = 2.days.ago
-			expect(@collector.active_boards.any?).to be_true
-		end 
-
 
   end
 
   describe "#admins" do
-		before do
-			@beg_date=1.week.ago
-			@end_date=Time.now
-		end
+
+		let(:user){FactoryGirl.create(:user,demo: nil)}
 
 		it "returns one" do
-			@viewing.created_at = 2.days.ago
-			@viewing.save
+      @collector = ActiveBoardCollector.new
+			expect(@collector.board_admins(@demo).count).to eq(1) 
+		end 
+
+		it "returns only admins" do
+			setup_board_membership BoardMembership.new, user, @demo, false
       @collector = ActiveBoardCollector.new
 			expect(@collector.board_admins(@demo).count).to eq(1) 
 		end 
 
 		it "returns muliple" do
-			user2=FactoryGirl.create(:user,demo: nil)
-      bm = BoardMembership.new
-      bm.user = user2
-      bm.demo = @demo
-      bm.is_client_admin=true;
-      bm.save
-			@completion.created_at = 2.days.ago
-			@viewing.save
+			setup_board_membership BoardMembership.new, user, @demo
       @collector = ActiveBoardCollector.new
 			expect(@collector.board_admins(@demo).count).to eq(2) 
 		end 
+
   end
 
 	describe "#send_mail_notifications" do
-		before do
-      @viewing.created_at = 2.days.ago
-      @viewing.save
-		end
 		it "sends notifications" do
-      @collector = ActiveBoardCollector.new
+			@collector = ActiveBoardCollector.new
 			@collector.expects(:send_for_admin).once
 			@collector.send_mail_notifications
 		end
 	end
+
+
+	def setup_viewing user, tile
+		FactoryGirl.create(:tile_viewing, tile: tile, user: user, created_at: 1.day.ago)
+	end
+
+	def setup_completion user, tile
+		FactoryGirl.create(:tile_completion, tile: tile, user: user, created_at: 1.day.ago)
+	end
+
+	def setup_board_membership bm, user, board, is_admin=true
+		bm.demo = board 
+		bm.user = user
+		bm.is_client_admin=is_admin;
+		bm.save
+	end
+
+	def setup_user_board
+		demo=FactoryGirl.create(:demo)
+		user=FactoryGirl.create(:user, demo: demo)
+		tile1=create_demo_tile(demo)
+		tile2=create_demo_tile(demo)
+		return demo, user, tile1, tile2
+	end
+
+	def create_demo_tile demo
+		FactoryGirl.create(:tile, demo: demo)
+	end
+
+
 
 end
