fc895880ade71f1d9f8be2916393756a4f99b028
hraynaud <herby.raynaud@gmail.com>
2015-06-02 01:31:07 -0400

    Modifies active board collector to use arel to build up the queries

--- spec/models/active_board_collector_spec.rb
+++ spec/models/active_board_collector_spec.rb
@@ -2,31 +2,32 @@ require 'spec_helper'
 
 describe ActiveBoardCollector do
 
+
 	before do
-		@demo, user, tile1, tile2 = setup_user_board
-		@viewing = setup_viewing user, tile1
-		@completion = setup_completion(user, tile2)
-		setup_board_membership BoardMembership.first, user, @demo
 		@beg_date=1.week.ago
 		@end_date=Time.now
+		@demo, @user, tile1, tile2 = setup_user_board
+		@viewing = setup_viewing @user, tile1
+		@completion = setup_completion(@user, tile2)
+		@bm = setup_board_membership BoardMembership.first, @user, @demo
 	end
 
-  describe "#boards" do
+	describe "#boards" do
 		before do
-		@collector = create_date_range_collector
+			@collector = create_date_range_collector
 		end
 
 		it "returns non-empty if tile viewing activity exists for date range" do
 			expect(@collector.active_boards.any?).to be_true
 		end 
 
-		it "returns non-empty if tile completion activity exists for date range" do
-			expect(@collector.active_boards.any?).to be_true
-		end
-
-	  it "has only one active board" do
+		it "has only one active board" do
 			expect(@collector.active_boards.count).to eq(1) 
 		end	
+
+		it "returns non-empty if tile viewing activity exists for date range" do
+			expect(@collector.active_boards.any?).to be_true
+		end 
 
 		it "returns empty if no tile activity exists for date range" do
 			@completion.update_attribute(:created_at, 2.weeks.ago)
@@ -39,36 +40,50 @@ describe ActiveBoardCollector do
 			demo, user, tile1, tile2 = setup_user_board
 			viewing = setup_viewing user, tile1
 			setup_board_membership demo.board_memberships.first, user, demo
-
 			@collector = create_date_range_collector
-
 			expect(@collector.active_boards.count).to eq(2) 
 		end 
 
-  end
+	end
 
-  describe "#admins" do
+	describe "#board_admins" do
 
 		let(:user){FactoryGirl.create(:user,demo: nil)}
 
 		it "returns one" do
-      @collector = ActiveBoardCollector.new
-			expect(@collector.board_admins(@demo).count).to eq(1) 
-		end 
-
-		it "returns only admins" do
-			setup_board_membership BoardMembership.new, user, @demo, false
-      @collector = ActiveBoardCollector.new
+			@collector = ActiveBoardCollector.new
 			expect(@collector.board_admins(@demo).count).to eq(1) 
 		end 
 
 		it "returns muliple" do
 			setup_board_membership BoardMembership.new, user, @demo
-      @collector = ActiveBoardCollector.new
+			@collector = ActiveBoardCollector.new
 			expect(@collector.board_admins(@demo).count).to eq(2) 
-		end 
+		end
 
-  end
+		it "returns non empty if user is a client admin and the user is on current board" do
+			@user.update_attribute(:is_client_admin, true)
+			@bm.update_attribute(:is_client_admin, false)
+			@collector = ActiveBoardCollector.new
+
+			expect(@collector.board_admins(@demo).count).to eq(1)
+		end
+
+		it "returns empty if user is not client admin on isn't a client admin on the board" do
+			@user.update_attribute(:is_client_admin, true)
+			@bm.update_attribute(:is_client_admin, false)
+			@bm.update_attribute(:is_current, false)
+			@collector = ActiveBoardCollector.new
+			expect(@collector.board_admins(@demo).count).to eq(0)
+		end
+
+		it "returns empty if user is not client admin on isn't a client admin on the board" do
+			@bm.update_attribute(:is_client_admin, false)
+			@collector = ActiveBoardCollector.new
+			expect(@collector.board_admins(@demo).count).to eq(0)
+		end
+
+	end
 
 	describe "#send_mail_notifications" do
 		it "sends notifications" do
@@ -77,7 +92,6 @@ describe ActiveBoardCollector do
 			@collector.send_mail_notifications
 		end
 	end
-
 
 	def setup_viewing user, tile
 		FactoryGirl.create(:tile_viewing, tile: tile, user: user, created_at: 1.day.ago)
@@ -92,11 +106,12 @@ describe ActiveBoardCollector do
 		bm.user = user
 		bm.is_client_admin=is_admin;
 		bm.save
+		bm
 	end
 
-	def setup_user_board
+	def setup_user_board user_is_client_admin=false
 		demo=FactoryGirl.create(:demo)
-		user=FactoryGirl.create(:user, demo: demo)
+		user=FactoryGirl.create(:user, demo: demo, is_client_admin: user_is_client_admin)
 		tile1=create_demo_tile(demo)
 		tile2=create_demo_tile(demo)
 		return demo, user, tile1, tile2
