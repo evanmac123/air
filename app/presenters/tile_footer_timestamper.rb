4f89557b6abd51a504e60cd55bf471a792a0afc4
avbrychak <a.v.brychak@gmail.com>
2015-06-02 14:29:08 -0400

    rewrite tile footer timestamper

--- app/presenters/tile_footer_timestamper.rb
+++ app/presenters/tile_footer_timestamper.rb
@@ -2,58 +2,115 @@ module TileFooterTimestamper
   include ActionView::Helpers::DateHelper
 
   def footer_timestamp
-    if tile.status == Tile::DRAFT
-      spanned_text(
-        "Created: " + tile.created_at.strftime('%-m/%-d/%Y'),
-          "tile-created-at")
-    elsif tile.status == Tile::USER_SUBMITTED || tile.status == Tile::IGNORED
-      spanned_text(
-        "Submitted: " + tile.created_at.strftime('%-m/%-d/%Y'),
-          "tile-submitted-at")
-    elsif tile.activated_at.nil?
-      spanned_text("Never activated")
-    elsif tile.status == Tile::ARCHIVE
-      if @format == :plain
-        [
-          active_time,
-          "Deactivated: " + tile.archived_at.strftime('%-m/%-d/%Y')
-        ].join(' ').html_safe
-      else
-        active_time
-      end
+    if @format == :html
+      timestamp_html
     else
-      if @format == :plain
-        [
-          active_time,
-          "Since: " + tile.activated_at.strftime('%-m/%-d/%Y')
-        ].join(' ')
-      else
-        active_time
-      end
+      timestamp_plain
     end
   end
 
   protected
 
-  def spanned_text(text, span_class = nil)
-    return text unless @format == :html
+  def timestamp_plain
+    case tile_type
+    when :active
+      [
+        spanned_text(span_params),
+        "Since: " + time_in_format(tile.archived_at)
+      ].join(' ')
+    when :archive
+      [
+        spanned_text(span_params),
+        "Deactivated: " + time_in_format(tile.activated_at)
+      ].join(' ')
+    when :archive_never_activated
+      spanned_text(span_params)
+    else
+      ''
+    end
+  end
+
+  def timestamp_html
+    if type? *html_allowed_types
+      spanned_text span_params
+    else
+      ''
+    end
+  end
+
+  def tile_type
+    if type?(:archive) && tile.activated_at.nil?
+      :archive_never_activated
+    else
+      type
+    end
+  end
+
+  def html_allowed_types
+    [:draft, :active, :archive, :user_submitted, :ignored]
+  end
+
+  def span_params
+    case tile_type
+    when :draft
+      {
+        title: "Created: ",
+        time: time_in_format(tile.created_at),
+        class: "tile-created-at"
+      }
+    when :active
+      {
+        title: "Active: ",
+        time: distance_of_time_in_words(tile.activated_at, Time.now),
+        class: 'tile-active-time'
+      }
+    when :archive
+      {
+        title: "Active: ",
+        time: distance_of_time_in_words(tile.activated_at, tile.archived_at),
+        class: 'tile-active-time'
+      }
+    when :archive_never_activated
+      {
+        title: "Never activated",
+        time: "",
+        class: nil
+      }
+    when :user_submitted
+      {
+        title: "Submitted: ",
+        time: time_in_format(tile.created_at),
+        class: "tile-submitted-at"
+      }
+    when :ignored
+      {
+        title: "Submitted: ",
+        time: time_in_format(tile.created_at),
+        class: "tile-submitted-at"
+      }
+    else
+      {
+        title: "",
+        time: "",
+        class: nil
+      }
+    end
+  end
+
+  def time_in_format(time)
+    time.strftime('%-m/%-d/%Y')
+  end
+
+  def spanned_text(params)
+    title, time, span_class = params.values_at(:title, :time, :class)
+    text = title + time
+
+    return text if @format == :plain
+
     if span_class
       ["<span class='#{span_class}'>", text, "</span>"].join.html_safe
     else
       "<span>#{text}</span>".html_safe
     end
   end
-
-  def active_time
-    time_baseline = if tile.status == Tile::ARCHIVE
-                      tile.archived_at
-                    else
-                      Time.now
-                    end
-    spanned_text(
-      "Active: " + distance_of_time_in_words(tile.activated_at, time_baseline), 
-      'tile-active-time')
-  end
-
-  attr_reader :tile
 end
