095cb440ab1d0c4322f0d14bea5a4be2e63a1e62
hraynaud <herby.raynaud@gmail.com>
2015-06-03 13:32:21 -0400

    Creates new weekly activity report present subclass of TilesDigestMailBasePresenter

---
+++ app/presenters/digest/tiles_digest_mail_activity_presenter.rb
@@ -0,0 +1,50 @@
+class TilesDigestMailActivityPresenter < TilesDigestMailBasePresenter
+  ACTIVITY_EMAIL = "weekly_activity_v".freeze
+  ACTIVITY_DIGEST_HEADING = "Weekly Activity Report".freeze
+
+  include ClientAdmin::TilesHelper
+  include Rails.application.routes.url_helpers
+
+	def initialize(user, demo, beg_date, end_date)
+    @user = user
+    @demo = demo
+		@beg_date = beg_date
+		@end_date = end_date
+  end
+
+  def from_email
+    if @custom_from.present?
+      @custom_from
+    else
+      @demo.reply_email_address
+    end
+  end
+
+  def email_heading
+    @custom_headline.present? ? @custom_headline : standard_email_heading
+  end
+
+	def pretty_date_range
+		"#{@beg_date.strftime('%A, %B %d')} to #{@end_date.strftime('%A, %B %d')}"
+	end
+
+  def standard_email_heading
+    ACTIVITY_DIGEST_HEADING
+  end
+
+  def digest_email_heading
+    ACTIVITY_DIGEST_HEADING
+  end
+
+  def title
+    join_demo_copy_or_digest_email_heading(@is_new_invite)
+  end
+
+  def email_type
+    ACTIVITY_EMAIL
+  end
+
+  def general_site_url
+    email_site_link(@user, @demo, is_preview, email_type)
+  end
+end
