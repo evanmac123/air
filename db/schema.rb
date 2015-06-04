bfbe3f3a02cd536f4327ed706b19d6dfd1c41ca1
hraynaud <herby.raynaud@gmail.com>
2015-06-03 21:33:08 -0400

    Adds send weekly activity report field to board memberships

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150603181535) do
+ActiveRecord::Schema.define(:version => 20150604010815) do
 
   create_table "acts", :force => true do |t|
     t.integer  "user_id"
@@ -120,6 +120,7 @@ ActiveRecord::Schema.define(:version => 20150603181535) do
     t.boolean  "digest_muted",                     :default => false
     t.boolean  "followup_muted",                   :default => false
     t.boolean  "allowed_to_make_tile_suggestions", :default => false, :null => false
+    t.boolean  "send_weekly_activity_report",      :default => true
   end
 
   add_index "board_memberships", ["demo_id"], :name => "index_board_memberships_on_demo_id"
