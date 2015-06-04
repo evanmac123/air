4833f3f9110c8d42d3de7ab74f5b68b1948f9bcd
avbrychak <a.v.brychak@gmail.com>
2015-06-04 15:22:36 -0400

    add tests for SB intro

--- spec/support/acceptance/suggestion_box.rb
+++ spec/support/acceptance/suggestion_box.rb
@@ -62,4 +62,65 @@ module SuggestionBox
   def show_more_button
     page.find(".all_draft")
   end
+  #
+  # => Manage Access Modal
+  #
+  def manage_access_link
+    page.find(".manage_access")
+  end
+
+  def all_users_switcher_on
+    page.find("#suggestion_switcher_on")
+  end
+
+  def specific_users_switcher_on
+    page.find("#suggestion_switcher_off")
+  end
+
+  def user_rows
+    page.all(".allowed_to_suggest_user")
+  end
+
+  def save_button
+    page.find("#save_suggestions_access")
+  end
+
+  def autocomplete_input
+    page.find("#name_substring")
+  end
+
+  def fill_in_username_autocomplete(name)
+    autocomplete_input.set(name)
+    page.execute_script("$('#name_substring').autocomplete('search')")
+    wait_for_ajax
+  end
+
+  def username_autocomplete_results_click num
+    selector = "#name_autocomplete_target li a:first"
+    page.execute_script %Q{ $('#{selector}').eq(#{num}).trigger('mouseenter').click() }
+  end
+
+  def autocomplete_result_names
+    page.all("#name_autocomplete_target li a").map(&:text)
+  end
+
+  def warning_modal_mess
+    "Are you sure you want to close this form? You haven't saved changes."
+  end
+
+  def suggestion_box_header
+    "Add people to suggestion box"
+  end
+
+  def suggestion_box_cancel
+    page.find("#cancel_suggestions_access")
+  end
+
+  def warning_cancel
+    page.find("#suggestions_access_warning_modal .cancel")
+  end
+
+  def warning_confirm
+    page.find("#suggestions_access_warning_modal .confirm")
+  end
 end
