12b0eb9f4b880a4ef3a1098cc5ffb221617468b8
avbrychak <a.v.brychak@gmail.com>
2015-06-08 14:28:37 -0400

    add mailer to tell about suggested tiles

---
+++ app/mailers/suggested_tile_to_review_mailer.rb
@@ -0,0 +1,32 @@
+class SuggestedTileToReviewMailer < ActionMailer::Base
+  has_delay_mail
+  layout "mailer"
+  helper :email
+
+  def notify_all tile_sender_id, _demo_id
+    user  = User.find tile_sender_id
+    client_admins_id = User.joins{board_memberships}
+                        .where do
+                          (board_memberships.is_client_admin == true) &
+                          (board_memberships.demo_id == _demo_id)
+                        end
+                        .pluck(:id)
+
+    client_admins_id.each do |client_admin_id|
+      self.delay.notify_one client_admin_id, _demo_id, user.name, user.email
+    end
+  end
+
+  def notify_one client_admin_id, demo_id, tile_sender_name, tile_sender_email
+    @user  = User.find client_admin_id
+    return nil unless @user.email.present?
+
+    @demo = Demo.find  demo_id
+    @name, @email = tile_sender_name, tile_sender_email
+    @link = client_admin_tiles_url(show_suggestion_box: true, suggested_tile_intro: true)
+
+    mail  from:     "suggestion_box@airbo.com",
+          to:       @user.email,
+          subject:  "New Tile Suggested To Review"
+  end
+end
