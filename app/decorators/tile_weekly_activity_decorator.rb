4f131333dbb4dd6ddb3d9d4700f6f237b8e76c52
Phil Darnowsky <phil@darnowsky.com>
2015-06-10 15:16:30 -0400

    SingleTilePresenter now explicitly initialized with flag to show admin buttons (or not)

--- app/decorators/tile_weekly_activity_decorator.rb
+++ app/decorators/tile_weekly_activity_decorator.rb
@@ -1,5 +1,5 @@
 class TileWeeklyActivityDecorator < TileDigestDecorator
 	def email_site_link
-		h.client_admin_tile_tile_completions_url(SingleTilePresenter.new(object, :html))
+		h.client_admin_tile_tile_completions_url(object)
 	end
 end
