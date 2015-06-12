07b4357a061bb0f890d52911cbeee191886e23d0
Phil Darnowsky <phil@darnowsky.com>
2015-06-12 17:05:26 -0400

    Copy inserted into menu for tiles in accepted states

---
+++ app/presenters/tile_preview/text/no_unsubmit.rb
@@ -0,0 +1,13 @@
+module TilePreview
+  module Text
+    class NoUnsubmit
+      def to_partial_path
+        'client_admin/tiles/tile_preview/text/no_unsubmit'
+      end
+
+      def locals
+        {}
+      end
+    end
+  end
+end
