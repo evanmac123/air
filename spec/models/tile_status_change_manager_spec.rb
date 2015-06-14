4c9a392b2ad7a25af6583adb2ed6689aee37145b
hraynaud <herby.raynaud@gmail.com>
2015-06-14 19:20:22 -0400

    Finishes tile_status_manager and tile_status_mailer spec

--- spec/models/tile_status_change_manager_spec.rb
+++ spec/models/tile_status_change_manager_spec.rb
@@ -1,8 +1,25 @@
+require 'spec_helper'
+
 describe TileStatusChangeManager do
+	let(:user){FactoryGirl.create(:user)}
+  let(:demo) { FactoryGirl.create :demo }
+	let(:tile) { FactoryGirl.create :multiple_choice_tile, status: Tile::USER_SUBMITTED, demo: demo, original_creator: user }
 
- 
+	describe "#process" do
+		it "sends email if status is draft" do
+			tile.status = Tile::DRAFT 
+			mgr = TileStatusChangeManager.new(tile)
+			mgr.expects(:send_acceptance_email)
+			mgr.process
+		end
 
-
+		it "sends email if status is draft" do
+			tile.status = Tile::ACTIVE 
+			mgr = TileStatusChangeManager.new(tile)
+			mgr.expects(:send_acceptance_email).never
+			mgr.process
+		end
+	end
 
 end
 
