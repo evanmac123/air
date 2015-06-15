17e41fb32c64d87c52adb0772ac5a2875c3d13de
hraynaud <herby.raynaud@gmail.com>
2015-06-15 09:33:20 -0400

    Refactors test for whether tile was user submitted
    Refactors out duplication in send messages

--- app/services/tile_status_change_manager.rb
+++ app/services/tile_status_change_manager.rb
@@ -6,8 +6,13 @@ class TileStatusChangeManager
 
 
   def process
-		if tile_approved?
+		case 
+		when tile_approved?
 			send_acceptance_email
+		when tile_posted?
+			send_posted_email
+		when tile_archived?
+			send_archived_email
 		end
 	end
     
@@ -15,12 +20,35 @@ class TileStatusChangeManager
 
 
   def send_acceptance_email
-		SuggestedTileStatusMailer.delay.accepted(@tile.demo.id,@tile.original_creator.id,@tile.id)
+		send_mail(:accepted)
 	end
 
+  def send_posted_email
+		send_mail(:posted)
+	end
+
+  def send_archived_email
+		send_mail(:archived)
+	end	
+
+	def send_mail msg_type
+		SuggestedTileStatusMailer.delay.send(:msg_type, @tile.demo.id,@tile.original_creator.id,@tile.id)
+	end
 
 	def tile_approved?
-		@tile.original_creator && [Tile::USER_SUBMITTED, Tile::DRAFT] == @tile.changes[:status]
+		is_eligible_with_state_change? [Tile::USER_SUBMITTED, Tile::DRAFT] 
+	end
+
+	def tile_posted?
+		is_eligible_with_state_change? [Tile::DRAFT, Tile::ACTIVE]
+	end
+
+	def tile_archived?
+		is_eligible_with_state_change? [Tile::ACTIVE, Tile::ARCHIVE]
+	end
+
+	def is_eligible_with_state_change? state_change
+		@tile.current_version_is_user_submitted? && state_change == @tile.changes[:status]
 	end
 
 end
