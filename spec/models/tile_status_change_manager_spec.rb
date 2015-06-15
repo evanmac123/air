ff2723f8a09bf595e87fa390b6a17d79e5addf8b
hraynaud <herby.raynaud@gmail.com>
2015-06-14 20:15:04 -0400

    Refactors to remove unneeded extra instance vars
    Checks for presence of original creator

--- spec/models/tile_status_change_manager_spec.rb
+++ spec/models/tile_status_change_manager_spec.rb
@@ -3,22 +3,48 @@ require 'spec_helper'
 describe TileStatusChangeManager do
 	let(:user){FactoryGirl.create(:user)}
   let(:demo) { FactoryGirl.create :demo }
-	let(:tile) { FactoryGirl.create :multiple_choice_tile, status: Tile::USER_SUBMITTED, demo: demo, original_creator: user }
-
 	describe "#process" do
-		it "sends email if status is draft" do
-			tile.status = Tile::DRAFT 
-			mgr = TileStatusChangeManager.new(tile)
-			mgr.expects(:send_acceptance_email)
-			mgr.process
+		context "original status user_submitted" do
+			let(:tile) { FactoryGirl.create :multiple_choice_tile, status: Tile::USER_SUBMITTED, demo: demo, original_creator: user }
+
+			it "sends email if status is change to draft" do
+				tile.status = Tile::DRAFT 
+				mgr = TileStatusChangeManager.new(tile)
+				mgr.expects(:send_acceptance_email)
+				mgr.process
+			end
+
+			it "doesn't send email if status has not changed to draft" do
+				tile.status = Tile::ACTIVE 
+				mgr = TileStatusChangeManager.new(tile)
+				mgr.expects(:send_acceptance_email).never
+				mgr.process
+			end
+		end
+		context "original status not user_submitted" do
+			let(:tile) { FactoryGirl.create :multiple_choice_tile, status: Tile::USER_DRAFT, demo: demo, original_creator: user }
+
+			it "it doesn't sends emails" do
+				tile.status = Tile::DRAFT 
+				mgr = TileStatusChangeManager.new(tile)
+				mgr.expects(:send_acceptance_email).never
+				mgr.process
+			end
+
 		end
 
-		it "sends email if status is draft" do
-			tile.status = Tile::ACTIVE 
-			mgr = TileStatusChangeManager.new(tile)
-			mgr.expects(:send_acceptance_email).never
-			mgr.process
+  		context "has no original creator" do
+			let(:tile) { FactoryGirl.create :multiple_choice_tile, status: Tile::USER_SUBMITTED, demo: demo, original_creator: nil }
+
+			it "it doesn't sends emails" do
+				tile.status = Tile::DRAFT 
+				mgr = TileStatusChangeManager.new(tile)
+				mgr.expects(:send_acceptance_email).never
+				mgr.process
+			end
+
 		end
+
 	end
 
 end
