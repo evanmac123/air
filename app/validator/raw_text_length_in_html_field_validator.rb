c7465e848b92e55ad0cdf951839301f1f150c390
avbrychak <a.v.brychak@gmail.com>
2015-06-17 12:44:53 -0400

    validate text from rich text editor with Nokogiri. much better then Sanitize

--- app/validator/raw_text_length_in_html_field_validator.rb
+++ app/validator/raw_text_length_in_html_field_validator.rb
@@ -2,13 +2,9 @@ class RawTextLengthInHTMLFieldValidator < ActiveModel::Validator
   def validate(record)
     field, maximum, message = options.values_at(:field, :maximum, :message)
     value = record[field]
-
-    sanitized_value = Sanitize.fragment(value).
-                               gsub(/(\u00A0)+/, ''). # remove all non-breaking spaces 
-                                                      # that Sanitize may leave after removed tags
-                               gsub(/\s+/, ' ').
-                               strip
-    if sanitized_value.length > maximum
+    raw_text = Nokogiri::HTML::Document.parse(value)
+    
+    if raw_text.length > maximum
       record.errors.add field, message
     end
   end
