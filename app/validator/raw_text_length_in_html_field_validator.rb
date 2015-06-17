7049701a12233fd482bc367105b4fca8ffa8279b
avbrychak <a.v.brychak@gmail.com>
2015-06-17 14:13:01 -0400

    fix nokogiri

--- app/validator/raw_text_length_in_html_field_validator.rb
+++ app/validator/raw_text_length_in_html_field_validator.rb
@@ -2,8 +2,8 @@ class RawTextLengthInHTMLFieldValidator < ActiveModel::Validator
   def validate(record)
     field, maximum, message = options.values_at(:field, :maximum, :message)
     value = record[field]
-    raw_text = Nokogiri::HTML::Document.parse(value)
-    
+    raw_text = Nokogiri::HTML::Document.parse(value).text
+
     if raw_text.length > maximum
       record.errors.add field, message
     end
