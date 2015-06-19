a3aeed3b116c98a5bff5faef607b3b60a34663a1
avbrychak <a.v.brychak@gmail.com>
2015-06-19 13:21:06 -0400

    remove old test

--- spec/acceptance/user/suggestion_box/creates_draft_tile_spec.rb
+++ spec/acceptance/user/suggestion_box/creates_draft_tile_spec.rb
@@ -3,22 +3,11 @@ require 'acceptance/acceptance_helper'
 feature 'Creates draft tile' do
   let(:user) {FactoryGirl.create(:user, allowed_to_make_tile_suggestions: true)}
 
-  it "should show the user's own drafts, but nobody else's, on an index page" do
-    tiles = FactoryGirl.create_list(:tile, 2, :user_drafted, creator: user)
-    # And a few that _shouldn't_ appear
-    FactoryGirl.create_list(:tile, 3, :user_drafted)
-    visit suggested_tiles_path(as: user)
-    draft_tab.should have_num_tiles(2)
-    tiles.each do |tile|
-      expect_content tile.headline
-    end
-  end
-
   it "should send ping on index page" do
     visit suggested_tiles_path(as: user)
     expect_ping 'Suggestion Box', {user_action: "Suggestion Box Opened"}, user
   end
-  
+
   it "should link properly from the tile to the appropriate edit page" do
     visit suggested_tiles_path(as: user)
     click_new_tile_placeholder
