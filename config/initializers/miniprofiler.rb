c10786b0af855ab636bccd3f4c5cdf6f9afe8b5f
hraynaud <herby.raynaud@gmail.com>
2015-07-08 18:57:59 -0400

    Adds redistore_for mini profiler

--- config/initializers/miniprofiler.rb
+++ config/initializers/miniprofiler.rb
@@ -4,4 +4,7 @@ Rack::MiniProfilerRails.initialize!(Rails.application)
 
 Rails.application.middleware.delete(Rack::MiniProfiler)
 Rails.application.middleware.insert_after(Rack::Deflater, Rack::MiniProfiler)
+uri = URI.parse(ENV["REDISTOGO_URL"])
+Rack::MiniProfiler.config.storage_options = { :host => uri.host, :port => uri.port,    :password => uri.password }
+Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
 PROFILABLE_USERS = ENV['PROFILABLE_USERS'].try(:split, ",") || []
