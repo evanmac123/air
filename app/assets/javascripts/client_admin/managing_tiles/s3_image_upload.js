95383476c2492cb8c98b14127dc625af73dfdb43
hraynaud <herby.raynaud@gmail.com>
2015-07-27 18:50:18 -0400

    Removes original implementation of image selection with better more modular code

--- app/assets/javascripts/client_admin/managing_tiles/s3_image_upload.js
+++ app/assets/javascripts/client_admin/managing_tiles/s3_image_upload.js
@@ -1,97 +1,71 @@
-$(function() {
+var Airbo = window.Airbo || {}
+Airbo.DirectToS3ImageUploader = (function(){ 
 
-    function imagePreview(data){
-      var node, imagePlaceholder, index = data.index ||0, file = data.files[index];
+  function defaultBuiltInPreview(file){
+    return file.preview;
+  }
 
-      data.context = $(".img-area");
-      imagePlaceholder =data.context.find(".image-placeholder"); 
-      node = data.context.find(".preview");
-      if (file.preview) {
-        //Built-in image preview (scales images)
-        node.prepend('<br>').html(file.preview);
-      }else{
-        //Manual image preview (no scaling)
-        //imagePlaceholder.hide();
+  function imagePreview(data){
+    var node, imagePlaceholder, index = data.index || 0, file = data.files[index];
+    data.context = $("#image_uploader");
+    node = data.context.find(".image_preview");
 
-        loadImage( file, function (img) { 
-          node.html(img);
-        },{canvas: true} );
-      }
-
-      if (file.error) {
-        node
-        .append('<br>')
-        .append($('<span class="text-danger"/>').text(file.error));
-      }
-      //if (index + 1 === data.files.length) {
-        //data.context.find('button.upload')
-        //.text('Upload')
-        //.prop('disabled', !!data.files.error);
-      //}
+    if (defaultBuiltInPreview(file)) {
+      //Built-in image preview (scales images)
+      node.html(file.preview);
+    }else{
+      //Manually perform our image preview (no scaling)
+      loadImage(file, function (img) { 
+        setPreviewImage(img.toDataURL());
+        showShadows();
+      },{canvas: true} );
     }
 
+    if (file.error) {
+      node
+      .append('<br>')
+      .append($('<span class="text-danger"/>').text(file.error));
+    }
+  }
 
   function initChooseFileDelegator(){
-    uploadForm= $("#fileupload");
-    $('body').on('click', "#clickDelegator", function(event){
+    var uploadForm= $("#fileupload");
+
+    $('body').on('click', ".upload_image", function(event){
       event.preventDefault();
       uploadForm.find('input[type=file]').click();
       return false;
     });
   }
 
-  setPreviewImage = function(imageUrl) {
-    return document.getElementById('upload_preview').src = imageUrl;
+  function setPreviewImage(imageUrl) {
+    $('#upload_preview').attr("src", imageUrl);
   };
 
-  isIE = function() {
-    var myNav;
-    myNav = navigator.userAgent.toLowerCase();
-    if (myNav.indexOf('msie') !== -1) {
-      return parseInt(myNav.split('msie')[1]);
-    } else {
-      return false;
-    }
-  };
-
-  getAttachedFile = function() {
-    return document.getElementById("media_file").files[0];
-  };
-
-  //showImgInPreview = function(file) {
-   //$(".image_preview").append(file.preview);
-   ////showShadows();
-  //};
-
-  showImgInPreview = function(imgFile) {
-    var oFReader;
-    oFReader = new FileReader;
-    oFReader.readAsDataURL(imgFile);
-    return oFReader.onload = function(oFREvent) {
-      return setPreviewImage(oFREvent.target.result);
-    };
-  };
-
-showShadows = function() {
+  function showShadows() {
     return $('.image_preview').removeClass('show_placeholder').addClass('show_shadows');
   };
 
-   $('#fileupload').fileupload({
-     replaceFileInput: false
-   }).on('fileuploadadd',  function(e, data) {
-      var file, types;
-      types = /(\.|\/)(gif|jpe?g|png)$/i;
-      file = data.files[0];
-      if (types.test(file.type) || types.test(file.name)) {
-        return data.submit();
-      } else {
-        return alert(file.name + " is not a gif, jpeg, or png image file");
+
+  function initFileUploader() {
+
+    $('#fileupload').fileupload(
+      {
+        replaceFileInput: false,
+        disableImagePreview: true
       }
+    ).on('fileuploadadd',  function(e, data) {
+      var file = data.files[0],
+          types = /(\.|\/)(gif|jpe?g|png|bmp)$/i;
+
+        if (types.test(file.type) || types.test(file.name)) {
+          return data.submit();
+        } else {
+          return alert(file.name + " is not a gif, jpeg, or png image file");
+        }
+
     }).on('fileuploadprocessalways',  function (e, data) {
-      attachedFile = $("#media_file")[0].files[0];
-      showImgInPreview(attachedFile);
-      showShadows();
-      //imagePreview(data);
+      imagePreview(data);
     }).on('fileuploadprogress', function(e, data) {
       var progress;
       if (data.context) {
@@ -103,16 +77,27 @@ showShadows = function() {
       file = data.files[0];
       domain = $('#fileupload').attr('action');
       path = $('#fileupload input[name=key]').val().replace('${filename}', file.name);
-      to = $('#fileupload').data('post');
-      content = {};
-      content[$('#fileupload').data('as')] = domain + path;
+      debugger
       //$.post(to, content);
       //if (data.context) {
-        //return data.context.remove();
+      //return data.context.remove();
       //}
     }).on('fileuploadfail',  function(e, data) {
-      alert(data.files[0].name + " failed to upload.");
-      console.log("Upload failed:");
-      return console.log(data);
+      console.log(data.files[0].name + " failed to upload.");
     });
+  }
+
+  function init(){
+    initChooseFileDelegator();
+    initFileUploader();
+  }
+
+  return {
+    init: init
+  };
+
+}());
+
+$(function() {
+Airbo.DirectToS3ImageUploader.init();
 });
---
+++ app/assets/javascripts/internal/tile_builder_form/image_preview_utils.js
@@ -0,0 +1,184 @@
+var Airbo = window.Airbo || {};
+
+function isIE() {
+  var myNav;
+  myNav = navigator.userAgent.toLowerCase();
+  if (myNav.indexOf('msie') !== -1) {
+    return parseInt(myNav.split('msie')[1]);
+  } else {
+    return false;
+  }
+};
+
+/************************************************
+ *
+ * Creates a wrapper around the image previewer
+ * and the image library modules
+ *
+ * **********************************************/
+
+Airbo.TileImagesMgr = (function(){
+  var previewer, 
+  library,
+  noImage = $('#no_image'), 
+    imageContainer = $('#image_container');
+
+  function updateHiddenImageFields(caller) {
+    imageContainer.val('');
+    noImage.val('');
+
+    if (caller === 'imageUploader') {
+
+    } else if (caller === 'clearImage') {
+      noImage.val('true');
+    } else if (caller === 'imageFromLibrary') {
+      library.setSelectedImageId();
+    }
+  };
+
+  function clearImage(){
+    updateHiddenImageFields('clearImage');
+    previewer.clearPreviewImage();
+    library.clearSelectedImage();
+  }
+
+  function showImagePreview(imgUrl){
+    previewer.setPreviewImage(imgUrl);
+  }
+
+  function initClearImage(){
+    $('.clear_image').click(function(event) {
+      clearImage()
+    });
+  }
+
+  function init(){
+    initClearImage();
+    previewer = Airbo.ImagePreviewer.init(this)
+    library = Airbo.ImageLibrary.init(this)
+  }
+
+  return {
+    init: init,
+    showImagePreview: showImagePreview,
+    updateHiddenImageFields: updateHiddenImageFields,
+  };
+
+}());
+
+
+/************************************************
+ *
+ * Provides the functionality for 
+ * interacting with the image library 
+ *
+ * **********************************************/
+
+Airbo.ImageLibrary = (function(){
+  var imageMgr,
+      imageFromLibraryField = $("#image_from_library"),
+      imageFromLibrarySelector = ".tile_image_block:not(.upload_image)",
+      imageFromLibrary=  $(imageFromLibrarySelector);
+
+  function selectedImageFromLibrary() {
+    return imageFromLibrary.filter(".selected");
+  }
+ 
+  function setSelectedState(imageBlock) {
+    imageFromLibrary.removeClass('selected');
+    imageBlock.addClass('selected');
+  }
+
+  function clearSelectedImage(){
+    imageFromLibraryField.val('');
+  }
+
+  function setSelectedImageId(){
+    imageFromLibraryField.val(selectedImageFromLibrary().data('tile-image-id'));
+  }
+
+  function select(imageBlock) {
+    imageMgr.showImagePreview(imageBlock.data('image-url'));
+    imageMgr.updateHiddenImageFields('imageFromLibrary');
+    setSelectedState(imageBlock);
+  };
+
+  function selectImageFromLibrary() {
+    var id = parseInt(imageFromLibraryField.val());
+    if (selectedImageFromLibrary().length === 0 && id > 0) {
+       setSelectedState(imageFromLibrary.filter("[data-tile-image-id=" + id + "]"));
+    }
+  };
+
+  function initImageChooser(){
+    $("body").on("click", imageFromLibrarySelector, function() {
+      select($(this));
+    });
+  }
+
+  function init(mgr){
+    imageMgr = mgr;
+    initImageChooser();
+    return this;
+  };
+
+  return {
+    setSelectedImageId: setSelectedImageId,
+    clearSelectedImage: clearSelectedImage,
+    init: init
+  }
+}());
+
+
+/************************************************
+ *
+ * Provides the image preview functionality for 
+ * both images selected from the library and 
+ * images that are uploaded by the user
+ *
+ * **********************************************/
+
+Airbo.ImagePreviewer = (function(){
+  var imageMgr;
+
+  function clearPreviewImage(){
+    showPlaceholder();
+    removeImageCredit();
+  }
+
+  function showPlaceholder() {
+    $('.image_preview').removeClass('show_shadows').addClass('show_placeholder');
+  };
+
+  function removeImageCredit() {
+     $('.image_credit_view').text('').trigger('keyup').trigger('focusout');
+  };
+
+  function showShadows() {
+     $('.image_preview').removeClass('show_placeholder').addClass('show_shadows');
+  };
+
+  function setPreviewImage(imageUrl) {
+    showPlaceholder();
+    showShadows();
+    $('#upload_preview').attr("src", imageUrl);
+  };
+
+  function init(mgr){
+    imageMgr = mgr
+    return this;
+  }
+
+  return {
+    init: init,
+    setPreviewImage: setPreviewImage,
+    clearPreviewImage: clearPreviewImage 
+  };
+
+})();
+
+$(function() {
+  //TODO consider not instantiating Airbo.ImagePreviewer and Airb.ImageLibrary
+  //unless neeed
+  Airbo.TileImagesMgr.init();
+});
