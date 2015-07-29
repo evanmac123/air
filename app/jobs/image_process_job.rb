efa81e0f6791e1d6a41b36e563ec58e5df8440dc
hraynaud <herby.raynaud@gmail.com>
2015-07-29 15:52:10 -0400

    Change job to save Id instead of tile

--- app/jobs/image_process_job.rb
+++ app/jobs/image_process_job.rb
@@ -3,14 +3,15 @@ require 'aws-sdk'
 class ImageProcessJob
 
   def initialize(tile_id)
-    @tile = Tile.find(tile_id)
+    @tile_id = tile_id
   end
 
 
   #TODO set obj in initializer
   def perform
-    @tile.thumbnail = URI.parse(@tile.remote_media_url)
-    @tile.save
+    tile Tile.find(@tile_id)
+    tile.thumbnail = URI.parse(tile.remote_media_url)
+    tile.save
   end
   handle_asynchronously :perform
 end
