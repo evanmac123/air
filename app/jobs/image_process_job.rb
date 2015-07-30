153eaa9c6bf9495f663635c39e32c8ba20762772
hraynaud <herby.raynaud@gmail.com>
2015-07-30 15:01:19 -0400

    Process both image and thumbnail attachments

--- app/jobs/image_process_job.rb
+++ app/jobs/image_process_job.rb
@@ -6,12 +6,11 @@ class ImageProcessJob
     @tile_id = tile_id
   end
 
-
-  #TODO set obj in initializer
   def perform
     tile = Tile.find(@tile_id)
-    tile.thumbnail = URI.parse(tile.remote_media_url)
+    tile.image = tile.thumbnail = URI.parse(tile.remote_media_url)
     tile.save
   end
+
   handle_asynchronously :perform
 end
