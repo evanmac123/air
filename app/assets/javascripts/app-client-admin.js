295fb81c3118486d1e5b3f381f3f1157acae136f
hraynaud <herby.raynaud@gmail.com>
2015-08-05 16:13:59 -0400

    Adds global application js to hold global application container and js utilities

--- app/assets/javascripts/app-client-admin.js
+++ app/assets/javascripts/app-client-admin.js
@@ -46,17 +46,6 @@
 
 var Airbo = window.Airbo || {};
 
-Airbo.Utils = {
-
-  Pages: {
-    TILE_BUILDER: "#new_tile_builder_form"
-  },
-
-  isAtPage: function(identifier){
-    return $(identifier).length > 0
-  },
-}
-
 $(document).ready(function() {
   $('.client_admin-users, .client_admins-show').foundation();
 });
---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,11 @@
+var Airbo = window.Airbo || {};
+Airbo.Utils = {
+
+  Pages: {
+    TILE_BUILDER: "#new_tile_builder_form"
+  },
+
+  isAtPage: function(identifier){
+    return $(identifier).length > 0
+  },
+}
