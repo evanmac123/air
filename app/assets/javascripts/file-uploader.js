3f217d94589dc17ac1d1e2083213a0c600814d44
hraynaud <herby.raynaud@gmail.com>
2015-08-07 18:56:54 -0400

    Adds separate js group for file uploader

---
+++ app/assets/javascripts/file-uploader.js
@@ -0,0 +1,9 @@
+//= require jquery-fileupload/vendor/jquery.ui.widget
+//= require jquery-fileupload/vendor/load-image.all.min
+//= require jquery-fileupload/vendor/canvas-to-blob
+//= require jquery-fileupload/jquery.iframe-transport
+//= require jquery-fileupload/jquery.fileupload
+//= require jquery-fileupload/jquery.fileupload-process
+//= require jquery-fileupload/jquery.fileupload-image
+//= require jquery-fileupload/jquery.fileupload-validate
+//= require ./client_admin/managing_tiles/s3_image_upload
