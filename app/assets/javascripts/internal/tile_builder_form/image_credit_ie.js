126fda7d1a642d8b163296d579f63e7c205d3ee5
hraynaud <herby.raynaud@gmail.com>
2015-08-07 17:39:25 -0400

    Refactors Globl IE image credit js into modular component

--- app/assets/javascripts/internal/tile_builder_form/image_credit_ie.js
+++ app/assets/javascripts/internal/tile_builder_form/image_credit_ie.js
@@ -1,47 +1,43 @@
-var imageCreditInput, imageCreditInputSelector, imageCreditView, maxLength, normalizedImageCreditInput, updateImageCreditView;
+var Airbo = window.Airbo ||{}
 
-maxLength = function() {
-  return 50;
-};
 
-imageCreditView = function() {
-  return $('.image_credit_view');
-};
+Airbo.ImageCreditIE = (function(){
+  var maxLength = 50,
+    imageCreditView = $('.image_credit_view'),
+    imageCreditInputSelector =   '#tile_builder_form_image_credit',
+    imageCreditInput =$(imageCreditInputSelector);
 
-imageCreditInputSelector = function() {
-  return '#tile_builder_form_image_credit';
-};
-
-imageCreditInput = function() {
-  return $(imageCreditInputSelector());
-};
-
-normalizedImageCreditInput = function() {
-  var inputted_text;
-  inputted_text = imageCreditInput().val();
-  if (inputted_text !== '') {
-    if (inputted_text.length > maxLength()) {
-      return inputted_text.substring(0, maxLength()) + '...';
+  function normalizedImageCreditInput() {
+    var inputted_text;
+    inputted_text = imageCreditInput().val();
+    if (inputted_text !== '') {
+      if (inputted_text.length > maxLength()) {
+        return inputted_text.substring(0, maxLength()) + '...';
+      } else {
+        return inputted_text;
+      }
     } else {
-      return inputted_text;
+      return 'Add Image Credit';
     }
-  } else {
-    return 'Add Image Credit';
   }
-};
 
-updateImageCreditView = function() {
-  var text;
-  text = normalizedImageCreditInput();
-  return imageCreditView().html(text);
-};
+  function updateImageCreditView() {
+    var text;
+    text = normalizedImageCreditInput();
+    return imageCreditView().html(text);
+  }
 
-window.imageCreditIE = function() {
-  $(document).ready(function() {
+  function init(){
     updateImageCreditView();
-    return addCharacterCounterFor(imageCreditInputSelector());
-  });
-  return imageCreditInput().bind('input propertychange', function() {
-    return updateImageCreditView();
-  });
-};
+    addCharacterCounterFor(imageCreditInputSelector);
+    imageCreditInput.bind('input propertychange', function() {
+      updateImageCreditView();
+    });
+  }
+
+  return {
+    init: init
+  };
+
+}());
+
