e5cb44dbcd8061fc8f6f03262169085140ba8ed7
avbrychak <a.v.brychak@gmail.com>
2015-08-19 10:35:32 -0400

    chart shows total views

--- app/services/tile_stats_chart.rb
+++ app/services/tile_stats_chart.rb
@@ -8,7 +8,7 @@ class TileStatsChart
     @tile = tile
     @content = params[:content] || 'unqiue_views'
     @point_interval = params[:point_interval] || 'daily'
-    @start_date = params[:start_date] || tile.created_at.to_s(:chart_start_end_day)
+    @start_date = params[:start_date] || (Time.now - 1.day).to_s(:chart_start_end_day) #tile.created_at.to_s(:chart_start_end_day)
     @end_date = params[:end_date] || Time.now.to_s(:chart_start_end_day)
     @value_type = params[:value_type] || 'cumulative'
   end
@@ -20,14 +20,14 @@ class TileStatsChart
       hc.xAxis x_axis_params
       hc.yAxis y_axis_params
       hc.plotOptions plot_options_params
-      hc.series name: 'Unique views',  data: (0..23).to_a
+      hc.series name: 'Unique views',  data: data
     end
   end
 
   protected
-    # export to PNG, JPEG, PDF, SVG
-    # Remove 'Print'
     def exporting_params
+      # export to PNG, JPEG, PDF, SVG
+      # Remove 'Print'
       {
         buttons: { 
           printButton: { 
@@ -78,5 +78,29 @@ class TileStatsChart
 
     def point_interval
       60 * 60
+    end
+
+    def data
+      #(0..23).to_a
+      @q_start_date = Highchart.convert_date(@start_date).beginning_of_day
+      @q_end_date = Highchart.convert_date(@end_date).end_of_day
+
+      grouped_views = tile.tile_viewings
+          .select("date_trunc('#{time_unit}', created_at), views")
+          .where(created_at: @q_start_date..@q_end_date)
+          .group("date_trunc('#{time_unit}', created_at)")
+          .sum(:views)
+      # => {"2015-08-19 11:00:00"=>12, "2015-08-19 10:00:00"=>27, "2015-08-19 13:00:00"=>1, "2015-08-19 09:00:00"=>3, "2015-08-19 12:00:00"=>15}
+      start = @q_start_date
+      stop  = @q_end_date
+      while start < stop
+        grouped_views[start] = grouped_views[start] || 0
+        start += 1.send(time_unit.to_sym)
+      end
+      grouped_views.values
+    end
+
+    def time_unit
+      'hour'
     end
 end
