f02e44bb942b1eb6bf63258f33749e598087f6c0
hraynaud <herby.raynaud@gmail.com>
2015-08-20 19:22:23 -0400

    Manages transition between modals Prevents full reload of image library if the modal has already been opened

--- app/assets/javascripts/client_admin/managing_tiles/new_tile.js
+++ app/assets/javascripts/client_admin/managing_tiles/new_tile.js
@@ -3,6 +3,7 @@ var Airbo = window.Airbo || {};
 Airbo.TileCreator = (function(){
   var newTileModal
     , imagesModal
+    , libaryLoaded
     , newTileSelector = "#add_new_tile.preview_menu_item a"
     , newTileModalSelector = "#new_tile_modal"
     , imagesModalSelector ="#images_modal"
@@ -20,7 +21,6 @@ Airbo.TileCreator = (function(){
         success: function(data, status,xhr){
           newTileModal.html(data);
           newTileModal.foundation("reveal", "open");
-          initImageLibraryModal();
         },
         error: function(jqXHR, textStatus, error){
           console.log(error);
@@ -39,6 +39,7 @@ Airbo.TileCreator = (function(){
           imagesModal.html(data);
           Airbo.ImageLibrary.init();
           $(imagesModalSelector).foundation("reveal", "open");
+          libaryLoaded = true;
         },
         error: function(jqXHR, textStatus, error){
           console.log(error);
@@ -48,8 +49,11 @@ Airbo.TileCreator = (function(){
 
   function initImageLibraryModal(){
     $("body").on("click", addImageSelector, function(event){
-
-      getImageLibrary($(this).data("libraryUrl"))
+      if(libaryLoaded){
+        $(imagesModalSelector).foundation("reveal", "open");
+      }else{
+        getImageLibrary($(this).data("libraryUrl"));
+      }
     });
   }
 
@@ -59,9 +63,19 @@ Airbo.TileCreator = (function(){
   }
 
   function init(){
+    $(document).on('opened.fndtn.reveal',imagesModalSelector, function () {
+    });
+
+    $(document).on('closed.fndtn.reveal', imagesModalSelector, function () {
+      newTileModal.foundation("reveal", "open");
+    });
+
     initJQueryObjects();
     initNewTileModal();
+    initImageLibraryModal();
   }
+
+  
 
   return {
 
