3fc234b6be6a8644e12673b4fa597695f780ba12
hraynaud <herby.raynaud@gmail.com>
2015-08-20 17:29:05 -0400

    Initializes the image library after modal is loaded

--- app/assets/javascripts/client_admin/managing_tiles/new_tile.js
+++ app/assets/javascripts/client_admin/managing_tiles/new_tile.js
@@ -37,6 +37,7 @@ Airbo.TileCreator = (function(){
         url: libaryUrl,
         success: function(data, status,xhr){
           imagesModal.html(data);
+          Airbo.ImageLibrary.init();
           $(imagesModalSelector).foundation("reveal", "open");
         },
         error: function(jqXHR, textStatus, error){
