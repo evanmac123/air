3a2dc0168f0d9fe8371fa70dae48848aaf673f2e
hraynaud <herby.raynaud@gmail.com>
2015-08-22 14:35:40 -0400

    Moves global functions to application js for future refactoring

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,14 +1,30 @@
 
+//function isIE() {
+  //var myNav;
+  //myNav = navigator.userAgent.toLowerCase();
+  //if (myNav.indexOf('msie') !== -1) {
+    //return parseInt(myNav.split('msie')[1]);
+  //} else {
+    //return false;
+  //}
+//};
+
+
+function isIE11() {
+  return !!window.MSInputMethodContext;
+};
+
 function isIE() {
   var myNav;
   myNav = navigator.userAgent.toLowerCase();
   if (myNav.indexOf('msie') !== -1) {
     return parseInt(myNav.split('msie')[1]);
+  } else if (isIE11()) {
+    return 11;
   } else {
     return false;
   }
 };
-
 
 
 
