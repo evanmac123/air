2db5416b76327b32469626cbaa051f457572da85
hraynaud <herby.raynaud@gmail.com>
2015-09-11 16:04:20 -0400

    Move supporting content manager into new_tiles js

--- app/assets/javascripts/internal/tile_builder_form/new_tile.js
+++ app/assets/javascripts/internal/tile_builder_form/new_tile.js
@@ -440,6 +440,115 @@ Airbo.TileCarouselPage = (function() {
 }());
 
 
+Airbo.TileSuportingContentTextManager = (function(){
+
+  var contentEditor
+    , contentInput
+    , contentEditorSelector = '#supporting_content_editor'
+    , contentInputSelector = '#tile_builder_form_supporting_content'
+  ;
+
+  function contentEditorMaxlength() {
+    return contentEditor.next().attr('maxlength');
+  };
+
+
+  function blockSubmitButton (counter) {
+    var errorContainer, submitBtn, textLeftLength;
+    textLeftLength = contentEditor.text().length;
+    submitBtn = $("#publish input[type=submit]");
+    errorContainer = $(".supporting_content_error");
+    if (textLeftLength > contentEditorMaxlength()) {
+      submitBtn.attr('disabled', 'disabled');
+      errorContainer.show();
+    } else {
+      submitBtn.removeAttr('disabled');
+      errorContainer.hide();
+    }
+  }
+
+  function updateContentInput() {
+    contentInput.val(contentEditor.html());
+  }
+
+  function initializePenEditor() {
+    var options = {
+      editor: contentEditor[0],
+      list: ['bold', 'italic', 'underline', 'insertorderedlist', 'insertunorderedlist', 'createlink'],
+      stay: false
+    };
+    new Pen(options);
+  }
+
+  function editorLength() {
+    return contentEditor.html().length;
+  };
+
+  function contentEditorModifiedEvents() {
+    window.lastEditorLength = editorLength();
+    return contentEditor.bind("DOMSubtreeModified", function() {
+      if (editorLength() !== window.lastEditorLength) {
+        window.lastEditorLength = editorLength();
+        blockSubmitButton();
+        return updateContentInput();
+      }
+    });
+  };
+
+  function initializeEditor() {
+    var pasteNoFormattingIE;
+    addCharacterCounterFor('#tile_builder_form_headline');
+    addCharacterCounterFor(contentEditorSelector);
+    //blockSubmitButton();
+    initializePenEditor();
+    contentEditorModifiedEvents();
+    pasteNoFormattingIE = function() {
+      var newNode, text;
+      text = window.clipboardData.getData("text") || "";
+      if (text !== "") {
+        if (window.getSelection) {
+          newNode = document.createElement("span");
+          newNode.innerHTML = text;
+          return window.getSelection().getRangeAt(0).insertNode(newNode);
+        } else {
+          return document.selection.createRange().pasteHTML(text);
+        }
+      }
+    };
+    return contentEditor.on('paste', function(e) {
+      var text;
+      e.preventDefault();
+      if (isIE()) {
+        return pasteNoFormattingIE();
+      } else {
+        text = (e.originalEvent || e).clipboardData.getData('text/plain');
+        return window.document.execCommand('insertText', false, text);
+      }
+    });
+  };
+
+  function initjQueryObjects(){
+    contentEditor = $(contentEditorSelector);
+    contentInput = $(contentInputSelector);
+  }
+
+
+  function init(){
+
+    if (Airbo.Utils.supportsFeatureByPresenceOfSelector(contentEditorSelector) ) {
+      initjQueryObjects();
+      //initializeEditor();
+      Airbo.Utils.mediumEditor.init();
+      return this;
+    }
+  }
+
+  return {
+    init: init
+  }
+
+
+}());
 
 
 $(function(){
