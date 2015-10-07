abaf995286e4eb485fa9feb2380da1b6b2d2f04e
hraynaud <herby.raynaud@gmail.com>
2015-10-07 18:22:41 -0400

    Fixes mailer text

--- app/mailers/suggested_tile_status_mailer.rb
+++ app/mailers/suggested_tile_status_mailer.rb
@@ -1,47 +1,47 @@
 class SuggestedTileStatusMailer < ActionMailer::Base
-	ACCEPTED="Your Tile Has Been Accepted!"
-	POSTED="Your Tile Has Been Posted!"
-	ARCHIVED="Your Tile Has Been Archived"
+  ACCEPTED="Your Tile Has Been Accepted!"
+  POSTED="Your Tile Has Been Posted!"
+  ARCHIVED="Your Tile Has Been Archived"
 
-	has_delay_mail
-	layout "mailer"
-	helper :email
-		
-	def accepted(demo_id, user_id, tile_id)
-		set_vars(demo_id, user_id, tile_id, 
-						 ACCEPTED, 
-						 "The administrator has reviewed your Tile and accepted it. We’ll let you know when it’s posted.") 
-		sendit
+  has_delay_mail
+  layout "mailer"
+  helper :email
+
+  def accepted(demo_id, user_id, tile_id)
+    set_vars(demo_id, user_id, tile_id, 
+             ACCEPTED, 
+             "The administrator has reviewed your Tile and accepted it. We’ll let you know when it’s posted.") 
+    sendit
   end
 
 
-	def posted(demo_id, user_id, tile_id)
-		set_vars(demo_id, user_id, tile_id, 
-						POSTED,
-						 "The administrator has posted your Tile. You can see how many people have viewed and completed the Tile" ) 
-		sendit
-	end
+  def posted(demo_id, user_id, tile_id)
+    set_vars(demo_id, user_id, tile_id, 
+             POSTED,
+             "The administrator has posted your Tile." ) 
+    sendit
+  end
 
-	def archived(demo_id, user_id, tile_id)
-		set_vars(demo_id, user_id, tile_id, 
-						 ARCHIVED, 
-						 "The administrator has taken your Tile down from the Board. You can see the total number of people that have viewed and completed the Tile.") 
-		sendit
-	end
+  def archived(demo_id, user_id, tile_id)
+    set_vars(demo_id, user_id, tile_id, 
+             ARCHIVED, 
+             "The administrator has taken your Tile down from the Board. You can see the total number of people that have viewed and completed the Tile.") 
+    sendit
+  end
 
-	private
+  private
 
   def set_vars demo_id, user_id, tile_id, subject, sub_header=""
-		@user  = User.find user_id
-		@tile = Tile.find(tile_id)
-		@demo = Demo.find demo_id
-		@subject = subject
-		@subhead_text = sub_header
-		@link = suggested_tiles_url
-	end	
+    @user  = User.find user_id
+    @tile = Tile.find(tile_id)
+    @demo = Demo.find demo_id
+    @subject = subject
+    @subhead_text = sub_header
+    @link = suggested_tiles_url
+  end	
 
-	def sendit
-		mail to: @user.email_with_name, from: @demo.reply_email_address, subject: @subject
-	end
+  def sendit
+    mail to: @user.email_with_name, from: @demo.reply_email_address, subject: @subject
+  end
 
 end
