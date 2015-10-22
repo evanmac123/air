7693eaa158ae9401501a5ed02254fa2fbda486e7
avbrychak <a.v.brychak@gmail.com>
2015-10-22 12:33:52 -0400

    add method to create topic with tags

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,21 @@
 class Topic < ActiveRecord::Base
   has_many :tile_tags
+
+  # full_topics = {
+  #   "Benefits" => ["Health Plan Basics", "Rx Benefits", "Health Care Reform", "Health Care Consumerism", "Dental", "Vision", "Open Enrollment Process", "Decision Support"],
+  #   "Compliance" => ["Policy", "Sexual Harassment", "Compliance Form"]
+  # }
+  def make_topics_with_tags full_topics
+    full_topics.each do |name, tag_titles|
+      topic = find_or_create name
+      tag_titles.each do |title|
+        tag = TileTag.have_tag(title) || TileTag.create(title: title)
+        topic.tile_tags.push(tag)
+      end
+    end
+  end
+
+  def find_or_create name
+    Topic.where(name: name).first || Topic.create(name: name)
+  end
 end
