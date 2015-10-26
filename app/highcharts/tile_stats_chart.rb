c0fb5ae0f71d9acf710417ce928901553d7c12ea
avbrychak <a.v.brychak@gmail.com>
2015-10-26 11:25:21 -0400

    change styles for tables and datepicker input

--- app/highcharts/tile_stats_chart.rb
+++ app/highcharts/tile_stats_chart.rb
@@ -108,7 +108,7 @@ JS
       {
         line: {
           pointStart: period.start_date(:date),
-          # FIXME maybe
+          # FIXME maybe - Carly Rae Jepsen
           # 0.001 is set if interval is month. gem multiplies it by 1000 to get Milliseconds.
           # so it's 1 in the end. And then pointIntervalUnit is used for monthes.
           pointInterval: (period.point_interval_unit ? 0.001 : period.point_interval),
