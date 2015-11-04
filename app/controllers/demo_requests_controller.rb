8dee65ea7f7c9b354aa71bcbaf8225abf2b1a3a1
hraynaud <herby.raynaud@gmail.com>
2015-11-04 14:31:38 -0500

    Plugs into the existing email notification when a demo is requested.

--- app/controllers/demo_requests_controller.rb
+++ app/controllers/demo_requests_controller.rb
@@ -2,7 +2,8 @@ class DemoRequestsController < ApplicationController
   skip_before_filter :authorize
 
   def create
-    DemoRequest.create(params[:demo_request])
+    request = EmailInfoRequest.create!(params[:demo_request])
+    request.notify_the_ks_of_demo_request
     head :ok
   end
 
