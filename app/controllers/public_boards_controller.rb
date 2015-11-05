c3b190b76e4e407c71d6ae4d1b88040852d5d90a
hraynaud <herby.raynaud@gmail.com>
2015-11-05 14:54:11 -0500

    removes ping from the client and performs in the controller

--- app/controllers/public_boards_controller.rb
+++ app/controllers/public_boards_controller.rb
@@ -1,8 +1,13 @@
 class PublicBoardsController < ApplicationController
+  include MixpanelHelper
   skip_before_filter :authorize
   before_filter :allow_guest_user
 
+ #FIXME why have a separate resource that simply redirects to another
+  #resource!!!!
+
   def show
+    ping "Public Parent Board", {:url => request.url}, current_user
     redirect_to public_activity_path(params[:public_slug])
   end
 
