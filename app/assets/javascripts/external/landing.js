8c392df177cfa583779bd4199831fed5c77fa68f
avbrychak <a.v.brychak@gmail.com>
2015-11-13 11:41:54 -0500

    style header for mobile

--- app/assets/javascripts/external/landing.js
+++ app/assets/javascripts/external/landing.js
@@ -9,6 +9,33 @@ Airbo.Landing = (function(){
         $('header').removeClass('sticky');
       }
     });
+    // Mobile Navigation
+    $('.mobile-toggle').click(function() {
+      $("header").toggleClass('open-nav');
+        // if ($('.main_h').hasClass('open-nav')) {
+        //     $('.main_h').removeClass('open-nav');
+        // } else {
+        //     $('.main_h').addClass('open-nav');
+        // }
+    });
+
+    // $('.main_h li a').click(function() {
+    //     if ($('.main_h').hasClass('open-nav')) {
+    //         $('.navigation').removeClass('open-nav');
+    //         $('.main_h').removeClass('open-nav');
+    //     }
+    // });
+
+    // navigation scroll lijepo radi materem
+    // $('nav a').click(function(event) {
+    //     var id = $(this).attr("href");
+    //     var offset = 70;
+    //     var target = $(id).offset().top - offset;
+    //     $('html, body').animate({
+    //         scrollTop: target
+    //     }, 500);
+    //     event.preventDefault();
+    // });
   }
   return {
   init: init
