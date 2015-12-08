25f59b148fc05b20f10f75e82f2f6a2892d1dfcd
avbrychak <a.v.brychak@gmail.com>
2015-12-08 14:42:25 -0500

    only edit icon opens edit state

--- app/assets/javascripts/vendor_customization/airbo_anchor_preview.js
+++ app/assets/javascripts/vendor_customization/airbo_anchor_preview.js
@@ -1,10 +1,22 @@
 var Airbo = window.Airbo || {};
 
 Airbo.CustomAnchorPreview = MediumEditor.extensions.anchorPreview.extend({
+  createPreview: function () {
+    var el = this.document.createElement('div');
+
+    el.id = 'medium-editor-anchor-preview-' + this.getEditorId();
+    el.className = 'medium-editor-anchor-preview';
+    el.innerHTML = this.getTemplate();
+
+    editIcon = $(el).find("i")[0];
+    this.on(editIcon, 'click', this.handleClick.bind(this));
+
+    return el;
+  },
   getTemplate: function () {
     return  '<div class="medium-editor-toolbar-anchor-preview" id="medium-editor-toolbar-anchor-preview">' +
-            '  <a class="medium-editor-toolbar-anchor-preview-inner"></a>' +
-            '  <span class="sep-vertical"></span>' +
+            '  <a target="_blank"></a>' +
+            '  <span class="sep-vertical medium-editor-toolbar-anchor-preview-inner"></span>' +
             '  <i class="fa fa-pencil"></i>' +
             '</div>';
   }
