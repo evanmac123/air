1299028c628b945d7c28a24e5576ac0ad517dc2f
avbrychak <a.v.brychak@gmail.com>
2015-12-29 15:01:55 -0500

    add thumb menu events for newly duplicated tile. has bug with its image

--- app/assets/javascripts/client_admin/managing_tiles/tile_thumbnail_menu.js
+++ app/assets/javascripts/client_admin/managing_tiles/tile_thumbnail_menu.js
@@ -1,50 +1,58 @@
 var Airbo = window.Airbo || {};
 
 Airbo.TileThumbnailMenu = (function() {
+  var tileCreator;
+
   function htmlDecode(input){
     var e = document.createElement('div');
     e.innerHTML = input;
     return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
   }
-  function init(tileCreator) {
+  function initMoreBtn(menu_button){
+    menu_button.tooltipster({
+      theme: "tooltipster-shadow",
+      interactive: true,
+      position: "bottom",
+      content: function(){
+        encodedMenu = menu_button.data('title');
+        decodedMenu = htmlDecode(encodedMenu);
+        return $(decodedMenu);
+      },
+      trigger: "click",
+      autoClose: true,
+      functionReady: function(){
+        $(".tile_thumbnail_menu .delete_tile").click(function(event){
+          event.preventDefault();
+          $(".more_button").tooltipster("hide");
+          tileCreator.confirmDeletion($(this));
+        });
+
+        $(".tile_thumbnail_menu .duplicate_tile").click(function(event){
+          event.preventDefault();
+          $(".more_button").tooltipster("hide");
+          $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: $(this).attr("href") ,
+            success: function(data, status,xhr){
+              tileCreator.updateTileSection(data);
+              new_tile = $(".tile_container[data-tile-id='"+data.tileId+"']");
+              initMoreBtn(new_tile.find(".more_button"));
+            },
+
+            error: function(jqXHR, textStatus, error){
+              console.log(error);
+            }
+          });
+        });
+      }
+    });
+  }
+  function init(AirboTileCreator) {
+    tileCreator = AirboTileCreator;
+
     $(".more_button").each(function(){
-      var menu_button = $(this);
-      menu_button.tooltipster({
-        theme: "tooltipster-shadow",
-        interactive: true,
-        position: "bottom",
-        content: function(){
-          encodedMenu = menu_button.data('title');
-          decodedMenu = htmlDecode(encodedMenu);
-          return $(decodedMenu);
-        },
-        trigger: "click",
-        autoClose: true,
-        functionReady: function(){
-          $(".tile_thumbnail_menu .delete_tile").click(function(event){
-            event.preventDefault();
-            $(".more_button").tooltipster("hide");
-            tileCreator.confirmDeletion($(this));
-          });
-
-          $(".tile_thumbnail_menu .duplicate_tile").click(function(event){
-            event.preventDefault();
-            $(".more_button").tooltipster("hide");
-            $.ajax({
-              type: "POST",
-              dataType: "json",
-              url: $(this).attr("href") ,
-              success: function(data, status,xhr){
-                tileCreator.updateTileSection(data);
-              },
-
-              error: function(jqXHR, textStatus, error){
-                console.log(error);
-              }
-            });
-          });
-        }
-      });
+      initMoreBtn($(this));
     });
   }
   return {
