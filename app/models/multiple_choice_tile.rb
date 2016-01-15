7209ed16d23359307777158d97a0d2e98f8f07aa
hraynaud <herby.raynaud@gmail.com>
2016-01-14 21:26:20 -0500

    Fixes handling of answers in the multiple choice tile class

--- app/models/multiple_choice_tile.rb
+++ app/models/multiple_choice_tile.rb
@@ -4,9 +4,10 @@ class MultipleChoiceTile < Tile
   validate :at_least_one_answer_present
   belongs_to :demo
 
-  before_validation :set_answers #FIXME should not need to have separate answers field but some processing on the field needs to happen and the client side creates an answers field instead of multiple_choice_answers
+  #FIXME should not need to have separate answers field but some processing on the field needs to happen and the client side creates an answers field instead of multiple_choice_answers
 
- attr_accessor :answers, :answers_normalizer
+
+  before_validation :set_answers, :if => :answers_populated? 
 
   def points_positive
     unless points.present? && points > 0
@@ -20,50 +21,47 @@ class MultipleChoiceTile < Tile
     end
   end
 
+  def answers
+    @answers || []
+  end
+
+  def answers= values
+    @answers= values
+  end
+
  private
 
  def set_answers
-   self.correct_answer_index =    normalized_correct_answer_index
+   self.correct_answer_index =  normalized_correct_answer_index
    self.multiple_choice_answers =  normalized_answers
  end
 
  def normalized_correct_answer_index
-   answers_normalizer.normalized_correct_answer_index
+   normalized_correct_answer_index
  end
 
  def normalized_answers
-   answers_normalizer.normalized_answers
+     answers.map(&:strip).select(&:present?).uniq
  end
 
- def answers_normalizer
-   @a ||= AnswersNormalizer.new( answers, correct_answer_index) 
+ def answers_changed?
+   changes.keys.include? "answers"
  end
 
-
- class AnswersNormalizer
-   def initialize(answers, index)
-     @answers = (answers || [])
-     @correct_answer_index = set_correct_answer_index(index)
+ def normalized_correct_answer_index
+   if correct_answer_index
+     answers[0, correct_answer_index + 1].reject(&:blank?).uniq.count - 1
+   else
+     -1
    end
-
-   def normalized_answers
-     @answers.map(&:strip).select(&:present?).uniq
-   end
-
-   def normalized_correct_answer_index
-     if @correct_answer_index
-       @answers[0, @correct_answer_index + 1].reject(&:blank?).uniq.count - 1
-     else
-       -1
-     end
-   end
-
-   protected
-
-   def set_correct_answer_index(index)
-     index.present? ? index.to_i : nil
-   end
-
  end
+
+ def set_correct_answer_index(index)
+   index.present? ? index.to_i : nil
+ end
+
+def answers_populated?
+  !answers.empty?
+end
 
 end
