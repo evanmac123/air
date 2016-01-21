d9bb0b2264f7a23bb419bae04cea68e9788d21bc
hraynaud <herby.raynaud@gmail.com>
2016-01-21 06:13:55 -0500

    changes field back to multiple choice answers

--- spec/factories.rb
+++ spec/factories.rb
@@ -243,7 +243,8 @@ FactoryGirl.define do
     question "Which of the following comes out of a bird?"
     points 99
     #TODO fix this so that the tile uses the native multiple_choice_answers_field
-    answers ["Ham", "Eggs", "A V8 Buick"]
+    #answers ["Ham", "Eggs", "A V8 Buick"]
+    multiple_choice_answers ["Ham", "Eggs", "A V8 Buick"]
     correct_answer_index 1
   end
 
