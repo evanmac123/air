d9d971158984e5c99120e83262994b027cc8e90b
hraynaud <herby.raynaud@gmail.com>
2016-02-02 18:21:17 -0500

    Fixes missing quotes around env var

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -7,5 +7,5 @@
 Health::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
                                             'x'*30
                                             else
-                                              ENV[SECRET_TOKEN]
+                                              ENV['SECRET_TOKEN']
                                             end
