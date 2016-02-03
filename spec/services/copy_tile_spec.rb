5fd79703461d22b60a0697a41d13894c57494eb7
avbrychak <a.v.brychak@gmail.com>
2016-02-03 13:55:08 -0500

    set position for created tile

--- spec/services/copy_tile_spec.rb
+++ spec/services/copy_tile_spec.rb
@@ -4,7 +4,11 @@ describe CopyTile do
   describe "#copy_tile" do
     before do
       @original_tile = FactoryGirl.create :multiple_choice_tile, :active, headline: "Copy me!"
-      @ct = CopyTile.new(@original_tile.demo, @original_tile.creator)
+      @demo = FactoryGirl.create :demo
+      FactoryGirl.create_list :multiple_choice_tile, 5, :draft
+      @copying_user = FactoryGirl.create :client_admin
+
+      @ct = CopyTile.new(@demo, @copying_user)
     end
 
     it "should copy tile" do
@@ -13,6 +17,12 @@ describe CopyTile do
       attr_list.each do |attr_name|
         expect(tile.send(attr_name)).to eq(@original_tile.send(attr_name))
       end
+    end
+
+    it "should set copied tile to first place in drafts" do
+      tile = @ct.copy_tile(@original_tile, false)
+      @demo.reload
+      @demo.draft_tiles.first.id == tile.id
     end
   end
   #
@@ -27,9 +37,9 @@ describe CopyTile do
       "points",
       "question",
       "supporting_content",
-      "type",
       "image_meta",
-      "thumbnail_meta"
+      "thumbnail_meta",
+      "use_old_line_break_css"
     ]
   end
 end
