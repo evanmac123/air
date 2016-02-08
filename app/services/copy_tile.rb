cd569f4fb3db74d7aa86ce6e59a07d2434a055d5
hraynaud <herby.raynaud@gmail.com>
2016-02-08 11:13:06 -0500

    Fixes bug where copied tiles don’t have a question type and subtype.

--- app/services/copy_tile.rb
+++ app/services/copy_tile.rb
@@ -39,7 +39,9 @@ class CopyTile
       "supporting_content",
       "image",
       "thumbnail",
-      "use_old_line_break_css"
+      "use_old_line_break_css",
+      "question_type",
+      "question_subtype"
     ].each do |field_to_copy|
       @copy.send("#{field_to_copy}=", @tile.send(field_to_copy))
     end
