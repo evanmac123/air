6d2e5b4ae45cbb3fd557cd06f5f154641ab8c80a
avbrychak <a.v.brychak@gmail.com>
2016-02-08 13:11:11 -0500

    style bullets

--- app/assets/javascripts/client_admin/explore_page.js
+++ app/assets/javascripts/client_admin/explore_page.js
@@ -4,7 +4,10 @@ Airbo.Explore = (function(){
   var modalObj = Airbo.Utils.StandardModal();
   function initModalObj() {
     modalObj.init({
-      modalId: "explore_intro_modal"
+      modalId: "explore_intro_modal",
+      onOpenedEvent: function() {
+        $(document).foundation();
+      }
     });
   }
   function init() {
