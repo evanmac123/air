8da7597142137db73cfcce66192e65a9e5aa48d3
hraynaud <herby.raynaud@gmail.com>
2016-02-09 18:40:00 -0500

    Adds basic validation and models specs for contract

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,3 +1,26 @@
 class Contract < ActiveRecord::Base
   belongs_to :organization
+  validates  :organization, :name, :start_date, :end_date, :max_users, :term, :estimate_type, :plan, presence: true
+  validates :max_users, :term, numericality: { only_integer: true }
+  validates :arr, :mrr, numericality: true, allow_nil: true
+  validate :arr_or_mrr_provided
+
+  PLANS= ["Activate", "Engage", "Enterprise"] 
+  ESTIMATE_TYPE= ["Actual", "Projected"] 
+
+  def self.plans
+    {activate: "Activate", engage: "Engage", enterprise: "Enterprise"}
+  end
+
+  def self.plan_name_for key
+   plans[key]  
+  end
+
+  private
+  def arr_or_mrr_provided
+    if !(arr || mrr)
+      errors.add(:mrr,"ARR or MRR must be provided") 
+      return false
+    end
+  end
 end
--- spec/models/contract_spec.rb
+++ spec/models/contract_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe Contract do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is invalid without all required fields" do
+   c = Contract.new
+   expect(c.save).to be_false
+ end
+
+  it "is valid with required fields" do
+   org = Organization.create({name: "Omni Corp"})
+   c = Contract.new
+   c.organization = org 
+
+   c.name = "Con Tract"
+   c.start_date = Date.today 
+   c.end_date = 1.year.from_now 
+   c.arr = 60000
+
+   c.term = 12
+   c.estimate_type = "Con Tract"
+   c.rank = "primary"
+   c.plan = Contract.plan_name_for :engage
+   c.max_users = 100
+   expect(c.save).to be_true
+ end
+
 end
