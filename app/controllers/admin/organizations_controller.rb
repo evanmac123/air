bab408df713f768974366112e2081a5876d3a5bd
hraynaud <herby.raynaud@gmail.com>
2016-02-08 23:49:29 -0500

    Makes organizations work under admin namespace

--- app/controllers/admin/organizations_controller.rb
+++ app/controllers/admin/organizations_controller.rb
@@ -1,91 +1,31 @@
-class OrganizationsController < ApplicationController
-  # GET /organizations
-  # GET /organizations.json
+require 'custom_responder'
+class Admin::OrganizationsController < AdminBaseController
+  include CustomResponder
+
+  before_filter :find_organization, only: [:edit, :update, :destroy]
+
   def index
     @organizations = Organization.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @organizations }
-    end
   end
 
-  # GET /organizations/1
-  # GET /organizations/1.json
-  def show
-    @organization = Organization.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @organization }
-    end
-  end
-
-  # GET /organizations/new
-  # GET /organizations/new.json
   def new
     @organization = Organization.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @organization }
-    end
+   new_or_edit @organization 
   end
 
-  # GET /organizations/1/edit
   def edit
-    @organization = Organization.find(params[:id])
+     new_or_edit @organization 
   end
 
-  # POST /organizations
-  # POST /organizations.json
-  def create
-    @organization = Organization.new(organization_params)
-
-    respond_to do |format|
-      if @organization.save
-        format.html { redirect_to @organization, notice: 'Organization was successfully created.' }
-        format.json { render json: @organization, status: :created, location: @organization }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @organization.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /organizations/1
-  # PATCH/PUT /organizations/1.json
   def update
-    @organization = Organization.find(params[:id])
-
-    respond_to do |format|
-      if @organization.update_attributes(organization_params)
-        format.html { redirect_to @organization, notice: 'Organization was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @organization.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /organizations/1
-  # DELETE /organizations/1.json
-  def destroy
-    @organization = Organization.find(params[:id])
-    @organization.destroy
-
-    respond_to do |format|
-      format.html { redirect_to organizations_url }
-      format.json { head :no_content }
-    end
+    update_or_create @organization
   end
 
   private
 
-    # Use this method to whitelist the permissible parameters. Example:
-    # params.require(:person).permit(:name, :age)
-    # Also, you can specialize this method with per-user checking of permissible attributes.
+  def find_organization
+    @Organization = Organization.find(id)
+  end
     def organization_params
       params.require(:organization).permit(:churn_reason, :churned, :name, :num_employees, :sales_channel)
     end
