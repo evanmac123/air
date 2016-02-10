cb3a1098b3458d48fed5663f4fdb222b4f8fc93c
hraynaud <herby.raynaud@gmail.com>
2016-02-10 14:42:35 -0500

    Adds logic to calculate contract start/enddates

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,4 +3,28 @@ class Organization < ActiveRecord::Base
 
   validates :name, presence: true
 
+  def customer_start_date
+    @cust_start ||=ordered_contracts.try(:first).start_date
+  end
+
+  def customer_end_date
+   @cust_end ||= ordered_contracts.try(:last).end_date
+  end
+
+  def active
+    customer_end_date >= Date.today 
+  end
+
+
+
+  def life_time
+    TimeDifference.between(customer_start_date, customer_end_date).in_months
+  end
+
+  private
+
+  def ordered_contracts
+    contracts.order("start_date asc")
+  end
+
 end
