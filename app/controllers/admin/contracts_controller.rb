1eb34062f76da632fbf301dcdb2a55c7da3a954a
hraynaud <herby.raynaud@gmail.com>
2016-02-10 17:23:44 -0500

    Fills out contacts and organizations controller actions

--- app/controllers/admin/contracts_controller.rb
+++ app/controllers/admin/contracts_controller.rb
@@ -1,6 +1,10 @@
 require 'custom_responder'
 class Admin::ContractsController < AdminBaseController
-  before_filter :find_contract, except: [:new, :index]
+  before_filter :find_contract, except: [:new, :create, :index]
+  before_filter :set_parent_org
+  before_filter :set_organizations
+  before_filter :set_related_contracts
+  before_filter :build_new_contract, only:[:new]
   include CustomResponder
 
   def index
@@ -8,7 +12,7 @@ class Admin::ContractsController < AdminBaseController
   end
 
   def new
-    @contract = Contract.new
+    @relationship = @parent_org ? "Primary" : "Upgrade"
     new_or_edit @contract
   end
 
@@ -17,8 +21,15 @@ class Admin::ContractsController < AdminBaseController
   end
 
   def update
-    update_or_create @contract
+    @contract.assign_parms(contract_params)
+    update_or_create @contract, admin_contract_path(@contract)
   end
+
+  def create
+    @contract = Contract.new(contract_params)
+    update_or_create @contract, admin_contracts_path
+  end
+
 
   private
 
@@ -26,10 +37,40 @@ class Admin::ContractsController < AdminBaseController
     # params.require(:person).permit(:name, :age)
     # Also, you can specialize this method with per-user checking of permissible attributes.
     def contract_params
-      params.require(:contract).permit(:amt_booked, :arr, :date_booked, :end_date, :estimate_type, :max_users, :mrr, :name, :notes, :organization, :plan, :rank, :start_date, :term)
+      params.require(:contract).permit(:organization_id,
+                                       :is_upgrade,
+                                       :amt_booked, :arr, :date_booked, 
+                                       :end_date, :estimate_type, :max_users, 
+                                       :mrr, :name, :notes, 
+                                       :plan, :rank, :start_date, :term)
     end
 
     def find_contract
       @contract = Contract.find(params[:id])
-    end 
+    end
+
+    def set_organizations
+      @organizations = Organization.all
+    end
+
+    def set_parent_org
+      if params[:organization_id]
+        @parent_org = Organization.where(id:  params[:organization_id]).first
+      end
+    end
+
+    def set_related_contracts
+      if params[:contract_id]
+        @parent_contract = Contract.where(id:  params[:contract_id]).first
+      end
+    end
+
+    def build_new_contract
+      @contract = if @parent_org
+                    @parent_org.contracts.build({name: @parent_org.name})
+                  else
+                    @parent_contract.upgrades.build({name: @parent_contract.name})
+                  end
+    end
+
 end
--- app/controllers/admin/organizations_controller.rb
+++ app/controllers/admin/organizations_controller.rb
@@ -2,10 +2,13 @@ require 'custom_responder'
 class Admin::OrganizationsController < AdminBaseController
   include CustomResponder
 
-  before_filter :find_organization, only: [:edit, :update, :destroy]
+  before_filter :find_organization, only: [:edit, :show, :update, :destroy]
 
   def index
     @organizations = Organization.all
+  end
+
+  def show
   end
 
   def new
@@ -17,17 +20,23 @@ class Admin::OrganizationsController < AdminBaseController
     new_or_edit @organization
   end
 
+  def create 
+    @organization = Organization.new(organization_params)
+    update_or_create @organization, admin_organizations_path(@organization)
+  end
+
   def update
-    update_or_create @organization
+    @organization.assign_attributes organization_params
+    update_or_create @organization, admin_organizations_path(@organization)
   end
 
   private
 
   def find_organization
-    @Organization = Organization.find(id)
+    @organization = Organization.find(params[:id])
   end
 
   def organization_params
-    params.require(:organization).permit(:churn_reason, :churned, :name, :num_employees, :sales_channel)
+    params.require(:organization).permit(:churn_reason, :name, :num_employees, :sales_channel)
   end
 end
