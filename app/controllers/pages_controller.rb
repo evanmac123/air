5e48160a13acaec84f0d840e59ad3b19a7024de9
avbrychak <a.v.brychak@gmail.com>
2016-02-12 10:05:23 -0500

    add hero section

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -35,7 +35,7 @@ class PagesController < HighVoltage::PagesController
     case page_name
     when 'privacy', 'terms'
       'external'
-    when 'welcome', 'product', 'asha', 'company'
+    when 'welcome', 'product', 'asha', 'company', 'home'
       'standalone'
     when 'more_info',  # FIXME dead url?
       @body_id = "homepage"
