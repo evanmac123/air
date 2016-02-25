4cc0ee76e65ef80a234e4212d2554aa91d519a4b
avbrychak <a.v.brychak@gmail.com>
2016-02-25 10:56:56 -0500

    add copy btn js

---
+++ app/assets/javascripts/client_admin/explore/explore_tile_preview.js
@@ -0,0 +1,37 @@
+var Airbo = window.Airbo || {};
+
+Airbo.ExploreTilePreview = (function(){
+  var copyBtnSel = ".copy_to_board"
+    , copyBtn
+  ;
+  function initEvents() {
+    copyBtn.click(function(event) {
+      event.preventDefault();
+      copyUrl = $(this).attr("href");
+      $.post(copyUrl, {},
+        function(data) {
+          if(data.success) {
+            Airbo.CopyAlert.open();
+          }
+        },
+        'json'
+      );
+    });
+  }
+  function initVars() {
+    copyBtn = $(copyBtnSel);
+  }
+  function init() {
+    initVars();
+    initEvents();
+  }
+  return {
+    init: init
+  }
+}());
+
+$(document).ready(function(){
+  if( $(".explore_menu").length > 0 ) {
+    Airbo.ExploreTilePreview.init();
+  }
+});
