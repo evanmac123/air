d95df8a8ea622ad2e822de4fac4dbc9216e2d5f4
avbrychak <a.v.brychak@gmail.com>
2016-03-02 10:15:01 -0500

    add sign up form to external preview

--- app/assets/javascripts/client_admin/sign_up_form.js
+++ app/assets/javascripts/client_admin/sign_up_form.js
@@ -1,101 +1,67 @@
-//
-//  =>  Sign up form
-//
-function creationStartCallback(event){ 
-  $("#submit_account_form").attr("disabled", "disabled");
-  $('#create_account_form').find(".errors_field").text("");
-}
-function creationResponseCallback(event, data){
-  $("#submit_account_form").removeAttr("disabled");
-  if(data.status == 'success'){
-    if( window.pathForActionAfterRegistration ){
-      localStorage.setItem("pathForActionAfterRegistration", window.pathForActionAfterRegistration);
-      window.location.href = window.location.href.match(/(.*)\?/i) ? window.location.href.match(/(.*)\?/i)[1] : window.location.href;
+var Airbo = window.Airbo || {};
+
+Airbo.SignUpFromPreview = (function(){
+  var modalObj = Airbo.Utils.StandardModal()
+    , modalId = "sign_up_modal"
+    , blockedElements = [ ".see_airbo",
+                          ".tag"
+                        ].join(", ")
+  ;
+  function creationStartCallback(event){ 
+    $("#submit_account_form").attr("disabled", "disabled");
+    $('#create_account_form').find(".errors_field").text("");
+  }
+  function creationResponseCallback(event, data){
+    $("#submit_account_form").removeAttr("disabled");
+    if(data.status == 'success'){
+      if( window.pathForActionAfterRegistration ){
+        localStorage.setItem("pathForActionAfterRegistration", window.pathForActionAfterRegistration);
+        window.location.href = window.location.href.match(/(.*)\?/i) ? window.location.href.match(/(.*)\?/i)[1] : window.location.href;
+      }else{
+        window.location.href = "/client_admin/tiles";
+      }
     }else{
-      window.location.href = "/client_admin/tiles";
+      $('#create_account_form').find(".errors_field").text(data.errors);
     }
-  }else{
-    $('#create_account_form').find(".errors_field").text(data.errors);
   }
-}
-$().ready(function(){
-  $('#create_account_form').on('submit', creationStartCallback).on('ajax:success', creationResponseCallback);
-})
-//
-//  =>  Tile preview page
-//
-function showSignUpModal(){
-  // This is a hack, since if we call introJs().exit() and there are no intros
-  // open, it'll throw a TypeError: Cannot read property 'style' of null. So
-  // presumably it's looking up some element that should contain an intro,
-  // not checking to see if one actually exists, and then attempting to mess
-  // with its style.
-
-  try {
-    introJs().exit();
-  } catch(err) {
+  function dompath( element ){
+    var inner = $(element).children().length == 0 ? $(element).text() : '';
+    var idSelector = $(element).attr("id") ? $.trim($(element).attr("id")).split(" ").join("#") : "";
+    var classSelector = $(element).attr("class") ? $.trim($(element).attr("class")).split(" ").join(".") : "";
+    var eleSelector = element.tagName.toLowerCase() + 
+      ( (idSelector.length > 0) ? ("#" + idSelector) : "" ) +
+      ( (classSelector.length > 0) ? ("." + classSelector) : "" ) +
+      ( (inner.length > 0) ? ':contains(\'' + inner + '\')' : '' );
+    return eleSelector;
   }
-  $("#modal_link").click();
-}
-
-function closeSignUpModal(){
-  $('#sign_up_modal').foundation('reveal', 'close');
-}
-/* This is full dompath but it doesn't work in tests because it's too long.
-function dompath( element )
-{
-    var path = '';
-    for ( ; element && element.nodeType == 1; element = element.parentNode )
-    {
-        var inner = $(element).children().length == 0 ? $(element).text() : '';
-        var idSelector = $(element).attr("id") ? $(element).attr("id").trim().split(" ").join("#") : "";
-        var classSelector = $(element).attr("class") ? $(element).attr("class").trim().split(" ").join(".") : "";
-        var eleSelector = element.tagName.toLowerCase() + 
-          ( (idSelector.length > 0) ? ("#" + idSelector) : "" ) +
-          ( (classSelector.length > 0) ? ("." + classSelector) : "" ) +
-          ((inner.length > 0) ? ':contains(\'' + inner + '\')' : '');
-        path = ' ' + eleSelector + path;
-    }
-    return path;
-}
-*/
-// this function returns only selectors for element
-function dompath( element ){
-  var inner = $(element).children().length == 0 ? $(element).text() : '';
-  var idSelector = $(element).attr("id") ? $.trim($(element).attr("id")).split(" ").join("#") : "";
-  var classSelector = $(element).attr("class") ? $.trim($(element).attr("class")).split(" ").join(".") : "";
-  var eleSelector = element.tagName.toLowerCase() + 
-    ( (idSelector.length > 0) ? ("#" + idSelector) : "" ) +
-    ( (classSelector.length > 0) ? ("." + classSelector) : "" ) +
-    ( (inner.length > 0) ? ':contains(\'' + inner + '\')' : '' );
-  return eleSelector;
-}
-
-$().ready(function(){
-  if(window.guestForTilePreview){
-    $("#save_progress_button").text("Create Board");
-
-    $(".close_sign_up_modal").click(function(){
-      closeSignUpModal();
-    });
-
-    blockedElements = [ ".not_like_button", 
-                        "a .copy_button",
-                        "#random-tile-link", 
-                        "#back-link", 
-                        ".tag a", 
-                        "#save_progress_button",
-                        "#prev",
-                        "#next",
-                        "#logo"
-                      ].join(", ")
+  function initEvents() {
     $(blockedElements).click( function(event){
       event.preventDefault();
       event.stopImmediatePropagation(); 
       window.pathForActionAfterRegistration = dompath(event.target);
-      showSignUpModal();
+      modalObj.open();
       return false; // prevents default for remote calls
     });
+
+    $('#create_account_form').on('submit', creationStartCallback).on('ajax:success', creationResponseCallback);
+  }
+  function initModalObj() {
+    modalObj.init({
+      modalId: modalId
+    });
+  }
+  function init() {
+    initModalObj();
+    initEvents();
+  }
+  return {
+    init: init
+  }
+}());
+
+$(document).ready(function(){
+  if( $(".single_tile_guest_layout").length > 0 ) {
+    Airbo.SignUpFromPreview.init();
   }
   //action after registration
   domPath = localStorage.getItem("pathForActionAfterRegistration") || "";
@@ -106,4 +72,4 @@ $().ready(function(){
       actionElement[0].click();
     }
   }
-})
+});
