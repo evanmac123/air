7b91f23e9ce7905f8bbbc9849b6cfa4ae261afdc
hraynaud <herby.raynaud@gmail.com>
2016-03-16 23:37:27 -0700

    moves intro js init and confess in separate js file

--- app/assets/javascripts/internal/single_tile_intro.js
+++ app/assets/javascripts/internal/single_tile_intro.js
@@ -15,13 +15,49 @@ Airbo.SingleTileIntro= (function(){
     intro.setOptions({
       showStepNumbers: false,
       doneLabel: 'Got it',
-      tooltipClass: "airbo_preview_intro"
+      tooltipClass: "airbo_preview_intro",
+
+      steps: [
+        { 
+          element: ".tile_holder",
+          intro: "Hi, this walkthrough helps you understand the anatomy of a tile.",
+          position: "top"
+        },
+        {
+          element: document.querySelector('.tile_full_image'),
+          intro: "All tiles have an image which is a powerful way to get your viewer's attention.",
+
+          position: 'top',
+          step: 1
+        },
+        {
+          element: document.querySelector('.tile_headline'),
+          intro: "The Headline provides a quick overview of what a Tile is about.",
+          position: 'top',
+
+          step: 2
+        },
+        {
+          element: '.tile_supporting_content',
+          intro: 'The Supporting Content section is where you fill in the core information you wish to share. There is a 750 character limit but you can always insert a link to documents on the web or your intranet if the 750 characters is not enough.',
+          position: 'top',
+
+          step: 3
+        },
+         {
+          element: '.tile_quiz',
+          intro: "<p>The Interaction section is where you engage your viewers with a short question which you'd like them to answer. </p>The response type can be one of various formats from a simple one button confirmation, to a quiz, multiple choice answer,  to a survey. </p><p>Finally, you can assign a number of points to each Tile which employees earn as they complete the Tile interactions. Use the point totals to setup incentive rewards for employees who engage with your content. </br>Airbo tracks both Tile views completions automatically so you don't have to.</p> Go ahead, try it now.",
+          position: 'bottom',
+
+          step: 4
+        },
+      ]
     });
 
 
     intro.onchange(function(targetElement) {
       var el = $(targetElement);
-      if(el.data("intro-last-step")==true){
+      if(el.hasClass("tile_quiz")==true){
         $(".introjs-skipbutton").addClass("button-outlined-big"); 
         $(".introjs-nextbutton").hide();
       } 
@@ -34,7 +70,7 @@ Airbo.SingleTileIntro= (function(){
   }
 
   function init(){
-    if(introable){
+    if(introable && $(".tile_holder").length >0){
       initIntro();
       run();
     }
