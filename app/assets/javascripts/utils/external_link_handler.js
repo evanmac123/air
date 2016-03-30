2d1fb093af15ac648d8a8b8e5e80df9e0d288e3b
hraynaud <herby.raynaud@gmail.com>
2016-03-30 12:28:47 -0700

    Adds Handler

---
+++ app/assets/javascripts/utils/external_link_handler.js
@@ -0,0 +1,30 @@
+var Airbo = window.Airbo ||{};
+Airbo.Utils = Airbo.Utils || {};
+Airbo.Utils.ExternalLinkHandler= (function(){
+  var tilePresenceSelector =  ".tile_holder";
+
+
+
+  function init(){
+    if(Airbo.Utils.supportsFeatureByPresenceOfSelector(tilePresenceSelector)){
+      $('a').each(function() {
+        var a = new RegExp('/' + window.location.host + '/');
+        if(!a.test(this.href)) {
+          $(this).click(function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+            window.open(this.href, '_blank');
+          });
+        }
+      });
+     }
+  }
+
+  return {
+   init: init,
+  }
+}());
+
+$(function(){
+  Airbo.Utils.ExternalLinkHandler.init();
+});
