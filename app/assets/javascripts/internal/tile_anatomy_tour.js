d06242b4d3db52b4d6283ee17ed48592f312049a
hraynaud <herby.raynaud@gmail.com>
2016-04-15 12:45:29 -0400

    Makes Intro Hint behave the way we want

--- app/assets/javascripts/internal/tile_anatomy_tour.js
+++ app/assets/javascripts/internal/tile_anatomy_tour.js
@@ -1,4 +1,5 @@
-Airbo = window.Airbo ||{}
+Airbo = window.Airbo ||{};
+
 
 Airbo.TileAnswerIntro= (function(){
   var intro
@@ -6,42 +7,87 @@ Airbo.TileAnswerIntro= (function(){
     , anatomyIntroSelector= ".public-board .tile_holder"
     , anatomyCookie= "tile-anatomy"
     , anatomySeen = Airbo.CookieMonster.getCookie(anatomyCookie)
-    , pingName = {
-  "tile_holder": "Tile",
-  "tile_full_image": "Image",
-  "tile_headline": "Text",
-  "tile_supporting_content": "Interaction"
-    }
-    ;
+    , hintClicked = false
+  ;
 
-    function getPingName(el) {
-      for (var elClass in pingName) {
-        if( el.hasClass(elClass) ){
-          return pingName[elClass];
+  function setCookie(){
+    Airbo.CookieMonster.setCookie(currCookie, "true");
+  }
+
+  function repositionTooltip(){
+    setLeftOffset();
+    setTop();
+  }
+
+  function repositionPulse(){
+    var hintTop = parseInt($(".introjs-hint").css("top"));
+    $(".introjs-hint").css("top", (hintTop+2) +"px")
+  }
+
+  function styleTooltipButton(){
+    $(".introjs-tooltiptext .introjs-button").addClass("button").addClass("outlined");
+  }
+
+  function setTop(){
+    var newTop = (parseInt($(".introjs-tooltip").css("top")) +15 )+"px";
+    $(".introjs-tooltip").css("top", newTop);
+  }
+
+  function setLeftOffset(){
+    var refWidth = parseInt($(".introjs-tooltipReferenceLayer").css("width"));
+    var introWidth = parseInt($(".introjs-tooltip").css("width"));
+    var left = (refWidth-introWidth)/2-10 + "px";
+    $(".introjs-tooltip").css("left", left);
+  }
+
+  function showHintToolTip(){
+    $(".introjs-hint").trigger("click")
+  }
+
+  function initOnTileInteraction(){
+    $("body").on("click", ".right_multiple_choice_answer",  function(event){
+      var id= $(this).data("tile-id");
+      var board= $("body").data("board-id");
+      setCookie();
+      Airbo.Utils.ping("Marketing Page - Answered Tile", {"id":id, "game": board});
+      removeHint();
+    });
+  }
+
+  function removeHint(){
+    $(".introjs-hintReference").remove();
+    $(".introjs-hints").remove();
+  }
+
+  function disableHintExitOnOutsideClick(){
+    $("html").on("click","body",  function(event){
+      if($(".introjs-tooltip").is(":visible")){
+        if($(event.target).is("#next, #prev, .right_multiple_choice_answer, .headerMenu a, .logo_image")){
+          removeHint();
+        }else{
+          event.stopPropagation();
+          event.stopImmediatePropagation();
+          event.preventDefault();
         }
       }
-      return "";
-    }
+    });
+  }
 
-    function introAnatomyPing(el) {
-      name = getPingName(el);
-      Airbo.Utils.ping("Tile - Viewed", {"Tile Onboarding - Viewed": name});
-    }
+  function initRedraw(){
+    showHintToolTip();
+    styleTooltipButton();
+    repositionTooltip();
+    repositionPulse();
+  }
 
-    function initTileAnatomyIntro(){
-     var points = $("#tile_point_value").text();
-     var prompt = "This is the right answer. </br>Click to earn " + points + " points"
-      options = {
-        overlayOpacity: 0,
+  function initIntro() {
+
+    if(!anatomySeen && $(anatomyIntroSelector).length >0){
+      var points = $("#tile_point_value").text();
+      var prompt = "This is the right answer. </br>Click to earn " + points + " points"; 
+      var options = {
         tooltipClass: "simple",
         scrollToElement: false,
-        showStepNumbers: false, 
-        skipLabel: 'Exit Intro',
-        exitOnOverlayClick: false,
-        doneLabel: 'Got it',
-        nextLabel: 'Got it',
-        prevLabel: 'Back', scrollToElement: false,
-        steps:[],
         hints: [
           {
             element: $('.right_multiple_choice_answer')[0],
@@ -52,122 +98,75 @@ Airbo.TileAnswerIntro= (function(){
       };
 
       currCookie= anatomyCookie;
-      intro.setOptions(options);
-    }
- 
-    
-    function introViewTilePing() {
-      Airbo.Utils.ping("Viewed Parent Board", {"Saw Tile Prompt": true});
-    }
 
-    function setCookie(){
-      Airbo.CookieMonster.setCookie(currCookie, "true");
-    }
-
-    function initCloseOnTileInteraction(){
-      $("body").on("click", ".right_multiple_choice_answer",  function(){
-        $(".introjs-hintReference").remove();
-        $(".introjs-hints").remove();
-        exit();
-      });
-    }
-
-    function initIntro() {
       intro = introJs();
-      intro.onexit(function(targetElement){
-        setCookie();
-        introViewTilePing();
-      });
 
-      intro.oncomplete(function(targetElement){
-        setCookie();
-        introViewTilePing();
+      intro.setOptions(options);
+
+      intro.onhintclick(function(event) {
+        setTimeout(repositionTooltip, 0);
       });
 
       intro.onhintclose(function() {
         $(".introjs-hint").removeClass("introjs-hidehint")
       }); 
 
-      if(!anatomySeen && $(anatomyIntroSelector).length >0){
-        initTileAnatomyIntro();
-        introAnatomyPing(el);
-        intro.addHints();
-      }
+   
 
+      intro.addHints();
     }
+  }
 
 
-    function repositionTooltip(){
-      setLeftOffset();
-      setTop();
-    }
+  function init(){
+    initIntro();
+    initRedraw();
+    initOnTileInteraction();
+    disableHintExitOnOutsideClick()
+  }
 
-    function repositionPulse(){
-      var hintTop = parseInt($(".introjs-hint").css("top"));
-      $(".introjs-hint").css("top", (hintTop+2) +"px")
-    }
-
-    function styleTooltipButton(){
-      $(".introjs-tooltiptext .introjs-button").addClass("button").addClass("outlined");
-    }
-
-    function setTop(){
-      var newTop = (parseInt($(".introjs-tooltip").css("top")) +15 )+"px";
-      $(".introjs-tooltip").css("top", newTop);
-    }
-
-    function setLeftOffset(){
-      var refWidth = parseInt($(".introjs-tooltipReferenceLayer").css("width"));
-      var introWidth = parseInt($(".introjs-tooltip").css("width"));
-      var left = (refWidth-introWidth)/2-10 + "px";
-      $(".introjs-tooltip").css("left", left);
-    }
-
-    function showHintToolTip(){
-      $(".introjs-hint").trigger("click")
-    }
-
-    function initRedraw(){
-      showHintToolTip();
-      styleTooltipButton();
-      repositionTooltip();
-      repositionPulse();
-    }
-
-    function init(){
-      initCloseOnTileInteraction();
-      initIntro();
-      initRedraw();
-
-      $("html").on("click","body",  function(event){
-        if($(".introjs-tooltip").is(":visible")){
-          if($(event.target).is("#next, #prev, .right_multiple_choice_answer, .headerMenu a, .logo_image")){
-
-            $(".introjs-hintReference").remove();
-            $(".introjs-hints").remove();
-          }else{
-            event.stopPropagation();
-            event.stopImmediatePropagation();
-            event.preventDefault();
-          }
-        }
-      });
-
-    }
-
-    function exit(){
-      intro.exit();
-    }
-
-
-    return {
-      init: init,
-      exit: exit,
-    }
+  return {
+    init: init,
+  }
 
 }())
 
+
+Airbo.LandingPageHandler = (function(){
+
+  function initPingOnBoardOpen(){
+    $(".topic").click(function(event){
+      event.preventDefault();
+      var name = $(this).data("name");
+      Airbo.Utils.ping("Marketing Page - Viewed Board", {"name":name});
+      window.location = $(this).attr('href');
+    })
+  }
+
+
+  function initPingOnTileOpen() {
+    $(".tile_thumbnail a").click(function(event){
+      var id= $(this).data("id");
+      Airbo.Utils.ping("Marketing Page - Viewed Tile", {"id": id});
+      window.location = $(this).attr('href');
+    });
+  }
+
+  function init(){
+    initPingOnBoardOpen();
+    initPingOnTileOpen();
+  }
+
+
+  return {
+   init: init
+  }
+
+}())
+
+
 $(function(){
+  Airbo.LandingPageHandler.init();
   $(window).on("load", function(){
     Airbo.TileAnswerIntro.init();
   });
