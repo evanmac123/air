ea6ac084ceef9cd31c7dcacf7b60a70a44a6af54
hraynaud <herby.raynaud@gmail.com>
2016-05-05 12:50:12 -0700

    Adds support for using this with stock boards

--- app/assets/javascripts/internal/show_more_tiles_link.js
+++ app/assets/javascripts/internal/show_more_tiles_link.js
@@ -1,30 +1,40 @@
 var bindShowMoreTilesLink;
 
 bindShowMoreTilesLink = function(moreTilesSelector, tileSelector, spinnerSelector, targetSelector, updateMethod, afterRenderCallback) {
-  return $(moreTilesSelector).live('click', function(event) {
+   $(moreTilesSelector).on('click', function(event) {
     var offset;
+    var self = $(this);
+    var path = self.data('tile-path') || self.attr("href");
+    var contentType = self.data('contentType') 
     event.preventDefault();
-    if ($(this).attr('disabled') !== 'disabled') {
-      $(spinnerSelector).show();
-      offset = $(tileSelector).length;
-      return $.get($(this).data('tile-path'), {
-        offset: offset,
-        partial_only: 'true'
-      }, (function(data) {
-        $(spinnerSelector).hide();
-        switch (updateMethod) {
-          case 'append':
-            $(targetSelector).append(data.htmlContent);
-          break;
-          case 'replace':
-            $(targetSelector).replaceWith(data.htmlContent);
-        }
-        if (data.lastBatch) {
-          $(moreTilesSelector).attr('disabled', 'disabled');
-        }
-        if (typeof afterRenderCallback === 'function') {
-          return afterRenderCallback();
-        }
-      }));
+
+    if (self.attr('disabled') == 'disabled') {
+      return;
     }
+
+    $(spinnerSelector).show();
+
+    offset = $(tileSelector).length;
+
+    $.get(path, { offset: offset, partial_only: 'true' }, (function(data) {
+    var content = data.htmlContent || data;
+      $(spinnerSelector).hide();
+      switch (updateMethod) {
+        case 'append':
+          $(targetSelector).append(content);
+        break;
+        case 'replace':
+          $(targetSelector).replaceWith(content);
+      }
+
+      if (data.lastBatch) {
+        $(moreTilesSelector).attr('disabled', 'disabled');
+      }
+
+      if (typeof afterRenderCallback === 'function') {
+        afterRenderCallback();
+      }
+
+    }), contentType);
   });
+};
