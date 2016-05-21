5cd0867730799e96b848308504be85e2e396e803
hraynaud <herby.raynaud@gmail.com>
2016-05-20 21:48:53 -0400

    add demo and current_tile_ids

--- app/controllers/client_admin/stock_tiles_controller.rb
+++ app/controllers/client_admin/stock_tiles_controller.rb
@@ -3,8 +3,10 @@ class ClientAdmin::StockTilesController < ClientAdminBaseController
   def show
     @current_user = current_user
     @tile = Tile.find(params[:id])
-
-    @prev, @next = @tile.demo.bracket @tile
+    @demo = @tile.demo
+    @prev, @next = @demo.bracket @tile
+   
+    @current_tile_ids = @demo.tiles.active.order("activated_at desc").map(&:id)
   end
 
 
