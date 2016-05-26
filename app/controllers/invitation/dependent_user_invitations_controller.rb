f75be6ea461f39fa7e35687b1ac6453c33a25d53
avbrychak <a.v.brychak@gmail.com>
2016-05-26 16:16:09 -0400

    add response

--- app/controllers/invitation/dependent_user_invitations_controller.rb
+++ app/controllers/invitation/dependent_user_invitations_controller.rb
@@ -3,9 +3,10 @@ class Invitation::DependentUserInvitationsController < ApplicationController
     dependent_user = PotentialUser.where(email: permit_params[:email], game_referrer: current_user, demo_id: current_user.demo.dependent_board_id).first_or_create
     if dependent_user
       dependent_user.invite_as_dependent(permit_params[:subject], permit_params[:body])
-      render status: 200
+
+      head :ok
     else
-      render status: 404
+      head :unprocessable_entity and return
     end
   end
 
