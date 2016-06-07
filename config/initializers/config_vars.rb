01b8b5483c1939792ef41ced0bd14d6ad4df3bcd
hraynaud <herby.raynaud@gmail.com>
2016-06-07 16:47:26 -0400

    updates config var

--- config/initializers/config_vars.rb
+++ config/initializers/config_vars.rb
@@ -4,5 +4,5 @@ DEFAULT_DEMO_PARENT_BOARD = ENV['DEFAULT_DEMO_PARENT_BOARD'] || "healthplanbasic
 
 DEFAULT_INVITE_DEPENDENT_SUBJECT_LINE = ENV['DEFAULT_INVITE_DEPENDENT_SUBJECT'] || "invited you to join the"
 
-DEFAULT_INVITE_DEPENDENT_BODY = ENV['DEFAULT_INVITE_DEPENDENT_BODY'] || "Join the Board to learn about wellness and benefits at my company. Read, take action at the bottom of the Tiles, and earn points" 
+DEFAULT_INVITE_DEPENDENT_EMAIL_BODY = ENV['DEFAULT_INVITE_DEPENDENT_EMAIL_BODY'] || "Join the Board to learn about wellness and benefits at my company. Read, take action at the bottom of the Tiles, and earn points" 
 
