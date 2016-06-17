2c626fc70a7bfacab5db001e68b23afa14d65648
avbrychak <a.v.brychak@gmail.com>
2016-06-17 11:03:05 -0400

    fix space after video

--- app/assets/javascripts/internal/tile_builder_form/embed_video.js
+++ app/assets/javascripts/internal/tile_builder_form/embed_video.js
@@ -15,8 +15,6 @@ Airbo.EmbedVideo = (function() {
     Airbo.TileImagesMgr.removeImage(); // image with video icon
   }
   function addVideo(embedCode) {
-    // if( $(embedCode).prop("tagName") != "IFRAME" ) return;
-
     $("#image_uploader").hide();
     $(".video_section").show();
     $(".video_frame_block").html(embedCode);
@@ -33,28 +31,20 @@ Airbo.EmbedVideo = (function() {
   }
   function initFormEvents() {
     // form events
-    // $("body").on("click", ".clear_video", function() {
     $(".clear_video").click(function() {
       removeVideo();
     });
-    // $("body").on("click", ".video_placeholder", function() {
     $(".video_placeholder").click(function() {
       modalObj.open();
     });
   }
-  function isIframe(text) {
-    return $( text ).prop("tagName") == "IFRAME";
-  }
   function getValidCode(text) {
-    if( isIframe(text) ){
-      return text;
-    }
-    text = $(text).find("iframe").prop('outerHTML');
+    text = $(text).filter("iframe").prop('outerHTML') || $(text).find("iframe").prop('outerHTML');
     return text;
   }
   function initModalEvents() {
     // modal events
-    $("#embed_video_field").on("keyup paste", function() {
+    $("#embed_video_field").bind('input', function() {
       var embedCode =  getValidCode( $(this).val() );
       var blockSubmit = embedCode == undefined;
       if( !blockSubmit ) {
@@ -64,11 +54,10 @@ Airbo.EmbedVideo = (function() {
       $(".embed_video_err").toggle(blockSubmit);
     });
     submitVideo.click(function() {
-      modalObj.close();
-
       var embedCode = $("#embed_video_field").val();
       addVideo( embedCode );
-      $("#embed_video_field").val("");
+
+      modalObj.close();
     });
   }
   function initModalObj() {
@@ -76,6 +65,8 @@ Airbo.EmbedVideo = (function() {
       modalId: modalId,
       smallModal: true,
       onClosedEvent: function() {
+        $("#embed_video_field").val("");
+        $(".embed_video_err").hide();
         Airbo.TileFormModal.openModal();
       }
     });
@@ -97,6 +88,7 @@ Airbo.EmbedVideo = (function() {
     initVars();
     initModalObj();
     initModalEvents();
+    autosize( $("#embed_video_field") );
   }
   return {
    init: init,
