870d1dd86d1a94addb15675d27c91094f2014373
avbrychak <a.v.brychak@gmail.com>
2016-06-17 12:10:13 -0400

    set host and protocol for video image

--- app/decorators/tile_digest_decorator.rb
+++ app/decorators/tile_digest_decorator.rb
@@ -1,11 +1,14 @@
 class TileDigestDecorator < Draper::Decorator
+  include EmailHelper
   decorates :tile
   delegate_all
 
   def email_img_url
     url = object.thumbnail(:email_digest) # Paperclip returns the full url in staging and production modes
-    url.prepend('http://localhost:3000') if Rails.env.development? or Rails.env.test?
-    url
+    full_url = Addressable::URI.parse(url)
+    full_url.scheme = email_link_protocol
+    full_url.host = email_link_host
+    full_url.to_s
   end
 
   def email_link_options
