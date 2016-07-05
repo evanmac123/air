ced6fb0bdf71afa03baac4053ba693f70e81d19d
hraynaud <herby.raynaud@gmail.com>
2016-07-05 18:43:48 -0400

    More extract method refactorings

--- app/services/reporting/client_usage.rb
+++ app/services/reporting/client_usage.rb
@@ -4,34 +4,24 @@ module Reporting
   class ClientUsage
 
     def self.run demo, beg_date=3.months.ago, end_date =Date.today, interval="week"
+      data ={}
       start = beg_date.beginning_of_week
       finish = end_date.beginning_of_week
-
-      data = {
-        demo_id: demo,
-        beg_date: start,
-        end_date: finish,
-        activation:{}, 
-        tile_activity: {
-          posts:{}, 
-          views:{}, 
-          completions:{} 
-        }
-      }
-
-      initialize_data_set data, start, finish
+      initialize_report_data_hash demo,data,start, finish
 
       if demo
         do_user_activation data, demo, start, finish, interval
         do_tile_activity data, demo, start, finish, interval
       end
 
-      data
+      data #should be fully initialized with subkeys even if the demo is not provided #TODO throw argument error
 
     end
 
     class << self
+
       private
+
       def do_user_activation data, demo,start, finish, interval
         activation = Reporting::Db::UserActivation.new(demo,start, finish, interval)
 
@@ -61,25 +51,26 @@ module Reporting
           populate_stats res, data[:tile_activity][:completions], start
         end
 
-        #data[timestamp][:tile_activity][:views_over_available] =activity.views_over_available
-        #data[timestamp][:tile_activity][:completions_over_views] =activity.completions_over_views
       end
-
 
 
       def populate_stats res, stat, start, &block
-          timestamp= Date.parse(res.interval)
-          return if timestamp < start.to_date
-          period = stat[timestamp]
-          period[:current] = res.interval_count
-          period[:total] = res.cumulative_count
-          yield period if block_given?
+        timestamp= Date.parse(res.interval)
+        return if timestamp < start.to_date
+        period = stat[timestamp]
+        period[:current] = res.interval_count
+        period[:total] = res.cumulative_count
+        yield period if block_given?
       end
 
 
-      #Prepolutates empty hash for for each period interval of the date range
-      def initialize_data_set data, start, finish
-        r = RailsDateRange.new(start, finish).every(weeks:1)
+      def initialize_report_data_hash demo, data, start, finish
+        prepare_empty_hash demo, data, start, finish
+        init_intervals data, start, finish
+      end
+
+      def init_intervals data, start, finish
+       r = RailsDateRange.new(start, finish).every(weeks:1)
         r.each do |timestamp|
           d = timestamp.to_date
           init_activation data, d
@@ -87,19 +78,33 @@ module Reporting
         end
       end
 
+      def prepare_empty_hash demo, data, start, finish
+
+        data[:demo_id]= demo
+        data[:beg_date]= start
+        data[:end_date] = finish
+        data[:activation]={} 
+        data[:tile_activity]= {
+          posts:{}, 
+          views:{}, 
+          completions:{} 
+        }
+
+      end
+
 
       def init_activation data, d
-          activation = data[:activation][d]={}
-          activation[:current] =0 
-          activation[:total] =0 
+        activation = data[:activation][d]={}
+        activation[:current] =0 
+        activation[:total] =0 
       end
 
       def init_activity data, d
 
-          activity = data[:tile_activity]
-          activity[:posts][d]={current:0, total:0}
-          activity[:views][d]={current:0, total:0}
-          activity[:completions][d]={current:0, total:0}
+        activity = data[:tile_activity]
+        activity[:posts][d]={current:0, total:0}
+        activity[:views][d]={current:0, total:0}
+        activity[:completions][d]={current:0, total:0}
 
       end
 
