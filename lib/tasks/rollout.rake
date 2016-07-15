29bc485e6e23e7e0b7f08ac0ed2019c7bb988ee9
avbrychak <a.v.brychak@gmail.com>
2016-07-15 16:19:09 -0400

    activate hint for all users in group

--- lib/tasks/rollout.rake
+++ lib/tasks/rollout.rake
@@ -2,10 +2,7 @@ namespace :rollout do
   desc "Activates intro tooltip for first tile for 50% of users"
   task activate_first_tile_hint: :environment do
     $rollout.define_group(:new_users) do |user|
-      new_user = !user.tile_completions.first.present?
-      # we need half of new users
-      half = user.id % 2 == 0
-      new_user && half
+      !user.tile_completions.first.present?
     end
 
     $rollout.activate_group(:first_tile_hint, :new_users)
--- spec/models/user_intro_spec.rb
+++ spec/models/user_intro_spec.rb
@@ -10,7 +10,7 @@ describe UserIntro do
     after do
       $rollout.deactivate(FEATURE_NAME)
     end
-    it "should activate 50% of existing users without tile completions" do
+    it "should activate for existing users without tile completions" do
       user1 = FactoryGirl.create :user
       FactoryGirl.create :tile_completion, user: user1
       user2 = FactoryGirl.create :user
@@ -22,33 +22,26 @@ describe UserIntro do
       # users with completions don't have feature
       expect( $rollout.active?(FEATURE_NAME, user1) ).to be_false
       expect( $rollout.active?(FEATURE_NAME, user2) ).to be_false
-      # half of new users have feature
-      f3 = $rollout.active?(FEATURE_NAME, user3)
-      f4 = $rollout.active?(FEATURE_NAME, user4)
-      exclusive_or = (f3 || f4) && !(f3 && f4)
-      expect(exclusive_or).to be_true
+
+
+      expect( $rollout.active?(FEATURE_NAME, user3) ).to be_true
+      expect( $rollout.active?(FEATURE_NAME, user4) ).to be_true
     end
 
-    it "should activate 50% of new users" do
+    it "should activate for new users" do
       activate_first_tile_hint
 
       user3 = FactoryGirl.create :user
       user4 = FactoryGirl.create :user
 
-      # half of new users have feature
-      f3 = $rollout.active?(FEATURE_NAME, user3)
-      f4 = $rollout.active?(FEATURE_NAME, user4)
-      exclusive_or = (f3 || f4) && !(f3 && f4)
-      expect(exclusive_or).to be_true
+      expect( $rollout.active?(FEATURE_NAME, user3) ).to be_true
+      expect( $rollout.active?(FEATURE_NAME, user4) ).to be_true
     end
   end
 
   def activate_first_tile_hint
     $rollout.define_group(GROUP_NAME) do |user|
-      new_user = !user.tile_completions.first.present?
-      # we need half of new users
-      half = user.id % 2 == 0
-      new_user && half
+      !user.tile_completions.first.present?
     end
 
     $rollout.activate_group(FEATURE_NAME, GROUP_NAME)
