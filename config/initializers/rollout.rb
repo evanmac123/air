d960fde889da127d68b8e823ef986b68f8c5234a
hraynaud <herby.raynaud@gmail.com>
2016-07-19 00:29:56 -0400

    Adds rollout to initializer

--- config/initializers/rollout.rb
+++ config/initializers/rollout.rb
@@ -1,3 +1,9 @@
 require Rails.root.join("config/initializers/redistogo")
 
 $rollout = Rollout.new(Redis.new(url: ENV['REDISTOGO_URL']))
+
+$rollout.define_group(:no_tile_completions) do |user|
+  user.tile_completions.count==0
+end
+
+
