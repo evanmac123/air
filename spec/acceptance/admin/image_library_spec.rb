dc891f0e99d76b5605fddff823d7fc7ec95e05f4
hraynaud <herby.raynaud@gmail.com>
2016-08-04 10:06:24 -0400

    Cleans up test

--- spec/acceptance/admin/image_library_spec.rb
+++ spec/acceptance/admin/image_library_spec.rb
@@ -7,28 +7,20 @@ feature "Site Admin works with Image Library" do
     visit admin_tile_images_path(as: an_admin)
     expect_content "Image Library"
     attach_tile "tile_image[image]", tile_fixture_path('cov1.jpg')
-
     expect_content "Image was saved"
-    TileImage.count.should == 1
+    page.find_all(".tile_image_block img").length.should == 1
 
-    ti = TileImage.last
-    tile_image_block(ti).should be_present
-    # shows gears while resizing
-    tile_image_block(ti).find("img")[:src].should =~ /resizing_gears_fullsize/
   end
 
   scenario "deletes image", js: true do
-    FactoryGirl.create_list(:tile_image, 2)
+    t1, t2  =   FactoryGirl.create_list(:tile_image, 2)
     visit admin_tile_images_path(as: an_admin)
-    page.all(tile_image_selector).count.should == 2
 
-    ti = TileImage.first
-    within tile_image_block(ti) do
+    within tile_image_block(t1) do
       click_link "Delete"
     end
 
     expect_content "Image was destroyed"
     page.all(tile_image_selector).count.should == 1
-    TileImage.all.include?(ti).should be_false
   end
 end
