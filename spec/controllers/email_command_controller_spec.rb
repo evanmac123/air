95b8b1e0fe67ec1f04f62a3260b2339e52511ca2
Nick Weiland <nickweiland@gmail.com>
2016-08-04 16:22:27 -0700

    resolve controller specs

--- spec/controllers/email_command_controller_spec.rb
+++ spec/controllers/email_command_controller_spec.rb
@@ -87,31 +87,24 @@ describe EmailCommandController do
         end
 
         it "should have an appropriate non-monitored-email response, referring them to support" do
-          crank_dj_clear
           open_email(@user.email)
           current_email.to_s.should include("Sorry, you've replied to an unmonitored account. For assistance please contact support@airbo.com.")
         end
 
         it "should reply-to set to support" do
-          crank_dj_clear
           open_email(@user.email)
           current_email.reply_to.should == %w(support@airbo.com)
         end
 
         it "should have the correct from address" do
-          fancy_email = "Foo Inc. <foo@ourairbo.com>"
-          Demo.any_instance.stubs(:reply_email_address).returns(fancy_email)
-
-          crank_dj_clear
           open_email(@user.email)
 
-          current_email.to_s.should include(%!From: "Foo Inc." <foo@ourairbo.com>!)
-          current_email.from.should == %w(foo@ourairbo.com)
+          current_email.to_s.should include(%!From: Airbo <play@ourairbo.com>!)
+          current_email.from.should == %w(play@ourairbo.com)
         end
 
         it "should have a good default from address when the email is not from a user we recognize" do
           EmailCommand.last.update_attributes(user_id: nil)
-          crank_dj_clear
           open_email(@user.email)
 
           current_email.to_s.should include(%!From: Airbo <play@ourairbo.com>!)
