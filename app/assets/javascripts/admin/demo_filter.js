bf5d0e4819d774c62f5def913d2470d7f612fcce
Nick Weiland <nickweiland@gmail.com>
2016-08-05 22:15:04 -0700

    Optimize Search and Filter on /admin
    Allow search and filter to work together. Add paid and unpaid filters.
    
    Change all button clases in admin to primary instead of success.

--- app/assets/javascripts/admin/demo_filter.js
+++ app/assets/javascripts/admin/demo_filter.js
@@ -1,28 +1,51 @@
 $(document).ready(function() {
-  $(".active-filter").toggle();
+  $(document).on('click','.filter-button', function() {
+    $(this).addClass("active-filter").removeClass("filter-button").css({ "background-color": "#fff", "color": "#48bfff" });
 
-  var demoFilter = function(filter) {
-    $(".demo").each(function() {
-      $(this).show();
-      if (filter === "all") return;
-      var quality = $(this).attr('data-companySize');
-      if (quality != filter) {
-        $(this).toggle();
-      }
-    });
-  };
+    demoFilter();
+  });
 
-  var checkFilter = function(filter) {
-    if ($(".active-filter").val() != "all") {
-      demoFilter(filter);
-    }
-  };
+  $(document).on('click','.active-filter', function() {
+    $(this).removeClass("active-filter").addClass("filter-button").css({ "background-color": "", "color": "" });
 
-  $(document).on('click','.filter-button',function() {
-    $(".active-filter").toggle().removeClass("active-filter");
-
-    $(this).addClass("active-filter").toggle();
-
-    demoFilter($(this).val());
+    demoReset(this);
   });
 });
+
+var demoFilter = function() {
+  var currentSearch = $("#demo-search").val();
+  demoSearch(currentSearch);
+  
+  $(".demo:visible").each(function() {
+    var demo = this;
+
+    $(".active-filter").each(function() {
+      toggleDemos(demo, this);
+    });
+  });
+};
+
+var demoReset = function(filter) {
+  $(".demo:hidden").each(function() {
+    var demo = this;
+    toggleDemos(demo, filter);
+  });
+
+  demoFilter();
+};
+
+var toggleDemos = function(demo, filter) {
+  if ($(filter).data().type === "size") {
+    var size = $(demo).attr('data-companySize');
+    if (size != $(filter).val()) {
+      return $(demo).toggle();
+    }
+  }
+
+  if ($(filter).data().type === "paidStatus") {
+    var isPaid = $(demo).attr('data-isPaid');
+    if (isPaid != $(filter).data().paid.toString()) {
+      return $(demo).toggle();
+    }
+  }
+};
