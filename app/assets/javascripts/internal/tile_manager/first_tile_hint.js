4d7c474d9ece2caa626f5a88ca1ebdc8d25f35cb
hraynaud <herby.raynaud@gmail.com>
2016-08-11 10:53:35 -0400

    Refactors tile_hint object. Adds ajax call to update user intro model

--- app/assets/javascripts/internal/tile_manager/first_tile_hint.js
+++ app/assets/javascripts/internal/tile_manager/first_tile_hint.js
@@ -2,39 +2,60 @@ Airbo = window.Airbo || {};
 
 
 Airbo.FirstTileHint = (function(){
-  var config = {
+
+  var tileSelector = ".tile-wrapper"
+    ,pingTitle = "Tile Tooltip Seen", 
+    config = {
     showStepNumbers: false,
     scrollToElement: false,
     overlayOpacity: 0,
     doneLabel: "Got it"
   };
-  function initIntro() {
-    if($('.tile-wrapper').length == 0){
-      return;
-    }
-    var prompt = "Click on the Tile to begin.";
-    var options = {
-      steps: [
-        {
-          element: $('.tile-wrapper')[0],
-          intro: prompt,
-          position: 'top'
-        },
-      ],
-    };
-    options = $.extend({},config,options);
-    intro = introJs();
-    intro.setOptions(options);
-    intro.oncomplete(function() {
-      Airbo.Utils.ping("Tile Tooltip Seen", {action: "Clicked 'Got it'"});
-    });
 
-    Airbo.Utils.ping("Tile Tooltip Seen", {action: "Displayed"});
-    intro.start();
+  function hasTiles(){
+    return $(tileSelector).length > 0
   }
+
+  function initIntro() {
+    if(hasTiles()) {
+      var options = {
+        steps: [
+          {
+            element: $('.tile-wrapper')[0],
+            intro: "Click on the Tile to begin.",
+            position: 'top'
+          },
+        ],
+      };
+
+      options = $.extend({},config,options);
+      intro = introJs();
+      intro.setOptions(options);
+      intro.oncomplete(function() {
+        Airbo.Utils.ping("Tile Tooltip Seen", {action: "Clicked 'Got it'"});
+        setFirstTileHintIntroToSeen()
+      });
+
+      Airbo.Utils.ping("Tile Tooltip Seen", {action: "Displayed"});
+      intro.start();
+    }
+  }
+
+  function setFirstTileHintIntroToSeen(){
+    $.ajax({
+      url: '/user_intros',
+      type: 'PUT',
+      data: {"intro": "displayed_first_tile_hint"},
+      success: function(response) {
+      },
+      error
+    });
+  }
+
   function init() {
     initIntro();
   }
+  
   return {
     init: init
   }
