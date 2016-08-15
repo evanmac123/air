b3b698e93d80134e9c15f57c881876a1b27a7671
hraynaud <herby.raynaud@gmail.com>
2016-08-15 19:05:04 -0400

    Rewrites spec to be more clear about the functionality under test.

--- spec/acceptance/client_admin/managing_tiles/tile_manager_and_tile_carousel_spec.rb
+++ spec/acceptance/client_admin/managing_tiles/tile_manager_and_tile_carousel_spec.rb
@@ -1,113 +1,133 @@
 require 'acceptance/acceptance_helper'
 
-feature 'The order of the tiles in the Tile Manager and the Tile Carousel are in synch' do
-  # Both of these helpers grab all of the tiles, in order, whether visible or not
+feature 'The order of the tiles in the tile mangaer and carousel are in synch', js:true do
 
-  def carousel_content
-    page.all('.headline .text').collect { |tile| tile.text }
-  end
+  let(:admin){FactoryGirl.create :client_admin}
+  let(:demo) {admin.demo}
+  let(:tiles) {[]}
 
-  def viewer_content
-    find('#slideshow').find('.tile_image')[:alt]
-  end
-
-  #------------------------------------------------------------------------------------
-
-  def check_manager(manager_tiles)
-    section_tile_headlines('#active').should == manager_tiles
-  end
-
-  def click_next_button
-    page.find('#next').click
-  end
-
-  # Doesn't matter what tile you click on; just want to get from the Carousel to the Viewer => called with different active tiles
-  def check_carousel_and_viewer(manager_tiles, carousel_tile)
-    user_tiles = manager_tiles.flatten
-
-    visit activity_path
-    carousel_content.should == user_tiles
-
-    click_carousel_tile(carousel_tile)
-    click_next_button
-
-    user_tiles.each do |user_tile|
-      next if user_tile == carousel_tile.headline
-      viewer_content.should == user_tile
-      click_next_button
-    end
-  end
-
-  #------------------------------------------------------------------------------------
-
-  scenario 'Bounce back and forth between the Manager and the Carousel', js: true do#, driver: :selenium do
-    admin = FactoryGirl.create :client_admin
-    demo = admin.demo
-
-    tiles = []
+  before do
+    UserIntro.any_instance.stubs(:displayed_first_tile_hint).returns(true)
     1.upto(4) { |i| tiles << FactoryGirl.create(:tile, demo: demo, headline: "Tile #{i}", created_at: Time.now + i.days) }
-
     bypass_modal_overlays(admin)
-    signin_as(admin, admin.password)
-
-    # -----------------------------------------------------------------------------
-
-    visit tile_manager_page
-    manager_tiles =  ["Tile 4", "Tile 3", "Tile 2", "Tile 1"]
-
-    check_manager(manager_tiles)
-    check_carousel_and_viewer(manager_tiles, Tile.first)
-
-    # -----------------------------------------------------------------------------
-    # Change the tile order in the Manager => should see the corresponding change in the Carousel and Viewer
-    #
-    # We now tweak the attributes directly rather than going through the manager
-    # page, since we can't easily hover over and click "Archive" via Poltergiest
-
-    Tile.find_by_headline('Tile 3').update_status('archive')
-    visit tile_manager_page
-
-    manager_tiles =  ["Tile 4", "Tile 2", "Tile 1"]
-    check_manager(manager_tiles)
-    check_carousel_and_viewer(manager_tiles, Tile.first)
-
-    # -----------------------------------------------------------------------------
-    # Re-activate the archived tile => should move to the head of the list
-    # See previous note about why we update_attributes for this
-    Tile.find_by_headline('Tile 3').update_status('active')
-
-    visit tile_manager_page
-    manager_tiles =  ["Tile 3", "Tile 4", "Tile 2", "Tile 1"]
-
-    check_manager(manager_tiles)
-    check_carousel_and_viewer(manager_tiles, Tile.find_by_headline("Tile 3"))
-
-    # -----------------------------------------------------------------------------
-    # Change the tile order in the Manager => should see the corresponding change in the Carousel and Viewer
-    Tile.find_by_headline('Tile 4').update_status('archive')
-    Tile.find_by_headline('Tile 1').update_status('archive')
-    visit tile_manager_page
-    manager_tiles =  ["Tile 3", "Tile 2"]
-
-    check_manager(manager_tiles)
-    check_carousel_and_viewer(["Tile 3", "Tile 2"], Tile.find_by_headline("Tile 3"))
-
-    #-------------------------------------------------------------------------------
-    # Re-activate the archived tile => should move to the head of the list
-    Tile.find_by_headline('Tile 1').update_status('active')
-    visit tile_manager_page
-    manager_tiles =  ["Tile 1", "Tile 3", "Tile 2"]
-
-    check_manager(manager_tiles)
-    check_carousel_and_viewer(manager_tiles, Tile.active.last)
-
-    #-------------------------------------------------------------------------------
-    # Re-activate the archived tile => should move to the head of the list
-    Tile.find_by_headline('Tile 4').update_status('active')
-    visit tile_manager_page
-    manager_tiles =  ["Tile 4", "Tile 1", "Tile 3", "Tile 2"]
-
-    check_manager(manager_tiles)
-    check_carousel_and_viewer(manager_tiles, Tile.active.last)
   end
+
+  context "carousel" do
+    scenario "archiving one tile" do
+      active_tile_headlines_order =  ["Tile 4", "Tile 2", "Tile 1"]
+
+      tiles[2].update_status('archive')
+
+      signin_as(admin, admin.password)
+      visit tile_manager_page
+
+      check_carousel_and_viewer(active_tile_headlines_order, Tile.first)
+    end
+
+    scenario "when reactivating one tile" do
+      active_tile_headlines_order =  ["Tile 3", "Tile 4", "Tile 2", "Tile 1"]
+
+      tiles[2].update_status('archive')
+      tiles[2].update_status('active')
+
+      signin_as(admin, admin.password)
+      visit tile_manager_page
+
+      check_manager(active_tile_headlines_order)
+      check_carousel_and_viewer(active_tile_headlines_order, tiles[2])
+   end
+
+
+   scenario "when reactivation mutiple tiles" do
+     active_tile_headlines_order =  ["Tile 4", "Tile 1", "Tile 3", "Tile 2"]
+
+     tiles[3].update_status('archive')
+     tiles[0].update_status('archive')
+     tiles[0].update_status('active')
+     tiles[3].update_status('active')
+
+     signin_as(admin, admin.password)
+     visit tile_manager_page
+
+     check_manager(active_tile_headlines_order)
+     check_carousel_and_viewer(active_tile_headlines_order, Tile.active.last)
+   end
+  end
+
+  context 'Tile Mananager' do
+   #NOTE really should be a client side using a js framework
+
+    it "displays recently created tiles in reversse creation order" do
+      active_tile_headlines_order =  ["Tile 4", "Tile 3", "Tile 2", "Tile 1"]
+
+      signin_as(admin, admin.password)
+      visit tile_manager_page
+      check_manager(active_tile_headlines_order)
+      check_carousel_and_viewer(active_tile_headlines_order, Tile.first)
+    end
+
+
+    scenario "Moves archived tile moves from active to archived section", js:true do
+      #FIXME this is a good candidate for pure js test.
+      signin_as(admin, admin.password)
+
+      visit tile_manager_page
+
+      selector= "#single-tile-#{tiles[2].id}.tile_thumbnail>.tile-wrapper"
+
+      within "#active_tiles" do
+        page.find(selector).hover
+        within selector do
+          within(".tile_buttons") do
+            click_link "Archive"
+          end
+        end
+      end
+
+      within "#archived_tiles" do
+        expect(page).to have_content(tiles[2].headline)
+      end
+
+      within "#active_tiles" do
+        expect(page).to have_no_content(tiles[2].headline)
+      end
+    end
+
+
+
+  end
+
+    def carousel_content
+      page.all('.headline .text').collect { |tile| tile.text }
+    end
+
+    def viewer_content
+      find('#slideshow').find('.tile_image')[:alt]
+    end
+
+
+    def check_manager(active_tile_headlines_order)
+      section_tile_headlines('#active').should == active_tile_headlines_order
+    end
+
+    def click_next_button
+      page.find('#next').click
+    end
+
+    # Doesn't matter what tile you click on; just want to get from the Carousel to the Viewer => called with different active tiles
+    def check_carousel_and_viewer(active_tile_headlines_order, carousel_tile)
+      user_tiles = active_tile_headlines_order
+
+      visit activity_path
+      carousel_content.should == user_tiles
+
+      click_carousel_tile(carousel_tile)
+      click_next_button
+
+      user_tiles.each do |user_tile|
+        next if user_tile == carousel_tile.headline
+        viewer_content.should == user_tile
+        click_next_button
+      end
+    end
 end
