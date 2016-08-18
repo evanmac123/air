bbc05259c522b1ff90910cdbb6893428a55ba30b
hraynaud <herby.raynaud@gmail.com>
2016-08-18 10:01:56 -0400

    Adds update action

--- app/controllers/client_admin/tiles_follow_up_email_controller.rb
+++ app/controllers/client_admin/tiles_follow_up_email_controller.rb
@@ -1,4 +1,9 @@
 class ClientAdmin::TilesFollowUpEmailController < ClientAdminBaseController
+
+  def update
+
+  end
+
   def destroy
     @follow_up_email = current_user.demo.follow_up_digest_emails.find params[:id]
     @follow_up_email.destroy
