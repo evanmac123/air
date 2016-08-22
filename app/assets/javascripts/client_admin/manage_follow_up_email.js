a8487d7f9af83c3cb4944357f5f446132ce1809f
hraynaud <herby.raynaud@gmail.com>
2016-08-22 09:09:53 -0400

    Implements update, save, cancel and send now functionality in js an controller

--- app/assets/javascripts/client_admin/manage_follow_up_email.js
+++ app/assets/javascripts/client_admin/manage_follow_up_email.js
@@ -2,52 +2,139 @@ var Airbo = window.Airbo || {}
 
 
 Airbo.DigestEmailFollowUpManager = (function(){
-  var commandSelector = ".commands .button"
-    , editSelector = ".commands .edit"
-    , saveSelector = ".commands .save"
-    , nowSelector = ".commands .now"
-    , deleteSelector = ".commands .delete"
+  var cmdSelector = ".commands .button"
   ;
 
-  function initToggleEditForm(){
-    $(editSelector).on("click", function(event){
-      event.preventDefault();
-      var row = $(this).parents("tr");
-      row.find("input").prop("disabled", false) 
-    });
-  }
 
-  function initSendNow(){
-    $(editSelector).on("click", function(event){
-      event.preventDefault();
-      var row = $(this).parents("tr");
-      row.find("input").prop("disabled", false) 
-    });
-  }
+  function initCommands(){
 
-  function initCommandHander(){
-    $(commandSelector).on("click", function(event){
+    $(cmdSelector).on("click", function(event){
       event.preventDefault();
-      var command = $(this)
-        , row = $(this).parents("tr")   
+
+      var cmd = $(this)
+        , row = $(this).parents("tr")
+        , inputs = row.find("input")
       ;
 
-      switch(command){
-      case  
-      case
-      case
+      switch(cmd.data("action")){
+        case "edit":
+          edit(cmd, inputs);
+        break
+        case "save":
+          save(cmd, inputs );
+        break;
+        case "cancel":
+          cancel(cmd, inputs );
+        break;
+        case "now":
+          now(cmd);
+        break;
+        case "destroy":
+          destroy(cmd);
+        break;
       }
-      row.find("input").prop("disabled", false) 
     });
   }
 
-  function initSave(){
+
+  function toggleEditMode(cmd, inputs){
+    var hiddenCmdCss
+      , disableEditing
+    ;
+
+    if(cmd.hasClass("edit")) {
+      hiddenCmdCss = ".save, .cancel"
+      visibleCmdCss = ".now, .destroy"
+      disableEditing=false; 
+    }else{
+      hiddenCmdCss = ".edit, .now, .destroy";
+      visibleCmdCss = ".save, .cancel"
+      disableEditing=true; 
+    }
+
+    cmd.hide();
+    cmd.siblings(hiddenCmdCss).show();
+    cmd.siblings(visibleCmdCss).hide();
+    inputs.prop("disabled", disableEditing);
+  }
+
+  function execute(meth, url, data, success, fail){
+    $.ajax({
+      type: meth,
+      url: url,
+      data: data 
+    })
+    .done(success)
+    .fail(fail)
+  }
+ 
+
+  function handleRemoval(cmd){
+    var tableBody = cmd.parents("tbody");
+    cmd.parents("tr").remove();
+
+    if (tableBody.children("tr:not(.no-follow-up)").length == 0){
+      tableBody.children("tr.no-follow-up").show();
+    }
+  }
+
+  function restoreValues(inputs){
+    inputs.each(function(index, input){
+      var $input =  $(input);
+      $input.val($input.data("original-val"));
+    }) 
+  }
+
+
+  function edit(cmd, inputs){
+    toggleEditMode(cmd, inputs)
+  }
+
+  function save(cmd, inputs){
+    function ok(data, status, xhr){
+      toggleEditMode(cmd,inputs)
+      console.log("saved");
+    }
+
+    function failed(xhr, status, error){
+    }
+
+    execute("PUT", cmd.attr("href"), inputs.serialize(), ok, failed);
+  }
+
+
+  function now(cmd){
+    function ok(data, status, xhr){
+      handleRemoval(cmd)
+    }
+
+    function failed(){
+      console.log("unabled to send now");
+    }
+
+    execute("PUT", cmd.attr("href"), {now: "true"}, ok, failed);
+  }
+
+  function destroy(cmd){
+    function ok(data, status, xhr){
+      handleRemoval(cmd)
+      Airbo.Utils.ping("Followup - Cancelled", data)
+    }
+
+    execute("DELETE", cmd.attr("href"),{}, ok);
 
   }
+
+  function cancel(cmd, inputs){
+    toggleEditMode(cmd,inputs)
+    restoreValues(inputs)
+  }
+
 
   function init(){
-    initToggleEditForm();
+    initCommands();
   }
+  
 
   return {
     init: init,
--- app/controllers/client_admin/tiles_follow_up_email_controller.rb
+++ app/controllers/client_admin/tiles_follow_up_email_controller.rb
@@ -1,18 +1,43 @@
 class ClientAdmin::TilesFollowUpEmailController < ClientAdminBaseController
+  before_filter :get_followup, only:[:update, :destroy]
 
   def update
+    if params[:now].present? 
+      @follow_up_email.trigger_deliveries
+      #@follow_up_email.destroy
+      head :ok
+    else
+      @follow_up_email.update_attributes(params.permit(:original_digest_subject, :send_on))
+      @follow_up_email.save
+      render json: update_response
+    end
 
   end
 
   def destroy
-    @follow_up_email = current_user.demo.follow_up_digest_emails.find params[:id]
-    @follow_up_email.destroy
-    schedule_followup_cancelled_ping
+    #@follow_up_email.destroy
+    render json: delete_response
   end
 
-  protected
+  private
+
+  def get_followup
+    @follow_up_email = current_user.demo.follow_up_digest_emails.find params[:id]
+  end
+
+  def send_now
+    @follow_up_email = current_user.demo.follow_up_digest_emails.find params[:id]
+  end
 
   def schedule_followup_cancelled_ping
     ping 'Followup - Cancelled', {}, current_user
+  end
+
+  def delete_response
+    @follow_up_email.as_json(root: false, only: [:id, :demo_id, :send_on])
+  end
+
+  def update_response
+    @follow_up_email.as_json(root: false, only: [:original_digest_subject,  :send_on])
   end
 end
