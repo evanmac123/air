004e1d697baaad7e2273bcba0f11e8465241588d
hraynaud <herby.raynaud@gmail.com>
2016-08-24 21:40:52 -0400

    WIP siteadmin nav

--- app/assets/javascripts/app-admin.js
+++ app/assets/javascripts/app-admin.js
@@ -30,3 +30,8 @@
 //= requre ./admin/contracts
 //= require ./admin/demo_search
 //= require ./admin/demo_filter
+
+//= require ../../../vendor/assets/javascripts/external/modernizr
+//= require ../../../vendor/assets/javascripts/external/foundation.min
+
+$(document).foundation();
