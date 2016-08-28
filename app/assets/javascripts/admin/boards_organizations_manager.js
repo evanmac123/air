efa17ad58df566e849b8753a3d2154aa8e55fa53
hraynaud <herby.raynaud@gmail.com>
2016-08-28 01:31:53 -0400

    Adds unlink functionality

--- app/assets/javascripts/admin/boards_organizations_manager.js
+++ app/assets/javascripts/admin/boards_organizations_manager.js
@@ -1,2 +1,44 @@
-var Airbo
+Airbo.BoardsAndOrganizationMgr = (function(){
 
+  function unlink(url){
+    $.ajax({
+      url: url,
+      type: "PUT",
+      data: {organization_id: nil}
+    })
+    .done(function(){
+      console.log("unlink worked");
+    })
+    .fail(function(){
+      console.log("unlink failed");
+    });
+  }
+
+  function initLink(){
+    $(".admin-demo .unlink").click(function(event){
+      event.preventDefault();
+      var self = $(this);
+
+      function doit(){
+        unlink(self.attr("href"));
+      }
+
+      if(confirm("Are you sure")){
+        doit();
+      }
+      //Airbo.Utils.approve("are you sure?", doit)
+    });
+  }
+
+  function init(){
+    debugger
+    if($(".admin-demo").length>0){
+      initLink();
+    }
+  }
+
+return{
+  init: init
+};
+
+}())
