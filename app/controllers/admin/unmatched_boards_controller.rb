ffb13464d4b8ab8a8dd69f827a807f24d117c8cd
hraynaud <herby.raynaud@gmail.com>
2016-08-29 19:29:25 -0400

    Adds flash and removes pry

--- app/controllers/admin/unmatched_boards_controller.rb
+++ app/controllers/admin/unmatched_boards_controller.rb
@@ -9,9 +9,8 @@ class Admin::UnmatchedBoardsController < AdminBaseController
  def update
    org = Organization.find(params[:organization])
    demos = Demo.where({id: params[:matched]})
-   binding.pry
    demos.update_all(organization_id: org.id)
-
+   flash[:success]="Boards successfull linked to #{org.name}"
    redirect_to admin_unmatched_boards_path
  end
 
