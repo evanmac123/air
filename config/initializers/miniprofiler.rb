4381bb1a4048ef5335d41031ba47bce3a5a870f7
Nick Weiland <nickweiland@gmail.com>
2016-08-29 20:06:50 -0700

    Reenable miniprof

--- config/initializers/miniprofiler.rb
+++ config/initializers/miniprofiler.rb
@@ -1,10 +1,10 @@
-# require 'rack-mini-profiler'
-#
-# Rack::MiniProfilerRails.initialize!(Rails.application)
-#
-# Rails.application.middleware.delete(Rack::MiniProfiler)
-# Rails.application.middleware.insert_after(Rack::Deflater, Rack::MiniProfiler)
-# uri = URI.parse(ENV["REDISTOGO_URL"] ||"redis://127.0.0.1:6379/0")
-# Rack::MiniProfiler.config.storage_options = { :host => uri.host, :port => uri.port,    :password => uri.password }
-# Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
-# PROFILABLE_USERS = ENV['PROFILABLE_USERS'].try(:split, ",") || []
+require 'rack-mini-profiler'
+
+Rack::MiniProfilerRails.initialize!(Rails.application)
+
+Rails.application.middleware.delete(Rack::MiniProfiler)
+Rails.application.middleware.insert_after(Rack::Deflater, Rack::MiniProfiler)
+uri = URI.parse(ENV["REDISTOGO_URL"] ||"redis://127.0.0.1:6379/0")
+Rack::MiniProfiler.config.storage_options = { :host => uri.host, :port => uri.port,    :password => uri.password }
+Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
+PROFILABLE_USERS = ENV['PROFILABLE_USERS'].try(:split, ",") || []
