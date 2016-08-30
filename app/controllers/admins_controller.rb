373fef08b7c5ec73fee2980b504a25fdc3428f41
hraynaud <herby.raynaud@gmail.com>
2016-08-29 22:32:22 -0400

    Changes admin home

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,6 +1,7 @@
 class AdminsController < AdminBaseController
   def show
     @current_user = current_user
+    @organizations = Organization.all
     @demos = Demo.list
     render template: 'admin/show'
   end
