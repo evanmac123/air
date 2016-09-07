fab5df0eb4f160ed1c61d615f610a0d829c7b068
Nick Weiland <nickweiland@gmail.com>
2016-09-07 16:38:00 -0700

    Finish front end mock up with tiles mock

--- app/controllers/client_admin/board_stats_grids_controller.rb
+++ app/controllers/client_admin/board_stats_grids_controller.rb
@@ -1,18 +1,17 @@
 class ClientAdmin::BoardStatsGridsController < ClientAdminBaseController
   def index
-    @tile = Tile.find(params[:tile_id])
+    @tile = Tile.first
     grid_builder = BoardStatsGrid.new(@tile, params["grid_type"], params[:answer_filter])
     @board_stats_grid = initialize_grid(*grid_builder.args)
     @current_grid = grid_builder.query_type
 
-    export_grid_if_requested('tile_stats_grid' => 'grid') do
-      # if the request is not a CSV export request
+    export_grid_if_requested('board_stats_grid' => 'grid') do
       render json: { grid: grid_to_string, success: true }
     end
   end
 
   def new_completions_count
-    tile = Tile.find(params[:tile_id])
+    tile = Tile.first
     start_time = Time.at params[:start_time_in_ms].to_i/1000
     count = tile.tile_completions.where{created_at >= start_time }.count
     text = count > 0 ? "Load #{count} new event".pluralize(count) : ""
@@ -22,7 +21,7 @@ class ClientAdmin::BoardStatsGridsController < ClientAdminBaseController
   protected
     def grid_to_string
       render_to_string(
-        partial: 'grid_section',
+        partial: 'client_admin/activity/activity_grid_section',
         formats: [:html]
       )
     end
