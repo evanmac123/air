8e2097d174f33a81ffe022b56bbf20f926d437bb
hraynaud <herby.raynaud@gmail.com>
2016-09-08 15:33:53 -0400

    Cleans up action

--- app/controllers/client_admin/reports_controller.rb
+++ app/controllers/client_admin/reports_controller.rb
@@ -7,13 +7,11 @@ class ClientAdmin::ReportsController < ClientAdminBaseController
   end
 
   def temporary_create
-    @board = Tile.first
-    @chart_form = BoardStatsLineChartForm.new @board, params[:board_stats_line_chart_form]
-    @chart = BoardStatsChart.new(@chart_form.period, @chart_form.plot_data).draw
+     @demo = current_user.demo
+     @chart_form = BoardStatsLineChartForm.new(@demo,  params[:board_stats_line_chart_form])
+     @chart = BoardStatsChart.new(@chart_form.period, @chart_form.plot_data).draw
+
     render json: { chart: chart_to_string, success: true}
-    # @demo = current_user.demo
-    # @chart_form = BoardStatsLineChartForm.new @demo, {action_type: params[:action_type]}
-    # @chart = BoardStatsChart.new(@chart_form.period, @chart_form.plot_data).draw
   end
 
   private
