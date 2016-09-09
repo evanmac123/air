9e09aebb4ff1821a4bf3cd08950645ecd41c95d4
Nick Weiland <nickweiland@gmail.com>
2016-09-09 10:57:23 -0700

    Uncomment grid partial

--- app/controllers/client_admins_controller.rb
+++ app/controllers/client_admins_controller.rb
@@ -4,6 +4,7 @@ class ClientAdminsController < ClientAdminBaseController
     @board = current_user.demo
     @chart_form = BoardStatsLineChartForm.new @board, {action_type: params[:action_type]}
     @chart = BoardStatsChart.new(@chart_form.period, @chart_form.plot_data).draw
+
     grid_builder = BoardStatsGrid.new(@board)
     @board_stats_grid = initialize_grid(*grid_builder.args)
     @current_grid = grid_builder.query_type
