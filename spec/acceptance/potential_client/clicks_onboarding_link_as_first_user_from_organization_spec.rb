5c6bd9d89715454d4921fdbb2ddb0b7dd9270d18
hraynaud <herby.raynaud@gmail.com>
2016-09-15 13:11:32 -0400

    Enhances onboarding initializing spec

--- spec/acceptance/potential_client/clicks_onboarding_link_as_first_user_from_organization_spec.rb
+++ spec/acceptance/potential_client/clicks_onboarding_link_as_first_user_from_organization_spec.rb
@@ -1,11 +1,24 @@
 require 'acceptance/acceptance_helper'
 
 feature "Potential client recieves an invitation to try airbo and clicks link in email", js: true do
+  before do
+    FactoryGirl.create_list(:topic_board, 3, :valid, is_reference: true)
+  end
+
   context 'As the first potential client in an organization' do
     context "when I click the onboarding link in the email I received from airbo" do
       it "takes me through the entire onboarding flow" do
         visit onboarding_email_link
+        expect(page).to have_css(".topic-board", count: TopicBoard.reference_board_set.count)
+
+        within ".topic-boards" do 
+          page.find(".topic-board", text: TopicBoard.reference_board_set.first.topic_name).click
+        end
+
+        expect(current_path).to eq("/myairbo/#{UserOnboarding.first.id}")
       end
+
+
     end
   end
 
