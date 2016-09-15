ec49d64256b5b2aa2b3acc8d7cee74d32847ca71
hraynaud <herby.raynaud@gmail.com>
2016-09-15 16:41:15 -0400

    Adds tile creation to spec to verify tile count

--- spec/acceptance/potential_client/clicks_onboarding_link_as_first_user_from_organization_spec.rb
+++ spec/acceptance/potential_client/clicks_onboarding_link_as_first_user_from_organization_spec.rb
@@ -4,7 +4,10 @@ require 'acceptance/acceptance_helper'
 feature "Potential client recieves an invitation to try airbo and clicks link in email", js: true do
   before do
     FactoryGirl.create_list(:topic_board, 3, :valid, is_reference: true)
+    @topic_board = TopicBoard.reference_board_set.first
+    FactoryGirl.create_list(:multiple_choice_tile, 3, :active, demo: @topic_board.board )
   end
+
 
   context 'As the first potential client in an organization' do
     context "when I click the onboarding link in the email I received from airbo" do
@@ -12,16 +15,15 @@ feature "Potential client recieves an invitation to try airbo and clicks link in
         visit onboarding_email_link
         expect(page).to have_css(".topic-board", count: TopicBoard.reference_board_set.count)
 
-        topic_board = TopicBoard.reference_board_set.first
 
         within ".topic-boards" do
-          page.find(".topic-board", text: topic_board.topic_name).click
+          page.find(".topic-board", text: @topic_board.topic_name).click
         end
 
         expect(current_path).to eq("/myairbo/#{UserOnboarding.first.id}")
 
         within "#tile_wall" do
-          expect(page).to have_css(".tile_container", count: topic_board.tiles.count)
+          expect(page).to have_css(".tile-wrapper", count: @topic_board.tiles.count)
         end
       end
     end
