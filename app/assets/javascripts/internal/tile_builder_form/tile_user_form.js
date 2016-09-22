7038221eee777adf811caaa21b86f934794b4e24
hraynaud <herby.raynaud@gmail.com>
2016-09-22 17:17:06 -0400

    Disables suggestion box functionality for IE8

--- app/assets/javascripts/internal/tile_builder_form/tile_user_form.js
+++ app/assets/javascripts/internal/tile_builder_form/tile_user_form.js
@@ -7,15 +7,20 @@ Airbo.TileUserForm = (function(){
   function initEvents() {
     $("#submit_tile").click(function(e) {
       e.preventDefault();
+     
+      if(Airbo.Utils.isOldIE()){
+        alert("You must upgrade to the newest version of Internet Explorer or use a different broswer suchs as Google Chrome, Apple Safari, or Mozilla Firefox to use this feature");
+        return;
+      }
 
       url = $(this).attr("href");
-
       tileForm = Airbo.TileFormModal;
       tileForm.init(Airbo.TileUserForm);
       tileForm.open(url);
     });
   }
   function init() {
+
     initEvents();
   }
   return {
