64335c866ba34aabf9e81ecde8acc5319726a789
hraynaud <herby.raynaud@gmail.com>
2016-09-30 10:26:30 -0400

    refactors test

--- spec/controllers/onboardings_controller_spec.rb
+++ spec/controllers/onboardings_controller_spec.rb
@@ -1,13 +1,19 @@
 require 'spec_helper'
 
 describe OnboardingsController do
+  let(:valid_params){{ 
+    name: "Test Name", email: "test@test.com", organization: "Test Company", topic_board_id: Demo.first.id } }
+  before do
+    Demo.create({name: "Reference Board"})
+  end
+
   describe '#create' do
     context 'with valid params' do
+
       it "redirects to Onboardings#show" do
         post :create, valid_params
 
         expect(response.status).to eq(302)
-
         expect(response).to redirect_to("/myairbo/#{Onboarding.last.id}")
       end
     end
@@ -16,7 +22,7 @@ describe OnboardingsController do
       context 'missing email' do
         it "redirects to root" do
           post :create, valid_params.except(:email)
-
+          expect(response.cookies[:user_onboarding]).to_not be_nil
           expect(response.status).to eq(302)
 
           expect(response).to redirect_to(root_path)
@@ -45,11 +51,4 @@ describe OnboardingsController do
     end
   end
 
-  def valid_params
-      {
-        name: "Test Name",
-        email: "test@test.com",
-        organization: "Test Company"
-      }
-  end
 end
