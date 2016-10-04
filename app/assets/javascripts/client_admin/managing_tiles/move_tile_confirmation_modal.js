5abfc0c0ebc097bceacae9fa63bd5610bd0e33db
hraynaud <herby.raynaud@gmail.com>
2016-10-04 18:20:12 -0400

    Code clean up

--- app/assets/javascripts/client_admin/managing_tiles/move_tile_confirmation_modal.js
+++ app/assets/javascripts/client_admin/managing_tiles/move_tile_confirmation_modal.js
@@ -3,16 +3,16 @@ window.moveTileConfirmationModal = function() {
   $(".move-tile-confirm .cancel").click(function(e) {
     e.preventDefault();
     window.moveConfirmationDeferred.reject();
-    return $(".move-tile-confirm").foundation('reveal', 'close');
+    $(".move-tile-confirm").foundation('reveal', 'close');
   });
 
 
   $(".move-tile-confirm .confirm").click(function(e) {
     e.preventDefault();
-    return $(".move-tile-confirm").foundation('reveal', 'close');
+    $(".move-tile-confirm").foundation('reveal', 'close');
   });
 
-  return $(document).on('close.fndtn.reveal', '.move-tile-confirm', function() {
-    return window.moveConfirmationDeferred.resolve();
+  $(document).on('close.fndtn.reveal', '.move-tile-confirm', function() {
+    window.moveConfirmationDeferred.resolve();
   });
 };
