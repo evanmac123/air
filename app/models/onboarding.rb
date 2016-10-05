c663daca0cbc62f6fb77b75aec16c0e3642ae7c6
hraynaud <herby.raynaud@gmail.com>
2016-10-05 15:21:05 -0400

    Adds quick and dirty purge method to clear while testing

--- app/models/onboarding.rb
+++ app/models/onboarding.rb
@@ -2,4 +2,16 @@ class Onboarding < ActiveRecord::Base
   belongs_to :organization
   belongs_to :board, class_name: Demo, foreign_key: :demo_id
   has_many :user_onboardings, dependent: :destroy
+
+  
+  private
+
+  def purge
+     user_onboardings.delete_all
+     board.delete
+     organization.delete
+     board.users.delete_all
+     self.destroy
+  end
+
 end
