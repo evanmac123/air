6c0b9cb0b2db02f3ca6fb2518e519867771bf22c
Nick Weiland <nickweiland@gmail.com>
2016-10-19 13:46:25 -0700

    Update pings

--- app/assets/javascripts/client_admin/onboarding/onboarding_kpis.js
+++ app/assets/javascripts/client_admin/onboarding/onboarding_kpis.js
@@ -2,23 +2,23 @@ var Airbo = window.Airbo || {};
 
 Airbo.OnboardingKpis = (function(){
   function selectPriorityPing() {
-    Airbo.Utils.ping("Onboarding", { kpi: "selects priority", user_onboarding_id: $(".onboarding-body").data("id"), user_onboarding_state: $(".onboarding-body").data("state") });
+    Airbo.Utils.ping("Onboarding: Step 1", { kpi: "selects priority", user_onboarding_id: $(".onboarding-body").data("id"), user_onboarding_state: $(".onboarding-body").data("state") });
   }
 
   function completeTilePing() {
-    Airbo.Utils.ping("Onboarding", { kpi: "completes tile", user_onboarding_id: $(".onboarding-body").data("id"), user_onboarding_state: $(".onboarding-body").data("state") });
+    Airbo.Utils.ping("Onboarding: Step 2", { kpi: "completes tile", user_onboarding_id: $(".onboarding-body").data("id"), user_onboarding_state: $(".onboarding-body").data("state") });
   }
 
   function viewsActivityDashboardPing() {
-    Airbo.Utils.ping("Onboarding", { kpi: "views activity dashboard", user_onboarding_id: $(".onboarding-body").data("id"), user_onboarding_state: $(".onboarding-body").data("state") });
+    Airbo.Utils.ping("Onboarding: Step 3", { kpi: "views activity dashboard", user_onboarding_id: $(".onboarding-body").data("id"), user_onboarding_state: $(".onboarding-body").data("state") });
   }
 
   function answersMoreInfoQuestionPing(response) {
-    Airbo.Utils.ping("Onboarding", { kpi: "answers more info cta", user_onboarding_id: $(".onboarding-body").data("id"), user_onboarding_state: $(".onboarding-body").data("state"),  response: response });
+    Airbo.Utils.ping("Onboarding: Step 4", { kpi: "answers more info cta", user_onboarding_id: $(".onboarding-body").data("id"), user_onboarding_state: $(".onboarding-body").data("state"),  response: response });
   }
 
   function fullyConvertedPing() {
-    Airbo.Utils.ping("Onboarding", { kpi: "fully converts to client admin" });
+    Airbo.Utils.ping("Onboarding: Step 5", { kpi: "fully converts to client admin" });
   }
 
   return {
