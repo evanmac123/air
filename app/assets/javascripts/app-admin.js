4d8665056fc2339774cd90ac566122713904c225
hraynaud <herby.raynaud@gmail.com>
2016-10-21 10:40:05 -0400

    Removes highcharts from gem

--- app/assets/javascripts/app-admin.js
+++ app/assets/javascripts/app-admin.js
@@ -33,8 +33,6 @@
 //= require ./admin/cheer
 //= require ../../../vendor/assets/javascripts/chosen.jquery.min
 //= require ../../../vendor/assets/javascripts/external/modernizr
-//= require highcharts/highcharts
-//= require highcharts/modules/exporting
 //
 $(document).ready(function($) {
   $('.sidebar-accordion').find('.sidebar-accordion-toggle').click(function(){
