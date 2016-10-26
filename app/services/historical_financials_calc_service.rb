73a72d005bb88228e8864f5a044b47685d14b34b
hraynaud <herby.raynaud@gmail.com>
2016-10-26 16:16:12 -0400

    moves methods to protected

--- app/services/historical_financials_calc_service.rb
+++ app/services/historical_financials_calc_service.rb
@@ -1,6 +1,8 @@
 class HistoricalFinancialsCalcService < FinancialsCalcService
   attr_accessor :report_date, :sdate, :edate, :last_week
 
+  protected
+
   def churned_customers
     Organization.possible_churn_during_period(sdate, edate).select{|o|o.contracts.where("start_date >  && start_date < ",edate, sdate.advance(weeks: 1) )}
   end
