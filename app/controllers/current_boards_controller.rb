6735176dc6d16d8f3198dd51f18717903f351ef5
Nick Weiland <nickweiland@gmail.com>
2016-10-27 17:14:10 -0700

    Implemet solution

--- app/controllers/current_boards_controller.rb
+++ app/controllers/current_boards_controller.rb
@@ -1,12 +1,15 @@
 class CurrentBoardsController < ApplicationController
   def update
+    binding.pry
     is_admin = current_user.is_client_admin? || current_user.is_site_admin?
 
     current_user.move_to_new_demo(params[:board_id])
 
     ping "Switched Board", {client_admin: is_admin, user: !is_admin}, current_user
 
-    if current_user.is_client_admin? || current_user.is_site_admin?
+    if params[:admin_view_board]
+      redirect_to activity_path
+    elsif current_user.is_client_admin? || current_user.is_site_admin?
       redirect_to :back
     else
       redirect_to activity_path
