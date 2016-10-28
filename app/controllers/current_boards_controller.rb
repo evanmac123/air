30f65efb0b9411bbdc7d625e86787a5765ba940e
Nick Weiland <nickweiland@gmail.com>
2016-10-27 17:15:12 -0700

    Remove pry

--- app/controllers/current_boards_controller.rb
+++ app/controllers/current_boards_controller.rb
@@ -1,6 +1,5 @@
 class CurrentBoardsController < ApplicationController
   def update
-    binding.pry
     is_admin = current_user.is_client_admin? || current_user.is_site_admin?
 
     current_user.move_to_new_demo(params[:board_id])
