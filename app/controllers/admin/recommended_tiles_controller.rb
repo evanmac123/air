08942126fbeb04d892db1c0189f803fd819fffec
hraynaud <herby.raynaud@gmail.com>
2016-11-08 18:06:09 -0500

    Adds better error handling

--- app/controllers/admin/recommended_tiles_controller.rb
+++ app/controllers/admin/recommended_tiles_controller.rb
@@ -15,10 +15,14 @@ class Admin::RecommendedTilesController < AdminBaseController
 
 
   def destroy
-    t = RecommendedTile.find(params[:id])
+    #NOTE the use where instead of find to avoid 404 if record is not found
+    t = RecommendedTile.where({id:params[:id]}).first
     if t
       t.delete
       render json: {id: t.tile_id, path: admin_recommended_tiles_path}, status: :ok
+    else
+      response.headers["X-Message"]="Recommended Tile not found"
+      head :unprocessible_entity
     end
   end
 
