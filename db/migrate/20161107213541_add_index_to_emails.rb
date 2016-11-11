b11e4870f68c0d6df6cb6b3b7e26eb66b180e656
hraynaud <herby.raynaud@gmail.com>
2016-11-11 11:43:06 -0500

    disable index creation migration

--- db/migrate/20161107213541_add_index_to_emails.rb
+++ db/migrate/20161107213541_add_index_to_emails.rb
@@ -1,9 +1,9 @@
 class AddIndexToEmails < ActiveRecord::Migration
   def up
-    add_index :users, :email, unique: true
+    #add_index :users, :email, unique: true
   end
 
   def down
-    remove_index :users, :email
+    #remove_index :users, :email
   end
 end
