be884fce20a13927d0f8331042435083a982bc63
Nick Weiland <nickweiland@gmail.com>
2016-11-11 11:57:10 -0800

    Fix homes controller bug

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,8 +2,10 @@ class HomesController < ApplicationController
   def show
     if request.cookies["user_onboarding"].present? && !current_user.user_onboarding.completed
       redirect_to user_onboarding_path(current_user.user_onboarding.id, return_onboarding: true)
-    else
+    elsif current_user.is_client_admin? || current_user.is_site_admin?
       redirect_to explore_path, :format => :html
+    else
+      redirect_to activity_path
     end
   end
 end
