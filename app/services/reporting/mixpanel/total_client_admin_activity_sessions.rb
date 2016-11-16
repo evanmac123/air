ed2f8ecd65f7eb867a0f6b4547009e4ca70785af
hraynaud <herby.raynaud@gmail.com>
2016-11-16 10:34:46 -0500

    Refactors out super class to remove duplication

--- app/services/reporting/mixpanel/total_client_admin_activity_sessions.rb
+++ app/services/reporting/mixpanel/total_client_admin_activity_sessions.rb
@@ -2,12 +2,7 @@ require 'airbo_mixpanel_client'
 require 'reporting/mixpanel'
 module Reporting
   module Mixpanel
-    class TotalClientAdminActivitySessions < Report 
-
-      def initialize opts
-        super(configure(opts))
-        @endpoint = "segmentation"
-      end
+    class TotalClientAdminActivitySessions < TotalByEventType 
 
       def configure opts
         opts.merge!({
@@ -18,24 +13,6 @@ module Reporting
         })
       end
 
-      def count_series
-        series.each do |date|
-          @summary_by_date[date] = with_events_by_date(date)
-        end
-        @summary_by_date
-      end
-
-      def values_for_segment
-        result_data.fetch("values", {})
-      end
-
-      def values
-        values_for_segment.values
-      end
-
-      def endpoint
-        @endpoint
-      end
     end
   end
 end
