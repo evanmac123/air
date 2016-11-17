35d0c8639e80d59faa80477acfe9065df1788051
Nick Weiland <nickweiland@gmail.com>
2016-11-17 10:45:21 -0800

    Revise date format for kpi report

--- app/services/reporting/client_kpi_report.rb
+++ app/services/reporting/client_kpi_report.rb
@@ -333,7 +333,7 @@ module Reporting
       end
 
       def week
-        Date.today.end_of_week + 1.day
+        (Date.today.end_of_week + 1.day).strftime("%m/%d/%y")
       end
 
       def adjust_current_percent_by_count(percent_hash)
