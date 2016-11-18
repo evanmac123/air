79b14e2e63d675cea5efbbc7b8f72778dfd3e227
Nick Weiland <nickweiland@gmail.com>
2016-11-17 17:16:10 -0800

    Update mixplanel data

--- app/assets/javascripts/client_admin/onboarding/onboarding_kpis.js
+++ app/assets/javascripts/client_admin/onboarding/onboarding_kpis.js
@@ -1,40 +1,42 @@
 var Airbo = window.Airbo || {};
 
 Airbo.OnboardingKpis = (function(){
-  var id;
-  var email;
-  var ip;
-  var currentUser;
-
   function initializeOnboardingPing() {
-    var properties = $.extend({ onboarding_id: id, onboarding_email: email, ip: ip}, currentUserData);
+    var properties = mixpanelData();
     Airbo.Utils.ping("Onboarding: Step 1", properties);
-    debugger
   }
 
   function selectPriorityPing() {
-    var properties = $.extend({ onboarding_id: id, onboarding_email: email, ip: ip}, currentUserData);
+    var properties = mixpanelData();
     Airbo.Utils.ping("Onboarding: Step 2", properties);
   }
 
   function completeTilePing() {
-    var properties = $.extend({ onboarding_id: id, onboarding_email: email, ip: ip}, currentUserData);
+    var properties = mixpanelData();
     Airbo.Utils.ping("Onboarding: Step 3", properties);
   }
 
   function viewsActivityDashboardPing() {
-    var properties = $.extend({ onboarding_id: id, onboarding_email: email, ip: ip}, currentUserData);
+    var properties = mixpanelData();
     Airbo.Utils.ping("Onboarding: Step 4", properties);
   }
 
   function answersMoreInfoQuestionPing() {
-    var properties = $.extend({ onboarding_id: id, onboarding_email: email, ip: ip}, currentUserData);
+    var properties = mixpanelData();
     Airbo.Utils.ping("Onboarding: Step 5", properties);
   }
 
   function fullyConvertedPing() {
-    var properties = $.extend({ onboarding_id: id, onboarding_email: email, ip: ip}, currentUserData);
+    var properties = mixpanelData();
     Airbo.Utils.ping("Onboarding: Step 6", properties);
+  }
+
+  function mixpanelData() {
+    var id    = $(".onboarding-body").data("id");
+    var email = $(".onboarding-body").data("email");
+    var ip    = $(".onboarding-body").data("ip");
+    var currentUser = $("body").data("currentUser");
+    return $.extend({ onboarding_id: id, onboarding_email: email, ip: ip}, currentUser);
   }
 
   return {
@@ -46,12 +48,3 @@ Airbo.OnboardingKpis = (function(){
     fullyConvertedPing: fullyConvertedPing,
   };
 }());
-
-$(function(){
-  if( $(".onboarding-body").length > 0 ) {
-    id    = $(".onboarding-body").data("id");
-    email = $(".onboarding-body").data("email");
-    ip    = $(".onboarding-body").data("ip");
-    currentUser = $("body").data("currentUser");
-  }
-});
