204da1259440536d52a7a40d8162aaaeac52841d
Nick Weiland <nickweiland@gmail.com>
2016-11-17 17:28:34 -0800

    Change onboarding kpi pings

--- app/assets/javascripts/client_admin/onboarding/onboarding_kpis.js
+++ app/assets/javascripts/client_admin/onboarding/onboarding_kpis.js
@@ -2,41 +2,41 @@ var Airbo = window.Airbo || {};
 
 Airbo.OnboardingKpis = (function(){
   function initializeOnboardingPing() {
-    var properties = mixpanelData();
-    Airbo.Utils.ping("Onboarding: Step 1", properties);
+    var properties = onboardingMixpanelData(1, "lands on onboarding");
+    Airbo.Utils.ping("Onboarding", properties);
   }
 
   function selectPriorityPing() {
-    var properties = mixpanelData();
-    Airbo.Utils.ping("Onboarding: Step 2", properties);
+    var properties = onboardingMixpanelData(2, "selects priority");
+    Airbo.Utils.ping("Onboarding", properties);
   }
 
   function completeTilePing() {
-    var properties = mixpanelData();
-    Airbo.Utils.ping("Onboarding: Step 3", properties);
+    var properties = onboardingMixpanelData(3, "completes tile");
+    Airbo.Utils.ping("Onboarding", properties);
   }
 
   function viewsActivityDashboardPing() {
-    var properties = mixpanelData();
-    Airbo.Utils.ping("Onboarding: Step 4", properties);
+    var properties = onboardingMixpanelData(4, "views activity dashboard");
+    Airbo.Utils.ping("Onboarding", properties);
   }
 
   function answersMoreInfoQuestionPing() {
-    var properties = mixpanelData();
-    Airbo.Utils.ping("Onboarding: Step 5", properties);
+    var properties = onboardingMixpanelData(5, "answers more info cta");
+    Airbo.Utils.ping("Onboarding", properties);
   }
 
   function fullyConvertedPing() {
-    var properties = mixpanelData();
-    Airbo.Utils.ping("Onboarding: Step 6", properties);
+    var properties = onboardingMixpanelData(6, "fully converts to client admin");
+    Airbo.Utils.ping("Onboarding", properties);
   }
 
-  function mixpanelData() {
+  function onboardingMixpanelData(step, kpi) {
     var id    = $(".onboarding-body").data("id");
     var email = $(".onboarding-body").data("email");
     var ip    = $(".onboarding-body").data("ip");
     var currentUser = $("body").data("currentUser");
-    return $.extend({ onboarding_id: id, onboarding_email: email, ip: ip}, currentUser);
+    return $.extend({ onboarding_id: id, onboarding_email: email, ip: ip, step: step, kpi: kpi }, currentUser);
   }
 
   return {
