ed7a694389253d0f1a4d3804a2059814a68fc6c6
hraynaud <herby.raynaud@gmail.com>
2016-11-21 11:18:21 -0500

    Adds utility method for determining current user role

--- app/assets/javascripts/airbo.js
+++ app/assets/javascripts/airbo.js
@@ -16,6 +16,12 @@ var Airbo = {
      return Airbo.currentUser.user_type === "site admin";
     },
 
+    userNotSiteAdmin: function(){
+      return !(this.userIsSiteAdmin());
+    },
+
+
+
     currentUser: function(){
        return $("body").data("current-user"); 
     },
@@ -86,7 +92,7 @@ var Airbo = {
     },
 
     ping: function(event, properties) {
-      if (Airbo.Utile.userIsSiteadmin === false){
+      if (Airbo.Utils.userNotSiteAdmin === true){
         mixpanel.track(event, properties);
       }
     },
