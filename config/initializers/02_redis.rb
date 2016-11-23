990bbe950db9c0dc3cc9e099cfe8706f823f8b1d
Nick Weiland <nickweiland@gmail.com>
2016-11-23 15:59:19 -0800

    Add helper method to find non-cache keys in redis with Redis.non_cache_keys

--- config/initializers/02_redis.rb
+++ config/initializers/02_redis.rb
@@ -4,3 +4,11 @@ when 'production', 'staging', "production_local"
 when 'test',  'development'
   $redis = Redis.new(:host => 'localhost', :port => 6379)
 end
+
+class Redis
+  class << self
+    def non_cache_keys
+      $redis.scan(0, { match: "[^cache]*" })[1]
+    end
+  end
+end
