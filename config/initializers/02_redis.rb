7585211c2566d45a9bb8e6a49ffa10f15512c6e7
Nick Weiland <nickweiland@gmail.com>
2016-11-23 16:14:38 -0800

    Update redis

--- config/initializers/02_redis.rb
+++ config/initializers/02_redis.rb
@@ -2,7 +2,7 @@ case Rails.env
 when 'production', 'staging', "production_local"
   $redis = Redis.new(url: ENV['REDISTOGO_URL'])
 when 'test',  'development'
-  $redis = Redis.new(:host => 'localhost', :port => 6379)
+  $redis = Redis.new(host: 'localhost', port: 6379)
 end
 
 class Redis
