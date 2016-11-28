e07e69aba80401647b00a6636733404228c1b9a3
Nick Weiland <nickweiland@gmail.com>
2016-11-28 09:24:20 -0800

    Update redis implementation

--- config/initializers/02_redis.rb
+++ config/initializers/02_redis.rb
@@ -12,3 +12,24 @@ class Redis
     end
   end
 end
+
+Nest.class_eval do
+  def initialize(key, redis = $redis)
+    super(key.to_param)
+    @redis = redis
+  end
+
+  def [](key)
+    self.class.new("#{self}:#{key.to_param}", @redis)
+  end
+end
+
+ActiveRecord::Base.class_eval do
+  def rdb
+    Nest.new(self.class.name)[id]
+  end
+
+  def self.rdb
+    Nest.new(name)
+  end
+end
