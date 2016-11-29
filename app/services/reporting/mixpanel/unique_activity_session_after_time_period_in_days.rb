bbb684676ab3f4682183002f1eba828334c59690
hraynaud <herby.raynaud@gmail.com>
2016-11-29 10:39:56 -0500

    finishes data summary

--- app/services/reporting/mixpanel/unique_activity_session_after_time_period_in_days.rb
+++ app/services/reporting/mixpanel/unique_activity_session_after_time_period_in_days.rb
@@ -15,6 +15,44 @@ module Reporting
 
       end
 
+      def init_data_hash
+
+        series.inject(@summary_by_date) do |h, k| 
+          h[k]={"30"=>{},"60"=>{},"120"=>{}}
+          h
+        end
+      end
+
+      def reported_by_date
+        series.each do |date|
+          values.each do |period, data|
+           @summary_by_date[date][period]=data[date]
+          end
+        end
+      end
+
+      def values_for_date date
+        summary_by_date[data]
+      end
+
+      private
+
+      def sample_data
+
+        {
+          "legend_size"=> 3, 
+          "data"=> {
+            "series"=> ["2016-10-24", "2016-10-31", "2016-11-07", "2016-11-14", "2016-11-21", "2016-11-28"], 
+            "values"=> {
+              "30"=> {"2016-11-28"=> 1, "2016-11-21"=> 0, "2016-10-24"=> 0, "2016-11-07"=> 0, "2016-10-31"=> 0, "2016-11-14"=> 0}, 
+              "20"=> {"2016-11-28"=> 1, "2016-11-21"=> 0, "2016-10-24"=> 0, "2016-11-07"=> 0, "2016-10-31"=> 0, "2016-11-14"=> 0}, 
+              "120"=> {"2016-11-28"=> 1, "2016-11-21"=> 0, "2016-10-24"=> 0, "2016-11-07"=> 0, "2016-10-31"=> 0, "2016-11-14"=> 0}
+            }
+          }
+        }
+      end
+
     end
   end
 end
+
