8e2229c17050085e8dcf1a20e8ec2543bc61024f
hraynaud <herby.raynaud@gmail.com>
2016-11-30 15:20:47 -0500

    Makes default mix panel report object conform weekly report behavior or mix panel

--- app/services/reporting/mixpanel.rb
+++ app/services/reporting/mixpanel.rb
@@ -3,6 +3,8 @@ module Reporting
 
 
     class Report
+      attr_reader :from, :to
+
       def initialize opts
         parse_dates(configure(opts))
         @params= config.reverse_merge!(opts).with_indifferent_access
@@ -32,18 +34,24 @@ module Reporting
 
 
       def parse_dates opts
-        @from = opts.delete(:from_date) || Date.today.beginning_of_week(:sunday).prev_week(:sunday).at_midnight
+        @from = opts.delete(:from_date) || Date.today.beginning_of_week(:monday)
         @to = opts.delete(:to_date) || @from.end_of_week(:sunday).end_of_day
       end
 
       def config
         {
-          from_date: date_format(@from), 
-          to_date:  date_format(@to),
+          from_date: formatted_from, 
+          to_date: formatted_to
         }
       end
 
+      def formatted_from
+        date_format(@from)
+      end
 
+      def formatted_to
+        date_format(@to)
+      end
 
       private
       def date_format d
