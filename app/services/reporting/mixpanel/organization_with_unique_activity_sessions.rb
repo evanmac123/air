bbdf16d2945ddaffd6bdcf32c9f77826aab89ce7
hraynaud <herby.raynaud@gmail.com>
2016-11-30 18:15:10 -0500

    Sets proper super class and restores to weekly data set

--- app/services/reporting/mixpanel/organization_with_unique_activity_sessions.rb
+++ app/services/reporting/mixpanel/organization_with_unique_activity_sessions.rb
@@ -2,13 +2,13 @@ require 'airbo_mixpanel_client'
 require 'reporting/mixpanel'
 module Reporting
   module Mixpanel
-    class OrganizationWithUniqueActivitySessions < UniqueEventsBase
+    class OrganizationWithUniqueActivitySessions < UniqueSegementedEventsBase
 
       def configure opts
         opts.merge!({
           event: "Activity Session - New",
           on: %Q|string(properties["organization"])|,
-          unit: 'day',
+          unit: 'week',
           where:%Q|(properties["user_type"] == "client admin") and (properties["board_type"] == "Paid") and properties["is_test_user"] == false|,
           type: 'unique',
         })
