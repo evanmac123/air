1f92d3a4658f4d6e1fc85071bee2a57e5b7723e1
hraynaud <herby.raynaud@gmail.com>
2016-11-30 17:13:54 -0500

    Refactors to remove unused methods and adds included modules

--- app/services/reporting/mixpanel/total_by_event_type.rb
+++ app/services/reporting/mixpanel/total_by_event_type.rb
@@ -3,29 +3,8 @@ require 'reporting/mixpanel'
 module Reporting
   module Mixpanel
     class TotalByEventType < Report 
-
-      def initialize opts
-        super
-      end
-
-      def count_series
-        series.each do |date|
-          @summary_by_date[date] = with_events_by_date(date)
-        end
-        @summary_by_date
-      end
-
-      def values_for_segment
-        result_data.fetch("values", {})
-      end
-
-      def values
-        values_for_segment.values
-      end
-
-      def endpoint
-        "segmentation"
-      end
+      include MixpanelUnsegmentedResult
+      include Segmentation
     end
   end
 end
