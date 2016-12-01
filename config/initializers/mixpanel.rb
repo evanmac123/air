72d1c026969d442868ff9d1efe0bddbedda777fa
hraynaud <herby.raynaud@gmail.com>
2016-12-01 12:05:25 -0500

    Updates funnel_id

--- config/initializers/mixpanel.rb
+++ config/initializers/mixpanel.rb
@@ -7,7 +7,7 @@ MIXPANEL_TOKEN = ENV['MIXPANEL_TOKEN'] || '05a30c487f3f60afa2e2a3876a4c05d6'
 MP_HOMPAGE_TAG_VERSION=ENV['MP_HOMPAGE_TAG_VERSION'] || "8/1/2016"
 
 if Rails.env.development? || Rails.env.production_local? ||RACK_ENV="development"
-  MIXPANEL_FUNNEL_REPORTS={"Tile Creation" => 2080300}
+  MIXPANEL_FUNNEL_REPORTS={"Tile Creation" => 2087554}
 elsif RACK_ENV="staging"
   MIXPANEL_FUNNEL_REPORTS={"Tile Creation" => ""}
 elsif RACK_ENV="production"
