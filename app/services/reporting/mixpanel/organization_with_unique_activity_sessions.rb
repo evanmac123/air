7a829a59cde834eb1052e6ed693964faa0d11281
hraynaud <herby.raynaud@gmail.com>
2016-11-30 20:33:47 -0500

    Fixes typo in class name

--- app/services/reporting/mixpanel/organization_with_unique_activity_sessions.rb
+++ app/services/reporting/mixpanel/organization_with_unique_activity_sessions.rb
@@ -2,7 +2,7 @@ require 'airbo_mixpanel_client'
 require 'reporting/mixpanel'
 module Reporting
   module Mixpanel
-    class OrganizationWithUniqueActivitySessions < UniqueSegementedEventsBase
+    class OrganizationWithUniqueActivitySessions < UniqueSegmentedEventsBase
 
       def configure opts
         opts.merge!({
