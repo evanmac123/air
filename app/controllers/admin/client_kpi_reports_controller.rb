e993aeb605f276ab5c31cfaa4cb2d1ef138856d9
hraynaud <herby.raynaud@gmail.com>
2016-12-01 14:18:16 -0500

    Fills out customer success kpi dashboard

--- app/controllers/admin/client_kpi_reports_controller.rb
+++ app/controllers/admin/client_kpi_reports_controller.rb
@@ -1,5 +1,19 @@
 class Admin::ClientKpiReportsController < AdminBaseController
+  before_filter :get_dates
+
   def show
-    @report_data = Reporting::ClientKPIReport.get_weekly_report_data
+    rep = Reporting::ClientKPIReport.new
+    @table_data = rep.get_data_by_date(@sdate, @edate)
+    @dates = @table_data["report_date"]["values"]
   end
+
+  private
+
+  def get_dates
+    if !(@sdate && @edate)
+      @sdate, @edate = Reporting::ClientKPIReport.new.default_date_range
+    end
+  end
+
+
 end
