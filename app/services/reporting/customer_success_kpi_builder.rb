441cd7fddbb9cfea27ead31386efa29e9fa1c69c
hraynaud <herby.raynaud@gmail.com>
2016-12-05 13:05:34 -0500

    Adds tiles added fields to customer_success_kpi fields

--- app/services/reporting/customer_success_kpi_builder.rb
+++ app/services/reporting/customer_success_kpi_builder.rb
@@ -68,6 +68,16 @@ module Reporting
 
       kpi.average_tile_creation_time = avg_tile_creation_time
 
+
+
+      kpis.total_tiles_added_by_paid_client_admin = total_tiles_added
+      kpis.total_tiles_added_from_copy_by_paid_client_admin = total_tiles_added_from_copy
+      kpis.total_tiles_added_from_scratch_by_paid_client_admin = total_tiles_added_from_scratch
+      kpis.percent_of_added_tiles_from_copy = percent_of_tiles_added_from_copy
+      kpis.percent_of_added_tiles_from_scratch = percent_of_tiles_added_from_scratch
+      kpis.unique_orgs_that_added_tiles = orgs_that_added_tiles
+      kpis.percent_orgs_that_added_tiles = percent_of_orgs_that_added_tiles 
+
       kpi.percent_retained_post_activation_30_days = retention_by_days("30")
       kpi.percent_retained_post_activation_60_days =  retention_by_days("60")
       kpi.percent_retained_post_activation_120_days =  retention_by_days("120")
@@ -156,27 +166,27 @@ module Reporting
       tiles_added_by_paid_client_admins.sum
     end
 
-    def total_tiles_added_created_from_scratch
+    def total_tiles_added_from_scratch
       tiles_added_by_paid_client_admins.results_by_segment["Self Created"]
     end
 
-    def total_tiles_added_copied
+    def total_tiles_added_from_copy
       tiles_added_by_paid_client_admins.results_by_segment["Explore Page"]
     end
 
     def percent_of_tiles_added_created_from_scratch
-      calc_percent(total_tiles_added_created_from_scratch,total_tiles_added)
+      calc_percent(total_tiles_added_from_scratch,total_tiles_added)
     end
 
-    def percent_of_tiles_added_copied
-      calc_percent(total_tiles_added_copied,total_tiles_added)
+    def percent_of_tiles_added_from_copy
+      calc_percent(total_tiles_added_from_copy,total_tiles_added)
     end
 
     def orgs_that_added_tiles
       @orgs_that_added_tiles ||= Reporting::Mixpanel::UniqueOrganizationsThatAddedTiles.new(opts).get_count
     end
 
-    def percent_of_orgs_that_added_tile
+    def percent_of_orgs_that_added_tiles
       calc_percent(orgs_that_added_tiles, total_paid_orgs)
     end
 
