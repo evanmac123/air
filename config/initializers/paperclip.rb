93fa4a5b931735f766b07441051fc12027d2e536
Nick Weiland <nickweiland@gmail.com>
2016-12-06 10:54:36 -0800

    Update Paperclip design ALL ENVS

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -3,65 +3,90 @@
 case Rails.env
 when 'production', 'staging', "production_local"
 
-  ATTACHMENT_CONFIG_BASE = {
-    storage: :s3,
-    s3_protocol: 'https',
-    s3_credentials: S3_CREDENTIALS,
-    s3_headers: {'Expires' => 1.year.from_now.httpdate, 'Cache-Control' => 'max-age=315576000'},
-    url:         ":s3_domain_url",
-    hash_secret: "Kid Sister Diary Secure",
-  }
+  Paperclip::Attachment.default_options.merge!(
+    {
+      storage: :s3,
+      s3_protocol: 'https',
+      s3_credentials: S3_CREDENTIALS,
+      s3_headers: {'Expires' => 1.year.from_now.httpdate, 'Cache-Control' => 'max-age=315576000'},
+      url:         ":s3_domain_url",
+      hash_secret: "Kid Sister Diary Secure",
+      hash_data: ":class/:attachment/:id/:style/:updated_at",
+      path: ":class/:id/:filename"
+    }
+  )
+
+  ATTACHMENT_CONFIG_BASE = {}
 
   TILE_IMAGE_OPTIONS = {
     path: "/tiles/:id/:hash__:filename",
-    hash_data: "tiles/:attachment/:id/:style/:updated_at",
+    hash_data: "tiles/:attachment/:id/:style/:updated_at"
   }
 
   TILE_THUMBNAIL_OPTIONS = {
     path: "/tile_thumbnails/:id/:hash__:filename",
-    hash_data: "tiles/:attachment/:id/:style/:updated_at",
+    hash_data: "tiles/:attachment/:id/:style/:updated_at"
   }
 
+#deprecate teh rest of these:
   DEMO_LOGO_OPTIONS = {
     path: "/demo/:id/:hash__:filename",
-    hash_data: "demos/:attachment/:id/:style/:updated_at",
   }
 
   TOPIC_OPTIONS = {
     path: "/topic/:id/:hash__:filename",
-    hash_data: "topics/:attachment/:id/:style/:updated_at",
   }
 
   TOPIC_BOARD_OPTIONS = {
     path: "/topic_board/:id/:hash__:filename",
-    hash_data: "topic_boards/:attachment/:id/:style/:updated_at",
   }
 
 when 'test',  'development'
 
-  ATTACHMENT_CONFIG_BASE = {
-    storage: :fog,
-    fog_credentials: {
-      provider: "Local",
-      local_root: "#{Rails.root}/public/system/attachments/#{Rails.env}"
-    },
-    fog_directory: "",
-    fog_host: "http://localhost:3000/system/attachments/#{Rails.env}",
-    path: ":class/:id/:filename",
+  Paperclip::Attachment.default_options.merge!(
+    {
+      storage: :fog,
+      fog_credentials: {
+        provider: "Local",
+        local_root: "#{Rails.root}/public/system/attachments/#{Rails.env}"
+      },
+      fog_directory: "",
+      fog_host: "http://localhost:3000/system/attachments/#{Rails.env}",
+      hash_secret: "Kid Sister Diary Secure",
+      path: ":class/:id/:filename",
+    }
+  )
+
+  #attachement_config_base is a poor pattern.  Begin replacing with config.paperclip_defaults in environments/*.rb
+  ATTACHMENT_CONFIG_BASE = {}
+
+  TILE_IMAGE_OPTIONS = {
+    path: "tiles/:id/:hash__:filename",
   }
 
-  TILE_IMAGE_OPTIONS     = {}
-  TILE_THUMBNAIL_OPTIONS = {}
-  DEMO_LOGO_OPTIONS = {}
-  TOPIC_OPTIONS = {}
-  TOPIC_BOARD_OPTIONS = {}
+  TILE_THUMBNAIL_OPTIONS = {
+    path: "tile_thumbnails/:id/:hash__:filename",
+  }
+
+#deprecate teh rest of these:
+  DEMO_LOGO_OPTIONS = {
+    path: "demo/:id/:hash__:filename",
+  }
+
+  TOPIC_OPTIONS = {
+    path: "topic/:id/:hash__:filename",
+  }
+
+  TOPIC_BOARD_OPTIONS = {
+    path: "topic_board/:id/:hash__:filename",
+  }
 
 else
   raise 'Environment Not Found'
 end
 
-TILE_IMAGE_OPTIONS.merge!(ATTACHMENT_CONFIG_BASE)
-TILE_THUMBNAIL_OPTIONS.merge!(ATTACHMENT_CONFIG_BASE)
-DEMO_LOGO_OPTIONS.merge!(ATTACHMENT_CONFIG_BASE)
-TOPIC_OPTIONS.merge!(ATTACHMENT_CONFIG_BASE)
-TOPIC_BOARD_OPTIONS.merge!(ATTACHMENT_CONFIG_BASE)
+# TILE_IMAGE_OPTIONS.merge!(ATTACHMENT_CONFIG_BASE)
+# TILE_THUMBNAIL_OPTIONS.merge!(ATTACHMENT_CONFIG_BASE)
+# DEMO_LOGO_OPTIONS.merge!(ATTACHMENT_CONFIG_BASE)
+# TOPIC_OPTIONS.merge!(ATTACHMENT_CONFIG_BASE)
+# TOPIC_BOARD_OPTIONS.merge!(ATTACHMENT_CONFIG_BASE)
