2d430f840686e7a4666f96bd446ef01b39b18bbd
Nick Weiland <nickweiland@gmail.com>
2016-12-12 15:02:03 -0800

    Create active scope for channels

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,10 +1,13 @@
 class Channel < ActiveRecord::Base
   before_save :update_slug
+
   has_attached_file :image,
     {
       styles: { explore: "190x90#" },
       default_style: :explore,
     }
+
+  scope :active, -> { where(active: true) }
 
   def update_slug
     self.slug = name.parameterize
