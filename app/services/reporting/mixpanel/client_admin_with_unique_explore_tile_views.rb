aaf62b481f7f8945f6574b6fd3e6ba194ac6aacf
hraynaud <herby.raynaud@gmail.com>
2016-12-13 09:15:00 -0800

    Adds new kpi class

---
+++ app/services/reporting/mixpanel/client_admin_with_unique_explore_tile_views.rb
@@ -0,0 +1,18 @@
+require 'airbo_mixpanel_client'
+require 'reporting/mixpanel'
+module Reporting
+  module Mixpanel
+    class ClientAdminWithUniqueExploreTileViews < UniqueEventsBase
+
+      def configure opts
+        opts.merge!({
+          event: "Tile - Viewed in Explore",
+          unit: 'week',
+          where:%Q|(properties["user_type"] == "client admin") and (properties["board_type"] == "Paid") and properties["organization"] != "#{AIRBO_ORG_ID}"|,
+          type: 'unique',
+        })
+      end
+
+    end
+  end
+end
