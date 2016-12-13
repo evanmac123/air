d62f5c9575f4818d50bf1ab8768ae210d9cd9c6b
hraynaud <herby.raynaud@gmail.com>
2016-12-13 11:44:48 -0800

    Fixes file name

--- app/services/reporting/mixpanel/unique_organizations_with_viewed_tiles.rb
+++ app/services/reporting/mixpanel/unique_organizations_with_viewed_tiles.rb
@@ -2,7 +2,7 @@ require 'airbo_mixpanel_client'
 require 'reporting/mixpanel'
 module Reporting
   module Mixpanel
-    class UniqueOrganizationsWithPostedTiles < UniqueSegmentedEventsBase
+    class UniqueOrganizationsWithViewedTiles < UniqueSegmentedEventsBase
 
       def configure opts
         opts.merge!({
