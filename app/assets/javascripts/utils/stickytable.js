4535f61ed135d2bc2903fe09340d52c125c9a40e
hraynaud <herby.raynaud@gmail.com>
2016-12-15 10:01:49 -0800

    resets sticky table functionality

--- app/assets/javascripts/utils/stickytable.js
+++ app/assets/javascripts/utils/stickytable.js
@@ -5,47 +5,37 @@ Airbo.Utils.StickyTable = (function(){
 
 
   function initTable(){
-
-    function stickifyTable(table){
+    $('table.sticky').each(function() {
+      if($(this).find('thead').length > 0 && $(this).find('th').length > 0) {
+        // Clone <thead>
+        var $w	   = $(window),
+          $t	   = $(this),
+          $thead = $t.find('thead').clone(),
+          $col   = $t.find('thead, tbody').clone();
 
         // Add class, remove margins, reset width and wrap table
-        table
+        $t
         .addClass('sticky-enabled')
         .css({
           margin: 0,
           width: '100%'
         }).wrap('<div class="sticky-wrap" />');
 
-    }
-
-
-    $('table.sticky').each(function() {
-        // Clone <thead>
-      var $w   = $(window)
-        , table  = $(this)
-        , $thead = table.find('thead').clone()
-        , $col   = table.find('thead, tbody').clone()
-        , $stickyHead  = table.siblings('.sticky-thead')
-        , $stickyCol   = table.siblings('.sticky-col')
-        , $stickyInsct = table.siblings('.sticky-intersect')
-        , $stickyWrap  = table.parent('.sticky-wrap')
-      ;
-
-      if(table.find('thead').length > 0 && table.find('th').length > 0) {
-         stickifyTable(table);
-
-
-        if(table.hasClass('overflow-y')) table.removeClass('overflow-y').parent().addClass('overflow-y');
+        if($t.hasClass('overflow-y')) $t.removeClass('overflow-y').parent().addClass('overflow-y');
 
         // Create new sticky table head (basic)
-        table.after('<table class="sticky-thead" />');
+        $t.after('<table class="sticky-thead" />');
 
         // If <tbody> contains <th>, then we create sticky column and intersect (advanced)
-
-        if(table.find('tbody th').length > 0) {
-          table.after('<table class="sticky-col" /><table class="sticky-intersect" />');
+        if($t.find('tbody th').length > 0) {
+          $t.after('<table class="sticky-col" /><table class="sticky-intersect" />');
         }
 
+        // Create shorthand for things
+        var $stickyHead  = $(this).siblings('.sticky-thead'),
+          $stickyCol   = $(this).siblings('.sticky-col'),
+          $stickyInsct = $(this).siblings('.sticky-intersect'),
+          $stickyWrap  = $(this).parent('.sticky-wrap');
 
         $stickyHead.append($thead);
 
@@ -55,31 +45,31 @@ Airbo.Utils.StickyTable = (function(){
         .end()
         .find('tbody td').remove();
 
-        $stickyInsct.html('<thead><tr><th>'+table.find('thead th:first-child').html()+'</th></tr></thead>');
+        $stickyInsct.html('<thead><tr><th>'+$t.find('thead th:first-child').html()+'</th></tr></thead>');
 
         // Set widths
         var setWidths = function () {
-         table 
+          $t
           .find('thead th').each(function (i) {
-            $stickyHead.find('th').eq(i).width(table.width());
+            $stickyHead.find('th').eq(i).width($(this).width());
           })
           .end()
           .find('tr').each(function (i) {
-            $stickyCol.find('tr').eq(i).height(table.height());
+            $stickyCol.find('tr').eq(i).height($(this).height());
           });
 
           // Set width of sticky table head
-          $stickyHead.width(table.width());
+          $stickyHead.width($t.width());
 
           // Set width of sticky table col
-          $stickyCol.find('th').add($stickyInsct.find('th')).width(table.find('thead th').width())
+          $stickyCol.find('th').add($stickyInsct.find('th')).width($t.find('thead th').width())
         },
         repositionStickyHead = function () {
           // Return value of calculated allowance
           var allowance = calcAllowance();
 
           // Check if wrapper parent is overflowing along the y-axis
-          if(table.height() > $stickyWrap.height()) {
+          if($t.height() > $stickyWrap.height()) {
             // If it is overflowing (advanced layout)
             // Position sticky header based on wrapper scrollTop()
             if($stickyWrap.scrollTop() > 0) {
@@ -98,11 +88,11 @@ Airbo.Utils.StickyTable = (function(){
           } else {
             // If it is not overflowing (basic layout)
             // Position sticky header based on viewport scrollTop
-            if($w.scrollTop() > table.offset().top && $w.scrollTop() < table.offset().top + table.outerHeight() - allowance) {
+            if($w.scrollTop() > $t.offset().top && $w.scrollTop() < $t.offset().top + $t.outerHeight() - allowance) {
               // When top of viewport is in the table itself
               $stickyHead.add($stickyInsct).css({
                 opacity: 1,
-                top: $w.scrollTop() - table.offset().top
+                top: $w.scrollTop() - $t.offset().top
               });
             } else {
               // When top of viewport is above or below table
@@ -130,8 +120,8 @@ Airbo.Utils.StickyTable = (function(){
         calcAllowance = function () {
           var a = 0;
           // Calculate allowance
-          table.find('tbody tr:lt(3)').each(function () {
-            a += table.height();
+          $t.find('tbody tr:lt(3)').each(function () {
+            a += $(this).height();
           });
 
           // Set fail safe limit (last three row might be too tall)
@@ -147,7 +137,7 @@ Airbo.Utils.StickyTable = (function(){
 
         setWidths();
 
-        table.parent('.sticky-wrap').scroll($.throttle(250, function() {
+        $t.parent('.sticky-wrap').scroll($.throttle(250, function() {
           repositionStickyHead();
           repositionStickyCol();
         }));
