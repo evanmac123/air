5012861cec24611ca92798dec394136d66f8b085
Nick Weiland <nickweiland@gmail.com>
2016-12-14 22:31:28 -0800

    Implement endless scroll util and implement for channels

--- app/controllers/explore/channels_controller.rb
+++ app/controllers/explore/channels_controller.rb
@@ -5,15 +5,21 @@ class Explore::ChannelsController < ClientAdminBaseController
 
   def show
     @channel = Channel.find_by_slug(params[:id])
-    @tiles = @channel.tiles.page(params[:page]).per(40)
+    @tiles = @channel.tiles.page(params[:page]).per(28)
 
-    respond_to do |format|
-      format.html {
-        @display_channels = Channel.display_channels(@channel.slug)
-      }
+    if request.xhr?
+      content = render_to_string(
+                  partial: "explore/tiles",
+                  locals: { tiles: @tiles, section: "Channel" })
 
-      format.json {
+      render json: {
+        success:   true,
+        content:   content,
+        added:     @tiles.count,
+        lastBatch: params[:count] == @tiles.total_count.to_s
       }
+    else
+      @display_channels = Channel.display_channels(@channel.slug)
     end
   end
 end
