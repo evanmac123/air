5f831ad3c4605f3b30facecbf54e8a2555c082db
hraynaud <herby.raynaud@gmail.com>
2016-12-16 05:31:50 -0800

    Adjust dates to map to beginning and end of interval

--- app/services/financials_reporter_service.rb
+++ app/services/financials_reporter_service.rb
@@ -38,6 +38,7 @@ class FinancialsReporterService
 
 
     def raw_data sdate, edate, interval=Metrics::WEEKLY
+      sdate, edate = interval== Metrics::WEEKLY ? [sdate.beginning_of_week, edate.end_of_week] : [sdate.beginning_of_month, edate.end_of_month]
       Metrics.select(query_select_fields).normalized_by_date_range_and_interval(sdate, edate, interval)
     end
 
