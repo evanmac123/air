eb42441237ac9c678e934eba18467db429790ad1
Nick Weiland <nickweiland@gmail.com>
2016-12-22 17:38:04 -0800

    Refactor clearance monkey patch

--- lib/monkey_patches/clearance/authentication.rb
+++ lib/monkey_patches/clearance/authentication.rb
@@ -5,53 +5,44 @@
 
 module Clearance
   SESSION_EXPIRED = %!You've been logged out due to inactivity. If needed, <a href="/passwords/new">create or reset your password</a>.!.freeze
-end
 
-module Clearance::Authentication
-  module InstanceMethods
-
-    module SignInWithRememberMe
-      def sign_in(user, remember_me=false)
-        cookies[:remember_me] = {value: remember_me, expires: 1.year.from_now}
-        super(user)
-        if signed_in?
-          # This bit is so that we can trigger the message in
-          # YouveBeenSignedOutMessage later
-          session[:session_open] = true
-        end
-
-        session.delete(:guest_user)
-      end
-    end
-
-    module YouveBeenSignedOutMessage
-      def authorize
-        super
-        unless signed_in?
-          # Clearance doesn't normally log you out, but we've added that feature.
-          # So this gives a nice message if you're not authenticated but you
-          # were, say just a minute ago
-          if session[:session_open]
-            session[:session_open] = false
-            flash[:failure] = Clearance::SESSION_EXPIRED
-            flash[:failure_allow_raw] = true
+  module Authentication
+    module InstanceMethods
+      module SignInWithRememberMe
+        def sign_in(user, remember_me=false)
+          cookies[:remember_me] = { value: remember_me, expires: 1.year.from_now }
+          super(user)
+          if signed_in?
+            # This bit is so that we can trigger the message in
+            # YouveBeenSignedOutMessage later
+            session[:session_open] = true
           end
 
-          # It doesn't make sense to carry over saved flashes from one session
-          # to the next, we we'll delete them here
+          session.delete(:guest_user)
         end
       end
+
+      module YouveBeenSignedOutMessage
+        def authorize
+          super
+          unless signed_in?
+            # Clearance doesn't normally log you out, but we've added that feature.
+            # So this gives a nice message if you're not authenticated but you
+            # were, say just a minute ago
+            if session[:session_open]
+              session[:session_open] = false
+              flash[:failure] = Clearance::SESSION_EXPIRED
+              flash[:failure_allow_raw] = true
+            end
+          end
+        end
+      end
+
+      include YouveBeenSignedOutMessage
+      include SignInWithRememberMe
     end
-
-    include YouveBeenSignedOutMessage
-    include SignInWithRememberMe
   end
-end
 
-
-
-
-module Clearance
   class Session
     def add_cookie_to_headers(headers)
       if cookies && cookies['remember_me'] == 1.to_s
@@ -61,13 +52,15 @@ module Clearance
       end
 
       if signed_in?
-        Rack::Utils.set_cookie_header!(headers,
-                                       REMEMBER_TOKEN_COOKIE,
-                                       value:   current_user.remember_token,
-                                       expires: expire_time,
-                                       path:    "/",
-                                       secure:  Rails.env.production?,
-                                       httponly: true)
+        Rack::Utils.set_cookie_header!(
+          headers,
+          REMEMBER_TOKEN_COOKIE,
+          value:   current_user.remember_token,
+          expires: expire_time,
+          path:    "/",
+          secure:  Rails.env.production?,
+          httponly: true
+        )
       end
     end
 
