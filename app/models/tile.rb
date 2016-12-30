b9191d1d0cce7ed74a1d8ceff19222d06e06f102
hraynaud <herby.raynaud@gmail.com>
2016-12-30 13:15:11 -0500

    remove client admin custom context

--- app/models/tile.rb
+++ app/models/tile.rb
@@ -53,9 +53,9 @@ class Tile < ActiveRecord::Base
   before_validation :nullify_remote_media_url_if_blank
 
   validates_presence_of :headline, :allow_blank => false, :message => "headline can't be blank"
-  validates_presence_of :supporting_content, :allow_blank => false, :message => "supporting content can't be blank", :on => :client_admin, if: :state_is_anything_but_draft?
-  validates_presence_of :question, :allow_blank => false, :message => "question can't be blank", :on => :client_admin, if: :state_is_anything_but_draft?
-  validates_presence_of :remote_media_url, message: "image is missing" , if: [:requires_remote_media_url, :state_is_anything_but_draft?]
+  validates_presence_of :supporting_content, :allow_blank => false, :message => "supporting content can't be blank", if: :state_is_anything_but_draft?
+  validates_presence_of :question, :allow_blank => false, :message => "question can't be blank",  if: :state_is_anything_but_draft?
+  validates_presence_of :remote_media_url, message: "image is missing" , if: [:requires_remote_media_url, :state_is_anything_but_draft?] 
   validates_inclusion_of :status, in: STATUS
 
   validates_length_of :headline, maximum: MAX_HEADLINE_LEN, message: "headline is too long (maximum is #{MAX_HEADLINE_LEN} characters)"
