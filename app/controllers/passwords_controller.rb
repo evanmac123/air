6b5b363a58b64fa860c79006093592f469f9a713
Nick Weiland <nickweiland@gmail.com>
2017-01-05 09:27:11 -0800

    Refactor passwords controller

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -50,16 +50,8 @@ class PasswordsController < Clearance::PasswordsController
 
   private
 
-    def forbid_non_existent_user
-      unless ::User.find_by_slug_and_confirmation_token(
-                    params[:user_id], params[:token])
-        if params[:token]
-          flash[:failure] = "For security reasons, you can use each password reset link just once. If you'd like to reset your password again, please request a new link from this form."
-          redirect_to new_password_path
-        else
-          raise ActionController::Forbidden, "non-existent user"
-        end
-      end
+    def find_user_by_id_and_confirmation_token
+      User.find_by_slug_and_confirmation_token(params[:user_id], params[:token])
     end
 
     def downcase_email
