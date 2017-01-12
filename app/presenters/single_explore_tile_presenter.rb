d4b90985569f1190251174d3b969c0b0d0a939b5
Nick Weiland <nickweiland@gmail.com>
2017-01-11 22:47:50 -0800

    Redesign the way we cache explore tiles

--- app/presenters/single_explore_tile_presenter.rb
+++ app/presenters/single_explore_tile_presenter.rb
@@ -16,33 +16,10 @@ class SingleExploreTilePresenter < BasePresenter
   end
 
   def copied?
-    if current_user.is_a?(GuestUser) || current_user.end_user?
-      false
-    else
-      $redis.sismember("Demo:#{current_user.try(:demo_id)}:copies", id)
-    end
+    $redis.sismember("Demo:#{current_user.try(:demo_id)}:copies", id)
   end
 
   def show_tile_path
     explore_tile_preview_path(tile)
-  end
-
-  # TODO: refactor inline style and mange cache for guests
-  def explore_tile_button_display
-    if current_user.is_a?(GuestUser) || current_user.end_user?
-      "none"
-    end
-  end
-
-  def cache_key
-    @cache_key ||= [
-      self.class,
-      thumbnail,
-      id,
-      headline,
-      copied?,
-      @is_ie,
-      section
-    ].join('-')
   end
 end
