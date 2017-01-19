58eeb25a1fe613c35b12ccb5433570ccc5edff3d
hraynaud <herby.raynaud@gmail.com>
2017-01-19 11:33:31 -0500

    require masonry and imagesloaded

--- app/assets/javascripts/app-internal.js
+++ app/assets/javascripts/app-internal.js
@@ -23,6 +23,9 @@
 //= require jquery.tooltipster.min
 //= require ./external/schedule_demo
 //= require ./external/request_form
+//= require masonry.pkgd.min.js
+//= require imagesloaded.pkgd.min.js
+
 $(document).ready(function() {
   $('.settings-edit').foundation();
 
