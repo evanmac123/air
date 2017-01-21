985cbd8f788d38d45aa01c05ea12185c96356b95
hraynaud <herby.raynaud@gmail.com>
2017-01-20 20:47:44 -0500

    Adds pub sub functionality

---
+++ app/assets/javascripts/utils/jquery_pub_sub.js
@@ -0,0 +1,25 @@
+jQuery.Topic = (function(  ) {
+
+  var topics = {};
+
+  return function( id ){
+    var callbacks
+      , topic = id && topics[ id ]
+    ;
+    if ( !topic ) {
+      callbacks = jQuery.Callbacks();
+      topic = {
+        publish: callbacks.fire,
+        subscribe: callbacks.add,
+        unsubscribe: callbacks.remove
+      };
+      if ( id ) {
+        topics[ id ] = topic;
+      }
+    }
+    return topic;
+  }
+})();
+
+
+
