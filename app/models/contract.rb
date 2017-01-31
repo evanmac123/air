cb1d24277eb524fe4377dc6e2910861cfbedb879
hraynaud <herby.raynaud@gmail.com>
2017-01-30 20:05:41 -0500

    Fixes current mrr/arr calculations

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -56,15 +56,19 @@ class Contract < ActiveRecord::Base
   end
 
   def self.active_mrr_today
-   active.sum(&:calc_mrr)
+    active_as_of_today.sum(&:calc_mrr)
   end
 
   def self.active_arr_today
-    active.sum(&:calc_mrr) * 12
+    active_as_of_today.sum(&:calc_mrr) * 12
   end
 
   def self.delinquent_mrr_as_of_date d
     delinquent_as_of(d).sum(&:calc_mrr) 
+  end
+
+  def self.active_as_of_today
+    active_as_of_date(Date.today)
   end
 
 
