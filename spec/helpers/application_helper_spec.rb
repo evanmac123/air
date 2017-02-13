eadf68ec815b46e1f286e1e6bdcaa2c625790efe
Nick Weiland <nickweiland@gmail.com>
2017-02-13 12:20:58 -0800

    Update styles for old campaigns profs

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -14,4 +14,36 @@ describe ApplicationHelper do
       expect(helper.non_site_admin(user)).to be_falsey
     end
   end
+
+  describe "#get_user_type" do
+    it "returns 'guest' if user is nil" do
+      user = nil
+
+      expect(helper.get_user_type(user)).to eq('guest')
+    end
+
+    it "returns 'guest' if user is a GuestUser" do
+      user = GuestUser.new
+
+      expect(helper.get_user_type(user)).to eq('guest')
+    end
+
+    it "returns 'user' if user is an end user" do
+      user = FactoryGirl.build(:user)
+
+      expect(helper.get_user_type(user)).to eq('user')
+    end
+
+    it "returns 'client_admin' if user is a client_admin" do
+      user = FactoryGirl.build(:client_admin)
+
+      expect(helper.get_user_type(user)).to eq('client_admin')
+    end
+
+    it "returns 'client_admin' if user is a site_admin" do
+      user = FactoryGirl.build(:site_admin)
+
+      expect(helper.get_user_type(user)).to eq('client_admin')
+    end
+  end
 end
