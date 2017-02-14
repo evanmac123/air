64921ab25db21ba32ed7c1e1d6d73e07f21f47c8
Nick Weiland <nickweiland@gmail.com>
2017-02-13 17:11:39 -0800

    Resolve specs

--- spec/services/airbo_search_spec.rb
+++ spec/services/airbo_search_spec.rb
@@ -22,48 +22,6 @@ describe AirboSearch do
     end
   end
 
-  describe '#user_tiles' do
-    let(:tile) { FactoryGirl.create(:tile, demo: demo) }
-    let(:tile2) { FactoryGirl.create(:tile, demo: demo) }
-    let(:tile3) { FactoryGirl.create(:tile, demo: demo) }
-
-    before do
-      tile.save!
-      tile2.save!
-      tile3.save!
-    end
-
-    it 'grabs records from elasticsearch and paginates on them' do
-      unpaginated_user_tiles = mock("unpaginated_user_tiles")
-      unpaginated_user_tiles.stubs(:records).returns(Tile)
-      service.stubs(:unpaginated_user_tiles).returns(unpaginated_user_tiles)
-
-      expect(service.user_tiles.count).to eql(2) # page 1
-      expect(service.user_tiles(2).count).to eql(1) # page 2
-    end
-  end
-
-  describe '#explore_tiles' do
-    let(:tile) { FactoryGirl.create(:tile, :public) }
-    let(:tile2) { FactoryGirl.create(:tile, :public) }
-    let(:tile3) { FactoryGirl.create(:tile, :public) }
-
-    before do
-      tile.save!
-      tile2.save!
-      tile3.save!
-    end
-
-    it 'grabs records from elasticsearch and paginates on them' do
-      unpaginated_explore_tiles = mock("unpaginated_explore_tiles")
-      unpaginated_explore_tiles.stubs(:records).returns(Tile.explore)
-      service.stubs(:unpaginated_explore_tiles).returns(unpaginated_explore_tiles)
-
-      expect(service.explore_tiles.count).to eql(2) # page 1
-      expect(service.explore_tiles(2).count).to eql(1) # page 2
-    end
-  end
-
   describe '#organizations' do
     it 'calls Organization.where with the correct filter (based on the explore tiles present)' do
       fake_results = mock("Organization")
