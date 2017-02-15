89899a8bc37a2c36b99900a460e276aa11307965
Nick Weiland <nickweiland@gmail.com>
2017-02-15 12:18:47 -0800

    Add search specs

--- app/controllers/explore/searches_controller.rb
+++ app/controllers/explore/searches_controller.rb
@@ -2,23 +2,6 @@ class Explore::SearchesController < ExploreBaseController
   layout 'search_layout'
 
   def show
-    # # basics
-    # search = AirboSearch.new('health insurance', demo)
-    # @my_tiles = search.my_tiles
-    # @explore_tiles = search.explore_tiles
-    # @campaigns = search.campaigns
-    # @organizations = search.organizations
-    #
-    # # pagination
-    # search = AirboSearch.new('health insurance', demo)
-    # @my_tiles_page_1 = search.my_tiles
-    # @my_tiles_page_2 = search.my_tiles(2)
-    # @my_tiles_page_7 = search.my_tiles(7)
-    # @explore_tiles_page_3 = search.explore_tiles(3)
-    #
-    # # customize number of results per page
-    # search = AirboSearch.new('health insurance', demo, { per_page: 24 })
-    # @my_tiles_page_2 = search.my_tiles(2)
     @search_service = AirboSearch.new(params[:query], current_user)
 
     if request.xhr? && params[:section]
---
+++ spec/acceptance/explore/search/search_spec.rb
@@ -0,0 +1,63 @@
+require 'acceptance/acceptance_helper'
+
+feature "Client admin searches", js: true, type: :feature do
+
+  let!(:demo) { FactoryGirl.create :demo }
+  let!(:client_admin) { FactoryGirl.create :site_admin, demo: demo }
+
+  before do
+    FactoryGirl.create_list(:tile, 5, :draft, demo: demo)
+    FactoryGirl.create_list(:tile, 5, :active, demo: demo)
+    FactoryGirl.create_list(:tile, 5, :archived, demo: demo)
+    FactoryGirl.create_list(:tile, 5, :public)
+    FactoryGirl.create_list(:campaign, 5)
+
+    visit explore_search_path(as: client_admin, query: '*')
+  end
+
+  it "should load 4 resources from each section on the overview page" do
+    my_tiles_section = page.find(".manage_section.client_admin_tiles")
+
+    within my_tiles_section do
+      expect(page).to have_selector('.tile_container', count: 4)
+    end
+
+    explore_section = page.find(".manage_section.explore_tiles")
+
+    within explore_section do
+      expect(page).to have_selector('.tile_container', count: 4)
+    end
+
+    campaign_section = page.find(".manage_section.campaigns")
+
+    within campaign_section do
+      expect(page).to have_selector('.tile_container', count: 4)
+    end
+  end
+
+  it "should load all tiles on resource specific tabs" do
+    find("#myTilesTab").click
+
+    my_tiles_section = page.find(".manage_section.client_admin_tiles")
+
+    within my_tiles_section do
+      expect(page).to have_selector('.tile_container', count: 15)
+    end
+
+    find("#exploreTilesTab").click
+
+    explore_section = page.find(".manage_section.explore_tiles")
+
+    within explore_section do
+      expect(page).to have_selector('.tile_container', count: 5)
+    end
+
+    find("#campaignsTab").click
+
+    campaign_section = page.find(".campaigns.row")
+
+    within campaign_section do
+      expect(page).to have_selector('.tile_container', count: 5)
+    end
+  end
+end
