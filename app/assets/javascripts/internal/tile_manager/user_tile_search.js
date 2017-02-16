49a67472d5819191bf063d5af1bf8fdc2a0af507
Nick Weiland <nickweiland@gmail.com>
2017-02-16 09:59:09 -0800

    Update

--- app/assets/javascripts/internal/tile_manager/user_tile_search.js
+++ app/assets/javascripts/internal/tile_manager/user_tile_search.js
@@ -4,10 +4,14 @@ Airbo.UserTileSearch = (function(){
   function init() {
     $(".tile_thumb_link").on("click", function(e) {
       e.preventDefault();
-      var tileIds = Airbo.TileThumbnailManagerBase.getTileIdsInContainer($(this));
-
+      var tileIds = getNeighboringTileIds($(this));
+      debugger
       window.location = $(this).attr('href') + "?from_search=true&tile_ids=" + tileIds;
     });
+  }
+
+  function getNeighboringTileIds(self) {
+    return Airbo.TileThumbnailManagerBase.getNeighboringTileIdsInContainer(self);
   }
 
   return {
