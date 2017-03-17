2914fe6658c1734f869916892b15f7f7dfa4568c
hraynaud <herby.raynaud@gmail.com>
2017-03-17 19:41:18 -0400

    fixes typo

--- app/models/demo.rb
+++ app/models/demo.rb
@@ -17,7 +17,7 @@ class Demo < ActiveRecord::Base
   has_one :custom_color_palette, dependent: :delete
 
   # NOTE m_tiles is an unfortunate hack to compensate for shitty code implementation of MultipleChoiceTile
-  has_many :m_tiles, :dependent => :destroy, class_name: 'MultipleChoiceTile', :delete_all
+  has_many :m_tiles, :dependent => :destroy, class_name: 'MultipleChoiceTile', dependent: :delete_all
   has_many :board_memberships, dependent: :delete_all
   has_many :tiles, :dependent => :delete_all
 
