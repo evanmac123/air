3d5fe7cb48473b9a20961a8c61d76a2081b3b95e
hraynaud <herby.raynaud@gmail.com>
2017-03-22 23:41:57 -0400

    reorganize js requires for internal

--- app/assets/javascripts/app-internal.js
+++ app/assets/javascripts/app-internal.js
@@ -1,33 +1,34 @@
+//= require application
 //= require jquery
-//= require jquery.effects.highlight
 //= require jquery_ujs
+//= require jquery.effects.highlight
+//= require jquery.validate
+//= require jquery.validate.additional-methods
+//= require jquery.ui.autocomplete
 //= require mobvious-rails
+//= require tooltipster/tooltipster.bundle.min
 //= require jquery.ba-throttle-debounce.min.js
+//= require ../../../vendor/assets/javascripts/client_admin/jquery.slider.min.js
+//= require ../../../vendor/assets/javascripts/client_admin/jquery.jscroll
 //= require ../../../vendor/assets/javascripts/intro.min
-//= require_tree ../../../vendor/assets/javascripts/external/
-//= require_tree ../../../vendor/assets/javascripts/internal/.
 //= require ../../../vendor/assets/javascripts/confirm_with_reveal.modified
 //= require ../../../vendor/assets/javascripts/client_admin/jquery.form.min
-//= require_self
+//= require_tree ../../../vendor/assets/javascripts/external/
+//= require_tree ../../../vendor/assets/javascripts/internal/.
 //= require_tree ./internal
+//= require_tree ./internal_and_external/.
 //= require ./explore/searches
 //= require ./client_admin/tile_preview/image_loading_placeholder
 //= require ./client_admin/tile_preview/tile_carousel
 //= require ./client_admin/tile_preview/sticky_menu
 //= require ./client_admin/tile_preview/tile_preview_arrows
 //= require ./client_admin/tile_preview/tile_preview_modal
-//= require_tree ./internal_and_external/.
-//= require jquery.validate
-//= require jquery.validate.additional-methods
-//= require jquery.ui.autocomplete
-//= require ../../../vendor/assets/javascripts/client_admin/jquery.slider.min.js
-//= require ../../../vendor/assets/javascripts/client_admin/jquery.jscroll
-//= require tooltipster/tooltipster.bundle.min
 //= require ./external/schedule_demo
 //= require ./external/request_form
 
 //= require ../../../vendor/assets/javascripts/flickity.pkgd.min.js
 
+//= require_self
 $(document).ready(function() {
   $('.settings-edit').foundation();
 
