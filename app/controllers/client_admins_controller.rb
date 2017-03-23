02fff52e9df9a5067435013133f6ff0b9b423725
Nick Weiland <nickweiland@gmail.com>
2017-03-23 11:03:09 -0700

    Remove activity grid from activity page

--- app/controllers/client_admins_controller.rb
+++ app/controllers/client_admins_controller.rb
@@ -4,10 +4,6 @@ class ClientAdminsController < ClientAdminBaseController
     @board = current_user.demo
     @chart_form = BoardStatsLineChartForm.new @board, { action_type: params[:action_type] }
 
-    grid_builder = BoardStatsGrid.new(@board)
-    @board_stats_grid = initialize_grid(*grid_builder.args)
-    @current_grid = grid_builder.query_type
-
     render template: "client_admin/show"
   end
 end
