0eb10056e213119f3a07aa01611d76aecf19dcd9
hraynaud <herby.raynaud@gmail.com>
2017-04-26 17:27:24 -0400

    Removes deprecated functionality

--- app/services/reporting/customer_success_kpi_calc_service.rb
+++ app/services/reporting/customer_success_kpi_calc_service.rb
@@ -197,30 +197,6 @@ module Reporting
       calc_avg(total_tiles_added_from_scratch, orgs_that_created_tiles_from_scratch)
     end
 
-    def get_avg_tile_creation_time
-      @avg_tile_creation_time = Reporting::Mixpanel::TileCreationFunnel.new(opts)
-    end
-
-
-    def avg_tile_creation_time
-      @avg_tile_creation_time.get_avg_time(@curr_interval_key)
-    end
-
-    def get_retention
-      @retention = Reporting::Mixpanel::UniqueActivitySessionAfterTimePeriodInDays.new(opts)
-    end
-
-
-    def retention
-      @retention = Reporting::Mixpanel::UniqueActivitySessionAfterTimePeriodInDays.new(opts)
-    end
-
-    def retention_by_days days
-      #TODO fix implementation
-      return nil
-      #retention.get_count_by_segment(days)
-    end
-
     def percent_of_orgs_that_posted_tiles
       calc_percent(orgs_that_posted_tiles, total_paid_orgs)
     end
@@ -229,17 +205,6 @@ module Reporting
       calc_avg(total_tiles_posted, orgs_that_posted_tiles)
     end
 
-    def percent_joined_current
-    end
-
-    def percent_joined_30
-    end
-
-    def percent_joined_60
-    end
-
-    def percent_joined_120
-    end
 
     def get_total_tiles_posted
       @total_tiles_posted = Reporting::Mixpanel::TotalTilesPostedByPaidClientAdmin.new(opts)
@@ -280,8 +245,6 @@ module Reporting
       get_orgs_that_created_tiles_from_scratch
       get_orgs_that_posted_tiles
       get_total_tiles_posted
-      get_avg_tile_creation_time
-      get_tiles_posted_by_media_source
     end
 
     #----------Utility Methods
