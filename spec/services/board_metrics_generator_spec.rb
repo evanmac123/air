3af23f0f153260b2f3ed172c8c1ddbf3b8382a4b
Nick Weiland <nickweiland@gmail.com>
2017-07-11 11:06:37 -0700

    Closes #948 optimizes reporting caches

--- spec/services/board_metrics_generator_spec.rb
+++ spec/services/board_metrics_generator_spec.rb
@@ -2,52 +2,28 @@ require 'spec_helper'
 
 describe BoardMetricsGenerator do
   describe ".set_cache" do
-    it "only sets the cache for the current demo if site admin" do
-      sa = FactoryGirl.create(:site_admin)
-      generator = BoardMetricsGenerator.new([sa.demo])
-
-      BoardMetricsGenerator.expects(:new).with([sa.demo]).returns(generator)
-      BoardMetricsGenerator.any_instance.expects(:update_metrics_caches)
-
-      BoardMetricsGenerator.set_cache(sa)
-    end
-
-    it "sets the cache for all current_user's demos if client admin" do
+    it "only sets the cache for the current demo" do
       ca = FactoryGirl.create(:client_admin)
-      second_board = FactoryGirl.create(:demo, name: "second board")
-      ca.demos << second_board
-      generator = BoardMetricsGenerator.new(ca.demos)
+      generator = BoardMetricsGenerator.new(board: ca.demo)
 
-      BoardMetricsGenerator.expects(:new).with(ca.demos.select(:id)).returns(generator)
+      BoardMetricsGenerator.expects(:new).with(board: ca.demo).returns(generator)
+      BoardMetricsGenerator.any_instance.expects(:update_metrics_caches_for_board)
 
-      BoardMetricsGenerator.any_instance.expects(:update_metrics_caches)
-
-      BoardMetricsGenerator.set_cache(ca)
+      BoardMetricsGenerator.set_cache(board: ca.demo)
     end
   end
 
   describe "#update_metrics_caches" do
-    it "calls update_metrics_caches_for_board for each board" do
-      ca = FactoryGirl.create(:client_admin)
-      second_board = FactoryGirl.create(:demo, name: "second board")
-      ca.demos << second_board
-      generator = BoardMetricsGenerator.new(ca.demos)
-
-      generator.expects(:update_metrics_caches_for_board).twice
-
-      generator.update_metrics_caches
-    end
-
     describe "#update_metrics_caches_for_board" do
       it "calls sets the cache for designated queries" do
-        sa = FactoryGirl.create(:site_admin)
-        generator = BoardMetricsGenerator.new([sa.demo])
+        ca = FactoryGirl.create(:client_admin)
+        generator = BoardMetricsGenerator.new(board: ca.demo)
 
         queries.each do |query|
           query.any_instance.expects(:set_cached_query).times(3)
         end
 
-        generator.send(:update_metrics_caches_for_board, sa.demo)
+        generator.update_metrics_caches_for_board
       end
     end
   end
