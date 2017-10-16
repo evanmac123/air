48f3e8bf180248cfaa09c9cc2407719ef8e96545
Nick Weiland <nickweiland@gmail.com>
2017-10-16 14:22:16 -0700

    Updates board health reports

--- app/services/reporting/board_health_report_builder.rb
+++ app/services/reporting/board_health_report_builder.rb
@@ -84,9 +84,10 @@ class Reporting::BoardHealthReportBuilder
 
   def days_since_tile_posted
     last_tile_posted = report.demo.tiles.where("activated_at IS NOT NULL").where("activated_at <= ?", report.to_date).order(:activated_at).last
+    to_date = Date.today < report.to_date ? Date.today : report.to_date
 
     if last_tile_posted && last_tile_posted.activated_at
-      (report.to_date - last_tile_posted.activated_at.to_date).to_i
+      (to_date - last_tile_posted.activated_at.to_date).to_i
     end
   end
 end
