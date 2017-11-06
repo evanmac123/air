dead8e5495b5c80d93dab1379876f41a9f72e2cf
Nick Weiland <nickweiland@gmail.com>
2017-11-06 15:12:38 -0800

    Removes unused lib and tasks

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -6,22 +6,5 @@ FactoryGirl.define do
       sequence(:name){|n| "Client-#{n}"}
       num_employees   5000
     end
-
-   trait :with_active_contract do
-      after(:create) do |org, evaluator|
-        create(:contract, :complete, :active, organization: org)
-      end
-   end
-
-    trait :with_contracts do
-      complete
-      after(:create) do |org, evaluator|
-        create(:contract, :complete, organization: org, start_date: '2012-01-01', end_date: '2012-12-31' )
-        create(:contract, :complete, organization: org, start_date: '2013-01-01', end_date: '2013-12-31' )
-        create(:contract, :complete, organization: org, start_date: '2014-01-01', end_date: '2014-12-31' )
-      end
-    end
-
   end
-
 end
