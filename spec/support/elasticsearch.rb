c5438eaef7b38a7863ce14e0b52dcb9560305589
Nick Weiland <nickweiland@gmail.com>
2017-11-29 18:16:34 -0800

    Updates automate component

--- spec/support/elasticsearch.rb
+++ spec/support/elasticsearch.rb
@@ -1,4 +1,11 @@
 RSpec.configure do |config|
+  config.before(:suite) do
+    Tile.reindex
+    Campaign.reindex
+
+    Searchkick.disable_callbacks
+  end
+
   config.around(:each, search: true) do |example|
     Searchkick.enable_callbacks
     example.run
