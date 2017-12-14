43b93b965fcd2f17f9cd17fb6b94cc3852334ad0
Nick Weiland <nickweiland@gmail.com>
2017-12-14 14:50:53 -0800

    Moves additional mime types

--- config/application.rb
+++ config/application.rb
@@ -14,10 +14,6 @@ Bundler.require(:default, Rails.env)
 
 module Health
   class Application < Rails::Application
-    config.generators do |generate|
-      generate.test_framework :rspec
-    end
-
     ###################  ASSET PIPELINE  ###########################
     # Enable the asset pipeline
     config.assets.enabled = true
@@ -56,17 +52,11 @@ module Health
     # config.autoload_paths += %W(#{config.root}/extras)
 
     config.autoload_paths += %W(
-      #{Rails.root}/app/presenters/board_settings #{config.root}/app/presenters/digest #{config.root}/app/presenters/tile_preview
+      #{config.root}/app/presenters/digest
       #{config.root}/app/controllers/concerns
       #{config.root}/app/models/concerns
     )
 
-    # Only load the plugins named here, in the order given (default is alphabetical).
-    # :all can be used as a placeholder for all plugins not explicitly named.
-    # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
-    # Activate observers that should always be running.
-    # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
@@ -74,7 +64,6 @@ module Health
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
     config.action_view.javascript_expansions[:defaults] = %w()
