8a3c0edafa835eaca6dcc23e910b8c5c4dda5381
Nick Weiland <nickweiland@gmail.com>
2017-12-14 13:57:37 -0800

    Updates tile_imageable config to resolve Paperclip spoofing issues with octet-stream content types

--- app/models/tile/tile_imageable.rb
+++ app/models/tile/tile_imageable.rb
@@ -15,8 +15,9 @@ module Tile::TileImageable
         },
         default_style: :viewer,
         default_url: MISSING_PREVIEW,
+        validate_media_type: false
       }.merge!(TILE_IMAGE_OPTIONS)
-    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+    validates_attachment_content_type :image, content_type: [/\Aimage\/.*\Z/, /\A*\/octet-stream\Z/]
 
     has_attached_file :thumbnail,
       {
@@ -26,9 +27,10 @@ module Tile::TileImageable
         },
         default_style: :carousel,
         default_url: MISSING_THUMBNAIL,
-        preserve_files: true
+        preserve_files: true,
+        validate_media_type: false
       }.merge!(TILE_THUMBNAIL_OPTIONS)
-    validates_attachment_content_type :thumbnail, content_type: /\Aimage\/.*\Z/
+    validates_attachment_content_type :thumbnail, content_type: [/\Aimage\/.*\Z/, /\A*\/octet-stream\Z/]
 
     process_in_background :image,
       processing_image_url: :processing_image_fallback,
