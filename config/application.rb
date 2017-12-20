e78739cf2e2c5b1e2057d51e153d84df25fb511b
Nick Weiland <nickweiland@gmail.com>
2017-12-20 15:55:36 -0800

    Run Rails 4.0 Update Task

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "sprockets/railtie"
-require "mobvious"
-require_relative "../lib/request_timestamp"
+require 'rails/all'
+require 'mobvious'
+require_relative '../lib/request_timestamp'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
