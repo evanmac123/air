dc81bc0774ad871f637f9dda94c886e6f1918f86
Nick Weiland <nickweiland@gmail.com>
2017-12-29 23:21:03 -0800

    Resolves email images

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -21,39 +21,27 @@ module EmailHelper
     edit_account_settings_url(host: email_link_host, protocol: email_link_protocol)
   end
 
-  def email_logo(demo)
+  def email_logo(demo = nil)
     image_options = { border: "0", style: "display:block;", width: "90"}
-    if demo.logo_file_name.blank?
-      # Pretty asinine that we have to roll our own here. But hi. Here we are.
-      expanded_logo_path = ActionController::Base.helpers.asset_path('airbo_logo_lightblue.png')
-      logo_url = "#{::Rails.application.config.action_mailer.asset_host}#{expanded_logo_path}"
+    if demo && demo.logo_file_name.blank?
+      logo_url = ActionController::Base.helpers.asset_path('logo.png')
       image_options.merge!(alt: 'Airbo')
     else
       logo_url = demo.logo.url
     end
 
-    image_tag logo_url, image_options
-  end
-
-  def internal_email_logo
-    image_options = { border: "0", style: "display:block;", width: "90"}
-    expanded_logo_path = ActionController::Base.helpers.asset_path('airbo_logo_lightblue.png')
-    logo_url = "#{::Rails.application.config.action_mailer.asset_host}#{expanded_logo_path}"
-    image_options.merge!(alt: 'Airbo')
-
-    image_tag logo_url, image_options
+    image_tag(logo_url, image_options)
   end
 
   def default_logo
     image_options = { border: "0", style: "display:block;", width: "72", height: "42"}
-    logo_url = assets_url 'logo-white.png'
+    logo_url = ActionController::Base.helpers.asset_path('logo-white.png')
     image_options.merge!(alt: 'Airbo')
     image_tag logo_url, image_options
   end
 
-  def assets_url filename
-    expanded_logo_path = ActionController::Base.helpers.asset_path(filename)
-    "#{::Rails.application.config.action_mailer.asset_host}#{expanded_logo_path}"
+  def assets_url(filename)
+    ActionController::Base.helpers.asset_path(filename)
   end
 
   # Had to define environment variables on Heroku so that SendGrid sends emails to the right place in staging and production
