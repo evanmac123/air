a8305451ff3ce8b22158019f91591c8d3bcc0c73
Nick Weiland <nickweiland@gmail.com>
2018-01-16 10:03:56 -0800

    Lints additional image upload js

--- app/assets/javascripts/app-user/tile_builder_form/tile_image_uploader.js
+++ app/assets/javascripts/app-user/tile_builder_form/tile_image_uploader.js
@@ -4,31 +4,37 @@ Airbo.TileImageUploader = (function() {
   var initialized;
   var remoteMediaUrl;
   var remoteMediaType;
-  var remoteMediaUrlSelector = '#remote_media_url';
-  var remoteMediaTypeSelector = '#remote_media_type';
+  var remoteMediaUrlSelector = "#remote_media_url";
+  var remoteMediaTypeSelector = "#remote_media_type";
   var eventPrefix = "/s3/tileImage/upload/";
-
-  var imageUploaderSelector = "#image-uploader"
-    , imagePreviewSelector = ".image_preview"
+  var imageUploaderSelector = "#image-uploader";
+  var imagePreviewSelector = ".image_preview";
 
   function directUploadCompleted(data, file, filepath) {
-    Airbo.PubSub.publish("image-done", { url: filepath, type: file.type, source: "image-upload" });
+    Airbo.PubSub.publish("image-done", {
+      url: filepath,
+      type: file.type,
+      source: "image-upload"
+    });
   }
 
   function notifyImageUploaded(imgUrl, imgWidth, imgHeight) {
-    Airbo.PubSub.publish("image-selected", { url: imgUrl, h: imgHeight, w: imgWidth });
+    Airbo.PubSub.publish("image-selected", {
+      url: imgUrl,
+      h: imgHeight,
+      w: imgWidth
+    });
   }
 
-  function defaultBuiltInPreview(file){
+  function defaultBuiltInPreview(file) {
     return file.preview;
   }
 
-  function imagePreview(data){
-    var node
-      , imagePlaceholder
-      , index = data.index || 0
-      , file = data.files[index]
-    ;
+  function imagePreview(data) {
+    var node;
+    var imagePlaceholder;
+    var index = data.index || 0;
+    var file = data.files[index];
 
     data.context = $(imageUploaderSelector);
     node = data.context.find(imagePreviewSelector);
@@ -36,64 +42,63 @@ Airbo.TileImageUploader = (function() {
     if (defaultBuiltInPreview(file)) {
       //Built-in image preview (scales images)
       node.html(file.preview);
-    }else{
+    } else {
       //Manually perform our image preview (no scaling)
-      loadImage(file, function (img) {
-        notifyImageUploaded(img.toDataURL(), img.width, img.height);
-      },{canvas: true} );
+      loadImage(
+        file,
+        function(img) {
+          notifyImageUploaded(img.toDataURL(), img.width, img.height);
+        },
+        { canvas: true }
+      );
     }
 
     if (file.error) {
       node
-      .append('<br>')
-      .append($('<span class="text-danger"/>').text(file.error));
+        .append("<br>")
+        .append($('<span class="text-danger"/>').text(file.error));
     }
   }
 
-  function fileAdded(event, data){
+  function fileAdded(event, data) {
     //no op
   }
 
-  function fileProcessed(event, data){
+  function fileProcessed(event, data) {
     data.submit();
     imagePreview(data);
   }
 
-  function fileProgress(event, data){
+  function fileProgress(event, data) {
     var progress;
     if (data.context) {
       progress = parseInt(data.loaded / data.total * 100, 10);
     }
   }
 
-  function fileDone(event, data){
+  function fileDone(event, data) {
     var content, domain, file, path, to;
     file = data.files[0];
-    domain = $("#image-uploader").attr('action');
-    path = $("#image-uploader" + ' input[name=key]').val().replace('${filename}', file.name);
-    directUploadCompleted(data, file, domain+path);
+    domain = $("#image-uploader").attr("action");
+    path = $("#image-uploader" + " input[name=key]")
+      .val()
+      .replace("${filename}", file.name);
+    directUploadCompleted(data, file, domain + path);
   }
 
-
-
-  function initUploadHandlers(){
-
+  function initUploadHandlers() {
     Airbo.PubSub.subscribe(eventPrefix + "added", fileAdded);
     Airbo.PubSub.subscribe(eventPrefix + "processed", fileProcessed);
     Airbo.PubSub.subscribe(eventPrefix + "progress", fileProgress);
     Airbo.PubSub.subscribe(eventPrefix + "done", fileDone);
   }
 
-
-  function init(){
-
-
-    initUploadHandlers()
+  function init() {
+    initUploadHandlers();
     return this;
   }
 
   return {
     init: init
   };
-
-}());
+})();
