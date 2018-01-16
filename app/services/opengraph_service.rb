ae570b311f75a5d3d15c56c56fe671046a28e041
Nick Weiland <nickweiland@gmail.com>
2018-01-16 13:25:39 -0800

    Updates OpengraphService

--- app/services/opengraph_service.rb
+++ app/services/opengraph_service.rb
@@ -4,30 +4,30 @@ class OpengraphService
       tile_to_display = board.tiles.active.first
 
       if tile_to_display.present?
-        tile_to_display.image
+        tile_to_display.thumbnail.url
       else
-        OpengraphService.default_image
+        OpengraphService.image
       end
     end
 
-    def image(image_path)
-      if image_path.present?
+    def image(image_path = nil)
+      if image_path
         image_path
       else
         ActionController::Base.helpers.asset_path("marketing_site/airbo-marketing-open-graph.png")
       end
     end
 
-    def title(custom_title)
-      if custom_title.present?
+    def title(custom_title = nil)
+      if custom_title
         custom_title
       else
         "Airbo"
       end
     end
 
-    def description(description)
-      if description.present?
+    def description(description = nil)
+      if description
         description
       else
         "Airbo is a simple micro site that's used by HR to drive employee education, appreciation, and participation."
