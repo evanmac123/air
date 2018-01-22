a26901538cceb410d422b85758e35026f92c50c0
Nick Weiland <nickweiland@gmail.com>
2018-01-22 12:45:09 -0800

    Closes #929 Prevents returns on headlines and tile answers

--- app/assets/javascripts/app-user/tile_builder_form/tile_interaction_manager.js
+++ app/assets/javascripts/app-user/tile_builder_form/tile_interaction_manager.js
@@ -1,17 +1,17 @@
 var Airbo = window.Airbo || {};
 
-Airbo.TileInteractionManager = (function(){
+Airbo.TileInteractionManager = (function() {
   var tileTypes = [];
   var tilebuilderform;
   var savedQuestion;
   var tileBuilderFormSelector = "#new_tile_builder_form";
   var sliderSelector = ".slider";
 
-  function initDom(){
+  function initDom() {
     tileBuilderForm = $(tileBuilderFormSelector);
   }
 
-  function showSlider(){
+  function showSlider() {
     $(sliderSelector).css("display", "block");
   }
 
@@ -21,86 +21,89 @@ Airbo.TileInteractionManager = (function(){
     tileBuilderForm.change();
   }
 
-  function resetQuizCorrectAnswerIndex(){
-    $("#quiz-answer input[type='radio']").each(function(idx, option){
-       $(option).val(idx);
+  function resetQuizCorrectAnswerIndex() {
+    $("#quiz-answer input[type='radio']").each(function(idx, option) {
+      $(option).val(idx);
     });
 
     tileBuilderForm.change();
   }
 
-  function renderSelectedInteraction(config){
+  function renderSelectedInteraction(config) {
     Airbo.TileInteractionBuilder.init(config, savedQuestion);
     Airbo.TileInteractionBuilder.render();
 
     Airbo.TileInteractionMenuHandler.setSelected(config.type, config.subtype);
     saveTypeToForm(config);
-    autosize($('#tile_question'));
-    autosize($('textarea.answer-editable'));
+    autosize($("#tile_question"));
+    autosize($("textarea.answer-editable"));
     toggleAnonBannerVisibility(config.isAnonymous);
     initToggleAnonymous();
     initAnonymousTooltip();
   }
 
   function setSavedQuestion(question) {
-    if(question && question.length > 0) {
+    if (question && question.length > 0) {
       return question;
     } else {
       return undefined;
     }
   }
 
-  function getSavedOrDefaultInteractionConfig(){
+  function getSavedOrDefaultInteractionConfig() {
     var defaults;
-    var config ={};
+    var config = {};
     var savedConfig = getSavedConfig();
 
-    if(Object.keys(savedConfig).length === 0) {
+    if (Object.keys(savedConfig).length === 0) {
       savedQuestion = undefined;
       defaults = Airbo.TileBuilderInteractionConfig.defaultKeys();
       config = interactionConfigByTypeAndSubType(defaults);
     } else {
       savedQuestion = setSavedQuestion(savedConfig.question);
       config = interactionConfigByTypeAndSubType(savedConfig);
-      config =  $.extend({}, config, savedConfig);
+      config = $.extend({}, config, savedConfig);
     }
 
     updateConfig(config);
     return config;
   }
 
-  function getSavedConfig(){
-    return $("#question_type_container").data('config');
+  function getSavedConfig() {
+    return $("#question_type_container").data("config");
   }
 
-  function updateConfig(config){
-    $("#question_type_container").data('config', config);
+  function updateConfig(config) {
+    $("#question_type_container").data("config", config);
   }
 
-  function interactionConfigByTypeAndSubType(selected){
-    var config = Airbo.TileBuilderInteractionConfig.get(selected.type, selected.subtype);
+  function interactionConfigByTypeAndSubType(selected) {
+    var config = Airbo.TileBuilderInteractionConfig.get(
+      selected.type,
+      selected.subtype
+    );
     config.type = selected.type;
     config.subtype = selected.subtype;
     return config;
   }
 
-  function handleSubTypeSelection(selected){
+  function handleSubTypeSelection(selected) {
     var config = interactionConfigByTypeAndSubType(selected);
     updateConfig(config);
     renderSelectedInteraction(config);
     initFreeFormTooltip();
   }
 
-  function toggleAnonBannerVisibility(isAnonymous){
-    if(isAnonymous){
+  function toggleAnonBannerVisibility(isAnonymous) {
+    if (isAnonymous) {
       $(".js-anon-banner-wrapper").removeClass("hidden");
-    }else{
+    } else {
       $(".js-anon-banner-wrapper").addClass("hidden");
     }
   }
 
-  function initAddAnswerOption(){
-    $("body").on("click", ".js-add-answer", function(event){
+  function initAddAnswerOption() {
+    $("body").on("click", ".js-add-answer", function(event) {
       event.preventDefault();
       event.stopPropagation();
       event.stopImmediatePropagation();
@@ -108,10 +111,12 @@ Airbo.TileInteractionManager = (function(){
     });
   }
 
-  function initRemoveAnswerOption(){
-    $("body").on("click", ".js-remove-answer", function(event){
+  function initRemoveAnswerOption() {
+    $("body").on("click", ".js-remove-answer", function(event) {
       event.preventDefault();
-      $(this).parents(".answer-div").remove();
+      $(this)
+        .parents(".answer-div")
+        .remove();
       resetQuizCorrectAnswerIndex();
     });
   }
@@ -121,27 +126,38 @@ Airbo.TileInteractionManager = (function(){
     input.select();
   }
 
-  function initAnswerEdit(){
-    $("body").on("click", ".js-answer-btn", function(event){
+  function initAnswerEdit() {
+    $("body").on("click", ".js-answer-btn", function(event) {
       event.preventDefault();
       var answer = $(this).parent(".answer-div");
       var field = answer.find(".answer-editable").first();
 
       answer.removeClass("read-mode").addClass("edit-mode");
-      field.data("text",field.val());
+
+      field.data("text", field.val());
       highlightText(field);
+
+      $(field).keypress(function(event) {
+        if (event.keyCode == 13) {
+          event.preventDefault();
+          field.focusout();
+        }
+      });
     });
   }
 
-
-  function initAnswerEditOnTabEntry(){
-    $('body').on('keydown', ".answer-editable", function openNextAnswerOnTab(e) {
-      var  code = e.keyCode || e.which;
-      var nextAnswer = $(this).parent(".answer-div").next(".answer-div");
+  function initAnswerEditOnTabEntry() {
+    $("body").on("keydown", ".answer-editable", function openNextAnswerOnTab(
+      e
+    ) {
+      var code = e.keyCode || e.which;
+      var nextAnswer = $(this)
+        .parent(".answer-div")
+        .next(".answer-div");
       var field = nextAnswer.first(".answer-editable");
 
       if (code === 9) {
-        if(nextAnswer.length > 0){
+        if (nextAnswer.length > 0) {
           nextAnswer.find(".js-answer-btn").click();
           highlightText(field);
           return false;
@@ -150,32 +166,36 @@ Airbo.TileInteractionManager = (function(){
     });
   }
 
-  function initQuestionitHighlight(){
-    $('body').on('focus', "#tile_question", function(event) {
+  function initQuestionitHighlight() {
+    $("body").on("focus", "#tile_question", function(event) {
       $(this).select();
     });
 
-    $('body').on('focusout blur', "#tile_question", function(event) {
-      $(this).val($(this).val().trim());
+    $("body").on("focusout blur", "#tile_question", function(event) {
+      $(this).val(
+        $(this)
+          .val()
+          .trim()
+      );
     });
   }
 
-  function initAnswerRead(){
-    $("body").on("focusout blur", ".answer-editable", function(event){
+  function initAnswerRead() {
+    $("body").on("focusout blur", ".answer-editable", function(event) {
       var field = $(this);
       var answer = field.parent(".answer-div");
       var btn = answer.find(".js-answer-btn").first();
       var text = field.val();
       var storedVal = field.data("text");
-      var defaultText =  "Add Ansswer Option";
+      var defaultText = "Add Ansswer Option";
 
-      if(text.length === 0) {
-        if(storedVal=== "" || storedVal === undefined) {
+      if (text.length === 0) {
+        if (storedVal === "" || storedVal === undefined) {
           storedVal = defaultText;
         }
 
-         field.val(storedVal);
-         btn.text(storedVal);
+        field.val(storedVal);
+        btn.text(storedVal);
       } else {
         field.data("text", text);
         btn.text(text);
@@ -184,20 +204,20 @@ Airbo.TileInteractionManager = (function(){
     });
   }
 
-  function initToggleAnonymous(){
-    $("body").on("change", ".js-chk-anonymous-reponse", function(event){
+  function initToggleAnonymous() {
+    $("body").on("change", ".js-chk-anonymous-reponse", function(event) {
       toggleAnonBannerVisibility($(this).is(":checked"));
     });
   }
 
-  function initToggleFreeResponse(){
-    $("body").on("change", ".js-chk-free-text", function(event){
+  function initToggleFreeResponse() {
+    $("body").on("change", ".js-chk-free-text", function(event) {
       var btnWrapper = $(".js-free-text-btn-wrapper");
       var target = event.target;
       var config = getSavedConfig();
       var mpEventName;
 
-      if(target.checked) {
+      if (target.checked) {
         btnWrapper.addClass("enabled");
         mpEventName = "Free Response Enabled";
       } else {
@@ -206,7 +226,7 @@ Airbo.TileInteractionManager = (function(){
       }
 
       Airbo.Utils.ping(mpEventName, {
-        tile_id:  tileId(),
+        tile_id: tileId(),
         type: config.type,
         subtype: config.subtype,
         allowFreeResponse: target.checked
@@ -214,27 +234,27 @@ Airbo.TileInteractionManager = (function(){
     });
   }
 
-  function initFreeFormTooltip(){
+  function initFreeFormTooltip() {
     $(".js-free-text-tooltip").tooltipster({
       theme: "tooltipster-shadow"
     });
   }
 
-  function initAnonymousTooltip(){
+  function initAnonymousTooltip() {
     $(".js-anonymous-tile-tooltip").tooltipster({
       theme: "tooltipster-shadow"
     });
   }
 
   //TODO move to utils?
-  function tileId(){
-    var id = $("#question_type_container").data('config').tileId;
+  function tileId() {
+    var id = $("#question_type_container").data("config").tileId;
     var pseudoId = $("#pseudo_tile_id").data("props").pseudoTileId;
 
     return id || pseudoId;
   }
 
-  function init (){
+  function init() {
     var config = getSavedOrDefaultInteractionConfig();
     initDom();
     initAddAnswerOption();
@@ -255,5 +275,4 @@ Airbo.TileInteractionManager = (function(){
   return {
     init: init
   };
-
-}());
+})();
