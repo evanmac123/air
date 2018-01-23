4bb7d2bb3bb42dd7f0993da70b829c682f3d3664
Nick Weiland <nickweiland@gmail.com>
2018-01-23 13:56:03 -0800

    Closes #1257 updates medium editor code

--- app/assets/javascripts/utils/medium_editor.js
+++ app/assets/javascripts/utils/medium_editor.js
@@ -4,163 +4,62 @@ Airbo.Utils = Airbo.Utils || {};
 Airbo.Utils.mediumEditor = (function() {
   var editor, field, fieldName;
 
-  function reset(){
-    if(editor){
+  function reset() {
+    if (editor) {
       editor.destroy();
     }
   }
 
-  function initExtensions(){
-
-    Airbo.CustomAnchorForm = MediumEditor.extensions.anchor.extend({
-      linkValidation: true,
-      // Called when the button the toolbar is clicked
-      // Overrides ButtonExtension.handleClick
-      handleClick: function (event) {
-        event.preventDefault();
-        event.stopPropagation();
-
-        var range = MediumEditor.selection.getSelectionRange(this.document);
-
-        if (range.startContainer.nodeName.toLowerCase() === 'a' ||
-            range.endContainer.nodeName.toLowerCase() === 'a' ||
-              MediumEditor.util.getClosestTag(MediumEditor.selection.getSelectedParentElement(range), 'a')) {
-          return this.execAction('unlink');
-        }
-
-        if (!this.isDisplayed()) {
-          selectionStr = MediumEditor.selection.getSelectionHtml(this.document);
-          selectionHTML = $.parseHTML( selectionStr );
-          link = $(selectionHTML);
-          var opt = null;
-          if (selectionHTML[0].nodeName.toLowerCase() == 'a' && link.attr("href")) {
-            opt = {
-              url: link.attr("href"),
-              target: link.attr('target'),
-              buttonClass: link.attr('class')
-            };
-            this.showForm(opt);
-          } else {
-            this.showForm();
-          }
-        }
-
-        return false;
-      }
-    });
-
-    Airbo.CustomAnchorPreview = MediumEditor.extensions.anchorPreview.extend({
-      createPreview: function () {
-        var el = this.document.createElement('div');
-
-        el.id = 'medium-editor-anchor-preview-' + this.getEditorId();
-        el.className = 'medium-editor-anchor-preview';
-        el.innerHTML = this.getTemplate();
-
-        editIcon = $(el).find("i")[0];
-        this.on(editIcon, 'click', this.handleClick.bind(this));
-
-        return el;
-      },
-      getTemplate: function () {
-        return HandlebarsTemplates["client-admin/tile-builder/linkEdit"]();
-      },
-      attachToEditables: function () {
-        // show link preview on click instead of mouse over
-        this.subscribe('editableClick', this.handleEditableMouseover.bind(this));
-      },
-      handleEditableMouseover: function (event) {
-        var target = MediumEditor.util.getClosestTag(event.target, 'a');
-
-        if (false === target) {
-          return;
-        }
-
-        // only show when toolbar is not present
-        var toolbar = this.base.getExtensionByName('toolbar');
-        if (!this.showWhenToolbarIsVisible && toolbar && toolbar.isDisplayed && toolbar.isDisplayed()) {
-          return true;
-        }
-
-        // detach handler for other anchor in case we hovered multiple anchors quickly
-        if (this.activeAnchor && this.activeAnchor !== target) {
-          this.detachPreviewHandlers();
-        }
-
-        this.anchorToPreview = target;
-        // Using setTimeout + delay because:
-        // - We're going to show the anchor preview according to the configured delay
-        //   if the mouse has not left the anchor tag in that time
-        this.base.delay(function () {
-          if (this.anchorToPreview) {
-            this.showPreview(this.anchorToPreview);
-          }
-        }.bind(this));
-      },
-      handlePreviewMouseout: function (event) {
-        if ($(event.target).parents('.medium-editor-anchor-preview').length <= 0) {
-          this.hidePreview();
-          $('body').off("click", '', this.instanceHandlePreviewMouseout);
-        }
-      },
-      attachPreviewHandlers: function () {
-        this.instanceHandlePreviewMouseout = this.handlePreviewMouseout.bind(this);
-        // actually it's click outside preview
-        $('body').click(this.instanceHandlePreviewMouseout);
-      }
-    });
-
-  }
-
-
   function init(params) {
     params = params || {};
     reset();
-    initExtensions();
 
-    $('.medium-editable').each(function(){
+    $(".medium-editable").each(function() {
       defaultParams = {
-        extensions: {
-          anchorPreview: new Airbo.CustomAnchorPreview(),
-          anchor:  new Airbo.CustomAnchorForm()
-        },
-        staticToolbar:true,
-        buttonLabels: 'fontawesome',
+        autoLink: true,
+        buttonLabels: "fontawesome",
         targetBlank: true,
-        // anchor: {
-        //   linkValidation: true,
-        // },
+        imageDragging: false,
         toolbar: {
-         buttons: ['bold', 'italic', 'underline', 'unorderedlist', 'orderedlist', "anchor"]
+          buttons: [
+            "bold",
+            "italic",
+            "underline",
+            "unorderedlist",
+            "orderedlist",
+            "anchor"
+          ]
         }
       };
 
-      editor = new MediumEditor(this, $.extend(defaultParams, params) );
+      editor = new MediumEditor(this, $.extend(defaultParams, params));
       editor.trigger("focus");
 
-      fieldName = $(this).data('field');
+      fieldName = $(this).data("field");
       field = $("#" + fieldName);
-      content =  field.val();
+      content = field.val();
       field.data("oldVal", content);
       editor.setContent(content);
 
-      editor.subscribe('blur', $.debounce( 2000, function(event, editable) {
-        var obj = $(editable);
-        var textLength = obj.text().trim().length;
-        var val = obj.html();
-        var oldVal = field.data("oldVal");
-        var re = new RegExp( /(<p><br><\/p>)+$/g);
+      editor.subscribe(
+        "blur",
+        $.debounce(2000, function(event, editable) {
+          var obj = $(editable);
+          var textLength = obj.text().trim().length;
+          var val = obj.html();
+          var oldVal = field.data("oldVal");
+          var re = new RegExp(/(<p><br><\/p>)+$/g);
 
-        field.val( val.replace(re, "") );
-        field.data("oldVal", field.val());
+          field.val(val.replace(re, ""));
+          field.data("oldVal", field.val());
 
-        if(oldVal !== field.val()){
-          field.change();
-        }
-      }));
+          if (oldVal !== field.val()) {
+            field.change();
+          }
+        })
+      );
 
-
-      editor.subscribe('editableInput', function (e, editable) {
+      editor.subscribe("editableInput", function(e, editable) {
         var obj = $(editable);
         var textLength = obj.text().trim().length;
 
@@ -178,5 +77,4 @@ Airbo.Utils.mediumEditor = (function() {
   return {
     init: init
   };
-
-}());
+})();
