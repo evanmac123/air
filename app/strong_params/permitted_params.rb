2e7e7b3b08bc3c57ea821f5ccaeb1221d2ac7a15
Nick Weiland <nickweiland@gmail.com>
2018-01-24 15:04:40 -0800

    Cleans up permitted_params.rb

--- app/strong_params/permitted_params.rb
+++ app/strong_params/permitted_params.rb
@@ -1,10 +1,11 @@
-# TODO: Rmove this and replace with Pundit permiited_params helper
 class PermittedParams
   include Params::User
   include Params::Demo
-  attr_accessor :current_user,:params
-  def initialize (params, current_user)
-   @current_user = current_user || User.new
-   @params = params
+
+  attr_reader :current_user, :params
+
+  def initialize(params, current_user)
+    @current_user = current_user || User.new
+    @params = params
   end
 end
