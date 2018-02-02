21ff0cfe737f03a72168a1534f3d1b09380b7a06
Nick Weiland <nickweiland@gmail.com>
2018-02-01 17:28:31 -0800

    Adds frozen string literal pragma to campaign_tile

--- app/models/campaign_tile.rb
+++ app/models/campaign_tile.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CampaignTile < ActiveRecord::Base
   belongs_to :tile
   # TODO: Move this touch to the object that manages Tile updates/Campaign subscriptions so that it only touches when the Tile is posted.
