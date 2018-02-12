ad4e9a1f2be889b79ab2571f38a73ba943b8e9f5
Nick Weiland <nickweiland@gmail.com>
2018-02-12 14:17:02 -0800

    Fixes tile button alignment and adds destroy option to ignored suggested tiles

--- app/presenters/single_admin_tile_presenter.rb
+++ app/presenters/single_admin_tile_presenter.rb
@@ -1,5 +1,6 @@
-class SingleAdminTilePresenter < BasePresenter
+# frozen_string_literal: true
 
+class SingleAdminTilePresenter < BasePresenter
   include TileFooterTimestamper
   delegate  :id,
             :status,
@@ -22,16 +23,16 @@ class SingleAdminTilePresenter < BasePresenter
 
   presents :tile
 
-  def initialize object,template, options
+  def initialize(object, template, options)
     super
     @tile_status = tile.status.to_sym
     @tiles_grouped_ids = options[:tile_ids]
-    @format =  options[:format]||:html
+    @format =  options[:format] || :html
     @options = options
   end
 
   def partial
-    'client_admin/tiles/manage_tiles/single_tile'
+    "client_admin/tiles/manage_tiles/single_tile"
   end
 
   def tile_id
@@ -42,18 +43,18 @@ class SingleAdminTilePresenter < BasePresenter
     !tile_fully_assembled?
   end
 
-  def tile_status_matches? *statuses
+  def tile_status_matches?(*statuses)
     if statuses.size == 0
       false
     elsif statuses.size == 1
       @tile_status == statuses.first
     else
-      statuses.any? {|t| t == @tile_status}
+      statuses.any? { |t| t == @tile_status }
     end
   end
 
   def tile_fully_assembled?
-   @fully_assembled ||= tile.is_fully_assembled?
+    @fully_assembled ||= tile.is_fully_assembled?
   end
 
   def completion_status
@@ -74,9 +75,9 @@ class SingleAdminTilePresenter < BasePresenter
 
   def incomplete_label
     content_tag :div, class: "activation_dates incomplete" do
-      content_tag :span,  class:'tile-created-at' do
-         s = content_tag :i, "",  class: 'fa fa-cog'
-         s+= "Incomplete"
+      content_tag :span,  class: "tile-created-at" do
+        s = content_tag :i, "",  class: "fa fa-cog"
+        s += "Incomplete"
       end
     end
   end
@@ -101,7 +102,7 @@ class SingleAdminTilePresenter < BasePresenter
 
   def show_tile_path
     if options[:from_search] == true
-      client_admin_tile_preview_path(tile, { from_search: true })
+      client_admin_tile_preview_path(tile, from_search: true)
     else
       client_admin_tile_path(tile)
     end
@@ -129,11 +130,11 @@ class SingleAdminTilePresenter < BasePresenter
   end
 
   def has_edit_button?
-     tile_status_matches?(:draft, :active, :archive) && tile_fully_assembled?
+    tile_status_matches?(:draft, :active, :archive) && tile_fully_assembled?
   end
 
   def has_destroy_button?
-   tile_status_matches? :draft, :active, :archive
+    tile_status_matches? :draft, :active, :archive
   end
 
   def has_accept_button?
@@ -161,7 +162,7 @@ class SingleAdminTilePresenter < BasePresenter
   end
 
   def post_link_text
-    'Post' + (tile_status_matches?(:archive) ? ' again' : '')
+    "Post" + (tile_status_matches?(:archive) ? " again" : "")
   end
 
 
@@ -187,7 +188,7 @@ class SingleAdminTilePresenter < BasePresenter
   end
 
   def tile_position
-   @tile.position || 0
+    @tile.position || 0
   end
 
   def to_param
@@ -211,8 +212,6 @@ class SingleAdminTilePresenter < BasePresenter
       unique_views,
       updated_at,
       @is_ie,
-    ].join('-')
+    ].join("-")
   end
-
-
 end
