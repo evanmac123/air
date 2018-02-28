03880e11a3aab7850aaf68428b49d36e5c04293f
Nick Weiland <nickweiland@gmail.com>
2018-02-28 11:23:32 -0800

    Closes #915 migrates tile question types to downcase

--- app/models/tile/tile_question_types.rb
+++ app/models/tile/tile_question_types.rb
@@ -1,22 +1,24 @@
+# frozen_string_literal: true
+
 module Tile::TileQuestionTypes
   # NOTE These question type constants are being migrated to the frontend. Evantually, we should remove this module.
 
   # Question Types
-  ACTION = "Action".freeze
-  QUIZ   = "Quiz".freeze
-  SURVEY = "Survey".freeze
+  ACTION = "action"
+  QUIZ   = "quiz"
+  SURVEY = "survey"
 
   # Question Subtypes
-  TAKE_ACTION           = "Take Action".parameterize("_").freeze
-  READ_TILE             = "Read Tile".parameterize("_").freeze
-  READ_ARTICLE          = "Read Article".parameterize("_").freeze
-  SHARE_ON_SOCIAL_MEDIA = "Share On Social Media".parameterize("_").freeze
-  VISIT_WEB_SITE        = "Visit Web Site".parameterize("_").freeze
-  WATCH_VIDEO           = "Watch Video".parameterize("_").freeze
-  CUSTOM                = "Custom...".parameterize("_").freeze
-  TRUE_FALSE            = "True / False".parameterize("_").freeze
-  MULTIPLE_CHOICE       = "Multiple Choice".parameterize("_").freeze
-  RSVP_TO_EVENT         = "RSVP to event".parameterize("_").freeze
-  INVITE_SPOUSE         = "Invite Spouse".parameterize("_").freeze
-  CHANGE_EMAIL          = "Change Email".parameterize("_").freeze
+  TAKE_ACTION           = "Take Action".parameterize("_")
+  READ_TILE             = "Read Tile".parameterize("_")
+  READ_ARTICLE          = "Read Article".parameterize("_")
+  SHARE_ON_SOCIAL_MEDIA = "Share On Social Media".parameterize("_")
+  VISIT_WEB_SITE        = "Visit Web Site".parameterize("_")
+  WATCH_VIDEO           = "Watch Video".parameterize("_")
+  CUSTOM                = "Custom...".parameterize("_")
+  TRUE_FALSE            = "True / False".parameterize("_")
+  MULTIPLE_CHOICE       = "Multiple Choice".parameterize("_")
+  RSVP_TO_EVENT         = "RSVP to event".parameterize("_")
+  INVITE_SPOUSE         = "Invite Spouse".parameterize("_")
+  CHANGE_EMAIL          = "Change Email".parameterize("_")
 end
