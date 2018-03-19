51df039ef09fd4608c671886dcc5b0c6739176b2
Nick Weiland <nickweiland@gmail.com>
2018-03-19 15:59:03 -0700

    Updates vendor/assets/images load path

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,7 @@ Rails.application.config.assets.version = '0.0.3'
 
 # Add additional assets to the asset load path
 Rails.application.config.assets.paths += Dir["#{Rails.root}/vendor/assets/stylesheets"].sort_by { |dir| -dir.size }
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "images")
 
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
