1df0937aaaac60614318ba4dbf9e3c165ba07787
Nick Weiland <nickweiland@gmail.com>
2018-04-02 17:13:53 -0700

    Hides plan_date in builder when tile is active and resolves suggested tile update status

--- app/assets/javascripts/app-admin/client_admin/managing_tiles/tile_admin_action_observer.js
+++ app/assets/javascripts/app-admin/client_admin/managing_tiles/tile_admin_action_observer.js
@@ -2,6 +2,14 @@ var Airbo = window.Airbo || {};
 
 Airbo.TileAdminActionObserver = (function() {
   var tileModalSelector = "#tile_preview_modal";
+  var sections = {
+    archive: "archive",
+    active: "active",
+    draft: "draft",
+    plan: "plan",
+    user_submitted: "suggested",
+    ignored: "suggested"
+  };
 
   function init() {
     Airbo.PubSub.subscribe("/tile-admin/tile-status-updated", function(
@@ -25,11 +33,24 @@ Airbo.TileAdminActionObserver = (function() {
   }
 
   function tileUpdateStatusSuccess(payload) {
-    var $tilesContainer = $("#" + payload.updatedTile.data("status"));
-    Airbo.TilesIndexLoader.resetTiles($tilesContainer);
-    payload.currTile.remove();
+    var status = payload.updatedTile.data("status");
+    var $tilesContainer = $("#" + sections[status]);
 
+    if (status === "ignored" || status === "user_submitted") {
+      moveSameSectionTile(payload.updatedTile, $tilesContainer);
+    } else {
+      Airbo.TilesIndexLoader.resetTiles($tilesContainer);
+    }
+
+    payload.currTile.remove();
     $(tileModalSelector).foundation("reveal", "close");
+  }
+
+  function moveSameSectionTile(updatedTile, $tilesContainer) {
+    $tilesContainer.prepend(updatedTile);
+
+    Airbo.TilePlaceHolderManager.perform();
+    Airbo.TileThumbnailMenu.initMoreBtn(updatedTile.find(".pill.more"));
   }
 
   return {
