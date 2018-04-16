317e2b088a7ac42022c9108c25e6b98d634291db
Nick Weiland <nickweiland@gmail.com>
2018-04-16 12:23:43 -0700

    Updates select down arrows

--- app/models/population_segment.rb
+++ app/models/population_segment.rb
@@ -5,4 +5,8 @@ class PopulationSegment < ActiveRecord::Base
   has_many :campaigns
   has_many :user_population_segments, dependent: :destroy
   has_many :users, through: :user_population_segments
+
+  def user_count
+    users.count
+  end
 end
