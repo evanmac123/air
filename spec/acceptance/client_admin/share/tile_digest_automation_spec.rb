9032acf4258446df242d22ca4177b244f8fbaec3
Ryan Workman <rdavid1099@gmail.com>
2018-05-16 12:02:19 -0600

    Remove commented code

--- spec/acceptance/client_admin/share/tile_digest_automation_spec.rb
+++ spec/acceptance/client_admin/share/tile_digest_automation_spec.rb
@@ -34,28 +34,4 @@ feature 'Client admin automates tile email', js: true do
       expect(admin.demo.tiles_digest_automator).to exist
     end
   end
-
-  # context 'without tiles in demo' do
-  #   context 'digest email' do
-  #     before do
-  #       visit email_client_admin_tiles_digest_preview_path(as: client_admin)
-  #     end
-  #
-  #     it 'should show empty digest email' do
-  #       expect_content 'Your New Tiles Are Here!'
-  #       expect_content 'No tiles posted'
-  #     end
-  #   end
-  #
-  #   context 'follow up email' do
-  #     before do
-  #       visit email_client_admin_tiles_digest_preview_path(follow_up_email: true, as: client_admin)
-  #     end
-  #
-  #     it 'should show empty follow up email' do
-  #       expect_content "Don't miss your new tiles"
-  #       expect_content 'No tiles posted'
-  #     end
-  #   end
-  # end
 end
