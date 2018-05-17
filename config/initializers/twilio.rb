ba44e956e7d90d913a8a22dd232cd3d327e16829
Manish Das <manishlaldas.md@gmail.com>
2018-05-16 18:35:41 -0700

    1475: Adds twilio copilot feature (most of the stuff is configured in Twilio website)

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -7,5 +7,5 @@ unless Rails.env.test?
   $twilio_client = Twilio::REST::Client.new
 end
 
-
+TWILIO_MESSAGE_SERVICE_ID = ENV['TWILIO_MESSAGE_SERVICE_ID']
 TWILIO_PHONE_NUMBER = ENV['TWILIO_PHONE_NUMBER'] || '+16176007511'
