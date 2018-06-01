44238a67decac443e27554e19a1b62f491727f60
Ryan Workman <rdavid1099@gmail.com>
2018-06-01 16:26:36 -0600

    Add tracking for UI of names being removed

--- app/controllers/admin/user_removal_controller.rb
+++ app/controllers/admin/user_removal_controller.rb
@@ -3,13 +3,15 @@
 class Admin::UserRemovalController < AdminBaseController
   def index
     @demo = Demo.find(params[:demo_id])
+    @removing_ids = params[:rmv].split('&') if params[:rmv]
   end
 
   def update
-    @demo = Demo.find(params[:demo_id])
-    UserRemovalJob.new(demo_id: @demo.id, user_ids: user_removal_ids).perform
-    flash[:success] = "You removal job for users in #{@demo.name} has been triggered"
-    redirect_to admin_demo_user_removal_index_path(@demo)
+    demo = Demo.find(params[:demo_id])
+    removing_ids = user_removal_ids
+    UserRemovalJob.new(demo_id: demo.id, user_ids: removing_ids).perform
+    flash[:success] = "Your removal job for users in #{demo.name} has been triggered"
+    redirect_to admin_demo_user_removal_index_path(demo, {rmv: sanitize(removing_ids)})
   end
 
   private
@@ -18,6 +20,12 @@ class Admin::UserRemovalController < AdminBaseController
       params.keys.reduce([]) do |result, key|
         split_key = key.split("_")
         split_key[0..1] == remove_user ? result.push(split_key.last) : result
+      end
+    end
+
+    def sanitize(removing_ids)
+      removing_ids.reduce('') do |result, id|
+        result += "#{id}&"
       end
     end
 end
