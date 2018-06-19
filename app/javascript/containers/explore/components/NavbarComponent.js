80584e83f975858839ec7fa4ec0e5129a4f38807
Ryan Workman <rdavid1099@gmail.com>
2018-06-19 09:48:47 -0700

    Add foundation for NavbarComponent

--- app/javascript/containers/explore/components/NavbarComponent.js
+++ app/javascript/containers/explore/components/NavbarComponent.js
@@ -2,11 +2,35 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const NavbarComponent = props => (
-  <div>
-    <a onClick={props.navbarRedirect}>{"< Back to Explore"}</a>
-  </div>
+  <section className="with-divider pt-0">
+    <div className="row campaign-nav-row">
+      <div className="large-12 columns">
+        <div className="campaign-nav-links" style={{display: "inherit"}}>
+          <div
+            className="campaign-nav-image-container"
+            style={{backgroundImage: "explore_campaign_nav_link_image"}}
+            onClick={props.navbarRedirect}
+            data-name="Explore"
+          >
+            <div className="campaign-nav-shadow-overlay"></div>
+            <div className="campaign-nav-overlay"></div>
+            <h3 className="name">Explore</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 );
 
+// <% campaigns.each do |campaign| %>
+// <% if campaign.display_tiles.present? %>
+// <div className="carousel-cell campaign-nav-image-container" style="background-image: url('<%= campaign_nav_tile_image(campaign) %>')" data-id="<%= campaign.id %>" data-name="<%= campaign.name %>" data-id="<%= campaign.id %>" data-path="<%= explore_campaign_path(campaign) %>">
+// <div className="campaign-nav-shadow-overlay"></div>
+// <div className="campaign-nav-overlay"></div>
+// <h3 className="name"><%= campaign.name  %></h3>
+// </div>
+// <% end %>
+// <% end %>
 NavbarComponent.propTypes = {
   navbarRedirect: PropTypes.func,
 };
