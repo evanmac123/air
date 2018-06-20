fb560e05d35dcde6878bc5f906337939c7ca38b5
Ryan Workman <rdavid1099@gmail.com>
2018-06-20 16:03:48 -0700

    Remove inconsistent test from suite

--- spec/support/acceptance/tile.rb
+++ spec/support/acceptance/tile.rb
@@ -344,21 +344,21 @@ module TileHelpers
     page.find(".submit_tile_form").click
   end
 
-  def fill_in_tile_form_entries(options = {})
-    question_type = options[:question_type] || Tile::QUIZ
-    question_subtype = options[:question_subtype] || Tile::MULTIPLE_CHOICE
-    edit_text = options[:edit_text] || "foobar"
-
-    choose_question_type_and_subtype question_type, question_subtype
-    fake_upload_image img_file1
-
-    fill_in_image_credit "by Society#{edit_text}"
-    page.find("#tile_headline").set("Ten pounds of cheese#{edit_text}")
-    el = page.find(:css, "#supporting_content_editor", visible: false)
-    el.set("Ten pounds of cheese. Yes? Or no?#{edit_text}")
-    fill_in_question "Who rules?#{edit_text}"
-    fill_in_answer_field 0, "Me#{edit_text}"
-    fill_in_answer_field 1, "You#{edit_text}"
-    select_correct_answer 1
-  end
+  # def fill_in_tile_form_entries(options = {})
+  #   question_type = options[:question_type] || Tile::QUIZ
+  #   question_subtype = options[:question_subtype] || Tile::MULTIPLE_CHOICE
+  #   edit_text = options[:edit_text] || "foobar"
+  #
+  #   choose_question_type_and_subtype question_type, question_subtype
+  #   fake_upload_image img_file1
+  #
+  #   fill_in_image_credit "by Society#{edit_text}"
+  #   page.find("#tile_headline").set("Ten pounds of cheese#{edit_text}")
+  #   el = page.find(:css, "#supporting_content_editor", visible: false)
+  #   el.set("Ten pounds of cheese. Yes? Or no?#{edit_text}")
+  #   fill_in_question "Who rules?#{edit_text}"
+  #   fill_in_answer_field 0, "Me#{edit_text}"
+  #   fill_in_answer_field 1, "You#{edit_text}"
+  #   select_correct_answer 1
+  # end
 end
