1304a586fd19467bb9f690b5cafd3390a7003eed
Ryan Workman <rdavid1099@gmail.com>
2018-07-24 15:53:05 +0100

    Wire up react foundation for client_admin edit functionality

---
+++ app/javascript/packs/clientAdminTiles.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ClientAdminTiles from "../containers/clientAdminTiles";
+
+document.addEventListener("DOMContentLoaded", () => {
+  // const node = document.getElementById("client-admin-tiles-data");
+  // const data = JSON.parse(node.getAttribute("data"));
+  ReactDOM.render(
+    <ClientAdminTiles />,
+    document.getElementById("client-admin-tiles-root")
+  );
+});
