f81acc59e13eec241b74d80de5939deec4d9b1f7
Ryan Workman <rdavid1099@gmail.com>
2018-07-26 11:05:40 -0700

    Remove MESSAGING_SERVICE_SID for short_code implementation

--- app/jobs/sms_sender_job.rb
+++ app/jobs/sms_sender_job.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class SmsSenderJob < ActiveJob::Base
   queue_as :default
 
@@ -8,7 +10,7 @@ class SmsSenderJob < ActiveJob::Base
       params = { to: to_number, body: body }
 
       if Rails.env.production?
-        params.merge!(messaging_service_sid: TWILIO_MESSAGE_SERVICE_ID, from_number: TWILIO_SHORT_CODE)
+        params.merge!(from_number: TWILIO_SHORT_CODE)
       else
         params.merge!(from_number: from_number)
       end
