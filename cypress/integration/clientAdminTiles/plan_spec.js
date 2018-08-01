cf4b9a78ca0fcc2131eed9c96963e8f3e9cc3da9
Ryan Workman <rdavid1099@gmail.com>
2018-08-01 12:18:35 -0700

    Add tests for edit tile flow plan tab

--- cypress/integration/clientAdminTiles/plan_spec.js
+++ cypress/integration/clientAdminTiles/plan_spec.js
@@ -25,6 +25,20 @@ context('Plan Tile status nav functionality', () => {
 
       cy.get(`#single-tile-${this.unplannedTile.id} .activation_dates span`).should('have.text', 'Unplanned');
       cy.get(`#single-tile-${this.plannedTile.id} .activation_dates span`).should('have.text', 'January 1');
+
+      cy.get(`[data-tile-container-id=${this.unplannedTile.id}] a.button.update_status`).invoke('show').click();
+      cy.get(`#single-tile-${this.unplannedTile.id}`).should('be.hidden');
+      cy.get(`#single-tile-${this.plannedTile.id}`).should('be.visible');
+
+      cy.contains('Plan').children('span').should('have.text', '(1)');
+      cy.contains('Ready to Send').children('span').should('have.text', '(1)');
+
+      cy.get(`[data-tile-container-id=${this.plannedTile.id}] a.button.update_status`).invoke('show').click();
+      cy.get(`#single-tile-${this.unplannedTile.id}`).should('be.hidden');
+      cy.get(`#single-tile-${this.plannedTile.id}`).should('be.hidden');
+
+      cy.contains('Plan').children('span').should('have.text', '(0)');
+      cy.contains('Ready to Send').children('span').should('have.text', '(2)');
     });
   });
 });
