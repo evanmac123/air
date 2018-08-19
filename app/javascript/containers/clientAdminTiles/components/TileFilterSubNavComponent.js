6c48f26e73dd3f0cbc376b1572d7ac0ee38789c2
Ryan Workman <rdavid1099@gmail.com>
2018-08-19 13:25:43 -0700

    Add react-select dropdown to subnav

--- app/javascript/containers/clientAdminTiles/components/TileFilterSubNavComponent.js
+++ app/javascript/containers/clientAdminTiles/components/TileFilterSubNavComponent.js
@@ -1,56 +1,83 @@
 import React from 'react';
 import PropTypes from "prop-types";
+import Select from 'react-select';
 
 import { DateMaker, MapWithIndex } from "../../../lib/helpers";
 
-const renderMonthOptions = activeStatus => (
-  MapWithIndex(activeStatus === 'plan' ? DateMaker.spelledOutMonths.concat('Unplanned') : DateMaker.spelledOutMonths, (month, index) => (
-    React.createElement('option', {className: "js-filter-option", value: "<%= index %>", key: index},
-      month,
-    )
-  ))
-);
+const renderMonthOptions = activeStatus => {
+  const result = [];
+  if (activeStatus === 'plan') { result.push({value: 0, label: 'Unplanned'}); }
+  return result.concat(MapWithIndex(DateMaker.spelledOutMonths, (label, index) => (
+    {label, className: 'date-option', value: (index + 1)}
+  )));
+};
 
-const renderYearOptions = () => (
-  // <option data-key="year" className="js-filter-option" value="<%= year %>"><%= year %></option>
-  []
-);
+const renderYearOptions = () => {
+  const date = new Date();
+  const years = [];
+  for (let year = 2014; year <= date.getFullYear(); year++) { years.push(year); }
+  return years.reverse().map(label => (
+    {label, className: 'year-option', value: label}
+  ));
+};
+
+const renderCampaignOptions = () => {
+  const result = [{label: 'Unassigned', className: 'campaign-option', value: 'unassigned'}];
+  // logic for rendering campaigns
+  result.push({label: '+ Create Campaign', className: 'campaign-option', value: 'create_campaign'});
+  return result;
+};
+
+const renderSortOptions = () => ([
+  {label: 'Sort by Drag & Drop', className: 'sort-option', value: 'dnd'},
+  {label: 'Sort by Date', className: 'sort-option', value: 'date-sort'},
+]);
 
 const TileFilterSubNavComponent = props => (
   <div className="tabs-component-full-width-sub-nav js-tiles-index-filter-bar">
     <div className="row">
       <ul className="sub-nav-options">
-        <li className="sub-nav-option">
-          <select className="dropdown-button-component transparent js-month-filter-options js-tile-filter-select">
-            <option data-key="month" className="js-filter-option" value="all">All months</option>
-            { renderMonthOptions(props.activeStatus) }
-          </select>
+        <li className="sub-nav-option" style={{width: '15%'}}>
+          <Select
+            className="react-select date-filter"
+            placeholder="All Months"
+            isClearable={true}
+            options={renderMonthOptions(props.activeStatus)}
+            isSearchable={false}
+          />
         </li>
 
         {
           props.activeStatus !== 'plan' &&
-          <li className="sub-nav-option">
-            <select className="dropdown-button-component transparent js-year-filter-options js-tile-filter-select">
-              <option data-key="year" className="js-filter-option" value="all">All years</option>
-              { renderYearOptions() }
-            </select>
+          <li className="sub-nav-option" style={{width: '12%'}}>
+            <Select
+              className="react-select date-filter"
+              placeholder="All Years"
+              isClearable={true}
+              options={renderYearOptions()}
+              isSearchable={false}
+            />
           </li>
         }
 
-        <li className="sub-nav-option">
-          <select className="js-tile-filter-select dropdown-button-component transparent campaign-filter-options js-campaign-filter-options">
-            <option data-key="campaign" className="js-filter-option" value="all">All campaigns</option>
-            <option data-key="campaign" className="js-filter-option" value="unassigned">Unassigned</option>
-            <option data-key="campaign" className="js-filter-option" value="<%= campaign.id %>">CAMPAIGN NAMES</option>
-            <option data-key="campaign" className="js-create-campaign create-campaign" disabled>+ Create Campaign</option>
-          </select>
+        <li className="sub-nav-option" style={{width: '15%'}}>
+          <Select
+            className="react-select date-filter"
+            placeholder="All Campaigns"
+            isClearable={true}
+            options={renderCampaignOptions()}
+            isSearchable={true}
+          />
         </li>
 
-        <li className="sub-nav-option end">
-          <select className="js-tile-filter-select dropdown-button-component transparent js-tile-sort">
-            <option data-key="sort" className="js-filter-option" value="position">Sort by drag and drop</option>
-            <option data-key="sort" className="js-filter-option" value="month">Sort by date</option>
-          </select>
+        <li className="sub-nav-option end" style={{width: '20%'}}>
+          <Select
+            className="react-select date-filter"
+            placeholder="Sort..."
+            isClearable={true}
+            options={renderSortOptions()}
+            isSearchable={false}
+          />
         </li>
       </ul>
     </div>
