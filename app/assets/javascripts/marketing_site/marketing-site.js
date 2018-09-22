b766c9f36646fb2f2c88daa85a6f261cc8edf785
Ryan Workman <rdavid1099@gmail.com>
2018-09-22 12:09:26 -0700

    Implement SweetAlert into marketing page

--- app/assets/javascripts/marketing_site/marketing-site.js
+++ app/assets/javascripts/marketing_site/marketing-site.js
@@ -1,21 +1,21 @@
-//Disclaimer: This code was from a contractor (with slight restructuring). #gitblamelies.
-
 var Airbo = window.Airbo || {};
 Airbo.MarketingSite = Airbo.MarketingSite || {};
 
 Airbo.MarketingSite.Base = (function() {
-  function initMarketingSiteHighlightsCarousels() {
-    $(".highlights-carousel").flickity({
-      groupCells: true,
-      wrapAround: true,
-      cellAlign: "left",
-      resize: true
-    });
+  function triggerDemoRequestModal(e) {
+    e.preventDefault();
+    swal("Hello world!");
+    console.log("Demo request");
+  }
+
+  function triggerLoginModal(e) {
+    e.preventDefault();
+    console.log("Let's login!!");
   }
 
   function init() {
-    initMarketingSiteHighlightsCarousels();
-    $(".airbo-marketing-site").foundation();
+    $(".js-request-demo").click(triggerDemoRequestModal);
+    $(".js-login").click(triggerLoginModal);
   }
 
   return {
