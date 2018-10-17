22538f7c1f97bc00369b144f2fc2898023ede21b
Ryan Workman <rdavid1099@gmail.com>
2018-10-17 13:52:40 -0700

    Add redux - not implemented, but it's officially part of our build and ready to be utilized

--- app/javascript/packs/ReactSPA.js
+++ app/javascript/packs/ReactSPA.js
@@ -2,13 +2,18 @@ import 'babel-polyfill';
 
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
+import store from "../lib/redux/store";
 import App from "../containers/app";
 
 document.addEventListener("DOMContentLoaded", () => {
   const node = document.getElementById("ctrl-data");
   const initData = JSON.parse(node.getAttribute("data"));
   ReactDOM.render(
-    <App initData={initData} />,
+    <Provider store={store}>
+      <App initData={initData} />
+    </Provider>,
     document.getElementById("root")
   );
 });
