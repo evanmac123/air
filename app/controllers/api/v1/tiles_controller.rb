8295d473f96b957cd42b8eb576791d7102eb0fcf
Ryan Workman <rdavid1099@gmail.com>
2018-11-12 16:26:00 -0800

    Tile show functionality to show all tile_completions for specific users

--- app/controllers/api/v1/tiles_controller.rb
+++ app/controllers/api/v1/tiles_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::TilesController < Api::ApiController
 
   def show
     tile = Tile.find(params[:id])
-    if params[:ping_tile_view] == 'true'
+    if params[:ping_tile_view] == "true"
       schedule_viewed_tile_ping(tile)
       tile.viewed_by(current_user)
     end
@@ -16,12 +16,32 @@ class Api::V1::TilesController < Api::ApiController
     tile = Tile.find(params[:id])
     schedule_viewed_tile_ping(tile)
     tile.viewed_by(current_user)
-    render json: {success: 'Pinged view'}
+    render json: { success: "Pinged view" }
   end
 
   private
     def render_full_display(tile)
-      render json: tile.sanitize_for_tile_show
+      tile_show = tile.sanitize_for_tile_show
+      result = if params[:include_completion] == "true"
+        get_tile_completion_data(tile, tile_show)
+      else
+        tile_show
+      end
+      render json: result
+    end
+
+    def get_tile_completion_data(tile, tile_show)
+      sanitized_tile_completion = JSON.parse(tile.tile_completions.where(user: current_user).first.to_json)["tile_completion"]
+      parsed_tile_completion = sanitized_tile_completion.keys.reduce({}) do |result, key|
+        case key
+        when "answer_index"
+          result[:answerIndex] = sanitized_tile_completion[key]
+        when "free_form_response"
+          result[:freeFormResponse] = sanitized_tile_completion[key]
+        end
+        result
+      end.merge(complete: true)
+      tile_show.merge(parsed_tile_completion)
     end
 
     def verify_origin
