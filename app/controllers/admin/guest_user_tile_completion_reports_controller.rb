0cce986d3770ae563cc91f9c44fde8ed725f30a2
Ryan Workman <rdavid1099@gmail.com>
2018-12-20 13:57:02 -0800

    Wiring up backend fucntionality for CSV generation and downloading

--- app/controllers/admin/guest_user_tile_completion_reports_controller.rb
+++ app/controllers/admin/guest_user_tile_completion_reports_controller.rb
@@ -3,9 +3,16 @@
 class Admin::GuestUserTileCompletionReportsController < AdminBaseController
   def show
     @demo = Demo.find(params[:demo_id])
+    generate_tile_completion_csv if formats.include?(:csv)
+    respond_to do |format|
+      format.html
+      format.csv { send_data @report, filename: "guest_user_tile_completions_#{Date.today}.csv" }
+    end
   end
 
-  def create
-    binding.pry
-  end
+  private
+    def generate_tile_completion_csv
+      @report = {}
+      binding.pry
+    end
 end
