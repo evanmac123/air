5c5c8df95bf1a0a901ad0d5844bdda6775b1650e
Ryan Workman <rdavid1099@gmail.com>
2018-12-20 16:20:43 -0800

    Wire backend to generate and deliver CSV for download

--- app/controllers/admin/guest_user_tile_completion_reports_controller.rb
+++ app/controllers/admin/guest_user_tile_completion_reports_controller.rb
@@ -3,16 +3,28 @@
 class Admin::GuestUserTileCompletionReportsController < AdminBaseController
   def show
     @demo = Demo.find(params[:demo_id])
-    generate_tile_completion_csv if formats.include?(:csv)
     respond_to do |format|
       format.html
-      format.csv { send_data @report, filename: "guest_user_tile_completions_#{Date.today}.csv" }
+      format.csv { send_data generate_tile_completion_csv, filename: "guest_user_tile_completions_#{Date.today}.csv" }
     end
   end
 
   private
+    def get_ids_from_params
+      params.keys.map do |param|
+        param.split("_").last if param.include?("generate_tile_")
+      end.compact
+    end
+
     def generate_tile_completion_csv
-      @report = {}
-      binding.pry
+      attrs = %w{tile_id user_id answer_index free_form_response}
+      ids = get_ids_from_params
+      CSV.generate(headers: true) do |csv|
+        csv << attrs
+        @demo.tiles.where(id: ids).each do |tile|
+          tile_completions = tile.tile_completions.where(user_type: "GuestUser")
+          tile_completions.each { |tc| csv << attrs.map { |attr| tc.send(attr) } }
+        end
+      end
     end
 end
