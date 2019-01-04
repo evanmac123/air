27a4bd104cde001d308f41fa17b4353084d241f2
Ryan Workman <rdavid1099@gmail.com>
2019-01-04 14:15:17 -0800

    Using User#receives_sms to determine if text is sent

--- app/jobs/sms_bulk_sender_job.rb
+++ app/jobs/sms_bulk_sender_job.rb
@@ -4,9 +4,9 @@ class SmsBulkSenderJob < ActiveJob::Base
   queue_as :default
 
   def perform(user_ids:, body:)
-    users = User.select(:phone_number).where(id: user_ids)
+    users = User.select(:phone_number, :receives_sms).where(id: user_ids)
     users.each do |user|
-      SmsSenderJob.perform_now(to_number: user.phone_number, body: body)
+      SmsSenderJob.perform_now(to_number: user.phone_number, body: body) if user.receives_sms
     end
   end
 end
