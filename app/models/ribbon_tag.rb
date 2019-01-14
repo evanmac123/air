3aed0372fc51718b49a512cf80eaf4af9f51685e
Ryan Workman <rdavid1099@gmail.com>
2019-01-14 10:53:25 -0800

    Add simple validation tests to RibbonTag model

--- app/models/ribbon_tag.rb
+++ app/models/ribbon_tag.rb
@@ -4,6 +4,6 @@ class RibbonTag < ActiveRecord::Base
   belongs_to :demo
   has_many :tiles
 
-  validates_presence_of :name, allow_blank: false, message: "name can't be blank"
-  validates_presence_of :color, allow_blank: false, message: "color can't be blank"
+  validates_presence_of :name, allow_blank: false
+  validates_presence_of :color, allow_blank: false
 end
