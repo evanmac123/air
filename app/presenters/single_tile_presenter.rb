0822513f5ed3a8d8ea76c53e0b058e21bec66a19
Ryan Workman <rdavid1099@gmail.com>
2019-01-17 17:18:12 -0800

    Elevate ribbon_tag to BasePresenter to ensure it shows up everywhere

--- app/presenters/single_tile_presenter.rb
+++ app/presenters/single_tile_presenter.rb
@@ -25,36 +25,6 @@ class SingleTilePresenter < BasePresenter
     @options = options
   end
 
-  def has_ribbon_tag?
-    if ribbon_tag = tile.ribbon_tag
-      @ribbon_tag_name = ribbon_tag.name
-      @ribbon_tag_color = ribbon_tag.color
-      true
-    else
-      false
-    end
-  end
-
-  def ribbon_tag_name
-    @ribbon_tag_name ||= tile.ribbon_tag.name
-  end
-
-  def ribbon_tag_color
-    @ribbon_tag_color ||= tile.ribbon_tag.color
-  end
-
-  def ribbon_tag_font_color
-    hex = if @ribbon_tag_color.length == 7
-      @ribbon_tag_color[1..-1]
-    else
-      @ribbon_tag_color[1] + @ribbon_tag_color[1] + @ribbon_tag_color[2] + @ribbon_tag_color[2] + @ribbon_tag_color[3] + @ribbon_tag_color[3]
-    end
-    red = Integer(hex[0..1], 16)
-    green = Integer(hex[2..3], 16)
-    blue = Integer(hex[4..5], 16)
-    (red * 0.299 + green * 0.587 + blue * 0.114) > 186 ? "#000000" : "#FFFFFF"
-  end
-
   def tile_id
     @tile_id ||= id
   end
