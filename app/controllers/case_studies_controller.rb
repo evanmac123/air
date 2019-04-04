f465098aad4b3fe648dabd160a1f215f0dde5c8d
Ryan Workman <rdavid1099@gmail.com>
2019-04-04 12:28:10 -0700

    Set up basic template for reading yaml data

--- app/controllers/case_studies_controller.rb
+++ app/controllers/case_studies_controller.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
 
 class CaseStudiesController < ApplicationController
-  layout "marketing_site"
+  layout "case_study"
+
+  def index
+    @case_studies = load_case_studies
+  end
 
   def show
+    @case_study = load_case_studies[params[:id]]
+    raise ActionController::RoutingError.new("Not Found") unless @case_study
   end
+
+  private
+    def load_case_studies
+      YAML.load(File.open("#{Rails.root}/config/case_studies.yml", "r"))
+    end
 end
