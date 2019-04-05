18c1cde6214ba5ed708aaa4210ce6d7a9c79c852
Ryan Workman <rdavid1099@gmail.com>
2019-04-05 14:20:20 -0700

    Add foundation for cheer saving and response

--- app/controllers/api/slack/airbot_controller.rb
+++ app/controllers/api/slack/airbot_controller.rb
@@ -4,36 +4,56 @@ class Api::Slack::AirbotController < Api::ApiController
   before_action :confirm_token
 
   def cheer
-    render json: {
-    attachments: [
+    cheer = Cheer.new(body: params[:text])
+    response = if cheer.save
       {
-        title: "*#{params[:user_name]}* submitted a new cheer!",
-        text: "\"#{params[:text]}\"",
-        fallback: params[:text],
-        color: "#48BFFF"
+        response_type: "in_channel",
+        attachments: [
+          {
+            title: "#{params[:user_name].upcase} submitted a new cheer!",
+            text: "\"#{params[:text]}\"",
+            fallback: params[:text],
+            color: "#48BFFF",
+            image_url: random_giphy("cheer")
+          }
+        ]
       }
-    ]
-  }
-    # {"token"=>"3e2RasO41aeO9cIpvgDW4LM3",
-    #  "team_id"=>"T02FKMPAZ",
-    #  "team_domain"=>"airbo",
-    #  "channel_id"=>"DAHC3RACQ",
-    #  "channel_name"=>"directmessage",
-    #  "user_id"=>"UAHC3R48Y",
-    #  "user_name"=>"ryan",
-    #  "command"=>"/cheer",
-    #  "text"=>"Hello world",
-    #  "response_url"=>"https://hooks.slack.com/commands/T02FKMPAZ/602050783620/wduFvZ9gTlbA3uo9xieQdVz3",
-    #  "trigger_id"=>"603522059398.2529737373.dc40dbdef877c3071e5a4f093fcd0d82",
-    #  "format"=>:json,
-    #  "controller"=>"api/slack/airbot",
-    #  "action"=>"cheer"}
+    else
+      {
+        response_type: "ephemeral",
+        attachments: [
+          {
+            title: "An error occurred",
+            text: "You're cheer did not save. Please try again later.",
+            fallback: "You're cheer did not save. Please try again later.",
+            color: "#C90404",
+            image_url: random_giphy("error")
+          }
+        ]
+      }
+    end
+
+    render json: response
   end
 
   private
     def confirm_token
       unless ENV["SLACK_APP_TOKEN"] == params[:token]
         raise ActionController::RoutingError.new("Not Found")
+      end
+    end
+
+    def giphy_api_endpoint(type)
+      "https://api.giphy.com/v1/gifs/random?api_key=#{ENV['GIPHY_API_KEY']}&tag=#{type}&rating=G"
+    end
+
+    def random_giphy(type)
+      begin
+        uri  = URI(giphy_api_endpoint(type))
+        resp = JSON.parse(Net::HTTP.get(uri), symbolize_names: true)
+        resp[:data][:images][:fixed_width][:url]
+      rescue
+        "https://media0.giphy.com/media/l41lNeVPFM7LH9X7q/200w.gif"
       end
     end
 end
