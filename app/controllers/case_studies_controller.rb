dfe273e77ec88f0f41736246c73555e9780f59f4
Ryan Workman <rdavid1099@gmail.com>
2019-04-10 17:35:11 -0700

    Add logic to render four adjacet case studies on show page

--- app/controllers/case_studies_controller.rb
+++ app/controllers/case_studies_controller.rb
@@ -8,12 +8,20 @@ class CaseStudiesController < ApplicationController
   end
 
   def show
-    @case_study = load_case_studies[params[:id]]
-    raise ActionController::RoutingError.new("Not Found") unless @case_study
+    raise ActionController::RoutingError.new("Not Found") unless @case_study = load_case_studies[params[:id]]
+    @adjacent_case_studies = get_adjacent_case_studies
   end
 
   private
     def load_case_studies
       YAML.load(File.open("#{Rails.root}/config/case_studies.yml", "r"))
+    end
+
+    def get_adjacent_case_studies
+      case_studies = load_case_studies
+      index = case_studies.keys.index(params[:id])
+      [index - 2, index - 1, ((index + 1) % case_studies.keys.length), ((index + 2) % case_studies.keys.length)].map do |index|
+        case_studies[case_studies.keys[index]].merge("key" => case_studies.keys[index])
+      end
     end
 end
