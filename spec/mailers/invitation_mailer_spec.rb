5e1d531b9fb570aa88d4c2ee54361eb0459f722c
Ryan Workman <rdavid1099@gmail.com>
2019-04-25 15:29:50 -0700

    Fix failing test

--- spec/mailers/invitation_mailer_spec.rb
+++ spec/mailers/invitation_mailer_spec.rb
@@ -3,6 +3,6 @@ require "spec_helper"
 describe Mailer do
   it '#invitation contains a do-not-forward warning message' do
     email = Mailer.invitation(FactoryBot.create :user)
-    expect(email.body).to include('Please do not forward it.')
+    expect(email.body).to include('Do not forward this')
   end
 end
